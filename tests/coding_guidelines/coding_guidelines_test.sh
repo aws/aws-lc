db898fdae1305f38a328409b659a4f9414f247ef
Bryce Shang <shanggu@amazon.com>
2020-06-26 13:08:00 -0700

    Add tests/coding_guidelines from old_master.

---
+++ tests/coding_guidelines/coding_guidelines_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+./tests/coding_guidelines/style.sh
+./tests/coding_guidelines/c99_gcc_test.sh
