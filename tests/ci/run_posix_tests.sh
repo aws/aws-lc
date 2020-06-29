638ecc8e6737d595d23f984e5ed8a750bd8a166f
Bryce Shang <shanggu@amazon.com>
2020-06-29 14:21:53 -0700

    Fix path.

--- tests/ci/run_posix_tests.sh
+++ tests/ci/run_posix_tests.sh
@@ -22,8 +22,7 @@ run_build -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
 if [[  "${AWSLC_FIPS}" == "1" ]]; then
   echo "Testing AWS-LC in FIPS release mode."
   build_and_test -DFIPS=1 -DCMAKE_BUILD_TYPE=Release
-  # TODO: check if this should be replaced with ./util/fipstools/break-tests.sh
-  ./util/fipstools/cavp/test_fips
+  ./test_build_dir/util/fipstools/cavp/test_fips
 fi
 
 if [[  "${AWSLC_CODING_GUIDELINES_TEST}" == "1" ]]; then
