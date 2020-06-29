a5c7148565be89b024e70546d4314352626b07db
Bryce Shang <shanggu@amazon.com>
2020-06-28 17:04:08 -0700

    Fix warning 'unused variable'.

--- crypto/test/abi_test.cc
+++ crypto/test/abi_test.cc
@@ -208,7 +208,7 @@ template <typename... Args>
     WriteFile(stderr_handle, buf, strlen(buf), &unused, nullptr);
   }
 #else
-  ssize_t unused_ret = write(STDERR_FILENO, buf, strlen(buf));
+  (void)write(STDERR_FILENO, buf, strlen(buf));
 #endif
   abort();
 }
