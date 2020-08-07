b6617eb51a126d3a389ac0517ed14262492ec5d7
Ajay Gupta <guptagr@amazon.com>
2020-08-07 00:03:04 -0400

    remove unnecessary file

--- tests/ci/docker_images/run_aws_lc_cryptofuzz.sh
+++ tests/ci/docker_images/run_aws_lc_cryptofuzz.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+# Build aws-lc and corresponding module in aws-lc-cryptofuzz
 aws s3 cp s3://${GITHUB_CODE_BUCKET}/${REPO_OWNER}/${REPO_NAME}/${REPO_OWNER}_${REPO_NAME}.zip ./ 
 unzip ${REPO_OWNER}_${REPO_NAME}.zip -d aws-lc 
 cd / 
@@ -13,6 +14,20 @@ make crypto -j$(nproc)
 cd / 
 cd aws-lc-cryptofuzz/modules/openssl/ 
 make 
-cd / 
-chmod +x cryptofuzz.sh 
-./cryptofuzz.sh 
+
+# Run aws-lc-cryptofuzz and write interesting inputs to corpus/S3 bucket
+cd /
+cd aws-lc-cryptofuzz/
+make
+./cryptofuzz /mount/efs/corpus/ -max_total_time=100
+mkdir ../${BUILD_CONFIGURATION}/
+cp crash* ../${BUILD_CONFIGURATION}/
+cp crash* /mount/efs/corpus/
+cp leak* ../${BUILD_CONFIGURATION}/
+cp crash* /mount/efs/corpus/
+cp timeout* ../${BUILD_CONFIGURATION}/
+cp timeout* /mount/efs/corpus/
+cd ../
+aws s3 mv ${BUILD_CONFIGURATION} s3://${INTERESTING_INPUT_BUCKET}/${COMMIT_ID}/${BUILD_CONFIGURATION}/ --recursive
+ls
+
