57d5c23884d00e466e3a0878bd6b169db73dc8ad
Ajay Gupta <guptagr@amazon.com>
2020-08-07 17:28:03 -0400

    Add headers

--- tests/ci/docker_images/build_configs_install_deps.sh
+++ tests/ci/docker_images/build_configs_install_deps.sh
@@ -1,10 +1,11 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 #!/bin/bash
 
-set -exuo pipefail
+set -exo pipefail
 
 # -e: Exit on any failure
 # -x: Print the command before running
-# -u: Any variable that is not set will cause an error if used
 # -o pipefail: Makes sure to exit a pipeline with a non-zero error code if any command in the pipeline exists with a
 #              non-zero error code.
 
