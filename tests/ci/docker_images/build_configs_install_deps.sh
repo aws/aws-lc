8f555471982a0a79c49cd22cf12f715b2b3a85e0
Ajay Gupta <guptagr@amazon.com>
2020-08-07 16:54:42 -0400

    add flags to set

--- tests/ci/docker_images/build_configs_install_deps.sh
+++ tests/ci/docker_images/build_configs_install_deps.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -exuo pipefail
+
+# -e: Exit on any failure
+# -x: Print the command before running
+# -u: Any variable that is not set will cause an error if used
+# -o pipefail: Makes sure to exit a pipeline with a non-zero error code if any command in the pipeline exists with a
+#              non-zero error code.
+
 # Install aws cli
 cd / 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
--- tests/ci/docker_images/run_aws_lc_cryptofuzz.sh
+++ tests/ci/docker_images/run_aws_lc_cryptofuzz.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -exuo pipefail
+
+# -e: Exit on any failure
+# -x: Print the command before running
+# -u: Any variable that is not set will cause an error if used
+# -o pipefail: Makes sure to exit a pipeline with a non-zero error code if any command in the pipeline exists with a
+#              non-zero error code.
 
 # Set necessary environment variables
 chmod +x env.sh
