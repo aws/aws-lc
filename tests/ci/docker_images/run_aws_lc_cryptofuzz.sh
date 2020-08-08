bb5a786a22b8040339e578bd1562fa82ae3d6d39
Ajay Gupta <guptagr@amazon.com>
2020-08-08 11:59:37 -0400

    change set flag

--- tests/ci/docker_images/run_aws_lc_cryptofuzz.sh
+++ tests/ci/docker_images/run_aws_lc_cryptofuzz.sh
@@ -2,9 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #!/bin/bash
 
-set -exuo pipefail
+set -xuo pipefail
 
-# -e: Exit on any failure
 # -x: Print the command before running
 # -u: Any variable that is not set will cause an error if used
 # -o pipefail: Makes sure to exit a pipeline with a non-zero error code if any command in the pipeline exists with a
