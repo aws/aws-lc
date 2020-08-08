115f7553f0ca0c2e868f06019b6d37fb1609227a
Ajay Gupta <guptagr@amazon.com>
2020-08-08 10:55:05 -0400

    add bash script to create build context for docker files

--- tests/ci/docker_images/configure_build_contexts.sh
+++ tests/ci/docker_images/configure_build_contexts.sh
@@ -10,4 +10,13 @@ set -exuo pipefail
 # -o pipefail: Makes sure to exit a pipeline with a non-zero error code if any command in the pipeline exists with a
 #              non-zero error code.
 
-
+cp -r aws-lc-cryptofuzz/ linux-aarch/cryptofuzz_ubuntu-19.10_clang-9x_sanitizer/
+cp -r aws-lc-cryptofuzz/ linux-x86/cryptofuzz_ubuntu-19.10_clang-9x_sanitizer/
+cp -r aws-lc-cryptofuzz/ linux-x86/cryptofuzz_fedora-31_clang-9x/
+cp -r aws-lc-cryptofuzz/ linux-x86/cryptofuzz_generate_corpus/
+cp build_configs_install_deps.sh linux-aarch/cryptofuzz_ubuntu-19.10_clang-9x_sanitizer/
+cp build_configs_install_deps.sh linux-x86/cryptofuzz_ubuntu-19.10_clang-9x_sanitizer/
+cp build_configs_install_deps.sh linux-x86/cryptofuzz_fedora-31_clang-9x/
+cp run_aws_lc_cryptofuzz.sh linux-aarch/cryptofuzz_ubuntu-19.10_clang-9x_sanitizer/
+cp run_aws_lc_cryptofuzz.sh linux-x86/cryptofuzz_ubuntu-19.10_clang-9x_sanitizer/
+cp run_aws_lc_cryptofuzz.sh linux-x86/cryptofuzz_fedora-31_clang-9x/
