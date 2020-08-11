417e7888ece0a07d49bcaabfa3ffab773da817d2
Ajay Gupta <guptagr@amazon.com>
2020-08-11 11:20:15 -0400

    change interesting input names

--- tests/ci/docker_images/run_aws_lc_cryptofuzz.sh
+++ tests/ci/docker_images/run_aws_lc_cryptofuzz.sh
@@ -34,11 +34,11 @@ make
 ./cryptofuzz /mount/efs/corpus/ -max_total_time=100
 mkdir ../${BUILD_CONFIGURATION}/
 touch ../${BUILD_CONFIGURATION}/empty
-cp crash* ../${BUILD_CONFIGURATION}/
+cp crash* ../${BUILD_CONFIGURATION}/crash
 cp crash* /mount/efs/corpus/
-cp leak* ../${BUILD_CONFIGURATION}/
+cp leak* ../${BUILD_CONFIGURATION}/leak
 cp crash* /mount/efs/corpus/
-cp timeout* ../${BUILD_CONFIGURATION}/
+cp timeout* ../${BUILD_CONFIGURATION}/timeout
 cp timeout* /mount/efs/corpus/
 cd ../
 aws s3 mv ${BUILD_CONFIGURATION} s3://${INTERESTING_INPUT_BUCKET}/${COMMIT_ID}/${BUILD_CONFIGURATION}/ --recursive
