e4cd2f6772facd9c1e58d84d106cab5ba3433a1f
Bryce Shang <shanggu@amazon.com>
2020-08-17 22:59:17 -0700

    Add customized prefix to look LibCrypto dir.

--- cmake/awslc-config.cmake
+++ cmake/awslc-config.cmake
@@ -1,12 +1,26 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copied from s2n https://github.com/awslabs/s2n/tree/master/cmake/modules
+# Inspired by s2n https://github.com/awslabs/s2n/tree/master/cmake/modules
 #
-# - Try to find LibCrypto include dirs and libraries
+# - Try to find LibCrypto include dirs and libraries.
+#
+# Normally, AWS-LC built artifacts includes three dirs:
+#    bin/
+#    include/
+#    lib/
+# But, to avoid lib naming collision, a unique folder name can be included:
+#    {folder name}/bin/
+#    {folder name}include/
+#    {folder name}lib/
 #
 # Usage of this module as follows:
 #
+# If artifact folder is provided:
+#     set(AWSLC_ARTIFACT_FOLDER "xxxx")
+#     find_package(AWS-LC HINTS "hits that can find AWSLC built artifacts")
+#     target_link_libraries(project-target LibCrypto::Crypto)
+# else:
 #     find_package(AWS-LC)
 #     target_link_libraries(project-target LibCrypto::Crypto)
 #
@@ -21,42 +35,68 @@
 #  LibCrypto_SHARED_LIBRARY    The path to libcrypto.so
 #  LibCrypto_STATIC_LIBRARY    The path to libcrypto.a
 
+# Define a function to build paths based on CMAKE_PREFIX_PATH and CMAKE_INSTALL_PREFIX.
+function(build_paths var_name suffixes)
+    set(local_var "")
+    foreach (path ${CMAKE_PREFIX_PATH})
+        foreach (suffix ${suffixes})
+            list(APPEND local_var "${path}/${suffix}")
+        endforeach (suffix)
+    endforeach (path)
+    foreach (path ${CMAKE_INSTALL_PREFIX})
+        foreach (suffix ${suffixes})
+            list(APPEND local_var "${path}/${suffix}")
+        endforeach (suffix)
+    endforeach (path)
+    set(${var_name} "${local_var}" PARENT_SCOPE)
+endfunction()
+
+# Define possible suffixes of libcrypto include and lib dirs.
+set(AWSLC_INCLUDE_SUFFIXES "")
+set(AWSLC_LIB_SUFFIXES "")
+if (AWSLC_ARTIFACT_FOLDER)
+    list(APPEND AWSLC_INCLUDE_SUFFIXES "${AWSLC_ARTIFACT_FOLDER}/include")
+    list(APPEND AWSLC_LIB_SUFFIXES "${AWSLC_ARTIFACT_FOLDER}/lib")
+    list(APPEND AWSLC_LIB_SUFFIXES "${AWSLC_ARTIFACT_FOLDER}/lib64")
+    list(APPEND AWSLC_LIB_SUFFIXES "${AWSLC_ARTIFACT_FOLDER}")
+    list(APPEND AWSLC_LIB_SUFFIXES "build/${AWSLC_ARTIFACT_FOLDER}")
+else ()
+    list(APPEND AWSLC_INCLUDE_SUFFIXES "include")
+    list(APPEND AWSLC_LIB_SUFFIXES "lib")
+    list(APPEND AWSLC_LIB_SUFFIXES "lib64")
+    list(APPEND AWSLC_LIB_SUFFIXES "")
+    list(APPEND AWSLC_LIB_SUFFIXES "build")
+endif ()
+message(STATUS "AWSLC_INCLUDE_SUFFIXES: ${AWSLC_INCLUDE_SUFFIXES}")
+message(STATUS "AWSLC_LIB_SUFFIXES: ${AWSLC_LIB_SUFFIXES}")
+
+# Define possible paths to AWSLC LibCrypto Include Dir.
+set(AWSLC_INCLUDE_PATHS "")
+build_paths(AWSLC_INCLUDE_PATHS ${AWSLC_INCLUDE_SUFFIXES})
+message(STATUS "AWSLC_INCLUDE_PATHS: ${AWSLC_INCLUDE_PATHS}")
+
+# Define possible paths to AWSLC LibCrypto Lib Dir.
+set(AWSLC_LIB_PATHS "")
+build_paths(AWSLC_LIB_PATHS ${AWSLC_LIB_SUFFIXES})
+message(STATUS "AWSLC_LIB_PATHS: ${AWSLC_LIB_PATHS}")
+
 find_path(LibCrypto_INCLUDE_DIR
-    NAMES openssl/crypto.h
-    HINTS
-        ${CMAKE_PREFIX_PATH}/include 
-        ${CMAKE_INSTALL_PREFIX}/include
-    )
+        NAMES openssl/crypto.h
+        HINTS
+        ${AWSLC_INCLUDE_PATHS}
+        )
 
 find_library(LibCrypto_SHARED_LIBRARY
-    NAMES libcrypto.so libcrypto.dylib
-    HINTS
-    ${CMAKE_PREFIX_PATH}/build/crypto
-    ${CMAKE_PREFIX_PATH}/build
-    ${CMAKE_PREFIX_PATH}
-    ${CMAKE_PREFIX_PATH}/lib64
-    ${CMAKE_PREFIX_PATH}/lib 
-    ${CMAKE_INSTALL_PREFIX}/build/crypto
-    ${CMAKE_INSTALL_PREFIX}/build
-    ${CMAKE_INSTALL_PREFIX}
-    ${CMAKE_INSTALL_PREFIX}/lib64
-    ${CMAKE_INSTALL_PREFIX}/lib
-    )
+        NAMES libcrypto.so libcrypto.dylib
+        HINTS
+        ${AWSLC_LIB_PATHS}
+        )
 
 find_library(LibCrypto_STATIC_LIBRARY
-    NAMES libcrypto.a
-    HINTS 
-    ${CMAKE_PREFIX_PATH}/build/crypto
-    ${CMAKE_PREFIX_PATH}/build
-    ${CMAKE_PREFIX_PATH}
-    ${CMAKE_PREFIX_PATH}/lib64
-    ${CMAKE_PREFIX_PATH}/lib   
-    ${CMAKE_INSTALL_PREFIX}/build/crypto
-    ${CMAKE_INSTALL_PREFIX}/build
-    ${CMAKE_INSTALL_PREFIX}
-    ${CMAKE_INSTALL_PREFIX}/lib64
-    ${CMAKE_INSTALL_PREFIX}/lib
-    )
+        NAMES libcrypto.a
+        HINTS
+        ${AWSLC_LIB_PATHS}
+        )
 
 if (BUILD_SHARED_LIBS)
     set(LibCrypto_LIBRARY ${LibCrypto_SHARED_LIBRARY})
