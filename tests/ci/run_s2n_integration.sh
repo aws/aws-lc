5b5b97cb4267087e180263b7b99e222b3d1606bd
Bryce Shang <shanggu@amazon.com>
2020-09-03 12:12:53 -0700

    Replace  with .

--- tests/ci/run_s2n_integration.sh
+++ tests/ci/run_s2n_integration.sh
@@ -19,7 +19,7 @@ cmake ${AWS_LC_DIR} -GNinja "-B${ROOT}/aws-lc-build" "-DCMAKE_INSTALL_PREFIX=${R
 ninja -C aws-lc-build install
 
 # While AWS-LC is a private project pretend to be BoringSSL so s2n uses the correct APIs
-CFLAGS="-DOPENSSL_IS_BORINGSSL=1"
+CFLAGS="-DOPENSSL_IS_AWSLC=1"
 export CFLAGS
 
 cmake s2n -GNinja "-B${ROOT}/s2n-build" "-DCMAKE_PREFIX_PATH=${ROOT}/aws-lc-install"
