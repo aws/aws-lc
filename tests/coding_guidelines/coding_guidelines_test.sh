284cc47c2a0557416cc478d2eeb490dee74a4cb2
Andrew Kaufman <57281817+andrew-kaufman@users.noreply.github.com>
2020-09-03 03:46:00 -0700

    Minor updates to c99_gcc_test.sh

--- tests/coding_guidelines/coding_guidelines_test.sh
+++ tests/coding_guidelines/coding_guidelines_test.sh
@@ -3,5 +3,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
 ./tests/coding_guidelines/style.sh
-# TODO: re-enable c99_gcc_test.sh when header files are moved to ./include/awslc
-# ./tests/coding_guidelines/c99_gcc_test.sh
+./tests/coding_guidelines/c99_gcc_test.sh
