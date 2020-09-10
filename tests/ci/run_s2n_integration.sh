0ac151273625145e4a811604ceefe70229ec532e
Bryce Shang <65570357+bryce-shang@users.noreply.github.com>
2020-09-09 21:30:15 -0700

    Update CI scripts. (#42)
    * Remove CFLAG - OPENSSL_IS_BORINGSSL.
    
    * Use run_posix_tests.sh for MacOS build.

--- tests/ci/run_s2n_integration.sh
+++ tests/ci/run_s2n_integration.sh
@@ -18,11 +18,6 @@ rm -rf aws-lc-build/*
 cmake ${AWS_LC_DIR} -GNinja "-B${ROOT}/aws-lc-build" "-DCMAKE_INSTALL_PREFIX=${ROOT}/aws-lc-install" -DBUILD_SHARED_LIBS=1
 ninja -C aws-lc-build install
 
-# While AWS-LC is a private project pretend to be BoringSSL so s2n uses the correct APIs
-# TODO: delete below CFLAGS when S2N merges PR/2273.
-CFLAGS="-DOPENSSL_IS_BORINGSSL=1"
-export CFLAGS
-
 cmake s2n -GNinja "-B${ROOT}/s2n-build" "-DCMAKE_PREFIX_PATH=${ROOT}/aws-lc-install"
 ninja -C s2n-build
 
