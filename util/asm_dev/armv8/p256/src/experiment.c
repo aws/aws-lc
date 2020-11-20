efac5d6bea31373125424ab3abf3a9838611f029
Nevine Ebeid <nebeid@amazon.com>
2020-11-20 12:16:11 -0500

    CryptoAlg-582: P256 ARMv8 Assembly development application
    This application was used to develop the 3 functions missing in
    OpenSSL ARMv8 P256 Perl Assembly which are developed in
    `crypto/fipsmodule/ec/asm/p256-armv8-asm.pl` and `p256_beeu-armv8-asm.pl`.
    In this application, the functions are written in assembly directly; no Perl.
    
    A python model of the 3rd function, beeu_mod_inverse_vartime,
    is included with details on how to understand it in light of
    the reference algorithm.

---
+++ util/asm_dev/armv8/p256/src/experiment.c
@@ -0,0 +1,70 @@
+//------------------------------------------------------------------------------------
+// Copyright (C) Amazon
+//------------------------------------------------------------------------------------
+
+#include <inttypes.h>
+
+typedef double *__attribute__((aligned(64))) aligned_double;
+
+
+uint64_t bn_add_words(uint64_t *restrict a, const uint64_t *restrict b) {
+  uint64_t c, l, t;
+
+  c = 0;
+//  while (n & ~3) {
+    t = a[0];
+    t += c;
+    c = (t < c);
+    l = t + b[0];
+    c += (l < t);
+    a[0] = l;
+    t = a[1];
+    t += c;
+    c = (t < c);
+    l = t + b[1];
+    c += (l < t);
+    a[1] = l;
+    t = a[2];
+    t += c;
+    c = (t < c);
+    l = t + b[2];
+    c += (l < t);
+    a[2] = l;
+    t = a[3];
+    t += c;
+    c = (t < c);
+    l = t + b[3];
+    c += (l < t);
+    a[3] = l;
+//    a += 4;
+//  }
+//  while (n) {
+    t = a[4];
+    t += c;
+    c = (t < c);
+    l = t + 0;
+    c += (l < t);
+    a[4] = l;
+//  }
+  return (uint64_t)c;
+}
+
+void bn_shift1_words(uint64_t *restrict a)
+{
+    a[0] = (a[0] >> 1) | (a[1] << 63);
+    a[1] = (a[1] >> 1) | (a[2] << 63);
+    a[2] = (a[2] >> 1) | (a[3] << 63);
+    a[3] = (a[3] >> 1) | (a[4] << 63);
+    a[4] >>= 1;
+}
+
+void bn_shift(uint64_t *restrict a, uint8_t count)
+{
+    if (count < 64)
+    {
+        a[0] = (a[0] >> count) | (a[1] << (64-count));
+        a[1] = (a[1] >> count) | (a[2] << (64-count));
+        a[2] = (a[2] >> count) | (a[3] << (64-count));
+        a[3] >>= count;
+    }
+}
