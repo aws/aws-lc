59fa7713cf043b3736a0b23f58b006b2e399ddc8
Bryce Shang <shanggu@amazon.com>
2020-11-23 21:10:52 -0800

    Enable urandom_test and CodeBuild priviledge mode.

--- util/all_tests.go
+++ util/all_tests.go
@@ -244,8 +244,8 @@ func runTest(test test) (bool, error) {
 }
 
 func fileExists(filename string) bool {
-    _, err := os.Stat(filename)
-    return err == nil
+	_, err := os.Stat(filename)
+	return err == nil
 }
 
 // setWorkingDirectory walks up directories as needed until the current working
@@ -391,8 +391,6 @@ func main() {
 	flag.Parse()
 	setWorkingDirectory()
 
-	inDocker := fileExists("/.dockerenv")
-
 	testCases, err := testconfig.ParseTestConfig("util/all_tests.json")
 	if err != nil {
 		fmt.Printf("Failed to parse input: %s\n", err)
@@ -431,9 +429,6 @@ func main() {
 					testForCPU.cpu = cpu
 					tests <- testForCPU
 				}
-			} else if inDocker && strings.Contains(baseTest.Cmd[0], "urandom_test") {
-				fmt.Printf("Running in Docker, skipping: %v\n", baseTest)
-				continue
 			} else {
 				shards, err := test.getGTestShards()
 				if err != nil {
