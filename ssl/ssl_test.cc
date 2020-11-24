831b471701656f05b6a18d910b253ed3ea5be573
Andrew Hopkins <andhop@amazon.com>
2020-11-24 02:31:23 -0800

    Temporarily turn off DefaultTicketKeyInitialization due to Docker instability (#69)

--- ssl/ssl_test.cc
+++ ssl/ssl_test.cc
@@ -2513,7 +2513,8 @@ TEST_P(SSLVersionTest, SessionTimeout) {
   }
 }
 
-TEST_P(SSLVersionTest, DefaultTicketKeyInitialization) {
+// These tests fail intermittently in Docker containers. Tracking root cause in CryptoAlg-534
+TEST_P(SSLVersionTest, DISABLED_DefaultTicketKeyInitialization) {
   static const uint8_t kZeroKey[kTicketKeyLen] = {};
   uint8_t ticket_key[kTicketKeyLen];
   ASSERT_EQ(1, SSL_CTX_get_tlsext_ticket_keys(server_ctx_.get(), ticket_key,
