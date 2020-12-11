3eb87b696b11f73b124d910fa0d0ea651fcf9659
Torben Hansen <htorben@amazon.com>
2020-12-11 15:45:45 +0000

    Improve c99 violation test

--- tests/coding_guidelines/c99_gcc_test.sh
+++ tests/coding_guidelines/c99_gcc_test.sh
@@ -15,7 +15,22 @@ INCLUDE_FILES=`ls $INCLUDE_DIR/openssl/*.h | grep -v $INCLUDE_DIR/openssl/arm_ar
 #   exists in the code.
 # - The -fsyntax-only tells the compiler to check the syntax without producing 
 #   any outputs.
+#
+# This test has some known limitations:
+# "Some users try to use -Wpedantic to check programs for strict ISO C
+# conformance. They soon find that it does not do quite what they want: it finds
+# some non-ISO practices, but not all—only those for which ISO C requires a
+# diagnostic, and some others for which diagnostics have been added."
+# https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
 
-$CC -std=c99 -c -I$INCLUDE_DIR -include $INCLUDE_FILES -Wpedantic -fsyntax-only
+${CC} -std=c99 -c -I${INCLUDE_DIR} -include ${INCLUDE_FILES} -Wpedantic -fsyntax-only -Werror
 
+# AWS C SDKs conforms to C99. They set `C_STANDARD 99` which will set the
+# flag `-std=gnu99`
+# https://github.com/awslabs/aws-c-common/blob/main/cmake/AwsCFlags.cmake
+# https://github.com/aws/aws-encryption-sdk-c/blob/master/cmake/AwsCryptosdkCFlags.cmake
+# https://cmake.org/cmake/help/latest/prop_tgt/C_STANDARD.html
+#
+# the c99 and gnu99 modes are different, so let's test both.
 
+${CC} -std=gnu99 -c -I${INCLUDE_DIR} -include ${INCLUDE_FILES} -Wpedantic -fsyntax-only -Werror
