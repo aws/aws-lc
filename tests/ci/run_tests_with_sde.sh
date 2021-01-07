476e8bdd63eb3c5576dc07d2c6abd491b349b9ad
Bryce Shang <shanggu@amazon.com>
2021-01-07 14:55:10 -0800

    Correct echo message.

--- tests/ci/run_tests_with_sde.sh
+++ tests/ci/run_tests_with_sde.sh
@@ -4,8 +4,8 @@
 
 source tests/ci/common_posix_setup.sh
 
-echo "Testing AWS-LC in debug mode."
+echo "Testing AWS-LC in debug mode under Intel's SDE."
 build_and_test_with_sde
 
-echo "Testing AWS-LC in release mode."
+echo "Testing AWS-LC in release mode under Intel's SDE."
 build_and_test_with_sde -DCMAKE_BUILD_TYPE=Release
