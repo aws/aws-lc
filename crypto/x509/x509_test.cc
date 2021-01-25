68e7ca8dec139c317c13bb7e50385a3725686005
Bryce Shang <shanggu@amazon.com>
2021-01-24 16:40:51 -0800

    Fix TODO comment.

--- crypto/x509/x509_test.cc
+++ crypto/x509/x509_test.cc
@@ -1484,7 +1484,7 @@ TEST(X509Test, TestRsaSsaPss) {
   bssl::UniquePtr<EVP_PKEY> pkey(X509_get_pubkey(cert.get()));
   ASSERT_TRUE(pkey);
 
-  // TODO(shang): reenable this verification when EVP_PKEY_RSA_PSS pmeth is implemented.
+  // TODO(shang): reenable this verification when EVP_PKEY_RSA_PSS ameth is able to decode rsapss parameters.
   // ASSERT_TRUE(X509_verify(cert.get(), pkey.get()));
 }
 
