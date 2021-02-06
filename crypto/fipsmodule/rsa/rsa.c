8fba0fedfb879f70e04b983eae278a6bdebfa46d
Bryce Shang <65570357+bryce-shang@users.noreply.github.com>
2021-02-05 16:03:48 -0800

    Update crypto/fipsmodule/rsa/rsa.c
    Co-authored-by: torben-hansen <50673096+torben-hansen@users.noreply.github.com>

--- crypto/fipsmodule/rsa/rsa.c
+++ crypto/fipsmodule/rsa/rsa.c
@@ -922,7 +922,7 @@ int RSA_blinding_on(RSA *rsa, BN_CTX *ctx) {
 
 int RSA_pkey_ctx_ctrl(EVP_PKEY_CTX *ctx, int optype, int cmd, int p1, void *p2)
 {
-  /* If key type not RSA or RSA-PSS return error */
+  /* If key type is not RSA or RSA-PSS return error */
   if (ctx != NULL && ctx->pmeth != NULL 
       && ctx->pmeth->pkey_id != EVP_PKEY_RSA
       && ctx->pmeth->pkey_id != EVP_PKEY_RSA_PSS) {
