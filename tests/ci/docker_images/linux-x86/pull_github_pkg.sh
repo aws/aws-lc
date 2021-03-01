26ae6f8aca24c21c199b0a1d7513ac9eafb9f4ce
Bryce Shang <shanggu@amazon.com>
2021-03-01 14:18:14 -0800

    Pre-pull aws crt Docker images.

---
+++ tests/ci/docker_images/linux-x86/pull_github_pkg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Note: this file is reserved for pulling Docker images from 'docker.pkg.github.com', which needs external credential.
+# Pull Docker images used by https://github.com/awslabs/aws-c-cal
+# These Docker images are hosted in 'docker.pkg.github.com', which needs GitHub personal access token.
+source ./aws-crt/aws-crt-common.sh
+login_github_docker_pkg
+pull_aws_crt_docker_img
