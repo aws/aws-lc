c044042cb59885ebceaab7953caa8626eb241f4c
Bryce Shang <shanggu@amazon.com>
2021-03-01 17:13:04 -0800

    Run pull_github_pkg.sh in CodeBuild.

--- tests/ci/cdk/run-cdk.sh
+++ tests/ci/cdk/run-cdk.sh
@@ -11,58 +11,85 @@ set -exuo pipefail
 # -o pipefail: Makes sure to exit a pipeline with a non-zero error code if any command in the pipeline exists with a
 #              non-zero error code.
 
-# Export customized environment variables.
-export CDK_DEPLOY_ACCOUNT=$1
-shift
-export CDK_DEPLOY_REGION=$1
-shift
-export GITHUB_REPO_OWNER=$1
-shift
-export GITHUB_REPO=$1
-shift
-export ACTION=$1
-shift
-
-# Export other environment variables.
-export DATE_NOW="$(date +%Y-%m-%d-%H-%M)"
-export ECR_LINUX_AARCH_REPO_NAME="aws-lc-docker-images-linux-aarch"
-export ECR_LINUX_X86_REPO_NAME="aws-lc-docker-images-linux-x86"
-export ECR_WINDOWS_X86_REPO_NAME="aws-lc-docker-images-windows-x86"
-export AWS_LC_S3_BUCKET_PREFIX="aws-lc-windows-docker-image-build"
-export S3_FOR_WIN_DOCKER_IMG_BUILD="${AWS_LC_S3_BUCKET_PREFIX}-${DATE_NOW}"
-export WIN_EC2_TAG_KEY="aws-lc"
-export WIN_EC2_TAG_VALUE="aws-lc-windows-docker-image-build-${DATE_NOW}"
-export WIN_DOCKER_BUILD_SSM_DOCUMENT="windows-ssm-document-${DATE_NOW}"
-
-# Functions
 function delete_s3_buckets() {
   aws s3api list-buckets --query "Buckets[].Name" | jq '.[]' | while read -r i; do
+    bucket_name=$(echo "${i}" | tr -d '"')
     # Delete the bucket if its name uses AWS_LC_S3_BUCKET_PREFIX.
-    if [[ ${i} == "${AWS_LC_S3_BUCKET_PREFIX}"* ]]; then
-      aws s3 rm "s3://${i}" --recursive
-      aws s3api delete-bucket --bucket "${i}"
+    if [[ "${bucket_name}" == *"${AWS_LC_S3_BUCKET_PREFIX}"* ]]; then
+      aws s3 rm "s3://${bucket_name}" --recursive
+      aws s3api delete-bucket --bucket "${bucket_name}"
     fi
   done
 }
 
 function delete_container_repositories() {
-  aws ecr delete-repository --repository-name ${ECR_LINUX_AARCH_REPO_NAME} --force
-  aws ecr delete-repository --repository-name ${ECR_LINUX_X86_REPO_NAME} --force
-  aws ecr delete-repository --repository-name ${ECR_WINDOWS_X86_REPO_NAME} --force
+  ecr_repos=$(aws ecr describe-repositories)
+  if [[ "${ecr_repos}" == *"${ECR_LINUX_AARCH_REPO_NAME}"* ]]; then
+    aws ecr delete-repository --repository-name "${ECR_LINUX_AARCH_REPO_NAME}" --force
+  fi
+  if [[ "${ecr_repos}" == *"${ECR_LINUX_X86_REPO_NAME}"* ]]; then
+    aws ecr delete-repository --repository-name "${ECR_LINUX_X86_REPO_NAME}" --force
+  fi
+  if [[ "${ecr_repos}" == *"${ECR_WINDOWS_X86_REPO_NAME}"* ]]; then
+    aws ecr delete-repository --repository-name "${ECR_WINDOWS_X86_REPO_NAME}" --force
+  fi
 }
 
-function destroy_all() {
+function delete_external_credential() {
+  # This deletion may take a few minutes to take effects.
+  # https://docs.aws.amazon.com/cli/latest/reference/secretsmanager/delete-secret.html
+  # https://aws.amazon.com/premiumsupport/knowledge-center/delete-secrets-manager-secret/
+  aws secretsmanager delete-secret --secret-id "${AWS_LC_CI_SECRET_NAME}" --force-delete-without-recovery
+}
+
+function store_external_credential() {
+  # Always delete the secret to avoid resource name conflicts.
+  delete_external_credential
+  # When creating secret with the same name repeatedly,
+  # below error may occur because the deletion may take a few minutes to take effects.
+  # "You can't create this secret because a secret with this name is already scheduled for deletion."
+  # To avoid that, sleep 1 min
+  sleep 60
+  # https://docs.aws.amazon.com/cli/latest/reference/secretsmanager/create-secret.html
+  key_value="{\"GITHUB_PERSONAL_ACCESS_TOKEN\":\"${GITHUB_ACCESS_TOKEN}\"}"
+  secret_arn=$(aws secretsmanager create-secret --name "${AWS_LC_CI_SECRET_NAME}" --secret-string "${key_value}" | jq -r '.ARN')
+  # Export this variable so CDK can create related IAM policy on this ARN.
+  export EXTERNAL_CREDENTIAL_SECRET_ARN="${secret_arn}"
+}
+
+function destroy_ci() {
   cdk destroy aws-lc-* --force
   # CDK stack destroy does not delete s3 bucket automatically.
   delete_s3_buckets
   # CDK stack destroy does not delete ecr automatically.
   delete_container_repositories
+  # Delete the secret from Secrets Manager.
+  delete_external_credential
 }
 
-function create_aws_resources() {
-  # Clean up resources before deployment.
-  destroy_all
-  cdk deploy aws-lc-* --require-approval never
+function destroy_docker_img_build_stack() {
+  # Destroy all temporary resources created for all docker image build.
+  cdk destroy aws-lc-docker-image-build-* --force
+  # CDK stack destroy does not delete s3 bucket automatically.
+  delete_s3_buckets
+  # Delete the secret from Secrets Manager.
+  delete_external_credential
+}
+
+function create_docker_img_build_stack() {
+  # Clean up build stacks if exists.
+  destroy_docker_img_build_stack
+  # Use SecretsManager to store GitHub personal access token.
+  store_external_credential
+  # Deploy aws-lc ci stacks.
+  # When repeatedly deploy, error 'EIP failed Reason: Maximum number of addresses has been reached' can happen.
+  # https://forums.aws.amazon.com/thread.jspa?messageID=952368
+  # Workaround: go to AWS EIP console, release unused IP.
+  cdk deploy aws-lc-docker-image-build-* --require-approval never
+}
+
+function create_github_ci_stack() {
+  cdk deploy aws-lc-ci-* --require-approval never
   # Need to use aws cli to change webhook build type because CFN is not ready yet.
   aws codebuild update-webhook --project-name aws-lc-ci-linux-x86 --build-type BUILD_BATCH
   aws codebuild update-webhook --project-name aws-lc-ci-linux-arm --build-type BUILD_BATCH
@@ -70,28 +97,35 @@ function create_aws_resources() {
 }
 
 function build_linux_img() {
-  aws codebuild start-build-batch --project-name aws-lc-docker-image-build-linux
+  # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/codebuild/start-build-batch.html
+  build_id=$(aws codebuild start-build-batch --project-name aws-lc-docker-image-build-linux | jq -r '.buildBatch.id')
+  export AWS_LC_LINUX_BUILD_BATCH_ID="${build_id}"
 }
 
 function build_windows_img() {
-  # EC2 takes 10 min to be ready for running command.
-  echo "Wait 10 min for EC2 ready for SSM command execution."
-  sleep 600
-
-  instance_id=$(aws ec2 describe-instances \
-    --filters "Name=tag:${WIN_EC2_TAG_KEY},Values=${WIN_EC2_TAG_VALUE}" | jq -r '.Reservations[0].Instances[0].InstanceId')
+  # EC2 takes several minutes to be ready for running command.
+  echo "Wait 3 min for EC2 ready for SSM command execution."
+  sleep 180
 
   # Run commands on windows EC2 instance to build windows docker images.
   for i in {1..60}; do
+    instance_id=$(aws ec2 describe-instances \
+      --filters "Name=tag:${WIN_EC2_TAG_KEY},Values=${WIN_EC2_TAG_VALUE}" | jq -r '.Reservations[0].Instances[0].InstanceId')
+    if [[ "${instance_id}" == "null" ]]; then
+      sleep 60
+      continue
+    fi
     instance_ping_status=$(aws ssm describe-instance-information \
       --filters "Key=InstanceIds,Values=${instance_id}" | jq -r '.InstanceInformationList[0].PingStatus')
     if [[ "${instance_ping_status}" == "Online" ]]; then
       # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ssm/send-command.html
-      aws ssm send-command \
+      command_id=$(aws ssm send-command \
         --instance-ids "${instance_id}" \
         --document-name "${WIN_DOCKER_BUILD_SSM_DOCUMENT}" \
         --output-s3-bucket-name "${S3_FOR_WIN_DOCKER_IMG_BUILD}" \
-        --output-s3-key-prefix 'runcommand'
+        --output-s3-key-prefix 'runcommand' | jq -r '.Command.CommandId')
+      # Export for checking command run status.
+      export WINDOWS_DOCKER_IMG_BUILD_COMMAND_ID="${command_id}"
       echo "Windows ec2 is executing SSM command."
       return
     else
@@ -103,48 +137,57 @@ function build_windows_img() {
   exit 1
 }
 
-function destroy_docker_img_build_stack() {
-  # Destroy all temporary resources created for all docker image build.
-  cdk destroy aws-lc-docker-image-build-* --force
-}
-
-function images_pushed_to_ecr() {
-  repo_name=$1
-  shift
-  target_images=("$@")
-  ecr_repo_name="${CDK_DEPLOY_ACCOUNT}.dkr.ecr.${CDK_DEPLOY_REGION}.amazonaws.com/${repo_name}"
-  docker_images=$*
-  echo "Checking if docker images [${docker_images}] are pushed to ${ecr_repo_name}."
-
-  # Every 5 min, this function checks if the target docker img is created.
+function linux_docker_img_build_status_check() {
+  # Every 5 min, this function checks if the linux docker image batch code build is finished successfully.
   # Normally, docker img build can take up to 1 hour. Here, we wait up to 30 * 5 min.
   for i in {1..30}; do
-    images_in_ecr=$(aws ecr describe-images --repository-name ${repo_name})
-    images_pushed=0
-    for target_image in "${target_images[@]}"; do
-      if [[ ${images_in_ecr} != *"${target_image}"* ]]; then
-        images_pushed=1
-        break
-      fi
-    done
-    if [[ ${images_pushed} == 0 ]]; then
-      echo "All required images are pushed to ${ecr_repo_name}."
+    # https://docs.aws.amazon.com/cli/latest/reference/codebuild/batch-get-build-batches.html
+    build_batch_status=$(aws codebuild batch-get-build-batches --ids "${AWS_LC_LINUX_BUILD_BATCH_ID}" | jq -r '.buildBatches[0].buildBatchStatus')
+    if [[ ${build_batch_status} == 'SUCCEEDED' ]]; then
+      echo "Code build ${AWS_LC_LINUX_BUILD_BATCH_ID} finished successfully."
       return
+    elif [[ ${build_batch_status} == 'FAILED' ]]; then
+      echo "Code build ${AWS_LC_LINUX_BUILD_BATCH_ID} failed."
+      exit 1
     else
-      echo "${i}: Wait 5 min for docker image creation."
+      echo "${i}: Wait 5 min for docker image build job finish."
       sleep 300
     fi
   done
-  echo "Error: Images are not pushed to ${ecr_repo_name}. Exit."
+  echo "Code build ${AWS_LC_LINUX_BUILD_BATCH_ID} takes more time than expected."
   exit 1
 }
 
-function deploy() {
+function win_docker_img_build_status_check() {
+  # Every 5 min, this function checks if the windows docker image build is finished successfully.
+  # Normally, docker img build can take up to 1 hour. Here, we wait up to 30 * 5 min.
+  for i in {1..30}; do
+    # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ssm/list-commands.html
+    command_run_status=$(aws ssm list-commands --command-id "${WINDOWS_DOCKER_IMG_BUILD_COMMAND_ID}" | jq -r '.Commands[0].Status')
+    if [[ ${command_run_status} == 'Success' ]]; then
+      echo "SSM command ${WINDOWS_DOCKER_IMG_BUILD_COMMAND_ID} finished successfully."
+      return
+    elif [[ ${command_run_status} == 'Failed' ]]; then
+      echo "SSM command ${WINDOWS_DOCKER_IMG_BUILD_COMMAND_ID} failed."
+      exit 1
+    else
+      echo "${i}: Wait 5 min for windows docker image build job finish."
+      sleep 300
+    fi
+  done
+  echo "SSM command ${WINDOWS_DOCKER_IMG_BUILD_COMMAND_ID} takes more time than expected."
+  exit 1
+}
+
+function build_docker_images() {
   # Always destroy docker build stacks (which include EC2 instance) on EXIT.
   trap destroy_docker_img_build_stack EXIT
 
-  echo "Creating AWS resources through CDK."
-  create_aws_resources
+  # Create/update aws-ecr repo.
+  cdk deploy aws-lc-ecr-* --require-approval never
+
+  # Create docker image build stack.
+  create_docker_img_build_stack
 
   echo "Activating AWS CodeBuild to build Linux aarch & x86 docker images."
   build_linux_img
@@ -153,49 +196,152 @@ function deploy() {
   build_windows_img
 
   echo "Waiting for docker images creation. Building the docker images need to take 1 hour."
-  linux_aarch_img_tags=("amazonlinux-2_gcc-7x_latest"
-    "ubuntu-20.04_clang-10x_latest"
-    "ubuntu-19.10_clang-9x_latest"
-    "ubuntu-19.10_clang-9x_sanitizer_latest")
-  images_pushed_to_ecr "${ECR_LINUX_AARCH_REPO_NAME}" "${linux_aarch_img_tags[@]}"
-  linux_x86_img_tags=("ubuntu-16.04_gcc-5x_latest"
-    "ubuntu-18.04_gcc-7x_latest"
-    "ubuntu-18.04_clang-6x_latest"
-    "ubuntu-19.04_gcc-8x_latest"
-    "ubuntu-19.04_clang-8x_latest"
-    "ubuntu-19.10_clang-9x_sanitizer_latest"
-    "ubuntu-19.10_clang-9x_latest"
-    "ubuntu-19.04_gcc-8x_latest"
-    "ubuntu-19.04_clang-8x_latest"
-    "ubuntu-20.04_clang-10x_latest"
-    "ubuntu-20.04_clang-10x_formal-verification_latest"
-    "centos-7_gcc-4x_latest"
-    "amazonlinux-2_gcc-7x_latest"
-    "amazonlinux-2_gcc-7x_intel-sde_latest"
-    "s2n_integration_clang-9x_latest")
-  images_pushed_to_ecr "${ECR_LINUX_X86_REPO_NAME}" "${linux_x86_img_tags[@]}"
-  windows_img_tags=("vs2015_latest" "vs2017_latest")
-  images_pushed_to_ecr "${ECR_WINDOWS_X86_REPO_NAME}" "${windows_img_tags[@]}"
+  # TODO(CryptoAlg-624): These image build may fail due to the Docker Hub pull limits made on 2020-11-01.
+  linux_docker_img_build_status_check
+  win_docker_img_build_status_check
 }
 
-# Main logics
+function setup_ci() {
+  build_docker_images
 
-case ${ACTION} in
-DEPLOY)
-  deploy
-  ;;
-DIFF)
-  cdk diff aws-lc*
-  ;;
-SYNTH)
-  cdk synth aws-lc*
-  ;;
-DESTROY)
-  destroy_all
-  ;;
-*)
-  echo "Action: ${ACTION} is not supported."
-  ;;
-esac
+  create_github_ci_stack
+}
 
-exit 0
+###########################
+# Main and related helper #
+###########################
+
+function script_helper() {
+  cat <<EOF
+This script uses CDK to deploy/destroy AWS resources defined in the aws-lc cdk app.
+
+For aws-lc continuous integration setup, this script uses aws cli to build some non-AWS resources(e.g. Docker image).
+
+Options:
+    --help                       Displays this help
+    --aws-account                AWS account for CDK deploy/destroy. Default to '620771051181'.
+    --aws-region                 AWS region for AWS resources creation. Default to 'us-west-2'.
+    --github-repo-owner          GitHub repository owner. Default to 'awslabs'.
+    --github-source-version      GitHub source version. Default to 'main'.
+    --github-access-token        GitHub personal access token. Currently, only 'read:package' permission is needed.
+                                 Only required for ci setup.
+    --action                     Required. The value can be
+                                   'deploy-ci': deploys aws-lc ci. This includes AWS and Docker image resources creation.
+                                   'destroy-ci': destroys AWS and Docker image resources used by aws-lc ci.
+                                   'build-img': builds Docker image used by aws-lc ci.
+                                                After image build, AWS resources are cleaned up.
+                                   'diff': compares the specified stack with the deployed stack.
+                                   'synth': synthesizes and prints the CloudFormation template for the stacks.
+EOF
+}
+
+function export_global_variables() {
+  # If these variables are not set or empty, defaults are export.
+  if [[ -z "${CDK_DEPLOY_ACCOUNT+x}" || -z "${CDK_DEPLOY_ACCOUNT}" ]]; then
+    export CDK_DEPLOY_ACCOUNT='620771051181'
+  fi
+  if [[ -z "${CDK_DEPLOY_REGION+x}" || -z "${CDK_DEPLOY_REGION}" ]]; then
+    export CDK_DEPLOY_REGION='us-west-2'
+  fi
+  if [[ -z "${GITHUB_REPO_OWNER+x}" || -z "${GITHUB_REPO_OWNER}" ]]; then
+    export GITHUB_REPO_OWNER='awslabs'
+  fi
+  if [[ -z "${GITHUB_SOURCE_VERSION+x}" || -z "${GITHUB_SOURCE_VERSION}" ]]; then
+    export GITHUB_SOURCE_VERSION='main'
+  fi
+  # Other variables for managing resources.
+  DATE_NOW="$(date +%Y-%m-%d-%H-%M)"
+  export GITHUB_REPO='aws-lc'
+  export ECR_LINUX_AARCH_REPO_NAME='aws-lc-docker-images-linux-aarch'
+  export ECR_LINUX_X86_REPO_NAME='aws-lc-docker-images-linux-x86'
+  export ECR_WINDOWS_X86_REPO_NAME='aws-lc-docker-images-windows-x86'
+  export AWS_LC_S3_BUCKET_PREFIX='aws-lc-windows-docker-image-build-s3'
+  export S3_FOR_WIN_DOCKER_IMG_BUILD="${AWS_LC_S3_BUCKET_PREFIX}-${DATE_NOW}"
+  export WIN_EC2_TAG_KEY='aws-lc'
+  export WIN_EC2_TAG_VALUE="aws-lc-windows-docker-image-build-${DATE_NOW}"
+  export WIN_DOCKER_BUILD_SSM_DOCUMENT="windows-ssm-document-${DATE_NOW}"
+  # During CI setup, used to create secret in AWS Secrets Manager for storing external credentials.
+  # After CI setup, used to delete secret from AWS Secrets Manager.
+  export AWS_LC_CI_SECRET_NAME='aws-lc-ci-external-credential'
+}
+
+function main() {
+  # parse arguments.
+  while [[ $# -gt 0 ]]; do
+    case ${1} in
+    --help)
+      script_helper
+      exit 0
+      ;;
+    --aws-account)
+      export CDK_DEPLOY_ACCOUNT="${2}"
+      shift
+      ;;
+    --aws-region)
+      export CDK_DEPLOY_REGION="${2}"
+      shift
+      ;;
+    --github-repo-owner)
+      export GITHUB_REPO_OWNER="${2}"
+      shift
+      ;;
+    --github-source-version)
+      export GITHUB_SOURCE_VERSION="${2}"
+      shift
+      ;;
+    --github-access-token)
+      export GITHUB_ACCESS_TOKEN="${2}"
+      shift
+      ;;
+    --action)
+      export ACTION="${2}"
+      shift
+      ;;
+    *)
+      echo "${1} is not supported."
+      exit 1
+      ;;
+    esac
+    # Check next option -- key/value.
+    shift
+  done
+
+  # Make sure action is set.
+  if [[ -z "${ACTION+x}" || -z "${ACTION}" ]]; then
+    echo "${ACTION} is required input."
+    exit 1
+  fi
+
+  # Export global variables, which provides the contexts needed by ci setup/destroy.
+  export_global_variables
+
+  # Execute the action.
+  case ${ACTION} in
+  deploy-ci)
+    if [[ -z "${GITHUB_ACCESS_TOKEN+x}" || -z "${GITHUB_ACCESS_TOKEN}" ]]; then
+      echo '--github-access-token is required for aws-lc ci setup.'
+      exit 1
+    fi
+    setup_ci
+    ;;
+  destroy-ci)
+    destroy_ci
+    ;;
+  build-img)
+    build_docker_images
+    ;;
+  synth)
+    cdk synth aws-lc-ci-*
+    ;;
+  diff)
+    cdk diff aws-lc-ci-*
+    ;;
+  *)
+    echo "--action is required. Use '--help' to see allowed enums."
+    exit 1
+    ;;
+  esac
+}
+
+# Invoke main
+main "$@"
