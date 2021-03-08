93554e182c5eca5b7c4532f7bdb74f57e4cbde95
John Harrison <jargh@amazon.com>
2021-03-08 09:28:40 -0800

    Self-contained bignum_negmodinv, proof build

--- X86/bignum_negmodinv.asm
+++ X86/bignum_negmodinv.asm
@@ -13,131 +13,159 @@
 
 ; ----------------------------------------------------------------------------
 ; Negated modular inverse, z := (-1/x) mod 2^{64k}
-; Input x[k]; output z[k]; temporary buffer t[>=k]
+; Input x[k]; output z[k]
 ;
 ;    extern void bignum_negmodinv
-;     (uint64_t k, uint64_t *z, uint64_t *t, uint64_t *x);
+;     (uint64_t k, uint64_t *z, uint64_t *x);
 ;
-; The output is z with t (at least k words) used as a temporary buffer.
 ; Assuming x is odd (otherwise nothing makes sense) the result satisfies
 ;
 ;       x * z + 1 == 0 (mod 2^{64 * k})
 ;
-; Standard x86-64 ABI: RDI = k, RSI = z, RDX = t, RCX = x
+; but is not necessarily reduced mod x.
+;
+; Standard x86-64 ABI: RDI = k, RSI = z, RDX = x
 ; ----------------------------------------------------------------------------
 
                 global bignum_negmodinv
-                extern wordnegmodinv
-                extern bignum_mul
-                extern bignum_mulc
 
                 section .text
 
-%define b       rbx
-%define b2      rbp
-%define k       r12
-%define x       r13
-%define y       r14
-%define a       r15
+%define k rdi
+%define z rsi
+%define x rcx           ; Moved from initial location to free rdx
+
+%define a rax
+%define d rdx
+%define i r8
+%define m r9
+%define h r10
+%define w r11
+%define t r12
+%define e rbx
 
 bignum_negmodinv:
+
                 push    rbx
-                push    rbp
                 push    r12
-                push    r13
-                push    r14
-                push    r15
 
-start:
+; If k = 0 do nothing (actually we could have avoiding the pushes and pops)
 
-; If length = 0 do nothing else (could even skip push/pops)
-
-                cmp     rdi, 0
+                test    k, k
                 jz      end
 
-; So now we can assume k >= 1.
-; Preserve the main parameters we need
+; Move the x pointer into its permanent home (rdx is needed for muls)
 
-                mov     k, rdi
-                mov     x, rsi
-                mov     y, rdx
-                mov     a, rcx
+                mov     x, rdx
 
-; Initially set up a 1-word modular inverse in x as our starting-point
+; Compute word-level negated modular inverse w for x[0].
 
-                mov     rdi, [a]
-                call    wordnegmodinv
-                mov     [x], rax
+                mov     a, [x]
 
-; If in fact we only have k = 1 then that's already the final answer
+                mov     d, a
+                mov     w, a
+                shl     d, 2
+                sub     w, d
+                xor     w, 2
 
-                mov     rax, k
-                sub     rax, 1
+                mov     d, w
+                imul    d, a
+                mov     a, 2
+                add     a, d
+                add     d, 1
+
+                imul    w, a
+
+                imul    d, d
+                mov     a, 1
+                add     a, d
+                imul    w, a
+
+                imul    d, d
+                mov     a, 1
+                add     a, d
+                imul    w, a
+
+                imul    d, d
+                mov     a, 1
+                add     a, d
+                imul    w, a
+
+; Write that as lowest word of the output, then if k = 1 we're finished
+
+                mov     [z], w
+                cmp     k, 1
                 jz      end
 
-; We can now assume k >= 2; set b = 1 to indicate 1-word accuracy
+; Otherwise compute and write the other digits (1..k-1) of w * x + 1
 
-                mov     b, 1
+                mov     a, [x]
+                xor     h, h
+                mul     w
+                add     a, 1
+                adc     h, d
+                mov     i, 1
+initloop:
+                mov     a, [x+8*i]
+                mul     w
+                add     a, h
+                adc     d, 0
+                mov     [z+8*i], a
+                mov     h, d
+                inc     i
+                cmp     i, k
+                jc      initloop
 
-; Now the start of the main loop; part of our loop invariant is that b < k
+; For simpler indexing, z := z + 8 and k := k - 1 per outer iteration
+; Then we can use the same index for x and for z and effective size k.
+;
+; But we also offset k by 1 so the "real" size is k + 1; after doing
+; the special zeroth bit we count with t through k more digits, so
+; getting k + 1 total as required.
+;
+; This lets us avoid some special cases inside the loop at the cost
+; of needing the additional "finale" tail for the final iteration
+; since we do one outer loop iteration too few.
 
-mainloop:
+                sub     k, 2
+                jz      finale
 
-; Set b2 = min(2 * b,k)
-; Note that we don't need to consider overflow in 2 * b
-; Besides being a crazy number, we must then violate nonoverlapping
-; since the numbers would be >= 2^63 words and so > 2^64 bytes long
+outerloop:
+                add     z, 8
 
-                lea     b2, [2*b]
-                cmp     k, b2
-                cmovc   b2, k
+                mov     h, [z]
+                mov     m, w
+                imul    m, h
+                mov     [z], m
+                mov     a, [x]
+                mul     m
+                add     a, h
+                adc     d, 0
+                mov     h, d
+                mov     i, 1
+                mov     t, k
+ innerloop:
+                adc     h, [z+8*i]
+                sbb     e, e
+                mov     a, [x+8*i]
+                mul     m
+                sub     d, e
+                add     a, h
+                mov     [z+8*i], a
+                mov     h, d
+                inc     i
+                dec     t
+                jnz     innerloop
 
-; Set y = a * x + 1
-; Actually because of the optimization below we could add
-; any constant >= 1, since the high part would be the same.
-; Could even use register b maybe, though it seems crazy
-; We know it's nonzero and might possibly be more efficient(?!?)
+                dec     k
+                jnz     outerloop
 
-                mov     QWORD [y], 1
-                mov     rdi, b2
-                mov     rsi, y
-                mov     rdx, b2
-                mov     rcx, a
-                mov     r8, b
-                mov     r9, x
-                call    bignum_mulc
-
-; Set x' = x * y + x = x * (y + 1) = x * (a * x + 2)
-; There is a nice optimization here: the low b of x
-; are already correct, by the inductive hypothesis.
-; And the bottom b of y = a * x + 1 is zero too.
-; Hence to get x + x * y we can do x * y_hi and store
-; it in the high part of x. Remember that in general
-; b2 - b < b, and we need to allow for sizes that
-; are not exactly a power of 2, hence the b2 - b to
-; get the size of the top "half".
-
-                mov     rdi, b2
-                sub     rdi, b
-                lea     rsi, [x+8*b]
-                mov     rdx, b
-                mov     rcx, x
-                mov     r8, rdi
-                lea     r9, [y+8*b]
-                call    bignum_mul
-
-; Set b = b2 and loop if it's not the whole bignum
-
-                mov     b, b2
-                cmp     b, k
-                jc      mainloop
+finale:
+                mov     a, [z+8]
+                imul    a, w
+                mov     [z+8], a
 
 end:
-                pop     r15
-                pop     r14
-                pop     r13
                 pop     r12
-                pop     rbp
                 pop     rbx
-
                 ret
