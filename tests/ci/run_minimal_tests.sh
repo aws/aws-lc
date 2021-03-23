abd555e80030df731bf749cb5073c34faca27feb
Bryce Shang <shanggu@amazon.com>
2021-03-23 15:00:26 -0700

    Remove 'AWS_LC' prefix.

--- tests/ci/run_minimal_tests.sh
+++ tests/ci/run_minimal_tests.sh
@@ -13,20 +13,20 @@ run_build -DBUILD_TESTING=0 -DBUILD_SSL=0 -DCMAKE_BUILD_TYPE=Release
 
 # build AWS-LC without Go.
 echo "Testing AWS-LC without Go in debug mode."
-build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1
+build_and_run_minimal_test -DBUILD_WITHOUT_GO=1
 
 echo "Testing AWS-LC without Go in release mode."
-build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1 -DCMAKE_BUILD_TYPE=Release
+build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DCMAKE_BUILD_TYPE=Release
 
 echo "Testing shared lib AWS-LC without Go in release mode."
-build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1 -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
+build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
 
 # build AWS-LC without Perl/Go.
 echo "Testing AWS-LC without Perl/Go in debug mode."
-build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1 -DAWS_LC_BUILD_WITHOUT_PERL=1
+build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DBUILD_WITHOUT_PERL=1
 
 echo "Testing AWS-LC without Perl/Go in release mode."
-build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1 -DAWS_LC_BUILD_WITHOUT_PERL=1 -DCMAKE_BUILD_TYPE=Release
+build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DBUILD_WITHOUT_PERL=1 -DCMAKE_BUILD_TYPE=Release
 
 echo "Testing shared lib AWS-LC without Perl/Go in release mode."
-build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1 -DAWS_LC_BUILD_WITHOUT_PERL=1 -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
+build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DBUILD_WITHOUT_PERL=1 -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
