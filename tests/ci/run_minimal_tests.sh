69387fabdc55f57c02d9d64808d4068735329e92
Bryce Shang <shanggu@amazon.com>
2021-03-23 13:45:19 -0700

    Adds build flavor -DBUILD_TESTING=0 -DBUILD_SSL=0.

--- tests/ci/run_minimal_tests.sh
+++ tests/ci/run_minimal_tests.sh
@@ -4,7 +4,11 @@
 
 source tests/ci/common_posix_setup.sh
 
-# build AWS-LC in Docker images without Go.
+# build AWS-LC without tests and ssl module.
+echo "Building AWS-LC without building tests code and ssl module."
+run_build -DBUILD_TESTING=0 -DBUILD_SSL=0
+
+# build AWS-LC without Go.
 echo "Testing AWS-LC without Go in debug mode."
 build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1
 
@@ -14,7 +18,7 @@ build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1 -DCMAKE_BUILD_TYPE=Releas
 echo "Testing shared lib AWS-LC without Go in release mode."
 build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1 -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
 
-# build AWS-LC in Docker images without Perl/Go.
+# build AWS-LC without Perl/Go.
 echo "Testing AWS-LC without Perl/Go in debug mode."
 build_and_run_minimal_test -DAWS_LC_BUILD_WITHOUT_GO=1 -DAWS_LC_BUILD_WITHOUT_PERL=1
 
