876c3fea365972951cef18364d95174364315851
Bryce Shang <shanggu@amazon.com>
2021-03-24 15:02:19 -0700

    Remove build flags - BUILD_WITHOUT_GO and BUILD_WITHOUT_PERL..

--- tests/ci/run_minimal_tests.sh
+++ tests/ci/run_minimal_tests.sh
@@ -6,20 +6,10 @@ source tests/ci/common_posix_setup.sh
 
 # build AWS-LC without Go.
 echo "Testing AWS-LC without Go in debug mode."
-build_and_run_minimal_test -DBUILD_WITHOUT_GO=1
+build_and_run_minimal_test
 
 echo "Testing AWS-LC without Go in release mode."
-build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DCMAKE_BUILD_TYPE=Release
+build_and_run_minimal_test -DCMAKE_BUILD_TYPE=Release
 
 echo "Testing shared lib AWS-LC without Go in release mode."
-build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
-
-# build AWS-LC without Perl/Go.
-echo "Testing AWS-LC without Perl/Go in debug mode."
-build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DBUILD_WITHOUT_PERL=1
-
-echo "Testing AWS-LC without Perl/Go in release mode."
-build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DBUILD_WITHOUT_PERL=1 -DCMAKE_BUILD_TYPE=Release
-
-echo "Testing shared lib AWS-LC without Perl/Go in release mode."
-build_and_run_minimal_test -DBUILD_WITHOUT_GO=1 -DBUILD_WITHOUT_PERL=1 -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
+build_and_run_minimal_test -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
