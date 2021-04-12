0fdbb09a20beb5668bf2c33cba90949b25c451e9
Bryce Shang <shanggu@amazon.com>
2021-04-12 13:28:07 -0700

    Remove test branch.

--- tests/ci/run_formal_verification.sh
+++ tests/ci/run_formal_verification.sh
@@ -7,9 +7,8 @@ cd ../
 ROOT=$(pwd)
 
 rm -rf aws-lc-verification-build
-git clone --recurse-submodules https://github.com/bryce-shang/aws-lc-verification.git aws-lc-verification-build
+git clone --recurse-submodules https://github.com/awslabs/aws-lc-verification.git aws-lc-verification-build
 cd aws-lc-verification-build
-git checkout aes-gcm-parallel
 # aws-lc-verification has aws-lc as one submodule under 'src' dir.
 # Below is to copy code of **target** aws-lc to 'src' dir.
 rm -rf ./src/* && cp -r "${ROOT}/${AWS_LC_DIR}/"* ./src
