b14c5449c3c18b6b799f6fbbc2a9ab5650cdb1c6
Andrew Hopkins <andhop@amazon.com>
2021-04-20 15:46:57 -0700

    Use latest version of cryptofuzz and pin to older version of Crypto++ to avoid ARM build issue in lsh256.cpp

--- tests/ci/docker_images/build_cryptofuzz_modules.sh
+++ tests/ci/docker_images/build_cryptofuzz_modules.sh
@@ -12,10 +12,8 @@ export CXXFLAGS="-fsanitize=address,undefined,fuzzer-no-link -D_GLIBCXX_DEBUG -O
 # Setup base of Cryptofuzz
 cd "$FUZZ_ROOT"
 MODULES_ROOT="${FUZZ_ROOT}/modules"
-#git clone --depth 1 https://github.com/guidovranken/cryptofuzz.git
-git clone https://github.com/guidovranken/cryptofuzz.git
+git clone --depth 1 https://github.com/guidovranken/cryptofuzz.git
 cd cryptofuzz
-git checkout 76ffeff944403cdd840f06b8fc42e131e6258f36
 git rev-parse HEAD
 CRYPTOFUZZ_SRC=$(pwd)
 python3 gen_repository.py
@@ -38,8 +36,11 @@ make -j$(nproc)
 
 # Crypto++ https://github.com/guidovranken/cryptofuzz/blob/master/docs/cryptopp.md
 cd "$MODULES_ROOT"
-git clone --depth 1 https://github.com/weidai11/cryptopp.git
+# Crypto++ broke arm compatibility with this change https://github.com/weidai11/cryptopp/commit/d1cea852f06008541966de7af433b57c4e9e2504
+#git clone --depth 1 https://github.com/weidai11/cryptopp.git
+git clone https://github.com/weidai11/cryptopp.git
 cd cryptopp/
+git checkout fee14910eaabfa098b1a7e1b05326a8831ee5e41
 git rev-parse HEAD
 make libcryptopp.a -j$(nproc)
 export CXXFLAGS="$CXXFLAGS -DCRYPTOFUZZ_CRYPTOPP"
