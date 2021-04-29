3d05d54d746884fa4fed5f4cfda5ed23b2765f1a
sachiang <sachiang@amazon.com>
2021-04-29 18:30:29 +0000

    update copyright headers

--- include/openssl/ocsp.h
+++ include/openssl/ocsp.h
@@ -1,5 +1,12 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * Copyright 2015-2021 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
 
 #ifndef AWSLC_OCSP_H
 #define AWSLC_OCSP_H
