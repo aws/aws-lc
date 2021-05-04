ebc7717bec289f58be4d23893f42ef34b7f29921
Samuel Chiang <samuelchungchiang@gmail.com>
2021-05-04 09:10:12 -0700

    OCSP structures and file import (#139)
    * adding basic ocsp files, structures, and asn1 functions
    
    * modify and strip down ocsp header file
    
    * added rudimentary OCSP structure related functions
    
    * add errordata file for ocsp
    
    * add dummy ocsp test file
    
    * modified OCSP_response_status function to align with OpenSSL's
    
    * remove unnecessary headers
    
    * remove dummy test file
    
    * Update build files in generated-src
    
    * remove auto generated files of comparison check
    
    * update copyright information
    
    * update copyright headers
    
    * updated comment styles
    
    * fixed small comments

---
+++ crypto/ocsp/ocsp_client.c
@@ -0,0 +1,32 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ocsp_internal.h"
+
+
+int OCSP_response_status(OCSP_RESPONSE *resp) {
+  if (resp == NULL){
+    OPENSSL_PUT_ERROR(OCSP, ERR_R_PASSED_NULL_PARAMETER);
+    return -1;
+  }
+  return ASN1_ENUMERATED_get(resp->responseStatus);
+}
+
+OCSP_BASICRESP *OCSP_response_get1_basic(OCSP_RESPONSE *resp) {
+  if (resp == NULL){
+    OPENSSL_PUT_ERROR(OCSP, ERR_R_PASSED_NULL_PARAMETER);
+    return NULL;
+  }
+
+  OCSP_RESPBYTES *rb = resp->responseBytes;
+  if (rb == NULL) {
+    OPENSSL_PUT_ERROR(OCSP, OCSP_R_NO_RESPONSE_DATA);
+    return NULL;
+  }
+  if (OBJ_obj2nid(rb->responseType) != NID_id_pkix_OCSP_basic) {
+    OPENSSL_PUT_ERROR(OCSP, OCSP_R_NOT_BASIC_RESPONSE);
+    return NULL;
+  }
+  return ASN1_item_unpack(rb->response, ASN1_ITEM_rptr(OCSP_BASICRESP));
+}
+
---
+++ include/openssl/ocsp.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015-2021 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef AWSLC_OCSP_H
+#define AWSLC_OCSP_H
+
+#include <openssl/asn1t.h>
+#include <openssl/safestack.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct ocsp_cert_id_st OCSP_CERTID;
+typedef struct ocsp_responder_id_st OCSP_RESPID;
+typedef struct ocsp_signature_st OCSP_SIGNATURE;
+typedef struct ocsp_resp_bytes_st OCSP_RESPBYTES;
+typedef struct ocsp_revoked_info_st OCSP_REVOKEDINFO;
+typedef struct ocsp_cert_status_st OCSP_CERTSTATUS;
+typedef struct ocsp_single_response_st OCSP_SINGLERESP;
+typedef struct ocsp_response_data_st OCSP_RESPDATA;
+typedef struct ocsp_response_st OCSP_RESPONSE;
+typedef struct ocsp_basic_response_st OCSP_BASICRESP;
+
+DEFINE_STACK_OF(OCSP_CERTID)
+DEFINE_STACK_OF(OCSP_RESPID)
+DEFINE_STACK_OF(OCSP_SINGLERESP)
+
+DECLARE_ASN1_FUNCTIONS(OCSP_SINGLERESP)
+DECLARE_ASN1_FUNCTIONS(OCSP_CERTSTATUS)
+DECLARE_ASN1_FUNCTIONS(OCSP_REVOKEDINFO)
+DECLARE_ASN1_FUNCTIONS(OCSP_BASICRESP)
+DECLARE_ASN1_FUNCTIONS(OCSP_RESPDATA)
+DECLARE_ASN1_FUNCTIONS(OCSP_RESPID)
+DECLARE_ASN1_FUNCTIONS(OCSP_RESPONSE)
+DECLARE_ASN1_FUNCTIONS(OCSP_RESPBYTES)
+DECLARE_ASN1_FUNCTIONS(OCSP_CERTID)
+DECLARE_ASN1_FUNCTIONS(OCSP_SIGNATURE)
+
+/* Returns response status from |OCSP_RESPONSE| */
+int OCSP_response_status(OCSP_RESPONSE *resp);
+
+/* Returns |OCSP_BASICRESP| from |OCSP_RESPONSE| */
+OCSP_BASICRESP *OCSP_response_get1_basic(OCSP_RESPONSE *resp);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#define OCSP_R_NOT_BASIC_RESPONSE                        104
+#define OCSP_R_NO_RESPONSE_DATA                          108
+
+#endif  // AWSLC_OCSP_H
