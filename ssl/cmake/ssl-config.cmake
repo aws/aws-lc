c9006a7069a0c71efb6adbd4c9ad5b8b09059466
sachiang <sachiang@amazon.com>
2021-05-04 20:02:22 +0000

    Revert "Merge branch 'main' into ocsp-test-framework"
    This reverts commit 5279bf492d30dd011afa9dc7b3978870e1f08af1, reversing
    changes made to 03985466f7bcd37995f56674acdd8956782330b2.

--- ssl/cmake/ssl-config.cmake
+++
@@ -1,9 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-find_dependency(crypto)
-
-if (BUILD_SHARED_LIBS)
-    include(${CMAKE_CURRENT_LIST_DIR}/shared/ssl-targets.cmake)
-else()
-    include(${CMAKE_CURRENT_LIST_DIR}/static/ssl-targets.cmake)
-endif()
--- tests/ci/run_minimal_tests.sh
+++ tests/ci/run_minimal_tests.sh
@@ -4,22 +4,12 @@
 
 source tests/ci/common_posix_setup.sh
 
+# build AWS-LC without Go.
 echo "Testing AWS-LC without Go in debug mode."
-build_and_run_minimal_test -DDISABLE_GO=ON
+build_and_run_minimal_test
 
 echo "Testing AWS-LC without Go in release mode."
-build_and_run_minimal_test -DDISABLE_GO=ON -DCMAKE_BUILD_TYPE=Release
+build_and_run_minimal_test -DCMAKE_BUILD_TYPE=Release
 
 echo "Testing shared lib AWS-LC without Go in release mode."
-build_and_run_minimal_test -DDISABLE_GO=ON -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
-
-# build AWS-LC without Perl/Go.
-echo "Testing AWS-LC without Perl/Go in debug mode."
-build_and_run_minimal_test -DDISABLE_GO=ON -DDISABLE_PERL=ON
-
-echo "Testing AWS-LC without Perl/Go in release mode."
-build_and_run_minimal_test -DDISABLE_GO=ON -DDISABLE_PERL=ON -DCMAKE_BUILD_TYPE=Release
-
-echo "Testing shared lib AWS-LC without Perl/Go in release mode."
-build_and_run_minimal_test -DDISABLE_GO=ON -DDISABLE_PERL=ON -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
-
+build_and_run_minimal_test -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
