0b8792fe5e2df8b5efcd3f2f19312f97c35b3b0e
Dusan Kostic <dkostic@amazon.com>
2021-05-28 18:53:26 +0200

    Added secp192r1 (P-192) curve

--- tool/speed.cc
+++ tool/speed.cc
@@ -726,7 +726,8 @@ static bool SpeedECDSACurve(const std::string &name, int nid,
 }
 
 static bool SpeedECDH(const std::string &selected) {
-  return SpeedECDHCurve("ECDH P-224", NID_secp224r1, selected) &&
+  return SpeedECDHCurve("ECDH P-192", NID_X9_62_prime192v1, selected) &&
+         SpeedECDHCurve("ECDH P-224", NID_secp224r1, selected) &&
          SpeedECDHCurve("ECDH P-256", NID_X9_62_prime256v1, selected) &&
          SpeedECDHCurve("ECDH P-384", NID_secp384r1, selected) &&
          SpeedECDHCurve("ECDH P-521", NID_secp521r1, selected) &&
@@ -734,7 +735,8 @@ static bool SpeedECDH(const std::string &selected) {
 }
 
 static bool SpeedECDSA(const std::string &selected) {
-  return SpeedECDSACurve("ECDSA P-224", NID_secp224r1, selected) &&
+  return SpeedECDSACurve("ECDSA P-192", NID_X9_62_prime192v1, selected) &&
+         SpeedECDSACurve("ECDSA P-224", NID_secp224r1, selected) &&
          SpeedECDSACurve("ECDSA P-256", NID_X9_62_prime256v1, selected) &&
          SpeedECDSACurve("ECDSA P-384", NID_secp384r1, selected) &&
          SpeedECDSACurve("ECDSA P-521", NID_secp521r1, selected) &&
