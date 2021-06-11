5a084bfad429859e397deed6d066d134254347b2
Bill Yang <wbyang@amazon.com>
2021-06-11 14:15:25 -0700

    playing around with openssl bm tool

--- tool/speed.cc
+++ tool/speed.cc
@@ -24,24 +24,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <openssl/aead.h>
-#include <openssl/aes.h>
-#include <openssl/bn.h>
-#include <openssl/curve25519.h>
-#include <openssl/crypto.h>
-#include <openssl/digest.h>
-#include <openssl/err.h>
-#include <openssl/ec.h>
-#include <openssl/ecdsa.h>
-//#include <../../boringssl/include/openssl/ecdsa.h>
-#include <openssl/ec_key.h>
-#include <openssl/evp.h>
-#include <openssl/hrss.h>
-#include <openssl/mem.h>
-#include <openssl/nid.h>
-#include <openssl/rand.h>
-#include <openssl/rsa.h>
-#include <openssl/trust_token.h>
+#if defined(OPENSSL_BENCHMARK)
+#include "bssl_bm.h"
+#else
+#include "ossl_bm.h"
+#endif
 
 #if defined(OPENSSL_WINDOWS)
 OPENSSL_MSVC_PRAGMA(warning(push, 3))
