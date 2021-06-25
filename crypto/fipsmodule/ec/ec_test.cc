567fb2ad1bb42a0bc22c0ba5f42aa51ef0aae17c
Dusan Kostic <dkostic@amazon.com>
2021-06-25 08:03:00 +0200

    Added scalar_base_mult tests for P-192 and secp256k1
    Signed-off-by: Dusan Kostic <dkostic@amazon.com>

--- crypto/fipsmodule/ec/ec_test.cc
+++ crypto/fipsmodule/ec/ec_test.cc
@@ -913,11 +913,10 @@ static bssl::UniquePtr<EC_GROUP> GetCurve(FileTest *t, const char *key) {
     return nullptr;
   }
 
-  // TODO: enable once the test vectors are added.
-//  if (curve_name == "P-192") {
-//    return bssl::UniquePtr<EC_GROUP>(EC_GROUP_new_by_curve_name(
-//        NID_X9_62_prime192v1));
-//  }
+  if (curve_name == "P-192") {
+    return bssl::UniquePtr<EC_GROUP>(EC_GROUP_new_by_curve_name(
+        NID_X9_62_prime192v1));
+  }
   if (curve_name == "P-224") {
     return bssl::UniquePtr<EC_GROUP>(EC_GROUP_new_by_curve_name(NID_secp224r1));
   }
@@ -931,10 +930,9 @@ static bssl::UniquePtr<EC_GROUP> GetCurve(FileTest *t, const char *key) {
   if (curve_name == "P-521") {
     return bssl::UniquePtr<EC_GROUP>(EC_GROUP_new_by_curve_name(NID_secp521r1));
   }
-  // TODO: enable once the test vectors are added.
-//  if (curve_name == "secp256k1") {
-//    return bssl::UniquePtr<EC_GROUP>(EC_GROUP_new_by_curve_name(NID_secp256k1));
-//  }
+  if (curve_name == "secp256k1") {
+    return bssl::UniquePtr<EC_GROUP>(EC_GROUP_new_by_curve_name(NID_secp256k1));
+  }
 
   t->PrintLine("Unknown curve '%s'", curve_name.c_str());
   return nullptr;
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module boringssl.googlesource.com/boringssl
 
 go 1.13
 
-require golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+require (
+	github.com/ethereum/go-ethereum v1.10.4 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+)
