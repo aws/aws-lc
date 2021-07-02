b5b4ee0bed39c09996ac0568c4c1d8db821c121e
Bill Yang <wbyang@amazon.com>
2021-07-02 10:38:08 -0700

    removing commented code

--- tool/ossl_bm.h
+++ tool/ossl_bm.h
@@ -1,21 +1,3 @@
-//#include <openssl/aead.h>
-//#include <openssl/aes.h>
-//#include <openssl/bn.h>
-//#include <openssl/curve25519.h>
-//#include <openssl/crypto.h>
-//#include <openssl/digest.h>
-//#include <openssl/err.h>
-//#include <openssl/ec.h>
-//#include <openssl/ecdsa.h>
-//#include <openssl/ec_key.h>
-//#include <openssl/evp.h>
-//#include <openssl/hrss.h>
-//#include <openssl/mem.h>
-//#include <openssl/nid.h>
-//#include <openssl/rand.h>
-//#include <openssl/rsa.h>
-//#include <openssl/trust_token.h>
-
 #include <openssl/aes.h>
 #include <openssl/bn.h>
 #include <openssl/crypto.h>
