f111d592a765a3e2ba9abe12868551742876bb2e
Bill Yang <wbyang@amazon.com>
2021-07-09 14:07:30 -0700

    checking tools

--- tests/ci/run_bm_framework.sh
+++ tests/ci/run_bm_framework.sh
@@ -3,6 +3,6 @@ set -exo pipefail
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-echo "this is a test"
-echo "will this work?"
-echo "hopefully i does :/"
+python3 --version
+python3 -m pip --version
+pip3 --version
