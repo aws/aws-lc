1a4a6e208844f308f1804768d9a548ff40b99e07
Bill Yang <wbyang@amazon.com>
2021-07-15 12:59:30 -0700

    change codebuild output to test code

--- tests/ci/run_bm_framework.sh
+++ tests/ci/run_bm_framework.sh
@@ -3,15 +3,18 @@ set -exo pipefail
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# temp
+echo TEST TEST TEST "${CODEBUILD_SRC_DIR}"
+
 # start ec2 instances
-instance_id=$(aws ec2 describe-instances --filters Name="instance.group-name",Values="bm_framework_ec2_sg" --query Reservations[*].Instances[*].[InstanceId] --output text)
-aws ec2 start-instances --instance-ids "${instance_id}"
-
-# wait until we've detected uploads to the s3 (for now just sleep 300 seconds)
-sleep 300
-
-# stop ec2 instances
-aws ec2 stop-instances --instance-ids "${instance_id}"
+#instance_id=$(aws ec2 describe-instances --filters Name="instance.group-name",Values="bm_framework_ec2_sg" --query Reservations[*].Instances[*].[InstanceId] --output text)
+#aws ec2 start-instances --instance-ids "${instance_id}"
+#
+## wait until we've detected uploads to the s3 (for now just sleep 300 seconds)
+#sleep 300
+#
+## stop ec2 instances
+#aws ec2 stop-instances --instance-ids "${instance_id}"
 
 # check correct s3 bucket results to see whether to pass/fail
 
