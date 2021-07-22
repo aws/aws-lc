06ddc295d9b1adf52dd86f08b7deeaf37172d7a2
Bryce Shang <shanggu@amazon.com>
2021-07-22 10:35:08 -0700

    more webhook test.

--- tests/ci/run_fips_tests.sh
+++ tests/ci/run_fips_tests.sh
@@ -1,20 +1,24 @@
 #!/bin/bash -ex
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
+export CC=gcc-7
+export CXX=g++-7
 
 source tests/ci/common_posix_setup.sh
 
-echo "Testing AWS-LC in FIPS debug mode."
-fips_build_and_test
-
-echo "Testing AWS-LC in FIPS release mode."
 fips_build_and_test -DCMAKE_BUILD_TYPE=Release
 
-echo "Testing shared AWS-LC in FIPS debug mode."
-fips_build_and_test -DBUILD_SHARED_LIBS=1
-
-echo "Testing shared AWS-LC in FIPS release mode."
-fips_build_and_test -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=1
+#echo "Testing AWS-LC in FIPS debug mode."
+#run_build -DFIPS=1
+#
+#echo "Testing AWS-LC in FIPS release mode."
+#run_build -DFIPS=1 -DCMAKE_BUILD_TYPE=Release
+#
+#echo "Testing shared AWS-LC in FIPS debug mode."
+#fips_build_and_test -DBUILD_SHARED_LIBS=1
+#
+#echo "Testing shared AWS-LC in FIPS release mode."
+#fips_build_and_test -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=1
 
 if [[  "${AWSLC_NO_ASM_FIPS}" == "1" ]]; then
   # This dimension corresponds to boringssl CI 'linux_fips_noasm_asan'.
