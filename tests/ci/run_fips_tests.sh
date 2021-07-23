55298ffa36fb43fb7f3c2a4dd145a2cc64e36331
Bryce Shang <shanggu@amazon.com>
2021-07-23 13:32:13 -0700

    Update TODO to trigger CI after new deployment.

--- tests/ci/run_fips_tests.sh
+++ tests/ci/run_fips_tests.sh
@@ -7,7 +7,7 @@ source tests/ci/common_posix_setup.sh
 echo "Testing AWS-LC in FIPS debug mode."
 fips_build_and_test
 
-# TODO(shang): investigate gcc fips static release build on aarch.
+# TODO(shang): investigate delocate transform errors when building static awslc with gcc fips release on aarch.
 if [[ ("${CC}" == 'clang'*) || ("$(uname -p)" == 'x86_64'*) ]]; then
   echo "Testing AWS-LC in FIPS release mode."
   fips_build_and_test -DCMAKE_BUILD_TYPE=Release
