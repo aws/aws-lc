36710d197a2cc3517446fd0a8fa619a829952c63
Dusan Kostic <dkostic@amazon.com>
2021-08-25 16:40:07 +0200

    Addressing CR comments 2
    Signed-off-by: Dusan Kostic <dkostic@amazon.com>

--- crypto/ecdh_extra/make_secp256k1_test_vectors.go
+++ crypto/ecdh_extra/make_secp256k1_test_vectors.go
@@ -6,10 +6,10 @@
 package main
 
 import (
-  "crypto/sha256"
-  "math/big"
-  "strconv"
-  "fmt"
+	"crypto/sha256"
+	"math/big"
+	"strconv"
+	"fmt"
 )
 
 import "github.com/ethereum/go-ethereum/crypto/secp256k1"
@@ -22,49 +22,55 @@ const numOfTests = 25
 var prng_ctr = 1
 
 func printPadded(key string, n, max *big.Int) {
-  padded := make([]byte, len(max.Bytes()))
-  b := n.Bytes()
-  copy(padded[len(padded)-len(b):], b)
-  fmt.Printf("%s = %x\n", key, padded)
+	padded := make([]byte, len(max.Bytes()))
+	b := n.Bytes()
+	copy(padded[len(padded)-len(b):], b)
+	fmt.Printf("%s = %x\n", key, padded)
 }
 
 func genRandModN(N *big.Int) *big.Int {
-  res := new(big.Int)
-  for {
-    dgst := sha256.Sum256([]byte("Dummy string" + strconv.Itoa(prng_ctr)))
-    prng_ctr++
-    res.SetBytes(dgst[:])
-    if res.Cmp(N) == -1 {
-      break
-    }
-  }
-  return res
+	res := new(big.Int)
+	for {
+		dgst := sha256.Sum256([]byte("Dummy string" + strconv.Itoa(prng_ctr)))
+		prng_ctr++
+		res.SetBytes(dgst[:])
+		if res.Cmp(N) == -1 {
+			break
+		}
+	}
+	return res
 }
 
 func main() {
 
-  curve := secp256k1.S256()
+	curve := secp256k1.S256()
 
-  for i := 0; i < numOfTests; i++ {
-    // Generate a private key for Alice and for Bob
-    sA := genRandModN(curve.Params().P)
-    sB := genRandModN(curve.Params().P)
+	for i := 0; i < numOfTests; i++ {
+		// Generate a private key for Alice and for Bob
+		sA := genRandModN(curve.Params().P)
+		sB := genRandModN(curve.Params().P)
 
-    // Compute the corresponding public key
-    xA, yA := curve.ScalarBaseMult(sA.Bytes())
-    xB, yB := curve.ScalarBaseMult(sB.Bytes())
+		// Compute the corresponding public key
+		xA, yA := curve.ScalarBaseMult(sA.Bytes())
+		xB, yB := curve.ScalarBaseMult(sB.Bytes())
 
-    // Compute the shared key Z
-    z, _ := curve.ScalarMult(xB, yB, sA.Bytes())
+		// Compute shared keys zA and zB for Alice and Bob
+		zA, _ := curve.ScalarMult(xB, yB, sA.Bytes())
+		zB, _ := curve.ScalarMult(xA, yA, sB.Bytes())
 
-    // Print all the required values
-    fmt.Printf("Curve = secp256k1\n")
-    printPadded("Private", sA, curve.Params().P)
-    printPadded("X", xA, curve.Params().P)
-    printPadded("Y", yA, curve.Params().P)
-    printPadded("PeerX", xB, curve.Params().P)
-    printPadded("PeerY", yB, curve.Params().P)
-    printPadded("Z", z, curve.Params().P)
-    fmt.Printf("\n")
-  }
+		if zA.Cmp(zB) != 0 {
+			fmt.Printf("Error, shared secret keys for Alice and Bob are different!")
+			return
+		}
+
+		// Print all the required values
+		fmt.Printf("Curve = secp256k1\n")
+		printPadded("Private", sA, curve.Params().P)
+		printPadded("X", xA, curve.Params().P)
+		printPadded("Y", yA, curve.Params().P)
+		printPadded("PeerX", xB, curve.Params().P)
+		printPadded("PeerY", yB, curve.Params().P)
+		printPadded("Z", zA, curve.Params().P)
+		fmt.Printf("\n")
+	}
 }
