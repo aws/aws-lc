e32d1d5742ac15c926de6a859394f29c915dea0b
jylama99 <67182918+jylama99@users.noreply.github.com>
2021-09-07 10:10:15 -0700

    SIKE implementation (#223)
    * rebase and added pq-crypto folder(CRLF/LF issue)
    
    * local clean up
    
    * clean up for PR
    
    * codebuild CMakeLists.txt issue
    
    * codebuild correct cmake issue2
    
    * updated pq CMakeLists file
    
    * fix bracket issues
    
    * fix bracket issues attempt 2
    
    * fix bracket issues attempt 3
    
    * cmake updated and braces fixed
    
    * API integration and windows codebuild fix
    
    * API integration and windows codebuild fix
    
    * clean up
    
    * apply PR feedback part 1
    
    * Update PR
    
    * Updated PR
    
    * Updated PR
    
    * Resolved all PR reviews to date
    
    * PR update
    
    * replace ct with SIKE version
    
    * fix SIKE ct function
    
    * This will fail CodeBuild
    
    * Back to working verison
    
    * Added helpful comments
    
    * Add helpful TODO comments

---
+++ pq-crypto/EVP_kem.c
@@ -0,0 +1,103 @@
+// -----------------------------------------------------------------------------
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// KEM Library
+// Abstract: Post-Quantum Algorithm API
+// -----------------------------------------------------------------------------
+
+#include <stdint.h>
+#include <stdlib.h>
+#include "EVP_kem.h"
+#include "sike_r3/sike_internal.h"
+#include "../include/openssl/mem.h"
+
+const struct pq_kem evp_sike_p434_r3 = {
+        .name = "SIKEp434r3-KEM",
+        .public_key_length = SIKE_P434_R3_PUBLIC_KEY_BYTES,
+        .private_key_length = SIKE_P434_R3_PRIVATE_KEY_BYTES,
+        .shared_secret_key_length = SIKE_P434_R3_SHARED_SECRET_BYTES,
+        .ciphertext_length = SIKE_P434_R3_CIPHERTEXT_BYTES,
+        .generate_keypair = &sike_p434_r3_crypto_kem_keypair,
+        .encapsulate = &sike_p434_r3_crypto_kem_enc,
+        .decapsulate = &sike_p434_r3_crypto_kem_dec,
+};
+
+int pq_kem_params_alloc(pq_kem *kem, pq_kem_params *kem_params) {
+    kem_params->kem = kem;
+    kem_params->public_key = OPENSSL_malloc(kem_params->kem->public_key_length);
+    if (kem_params->public_key == NULL) {
+        pq_kem_params_free(kem_params);
+        return 0;
+    }
+    kem_params->private_key = OPENSSL_malloc(kem_params->kem->private_key_length);
+    if (kem_params->private_key == NULL) {
+        pq_kem_params_free(kem_params);
+        return 0;
+    }
+    kem_params->ciphertext = OPENSSL_malloc(kem_params->kem->ciphertext_length);
+    if (kem_params->ciphertext == NULL) {
+        pq_kem_params_free(kem_params);
+        return 0;
+    }
+    kem_params->shared_secret = OPENSSL_malloc(kem_params->kem->shared_secret_key_length);
+    if (kem_params->shared_secret == NULL) {
+        pq_kem_params_free(kem_params);
+        return 0;
+    }
+
+    return 1;
+}
+
+int pq_kem_params_free(pq_kem_params *kem_params) {
+    if (kem_params != NULL) {
+        OPENSSL_free(kem_params->public_key);
+        OPENSSL_free(kem_params->private_key);
+        OPENSSL_free(kem_params->ciphertext);
+        OPENSSL_free(kem_params->shared_secret);
+    }
+
+    return 1;
+}
+
+int EVP_kem_generate_keypair(pq_kem_params *kem_params) {
+
+    if (kem_params == NULL || kem_params->kem == NULL) {
+        return 0;
+    }
+    pq_kem *kem = kem_params->kem;
+    if (kem->generate_keypair == NULL ||
+        kem_params->public_key == NULL ||
+        kem_params->private_key == NULL) {
+        return 0;
+    }
+    return kem->generate_keypair(kem_params->public_key, kem_params->private_key);
+}
+
+int EVP_kem_encapsulate(pq_kem_params *kem_params) {
+
+    if (kem_params == NULL || kem_params->kem == NULL) {
+        return 0;
+    }
+    pq_kem *kem = kem_params->kem;
+    if (kem->encapsulate == NULL ||
+        kem_params->public_key == NULL ||
+        kem_params->ciphertext == NULL) {
+        return 0;
+    }
+    return kem->encapsulate(kem_params->ciphertext, kem_params->shared_secret, kem_params->public_key);
+}
+
+int EVP_kem_decapsulate(pq_kem_params *kem_params) {
+
+    if (kem_params == NULL || kem_params->kem == NULL) {
+        return 0;
+    }
+    pq_kem *kem = kem_params->kem;
+    if (kem->decapsulate == NULL ||
+        kem_params->private_key == NULL ||
+        kem_params->ciphertext == NULL) {
+        return 0;
+    }
+    return kem->decapsulate(kem_params->shared_secret, kem_params->ciphertext, kem_params->private_key);
+}
---
+++ pq-crypto/README.md
@@ -0,0 +1,13 @@
+# Post-quantum cryptography
+This directory contains code for new post-quantum key exchange mechanisms. There are no known computationally feasible
+attacks (classical or quantum) against these algorithms when used with the recommended key lengths.
+
+## Post-quantum cryptography
+Post-quantum public-key cryptographic algorithms run on a classical computer and are conjectured secure against both
+classical and quantum attacks. NIST is in the process of reviewing submissions and standardizing them,
+see more info on the [NIST website](https://csrc.nist.gov/Projects/Post-Quantum-Cryptography/Post-Quantum-Cryptography-Standardization).
+
+
+## SIKE (Supersingular Isogeny Key Encapsulation)
+The code in the pq-crypto/sike_r3 directory was moved from [S2N](https://github.com/aws/s2n-tls/tree/main/pq-crypto), 
+which imports code from [SIKE'S](https://sike.org/#nist-submission) repository.
