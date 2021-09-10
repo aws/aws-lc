06277236130fb01642a2cabf3b0d89f62d1b7f7d
jylama99 <67182918+jylama99@users.noreply.github.com>
2021-09-10 10:26:55 -0700

    AWS license removal for SIKE (#240)
    clean up comments and remove AWS license from SIKE.

--- pq-crypto/sike_r3/sikep434r3.c
+++ pq-crypto/sike_r3/sikep434r3.c
@@ -1,7 +1,4 @@
 // -----------------------------------------------------------------------------
-// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-//
 // Supersingular Isogeny Key Encapsulation Library
 //
 // Abstract: supersingular isogeny parameters and generation of functions for P434
