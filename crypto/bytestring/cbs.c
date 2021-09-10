0c52a41d3210bb50188e16da5d280fb047364f0b
Samuel Chiang <sachiang@amazon.com>
2021-09-10 15:38:55 -0700

    x86 and ARM Docker images for gcc-11 (#227)
    * add gcc11 docker for x86
    
    * add gcc11 docker for arm
    
    * fix typos
    
    * add gcc11 specifications to cdk
    
    * add dash for test
    
    * test
    
    * rollback test
    
    * fix build failure with gcc11 release mode on arm
    
    * Enable pragma GCC only when gcc compiler
    
    * define specific GCC version to suppress warning
    
    * test ci
    
    * kick ci

--- crypto/bytestring/cbs.c
+++ crypto/bytestring/cbs.c
@@ -477,12 +477,13 @@ int CBS_get_asn1_uint64(CBS *cbs, uint64_t *out) {
 
 // Adding warning suppression as temporary fix for gcc11 ARM build issue
 // https://github.com/awslabs/aws-lc/issues/184
-#if defined(__has_warning)
-#  if  __has_warning("-Wstringop-overflow")
-#    pragma GCC diagnostic push
-#    pragma GCC diagnostic ignored "-Wstringop-overflow"
-#  endif
-#endif
+// First two warnings are turned on to avoid incompatibilites with older compilers.
+#if defined(__GNUC__) && __GNUC__ == 11 // gcc x 11
+
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wstringop-overflow"
+
+#endif // __GNUC__
 
 int CBS_get_asn1_int64(CBS *cbs, int64_t *out) {
   int is_negative;
@@ -507,12 +508,11 @@ int CBS_get_asn1_int64(CBS *cbs, int64_t *out) {
   *out = u.i;
   return 1;
 }
+#if defined(__GNUC__) && __GNUC__ == 11 // gcc x 11
 
-#if defined(__has_warning)
-#  if  __has_warning("-Wstringop-overflow")
-#    pragma GCC diagnostic pop
-#  endif
-#endif
+# pragma GCC diagnostic pop
+
+#endif // __GNUC__
 
 int CBS_get_asn1_bool(CBS *cbs, int *out) {
   CBS bytes;
