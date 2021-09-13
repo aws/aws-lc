d03f6fcbfb902fc762d25fb8bb958a898ffcd806
Dusan Kostic <dkostic@amazon.com>
2021-09-13 18:34:47 +0200

    Addresing CR comments

--- crypto/fipsmodule/rand/cpu_jitter_test.cc
+++ crypto/fipsmodule/rand/cpu_jitter_test.cc
@@ -1,16 +1,5 @@
-/* Copyright (c) 2017, Google Inc.
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 #if defined(BORINGSSL_FIPS)
 
@@ -19,45 +8,58 @@
 #include "../../test/test_util.h"
 #include "../../../third_party/jitterentropy/jitterentropy.h"
 
+// Struct for Jitter entropy collector instace with constructor/desctructor.
+struct JitterEC {
+  rand_data *instance;
+
+  JitterEC(unsigned int osr, unsigned int flags) {
+    instance = nullptr;
+    instance = jent_entropy_collector_alloc(osr, flags);
+  }
+
+  ~JitterEC() {
+    jent_entropy_collector_free(instance);
+    instance = nullptr;
+  }
+};
+
 TEST(CPUJitterEntropyTest, Basic) {
 
-  rand_data *jitter_ec = nullptr;
-
   // Allocate Jitter instance with default oversampling rate.
-  jitter_ec = jent_entropy_collector_alloc(0, JENT_FORCE_FIPS);
+  JitterEC jitter_ec(0, JENT_FORCE_FIPS);
 
   // Check that the instance is properly allocated and initialized.
-  EXPECT_NE(jitter_ec, nullptr);
+  EXPECT_NE(jitter_ec.instance, nullptr);
 
   // Check that the default oversampling rate is 3 as expected.
   unsigned int default_osr = 3;
-  EXPECT_EQ(jitter_ec->osr, default_osr);
+  EXPECT_EQ(jitter_ec.instance->osr, default_osr);
 
   const ssize_t data_len = 48;
   uint8_t data0[data_len], data1[data_len];
 
   // Draw some entropy to check if it works.
-  EXPECT_EQ(jent_read_entropy(jitter_ec, (char*) data0, data_len), data_len);
+  EXPECT_EQ(jent_read_entropy(jitter_ec.instance,
+                              (char*) data0, data_len), data_len);
 
   // Draw some entropy with the "safe" API to check if it works.
-  EXPECT_EQ(
-      jent_read_entropy_safe(&jitter_ec, (char*) data1, data_len), data_len);
+  EXPECT_EQ(jent_read_entropy_safe(&jitter_ec.instance,
+                                   (char*) data1, data_len), data_len);
 
   // Basic check that the random data is not equal.
   EXPECT_NE(Bytes(data0), Bytes(data1));
 
   // Free Jitter instance and initialize a new one with different osr.
-  jent_entropy_collector_free(jitter_ec);
+  jent_entropy_collector_free(jitter_ec.instance);
 
   unsigned int osr = 5;
-  jitter_ec = jent_entropy_collector_alloc(osr, JENT_FORCE_FIPS);
-  EXPECT_EQ(jitter_ec->osr, osr);
+  jitter_ec.instance = jent_entropy_collector_alloc(osr, JENT_FORCE_FIPS);
+  EXPECT_NE(jitter_ec.instance, nullptr);
+  EXPECT_EQ(jitter_ec.instance->osr, osr);
 
-  // Verify that the Jitter library version is v3.1.0
+  // Verify that the Jitter library version is v3.1.0.
   unsigned int jitter_version = 3010000;
   EXPECT_EQ(jitter_version, jent_version());
-
-  jent_entropy_collector_free(jitter_ec);
 }
 
 #endif
