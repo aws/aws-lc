aws/aws-lc

commit 20c8fe7caa612ae9fe3da0b2f834159f961f4c0d

Hrss fixes (#165)
* Fix error for unnamed struct/union

* Fix error related to accessing values of __m128i type

* Use methods also available on 32-bit systems

* Add missing alignas(16)

crypto/hrss/hrss.c
@@ -817,7 +817,11 @@ static void poly3_invert_vec(struct poly3 *out, const struct poly3 *in) {
     const vec_t c_a = vec_broadcast_bit(f_a[0] & g_a[0]);
     const vec_t c_s = vec_broadcast_bit((f_s[0] ^ g_s[0]) & c_a);
 
-    delta = constant_time_select_int(lsb_to_all(mask[0]), -delta, delta);
+    // This is necessary because older versions of GCC, such as version 4.1.2,
+    // do not support accessing individual elements of the __m128i type
+    alignas(16) uint64_t mask_tmp[2];
+    _mm_store_si128((void*) mask_tmp, mask);
+    delta = constant_time_select_int(lsb_to_all(mask_tmp[0]), -delta, delta);
     delta++;
 
     poly3_vec_cswap(f_s, f_a, g_s, g_a, mask);
@@ -913,19 +917,25 @@ void HRSS_poly3_invert(struct poly3 *out, const struct poly3 *in) {
 // Coefficients are ordered little-endian, thus the coefficient of x^0 is the
 // first element of the array.
 struct poly {
-#if defined(HRSS_HAVE_VECTOR_UNIT)
-  union {
-    // N + 3 = 704, which is a multiple of 64 and thus aligns things, esp for
-    // the vector code.
-    uint16_t v[N + 3];
-    vec_t vectors[VECS_PER_POLY];
-  };
-#else
-  // Even if !HRSS_HAVE_VECTOR_UNIT, external assembly may be called that
-  // requires alignment.
-  alignas(16) uint16_t v[N + 3];
-#endif
+  alignas(16) uint16_t v[N+3];
 };
+
+#if defined(HRSS_HAVE_VECTOR_UNIT)
+struct poly_vec {
+  // N + 3 = 704, which is a multiple of 64 and thus aligns things, esp for
+  // the vector code.
+  vec_t vectors[VECS_PER_POLY];
+};
+
+static void poly_vec2poly(struct poly *p, const struct poly_vec *pv)
+{
+  OPENSSL_memcpy(p, pv, sizeof(*p));
+}
+static void poly2poly_vec(struct poly_vec *pv, const struct poly *p)
+{
+  OPENSSL_memcpy(pv, p, sizeof(*p));
+}
+#endif
 
 OPENSSL_UNUSED static void poly_print(const struct poly *p) {
   printf("[");
@@ -1190,25 +1200,30 @@ static void poly_mul_vec(struct poly *out, const struct poly *x,
 
   OPENSSL_STATIC_ASSERT(sizeof(out->v) == sizeof(vec_t) * VECS_PER_POLY,
                         struct_poly_is_the_wrong_size)
-  OPENSSL_STATIC_ASSERT(alignof(struct poly) == alignof(vec_t),
-                        struct_poly_has_incorrect_alignment)
+
+  struct poly_vec x_vec = {{{0}}};
+  struct poly_vec y_vec = {{{0}}};
+  poly2poly_vec(&x_vec, x);
+  poly2poly_vec(&y_vec, y);
+
 
   vec_t prod[VECS_PER_POLY * 2];
   vec_t scratch[172];
-  poly_mul_vec_aux(prod, scratch, x->vectors, y->vectors, VECS_PER_POLY);
+  poly_mul_vec_aux(prod, scratch, x_vec.vectors, y_vec.vectors, VECS_PER_POLY);
 
   // |prod| needs to be reduced mod (𝑥^n - 1), which just involves adding the
   // upper-half to the lower-half. However, N is 701, which isn't a multiple of
   // the vector size, so the upper-half vectors all have to be shifted before
   // being added to the lower-half.
-  vec_t *out_vecs = (vec_t *)out->v;
+  struct poly_vec out_vecs = {{{0}}};
 
   for (size_t i = 0; i < VECS_PER_POLY; i++) {
     const vec_t prev = prod[VECS_PER_POLY - 1 + i];
     const vec_t this = prod[VECS_PER_POLY + i];
-    out_vecs[i] = vec_add(prod[i], vec_merge_3_5(prev, this));
+    out_vecs.vectors[i] = vec_add(prod[i], vec_merge_3_5(prev, this));
   }
 
+  poly_vec2poly(out, &out_vecs);
   OPENSSL_memset(&out->v[N], 0, 3 * sizeof(uint16_t));
 }
 

commit c81510d4f8915e930b975ddc955d4de161e7d60c

Don't use Intel-specific intrinsic on the Arm path and force alignment of Arm vector type (#265)
Intel-specific intrinsic

20c8fe7 introduced a breaking change. The CI didn't exercise this code path because it doesn't have an Arm 32-bit w/ Neon dimension. So it didn't pick up that it now uses Intel specific intrinsics.

Revert back to the original implementation for the Arm code path but use the fix in the PR for the x86 code path.

alignment

struct poly is aligned to 16-byte boundary:
https://github.com/awslabs/aws-lc/blob/main/crypto/hrss/hrss.c#L921

For SSE2 vec_t is automatically 16-byte boundary aligned because __m128i is automatically 16-byte aligned.

However, for Arm vec_t is typedef'ed to uint16x8_t, which I can't find any information about is automatically 16-byte aligned.

I can't say if this is required, but we statically assert that vec_t and struct poly should have the same alignment:
https://github.com/awslabs/aws-lc/blob/main/crypto/hrss/hrss.c#L1232

Force this alignment.

crypto/hrss/hrss.c
@@ -192,7 +192,11 @@ static inline vec_t vec_broadcast_bit(vec_t a) {
     (defined(__ARM_NEON__) || defined(__ARM_NEON))
 
 #define HRSS_HAVE_VECTOR_UNIT
-typedef uint16x8_t vec_t;
+// The Intel vector type |__m128i| is automatically aligned to a 16-byte
+// boundary. But, I can't find any information about |uint16x8_t| should
+// necessarily be aligned to a 16-byte boundary. We verify 16-byte alignment in
+// |poly_mul_vec()|, so try to force alignment here.
+typedef uint16x8_t vec_t __attribute__ ((aligned (16)));
 
 // These functions perform the same actions as the SSE2 function of the same
 // name, above.
@@ -818,11 +822,17 @@ static void poly3_invert_vec(struct poly3 *out, const struct poly3 *in) {
     const vec_t c_a = vec_broadcast_bit(f_a[0] & g_a[0]);
     const vec_t c_s = vec_broadcast_bit((f_s[0] ^ g_s[0]) & c_a);
 
+    // SSE2 is an Intel thing. So, checking for |OPENSSL_SSE2| should avoid
+    // hitting the Arm specific path under the #else. 
+#if defined(OPENSSL_SSE2)
     // This is necessary because older versions of GCC, such as version 4.1.2,
     // do not support accessing individual elements of the __m128i type
     alignas(16) uint64_t mask_tmp[2];
     _mm_store_si128((void*) mask_tmp, mask);
     delta = constant_time_select_int(lsb_to_all(mask_tmp[0]), -delta, delta);
+#else
+    delta = constant_time_select_int(lsb_to_all(mask[0]), -delta, delta);
+#endif
     delta++;
 
     poly3_vec_cswap(f_s, f_a, g_s, g_a, mask);