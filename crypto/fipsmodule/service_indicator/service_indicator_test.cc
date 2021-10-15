30df7ed59f17e733a5d0fd4777f44bcdcfb00dc7
sachiang <sachiang@amazon.com>
2021-10-15 18:54:03 +0000

    add lock for SHA512_final and tests for SHA interfaces

--- crypto/fipsmodule/service_indicator/service_indicator_test.cc
+++ crypto/fipsmodule/service_indicator/service_indicator_test.cc
@@ -330,48 +330,48 @@ static const uint8_t kAESCMACOutput[16] = {
 };
 
 static const uint8_t kOutput_md5[MD5_DIGEST_LENGTH] = {
-    0xc8, 0xbe, 0xdc, 0x96, 0xbe, 0xb0, 0xd6, 0x7b, 0x96, 0x7d, 0x3b,
-    0xd4, 0x24, 0x29, 0x30, 0xde
+    0xe9, 0x70, 0xa2, 0xf7, 0x9c, 0x55, 0x57, 0xac, 0x4e, 0x7f, 0x6b,
+    0xbc, 0xa3, 0xb9, 0xb7, 0xdb
 };
 
 static const uint8_t kOutput_sha1[SHA_DIGEST_LENGTH] = {
-    0x5b, 0xed, 0x47, 0xcc, 0xc8, 0x8d, 0x6a, 0xf8, 0x91, 0xc1, 0x85,
-    0x84, 0xe9, 0xd1, 0x31, 0xe6, 0x3e, 0x62, 0x61, 0xd9
+    0xaa, 0x18, 0x71, 0x34, 0x00, 0x71, 0x67, 0x9f, 0xa1, 0x6d, 0x20,
+    0x82, 0x91, 0x0f, 0x53, 0x0a, 0xcd, 0x6e, 0xa4, 0x34
 };
 
 static const uint8_t kOutput_sha224[SHA224_DIGEST_LENGTH] = {
-    0xef, 0xad, 0x36, 0x20, 0xc6, 0x16, 0x17, 0x24, 0x49, 0x80, 0x53,
-    0x7a, 0x46, 0x5b, 0xed, 0xde, 0x59, 0x9d, 0xa9, 0x19, 0xb0, 0xb8,
-    0x1f, 0xbe, 0x4b, 0xa7, 0xc0, 0xea
+    0x5f, 0x1a, 0x9e, 0x68, 0x4c, 0xb7, 0x42, 0x68, 0xa0, 0x8b, 0x87,
+    0xd7, 0x96, 0xb6, 0xcf, 0x1e, 0x4f, 0x85, 0x1c, 0x47, 0xe9, 0x29,
+    0xb3, 0xb2, 0x73, 0x72, 0xd2, 0x69
 };
 
 static const uint8_t kOutput_sha256[SHA256_DIGEST_LENGTH] = {
-    0x03, 0x19, 0x41, 0x4c, 0x62, 0x51, 0x83, 0xe5, 0x2b, 0x73, 0xf0,
-    0x55, 0x51, 0x5e, 0x8e, 0x7d, 0x6f, 0x3a, 0x91, 0xf1, 0xac, 0xe0,
-    0x7b, 0xb2, 0xac, 0x13, 0x65, 0x18, 0x55, 0x2c, 0x98, 0x0f
+    0xe7, 0x63, 0x1c, 0xbb, 0x12, 0xb5, 0xbf, 0x4f, 0x99, 0x05, 0x9d,
+    0x40, 0x15, 0x55, 0x34, 0x9c, 0x26, 0x36, 0xd2, 0xfe, 0x6a, 0xd6,
+    0x26, 0xb4, 0x9d, 0x33, 0x07, 0xf5, 0xe6, 0x29, 0x13, 0x92
 };
 
 static const uint8_t kOutput_sha384[SHA384_DIGEST_LENGTH] = {
-    0x0b, 0xbf, 0xc2, 0x06, 0x7a, 0x1e, 0xeb, 0x4a, 0x11, 0x57, 0x41,
-    0x20, 0x7b, 0xfb, 0xf7, 0x2c, 0x22, 0x6b, 0x96, 0xcb, 0xc6, 0x00,
-    0x81, 0xe3, 0x19, 0xf2, 0x0e, 0xcc, 0xb9, 0x5d, 0xee, 0x71, 0xda,
-    0x34, 0x10, 0xae, 0x02, 0x64, 0x31, 0x07, 0x13, 0xff, 0x47, 0xf2,
-    0xdf, 0xb0, 0x05, 0x03
+    0x15, 0x81, 0x48, 0x8d, 0x95, 0xf2, 0x66, 0x84, 0x65, 0x94, 0x3e,
+    0xb9, 0x8c, 0xda, 0x36, 0x30, 0x2a, 0x85, 0xc0, 0xcd, 0xec, 0x38,
+    0xa0, 0x1f, 0x72, 0xe2, 0x68, 0xfe, 0x4e, 0xdb, 0x27, 0x8b, 0x50,
+    0x15, 0xe0, 0x24, 0xc3, 0x65, 0xd1, 0x66, 0x2a, 0x3e, 0xe7, 0x00,
+    0x16, 0x51, 0xf5, 0x18
 };
 
 static const uint8_t kOutput_sha512[SHA512_DIGEST_LENGTH] = {
-    0x9d, 0xa3, 0xfa, 0xaf, 0xae, 0x0a, 0xf4, 0xe4, 0x2e, 0x68, 0xcb,
-    0x7c, 0x65, 0x04, 0x76, 0x26, 0x91, 0x2a, 0x52, 0xb6, 0xb0, 0xa9,
-    0x40, 0xa7, 0xf7, 0xcb, 0xc8, 0x8d, 0x4b, 0x55, 0x1b, 0x44, 0xe2,
-    0x13, 0xcb, 0x6a, 0x28, 0x89, 0xa3, 0x15, 0x94, 0xc6, 0xbb, 0xcb,
-    0x5d, 0xf5, 0xb3, 0x4f, 0x47, 0x8f, 0x1a, 0x44, 0x39, 0x51, 0xd2,
-    0x63, 0xb1, 0x0c, 0xe1, 0x2c, 0x8d, 0x07, 0x08, 0x2f
+    0x71, 0xcc, 0xec, 0x03, 0xf8, 0x76, 0xf4, 0x0b, 0xf1, 0x1b, 0x89,
+    0x27, 0x83, 0xa1, 0x70, 0x02, 0x00, 0x2b, 0xe9, 0x3c, 0x3c, 0x65,
+    0x12, 0xb9, 0xa8, 0x8c, 0xc5, 0x9d, 0xae, 0x3c, 0x73, 0x43, 0x76,
+    0x4d, 0x98, 0xed, 0xd0, 0xbe, 0xb4, 0xf9, 0x0b, 0x5c, 0x5d, 0x34,
+    0x46, 0x30, 0x18, 0xc2, 0x05, 0x88, 0x8a, 0x3c, 0x25, 0xcc, 0x06,
+    0xf8, 0x73, 0xb9, 0xe4, 0x18, 0xa8, 0xc2, 0xf0, 0xe5
 };
 
 static const uint8_t kOutput_sha512_256[SHA512_256_DIGEST_LENGTH] = {
-    0x4f, 0x8a, 0x34, 0x49, 0xfd, 0xc8, 0x42, 0xb7, 0xc1, 0x2b, 0x6d,
-    0x2a, 0x89, 0xb8, 0x10, 0x73, 0xde, 0x4a, 0x33, 0x7d, 0x3c, 0x8c,
-    0xa5, 0xff, 0xee, 0xc9, 0xbb, 0x92, 0x3d, 0x47, 0x60, 0x34
+    0x1a, 0x78, 0x68, 0x6b, 0x69, 0x6d, 0x28, 0x14, 0x6b, 0x37, 0x11,
+    0x2d, 0xfb, 0x72, 0x35, 0xfa, 0xc1, 0xc4, 0x5f, 0x5c, 0x49, 0x91,
+    0x08, 0x95, 0x0b, 0x0f, 0xc9, 0x88, 0x44, 0x12, 0x01, 0x6a
 };
 
 static const uint8_t kHMACOutput_sha1[SHA_DIGEST_LENGTH] = {
@@ -552,7 +552,7 @@ struct MD {
   // length of digest.
   const int length;
   // func is the digest to test.
-  const EVP_MD *(*func)(void);
+  const EVP_MD *(*func)();
   // one_shot_func is the convenience one-shot version of the digest.
   uint8_t *(*one_shot_func)(const uint8_t *, size_t, uint8_t *);
 };
@@ -568,20 +568,18 @@ static const MD sha512_256 = { "KAT for SHA512-256", SHA512_256_DIGEST_LENGTH, &
 struct DigestTestVector {
   // md is the digest to test.
   const MD &md;
-  // input is a NUL-terminated string to hash.
-  const uint8_t *input;
   // expected_digest is the expected digest.
   const uint8_t *expected_digest;
   // expected to be approved or not.
   const int expect_approved;
 } kDigestTestVectors[] = {
-    { md5, kPlaintext, kOutput_md5, AWSLC_NOT_APPROVED },
-    { sha1, kPlaintext, kOutput_sha1, AWSLC_APPROVED },
-    { sha224, kPlaintext, kOutput_sha224, AWSLC_APPROVED },
-    { sha256, kPlaintext, kOutput_sha256, AWSLC_APPROVED },
-    { sha384, kPlaintext, kOutput_sha384, AWSLC_APPROVED },
-    { sha512, kPlaintext, kOutput_sha512, AWSLC_APPROVED },
-    { sha512_256, kPlaintext, kOutput_sha512_256, AWSLC_APPROVED }
+    { md5, kOutput_md5, AWSLC_NOT_APPROVED },
+    { sha1, kOutput_sha1, AWSLC_APPROVED },
+    { sha224, kOutput_sha224, AWSLC_APPROVED },
+    { sha256, kOutput_sha256, AWSLC_APPROVED },
+    { sha384, kOutput_sha384, AWSLC_APPROVED },
+    { sha512, kOutput_sha512, AWSLC_APPROVED },
+    { sha512_256, kOutput_sha512_256, AWSLC_APPROVED }
 };
 
 class EVP_MD_ServiceIndicatorTest : public testing::TestWithParam<DigestTestVector> {};
@@ -593,6 +591,7 @@ TEST_P(EVP_MD_ServiceIndicatorTest, EVP_Digests) {
 
   int approved = AWSLC_NOT_APPROVED;
   bssl::ScopedEVP_MD_CTX ctx;
+  std::vector<uint8_t> plaintext(kPlaintext, kPlaintext + sizeof(kPlaintext));
   std::vector<uint8_t> digest(digestTestVector.md.length);
   unsigned digest_len;
 
@@ -602,7 +601,7 @@ TEST_P(EVP_MD_ServiceIndicatorTest, EVP_Digests) {
   // indicate that a service has been fully completed yet.
   CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(EVP_DigestInit_ex(ctx.get(), digestTestVector.md.func(), nullptr)));
   ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
-  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(EVP_DigestUpdate(ctx.get(), digestTestVector.input, sizeof(digestTestVector.input))));
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(EVP_DigestUpdate(ctx.get(), plaintext.data(), plaintext.size())));
   ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
   CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(EVP_DigestFinal_ex(ctx.get(), digest.data(), &digest_len)));
   ASSERT_EQ(approved, digestTestVector.expect_approved);
@@ -610,14 +609,14 @@ TEST_P(EVP_MD_ServiceIndicatorTest, EVP_Digests) {
 
 
   // Test using the one-shot |EVP_Digest| function for approval.
-  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(EVP_Digest(digestTestVector.input, sizeof(digestTestVector.input),
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(EVP_Digest(plaintext.data(), plaintext.size(),
                                                digest.data(), &digest_len, digestTestVector.md.func(), nullptr)));
   ASSERT_EQ(approved, digestTestVector.expect_approved);
   ASSERT_TRUE(check_test(digestTestVector.expected_digest, digest.data(), digest_len, digestTestVector.md.name));
 
 
   // Test using the one-shot API for approval.
-  CALL_SERVICE_AND_CHECK_APPROVED(approved, digestTestVector.md.one_shot_func(digestTestVector.input, sizeof(digestTestVector.input), digest.data()));
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, digestTestVector.md.one_shot_func(plaintext.data(), plaintext.size(), digest.data()));
   ASSERT_EQ(approved, digestTestVector.expect_approved);
   ASSERT_TRUE(check_test(digestTestVector.expected_digest, digest.data(), digestTestVector.md.length, digestTestVector.md.name));
 }
@@ -745,7 +744,7 @@ TEST_P(ECDH_ServiceIndicatorTest, ECDH) {
 
 struct KDFTestVector {
   // func is the hash function for KDF to test.
-  const EVP_MD *(*func)(void);
+  const EVP_MD *(*func)();
   const uint8_t *expected_output;
   const int expect_approved;
 } kKDFTestVectors[] = {
@@ -873,6 +872,91 @@ TEST(ServiceIndicatorTest, BasicTest) {
   ASSERT_TRUE(check_test(kAESOFBCiphertext, output, sizeof(kAESOFBCiphertext),
                          "AES-OFB Encryption KAT"));
   ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+}
+
+// Test the SHA interfaces one by one and check that only |*_Final| does the
+// approval at the end.
+TEST(ServiceIndicatorTest, SHA) {
+  int approved = AWSLC_NOT_APPROVED;
+
+  std::vector<uint8_t> plaintext(kPlaintext, kPlaintext + sizeof(kPlaintext));
+  std::vector<uint8_t> digest;
+
+  digest.resize(MD5_DIGEST_LENGTH);
+  MD5_CTX md5_ctx;
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(MD5_Init(&md5_ctx)));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(MD5_Update(&md5_ctx, plaintext.data(), plaintext.size())));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(MD5_Final(digest.data(), &md5_ctx)));
+  ASSERT_TRUE(check_test(kOutput_md5, digest.data(), sizeof(kOutput_md5), "MD5 Hash KAT"));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+
+  digest.clear();
+  digest.resize(SHA_DIGEST_LENGTH);
+  SHA_CTX sha_ctx;
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA1_Init(&sha_ctx)));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA1_Update(&sha_ctx, plaintext.data(), plaintext.size())));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA1_Final(digest.data(), &sha_ctx)));
+  ASSERT_TRUE(check_test(kOutput_sha1, digest.data(), sizeof(kOutput_sha1), "SHA1 Hash KAT"));
+  ASSERT_EQ(approved, AWSLC_APPROVED);
+
+  digest.clear();
+  digest.resize(SHA224_DIGEST_LENGTH);
+  SHA256_CTX sha224_ctx;
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA224_Init(&sha224_ctx)));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA224_Update(&sha224_ctx, plaintext.data(), plaintext.size())));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA224_Final(digest.data(), &sha224_ctx)));
+  ASSERT_TRUE(check_test(kOutput_sha224, digest.data(), sizeof(kOutput_sha224), "SHA224 Hash KAT"));
+  ASSERT_EQ(approved, AWSLC_APPROVED);
+
+  digest.clear();
+  digest.resize(SHA256_DIGEST_LENGTH);
+  SHA256_CTX sha256_ctx;
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA256_Init(&sha256_ctx)));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA256_Update(&sha256_ctx, plaintext.data(), plaintext.size())));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA256_Final(digest.data(), &sha256_ctx)));
+  ASSERT_TRUE(check_test(kOutput_sha256, digest.data(), sizeof(kOutput_sha256), "SHA256 Hash KAT"));
+  ASSERT_EQ(approved, AWSLC_APPROVED);
+
+  digest.clear();
+  digest.resize(SHA384_DIGEST_LENGTH);
+  SHA512_CTX sha384_ctx;
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA384_Init(&sha384_ctx)));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA384_Update(&sha384_ctx, plaintext.data(), plaintext.size())));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA384_Final(digest.data(), &sha384_ctx)));
+  ASSERT_TRUE(check_test(kOutput_sha384, digest.data(), sizeof(kOutput_sha384), "SHA384 Hash KAT"));
+  ASSERT_EQ(approved, AWSLC_APPROVED);
+
+  digest.clear();
+  digest.resize(SHA512_DIGEST_LENGTH);
+  SHA512_CTX sha512_ctx;
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA512_Init(&sha512_ctx)));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA512_Update(&sha512_ctx, plaintext.data(), plaintext.size())));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA512_Final(digest.data(), &sha512_ctx)));
+  ASSERT_TRUE(check_test(kOutput_sha512, digest.data(), sizeof(kOutput_sha512), "SHA512 Hash KAT"));
+  ASSERT_EQ(approved, AWSLC_APPROVED);
+
+  digest.clear();
+  digest.resize(SHA512_256_DIGEST_LENGTH);
+  SHA512_CTX sha512_256_ctx;
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA512_256_Init(&sha512_256_ctx)));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA512_256_Update(&sha512_256_ctx, plaintext.data(), plaintext.size())));
+  ASSERT_EQ(approved, AWSLC_NOT_APPROVED);
+  CALL_SERVICE_AND_CHECK_APPROVED(approved, ASSERT_TRUE(SHA512_256_Final(digest.data(), &sha512_256_ctx)));
+  ASSERT_TRUE(check_test(kOutput_sha512_256, digest.data(), sizeof(kOutput_sha512_256), "SHA512-256 Hash KAT"));
+  ASSERT_EQ(approved, AWSLC_APPROVED);
 }
 
 TEST(ServiceIndicatorTest, AESECB) {
