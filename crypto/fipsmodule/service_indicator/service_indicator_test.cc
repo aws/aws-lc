a4155ffcf3ecf82bd798150135ce6d67efe99c59
Dominic Dams <domdams@amazon.com>
2021-11-23 17:48:53 +0000

    Update version to 1.0.0 for new FIPS release

--- crypto/fipsmodule/service_indicator/service_indicator_test.cc
+++ crypto/fipsmodule/service_indicator/service_indicator_test.cc
@@ -2134,7 +2134,7 @@ TEST(ServiceIndicatorTest, DRBG) {
 // Since this is running in FIPS mode it should end in FIPS
 // Update this when the AWS-LC version number is modified
 TEST(ServiceIndicatorTest, AWSLCVersionString) {
-  ASSERT_STREQ(awslc_version_string(), "AWS-LC FIPS 0.0.2");
+  ASSERT_STREQ(awslc_version_string(), "AWS-LC FIPS 1.0.0");
 }
 
 #else
@@ -2179,6 +2179,6 @@ TEST(ServiceIndicatorTest, BasicTest) {
 // Since this is not running in FIPS mode it shouldn't end in FIPS
 // Update this when the AWS-LC version number is modified
 TEST(ServiceIndicatorTest, AWSLCVersionString) {
-  ASSERT_STREQ(awslc_version_string(), "AWS-LC 0.0.2");
+  ASSERT_STREQ(awslc_version_string(), "AWS-LC 1.0.0");
 }
 #endif // AWSLC_FIPS
