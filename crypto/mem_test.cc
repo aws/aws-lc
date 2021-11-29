659cbfae747950f888a3cfba353ad658531f7f06
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2021-11-29 14:17:35 -0800

    Power through a large amount of test cases

--- crypto/mem_test.cc
+++ crypto/mem_test.cc
@@ -21,21 +21,22 @@ TEST(MemTest, AlignedHeapAllocation) {
   size_t power_of_two = 1;
 
   do {
-  	for (size_t memory_size : {1, 10, 32, 1001}) {
-  		void *aligned_memory = OPENSSL_malloc_align_internal(memory_size, power_of_two);
-  		ASSERT_TRUE(aligned_memory);
-      ASSERT_TRUE(verify_memory_alignment(aligned_memory, power_of_two));
-      OPENSSL_free_align_internal(aligned_memory);
-  		aligned_memory = NULL;
-  	}
-  	power_of_two <<= 1;
+      for (size_t memory_size = 1; memory_size <= 2*power_of_two; memory_size++) {
+        fprintf(stderr, "memory_size = %zu\n", memory_size);
+        void *aligned_memory = OPENSSL_malloc_align_internal(memory_size, power_of_two);
+        ASSERT_TRUE(aligned_memory);
+        ASSERT_TRUE(verify_memory_alignment(aligned_memory, power_of_two));
+        OPENSSL_free_align_internal(aligned_memory);
+        aligned_memory = NULL;
+    }
+    power_of_two <<= 1;
   } while (power_of_two <= 128);
 
   EXPECT_EQ(power_of_two, static_cast<size_t>(256));
 
   for (size_t alignment_boundary_should_fail : {0, 3, 24, 129}) {
-  	void *aligned_memory = OPENSSL_malloc_align_internal(64, alignment_boundary_should_fail);
-  	ASSERT_FALSE(aligned_memory);
-  	aligned_memory = NULL;
+    void *aligned_memory = OPENSSL_malloc_align_internal(64, alignment_boundary_should_fail);
+    ASSERT_FALSE(aligned_memory);
+    aligned_memory = NULL;
   }
 }
