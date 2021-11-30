719c504ba49026f2fb4e8d375ad1baedfc7c5f5b
Brian Jarvis <brianjar@amazon.com>
2021-11-30 13:50:00 -0500

    Add some more test assertions to kyber keygen test

--- crypto/kyber/p_kyber_test.cc
+++ crypto/kyber/p_kyber_test.cc
@@ -2,7 +2,7 @@
 
 #include "openssl/base.h"
 #include "openssl/evp.h"
-
+#include "../crypto/evp_extra/internal.h"
 
 TEST(Kyber512Test, EVP_keygen) {
   EVP_PKEY_CTX *kyber_pkey_ctx =
@@ -14,6 +14,10 @@ TEST(Kyber512Test, EVP_keygen) {
 
   ASSERT_TRUE(EVP_PKEY_keygen_init(kyber_pkey_ctx));
   ASSERT_TRUE(EVP_PKEY_keygen(kyber_pkey_ctx, &kyber_pkey));
+  EXPECT_NE(kyber_pkey->pkey.ptr, nullptr);
+
+  const KYBER_512_KEY *kyber512Key = (KYBER_512_KEY *)(kyber_pkey->pkey.ptr);
+  ASSERT_TRUE(kyber512Key->has_private);
 
   EVP_PKEY_CTX_free(kyber_pkey_ctx);
 }
