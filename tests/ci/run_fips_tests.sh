393e210ec8b55110b8e4f4820d2b13c1d3d70ab9
Bryce Shang <65570357+bryce-shang@users.noreply.github.com>
2021-12-12 16:50:01 -0800

    Use RelWithDebInfo build type for vendor affirm. (#339)

--- tests/ci/run_fips_tests.sh
+++ tests/ci/run_fips_tests.sh
@@ -17,8 +17,8 @@ else
 fi
 
 if [[ ("$(uname -p)" == 'x86_64'*) ]]; then
-  echo "Testing AWS-LC in FIPS Release and DISABLE_GETAUXVAL mode."
-  fips_build_and_test -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=1 -DDISABLE_GETAUXVAL=1
+  echo "Testing AWS-LC in FIPS RelWithDebInfo and DISABLE_GETAUXVAL mode."
+  fips_build_and_test -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=1 -DDISABLE_GETAUXVAL=1
 fi
 
 echo "Testing shared AWS-LC in FIPS Debug mode in a different folder."
