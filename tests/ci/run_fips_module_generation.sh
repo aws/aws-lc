414f4e665c358b723f91686e52743a8efe7bbfd2
Bryce Shang <65570357+bryce-shang@users.noreply.github.com>
2021-12-13 10:33:24 -0800

    Use RelWithDebInfo build type for vendor affirm. (#339) (#341)

--- tests/ci/run_fips_module_generation.sh
+++ tests/ci/run_fips_module_generation.sh
@@ -4,4 +4,4 @@
 
 source tests/ci/common_posix_setup.sh
 
-fips_build_and_test -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=1 -DDISABLE_GETAUXVAL=1
+fips_build_and_test -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=1 -DDISABLE_GETAUXVAL=1
