1c294dd87deafbd0d3378119974a374568f8b567
Nevine Ebeid <66388554+nebeid@users.noreply.github.com>
2021-12-22 14:34:27 -0500

    Partial public-key validation for peer key in ECDH (#358)
    Continuing on the requirements of public key validation in SP 800-56Ar3, this commit satisfies Sec. 5.6.2.3.4, where a peer public key should undergo a partial key validation.
    The check is effectively performed in `EVP_PKEY_derive` and `ECDH_compute_key_fips`.
    
    The check is currently restricted to FIPS build since it cannot be covered by formal verification proofs quickly.
    But its scope can be widened to non-FIPS build, i.e. `ECDH_compute_key`, since the check is computationally cheap and didn't affect ECDH performance on Armv8 nor x86.

--- crypto/fipsmodule/ecdh/ecdh.c
+++ crypto/fipsmodule/ecdh/ecdh.c
@@ -79,26 +79,52 @@
 
 int ECDH_compute_shared_secret(uint8_t *buf, size_t *buflen, const EC_POINT *pub_key,
                                const EC_KEY *priv_key) {
+  int ret = 0;
   if (priv_key->priv_key == NULL) {
     OPENSSL_PUT_ERROR(ECDH, ECDH_R_NO_PRIVATE_VALUE);
-    return 0;
+    goto err;
   }
   const EC_SCALAR *const priv = &priv_key->priv_key->scalar;
   const EC_GROUP *const group = EC_KEY_get0_group(priv_key);
   if (EC_GROUP_cmp(group, pub_key->group, NULL) != 0) {
     OPENSSL_PUT_ERROR(EC, EC_R_INCOMPATIBLE_OBJECTS);
-    return 0;
+    goto err;
   }
+
+#if defined(AWSLC_FIPS)
+  // |EC_KEY_check_fips| is not an expensive operation on an external
+  // public key.
+  EC_KEY *key_pub_key = NULL;
+  key_pub_key = EC_KEY_new();
+  if (key_pub_key == NULL) {
+    goto end;
+  }
+
+  if (!EC_KEY_set_group(key_pub_key, group) ||
+      !EC_KEY_set_public_key(key_pub_key, pub_key) || // Creates a copy of pub_key within key_pub_key
+      !EC_KEY_check_fips(key_pub_key)) {
+    OPENSSL_PUT_ERROR(EC, EC_R_PUBLIC_KEY_VALIDATION_FAILED);
+    goto end;
+  }
+#endif
 
   EC_RAW_POINT shared_point;
   if (!ec_point_mul_scalar(group, &shared_point, &pub_key->raw, priv) ||
       !ec_get_x_coordinate_as_bytes(group, buf, buflen, *buflen,
                                     &shared_point)) {
     OPENSSL_PUT_ERROR(ECDH, ECDH_R_POINT_ARITHMETIC_FAILURE);
-    return 0;
+    goto end;
   }
 
-  return 1;
+  ret = 1;
+end:
+#if defined(AWSLC_FIPS)
+  if (key_pub_key != NULL) {
+    EC_KEY_free(key_pub_key);
+  }
+#endif
+err:
+  return ret;
 }
 
 int ECDH_compute_key_fips(uint8_t *out, size_t out_len, const EC_POINT *pub_key,
