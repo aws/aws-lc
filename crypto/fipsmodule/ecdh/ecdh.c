fedf2c2f3975e1231d8fdb5160f85b7599d832e3
Nevine Ebeid <nebeid@amazon.com>
2021-12-22 13:17:09 -0500

    Formal verification remedy with ing out public key validation.

--- crypto/fipsmodule/ecdh/ecdh.c
+++ crypto/fipsmodule/ecdh/ecdh.c
@@ -80,33 +80,33 @@
 int ECDH_compute_shared_secret(uint8_t *buf, size_t *buflen, const EC_POINT *pub_key,
                                const EC_KEY *priv_key) {
   int ret = 0;
-  EC_KEY *key_pub_key = NULL;
   if (priv_key->priv_key == NULL) {
     OPENSSL_PUT_ERROR(ECDH, ECDH_R_NO_PRIVATE_VALUE);
-    goto end;
+    goto err;
   }
   const EC_SCALAR *const priv = &priv_key->priv_key->scalar;
   const EC_GROUP *const group = EC_KEY_get0_group(priv_key);
   if (EC_GROUP_cmp(group, pub_key->group, NULL) != 0) {
     OPENSSL_PUT_ERROR(EC, EC_R_INCOMPATIBLE_OBJECTS);
-    goto end;
+    goto err;
   }
 
+#if defined(AWSLC_FIPS)
   // |EC_KEY_check_fips| is not an expensive operation on an external
   // public key.
+  EC_KEY *key_pub_key = NULL;
   key_pub_key = EC_KEY_new();
   if (key_pub_key == NULL) {
     goto end;
   }
 
-  if (is_fips_build()) {
-    if (!EC_KEY_set_group(key_pub_key, group) ||
-        !EC_KEY_set_public_key(key_pub_key, pub_key) || // Creates a copy of pub_key within key_pub_key
-        !EC_KEY_check_fips(key_pub_key)) {
-      OPENSSL_PUT_ERROR(EC, EC_R_PUBLIC_KEY_VALIDATION_FAILED);
-      goto end;
-    }
+  if (!EC_KEY_set_group(key_pub_key, group) ||
+      !EC_KEY_set_public_key(key_pub_key, pub_key) || // Creates a copy of pub_key within key_pub_key
+      !EC_KEY_check_fips(key_pub_key)) {
+    OPENSSL_PUT_ERROR(EC, EC_R_PUBLIC_KEY_VALIDATION_FAILED);
+    goto end;
   }
+#endif
 
   EC_RAW_POINT shared_point;
   if (!ec_point_mul_scalar(group, &shared_point, &pub_key->raw, priv) ||
@@ -118,9 +118,12 @@ int ECDH_compute_shared_secret(uint8_t *buf, size_t *buflen, const EC_POINT *pub
 
   ret = 1;
 end:
+#if defined(AWSLC_FIPS)
   if (key_pub_key != NULL) {
     EC_KEY_free(key_pub_key);
   }
+#endif
+err:
   return ret;
 }
 
