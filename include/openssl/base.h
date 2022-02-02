df8e76f67735ff3b4eb8cdfccb90afbf415ff22d
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2022-02-02 07:41:48 -0800

    Bump version to 1.0.1 instead

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -209,7 +209,7 @@ extern "C" {
 // ServiceIndicatorTest.AWSLCVersionString
 // Note: there are two versions of this test. Only one test is compiled
 // depending on FIPS mode.
-#define AWSLC_VERSION_NUMBER_STRING "0.0.3"
+#define AWSLC_VERSION_NUMBER_STRING "1.0.1"
 
 #if defined(BORINGSSL_SHARED_LIBRARY)
 
