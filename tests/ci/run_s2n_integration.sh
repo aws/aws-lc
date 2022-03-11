d69d8c29861a2be42f45ef4ab0e7919f6baf7e75
bbutch <bbutch@amazon.com>
2022-03-11 13:13:12 -0800

    Testing CI

--- tests/ci/run_s2n_integration.sh
+++ tests/ci/run_s2n_integration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
+# Testing something :)
 
 source tests/ci/common_posix_setup.sh
 
