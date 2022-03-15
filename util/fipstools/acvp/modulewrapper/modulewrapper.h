1bca8b8d96648c7f3068724631350b1bd30716e9
Andrew Hopkins <andhop@amazon.com>
2022-03-15 11:58:03 -0700

    Update comments with feedback from PR

--- util/fipstools/acvp/modulewrapper/modulewrapper.h
+++ util/fipstools/acvp/modulewrapper/modulewrapper.h
@@ -41,13 +41,13 @@ class RequestBuffer {
 };
 
 // ParseArgsFromStream returns a span of arguments, the first of which is the name
-// of the requested function, from |fd|. The return values point into |buffer|
+// of the requested function, from |stream|. The return values point into |buffer|
 // and so must not be used after |buffer| has been freed or reused for a
 // subsequent call. It returns an empty span on error, because std::optional
 // is still too new.
 Span<const Span<const uint8_t>> ParseArgsFromStream(std::istream *stream, RequestBuffer *buffer);
 
-// WriteReplyToStream writes a reply to the given file descriptor.
+// WriteReplyToStream writes a reply to the given stream.
 bool WriteReplyToStream(std::ostream *stream, const std::vector<Span<const uint8_t>> &spans);
 
 // ReplyCallback is the type of a callback that writes a reply to an ACVP
