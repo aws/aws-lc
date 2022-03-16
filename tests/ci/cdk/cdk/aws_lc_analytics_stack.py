bfbb6fae68b2ff56b132f1d4bd9a0092997dca79
Andrew Hopkins <andhop@amazon.com>
2022-03-16 16:21:47 -0700

    Add a minimal analytics build that collects size, build, and test time of several build dimensions.

---
+++ tests/ci/cdk/cdk/aws_lc_analytics_stack.py
@@ -0,0 +1,77 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from aws_cdk import core, aws_codebuild as codebuild, aws_iam as iam, aws_ec2 as ec2, aws_efs as efs
+
+from util.ecr_util import ecr_arn
+from util.iam_policies import code_build_batch_policy_in_json, \
+    code_build_publish_metrics_in_json
+from util.metadata import AWS_ACCOUNT, AWS_REGION, GITHUB_REPO_OWNER, GITHUB_REPO_NAME
+from util.yml_loader import YmlLoader
+
+
+class AwsLcGitHubAnalkyticsStack(core.Stack):
+    """Define a stack used to batch execute AWS-LC tests in GitHub."""
+
+    def __init__(self,
+                 scope: core.Construct,
+                 id: str,
+                 x86_ecr_repo_name: str,
+                 arm_ecr_repo_name: str,
+                 spec_file_path: str,
+                 **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Define CodeBuild resource.
+        git_hub_source = codebuild.Source.git_hub(
+            owner=GITHUB_REPO_OWNER,
+            repo=GITHUB_REPO_NAME,
+            webhook=True,
+            webhook_filters=[
+                codebuild.FilterGroup.in_event_of(
+                    codebuild.EventAction.PULL_REQUEST_CREATED,
+                    codebuild.EventAction.PULL_REQUEST_UPDATED,
+                    codebuild.EventAction.PULL_REQUEST_REOPENED)
+            ],
+            clone_depth=1)
+
+        # Define a IAM role for this stack.
+        code_build_batch_policy = iam.PolicyDocument.from_json(
+            code_build_batch_policy_in_json([id])
+        )
+        metrics_policy = iam.PolicyDocument.from_json(code_build_publish_metrics_in_json())
+        inline_policies = {"code_build_batch_policy": code_build_batch_policy,
+                           "fuzz_policy": metrics_policy}
+        role = iam.Role(scope=self,
+                        id="{}-role".format(id),
+                        assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com"),
+                        inline_policies=inline_policies)
+
+        # Create build spec.
+        placeholder_map = {"X86_ECR_REPO_PLACEHOLDER": ecr_arn(x86_ecr_repo_name),
+                           "ARM_ECR_REPO_PLACEHOLDER": ecr_arn(arm_ecr_repo_name)}
+        build_spec_content = YmlLoader.load(spec_file_path, placeholder_map)
+
+        # Define CodeBuild.
+        fuzz_codebuild = codebuild.Project(
+            scope=self,
+            id="AnalyticsCodeBuild",
+            project_name=id,
+            source=git_hub_source,
+            role=role,
+            timeout=core.Duration.minutes(120),
+            environment=codebuild.BuildEnvironment(compute_type=codebuild.ComputeType.LARGE,
+                                                   privileged=True,
+                                                   build_image=codebuild.LinuxBuildImage.STANDARD_4_0),
+            build_spec=codebuild.BuildSpec.from_object(build_spec_content))
+
+        # TODO: add build type BUILD_BATCH when CFN finishes the feature release. See CryptoAlg-575.
+
+        # Add 'BuildBatchConfig' property, which is not supported in CDK.
+        # CDK raw overrides: https://docs.aws.amazon.com/cdk/latest/guide/cfn_layer.html#cfn_layer_raw
+        # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-codebuild-project.html#aws-resource-codebuild-project-properties
+        cfn_codebuild = fuzz_codebuild.node.default_child
+        cfn_codebuild.add_override("Properties.BuildBatchConfig", {
+            "ServiceRole": role.role_arn,
+            "TimeoutInMins": 120
+        })
---
+++ tests/ci/run_analytics.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -exo pipefail
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+source tests/ci/common_posix_setup.sh
+
+# The build and test time is not meant to be scientific benchmark, just to get a rough sense of changes overtime.
+# In case there is a weird change over time this recorded additional information about the host to the logs.
+lscpu
+
+function put_metric {
+  # This call to publish the metric could fail but we don't want to fail the build +e turns off exit on error
+  set +e
+  aws cloudwatch put-metric-data \
+    --namespace AWS-LC \
+    "$@" || echo "Publishing metric failed, continuing with the rest of the build"
+  # Turn it back on for the rest of the build
+  set -e
+}
+
+# Return the size of an object or total for the folder (summarize)
+function size {
+  du --bytes --apparent-size --summarize "$@" | cut -f1
+}
+
+SOURCE_CODE_SIZE=$(size "$SRC_ROOT")
+put_metric --metric-name FolderSize --value "$SOURCE_CODE_SIZE" --unit Bytes --dimensions "Folder=root"
+
+for FOLDER_PATH in "$SRC_ROOT"/*/ ; do
+    FOLDER_SIZE=$(size "$FOLDER_PATH")
+    FOLDER=$(basename "$FOLDER_PATH")
+    put_metric --metric-name FolderSize --value "$FOLDER_SIZE" --unit Bytes --dimensions "Folder=${FOLDER}"
+done
+
+function run_build_and_collect_metrics {
+  common_dimensions="CMAKE_BUILD_TYPE=Release,OPENSSL_SMALL=${small},OPENSSL_NO_ASM=${no_assembly},Platform=${PLATFORM},BUILD_SHARED_LIBS=${shared_library},FIPS=${fips}"
+
+  build_start=$(date +%s)
+  run_build -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS="$shared_library" \
+      -DOPENSSL_SMALL="$small" \
+      -DOPENSSL_NO_ASM="$no_assembly" \
+      -DFIPS="$fips"
+  build_end=$(date +%s)
+  build_time=$((build_end-build_start))
+  put_metric --metric-name BuildTime --value "$build_time" --unit Seconds --dimensions "$common_dimensions"
+
+  test_start=$(date +%s)
+  run_cmake_custom_target 'run_tests'
+  test_end=$(date +%s)
+  test_time=$((test_end-test_start))
+  put_metric --metric-name TestTime --value "$test_time" --unit Seconds --dimensions "$common_dimensions"
+
+  libcrypto_size=$(du --bytes --apparent-size "${BUILD_ROOT}"/crypto/libcrypto.* | cut -f1)
+  libssl_size=$(du --bytes --apparent-size "${BUILD_ROOT}"/ssl/libssl.* | cut -f1)
+  put_metric --metric-name LibrarySize --value "$libcrypto_size" --unit Bytes --dimensions "Library=libcrypto,${common_dimensions}"
+  put_metric --metric-name LibrarySize --value "$libssl_size" --unit Bytes --dimensions "Library=libssl,${common_dimensions}"
+}
+
+fips=OFF
+for shared_library in ON OFF; do
+  for small in ON OFF; do
+    for no_assembly in ON OFF; do
+      run_build_and_collect_metrics
+    done
+  done
+done
+
+# We only care about one FIPS dimension
+shared_library=ON
+small=OFF
+no_assembly=OFF
+fips=ON
+run_build_and_collect_metrics
