a4a066f78837fb93c9661f6fbf11f97a7a85bfe7
Bryce Shang <shanggu@amazon.com>
2022-04-05 16:06:41 -0700

    Uncomment.

--- tests/ci/run_posix_tests.sh
+++ tests/ci/run_posix_tests.sh
@@ -2,49 +2,41 @@
 set -exo pipefail
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-export CC=clang-10;
-export CXX=clang++-10;
 
 source tests/ci/common_posix_setup.sh
 
 echo "Testing AWS-LC in debug mode."
-run_build -DASAN=1
-# gdb --args ./test_build_dir/crypto/crypto_test --gtest_filter=All/PerTLSLegacyAEADTest*
+build_and_test
 
-./test_build_dir/crypto/crypto_test --gtest_filter=All/PerTLSLegacyAEADTest*
-./test_build_dir/crypto/crypto_test --gtest_filter=CipherTest.SHA256WithSecretSuffix*
+echo "Testing AWS-LC in release mode."
+build_and_test -DCMAKE_BUILD_TYPE=Release
 
+echo "Testing AWS-LC small compilation."
+build_and_test -DOPENSSL_SMALL=1 -DCMAKE_BUILD_TYPE=Release
 
+echo "Testing AWS-LC in no asm mode."
+build_and_test -DOPENSSL_NO_ASM=1 -DCMAKE_BUILD_TYPE=Release
 
-# echo "Testing AWS-LC in release mode."
-# build_and_test -DCMAKE_BUILD_TYPE=Release
+echo "Testing building shared lib."
+run_build -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
 
-# echo "Testing AWS-LC small compilation."
-# build_and_test -DOPENSSL_SMALL=1 -DCMAKE_BUILD_TYPE=Release
+if [[ "${AWSLC_FIPS}" == "1" ]]; then
+  echo "Testing AWS-LC in FIPS release mode."
+  build_and_test -DFIPS=1 -DCMAKE_BUILD_TYPE=Release
+  "${BUILD_ROOT}/util/fipstools/cavp/test_fips"
+fi
 
-# echo "Testing AWS-LC in no asm mode."
-# build_and_test -DOPENSSL_NO_ASM=1 -DCMAKE_BUILD_TYPE=Release
+if [[ "${AWSLC_C99_TEST}" == "1" ]]; then
+    echo "Testing the C99 compatability of AWS-LC headers."
+    ./tests/coding_guidelines/c99_gcc_test.sh
+fi
 
-# echo "Testing building shared lib."
-# run_build -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release
+if [[ "${AWSLC_CODING_GUIDELINES_TEST}" == "1" ]]; then
+  echo "Testing that AWS-LC is compliant with the coding guidelines."
+  source ./tests/coding_guidelines/coding_guidelines_test.sh
+fi
 
-# if [[ "${AWSLC_FIPS}" == "1" ]]; then
-#   echo "Testing AWS-LC in FIPS release mode."
-#   build_and_test -DFIPS=1 -DCMAKE_BUILD_TYPE=Release
-#   "${BUILD_ROOT}/util/fipstools/cavp/test_fips"
-# fi
-
-# if [[ "${AWSLC_C99_TEST}" == "1" ]]; then
-#     echo "Testing the C99 compatability of AWS-LC headers."
-#     ./tests/coding_guidelines/c99_gcc_test.sh
-# fi
-
-# if [[ "${AWSLC_CODING_GUIDELINES_TEST}" == "1" ]]; then
-#   echo "Testing that AWS-LC is compliant with the coding guidelines."
-#   source ./tests/coding_guidelines/coding_guidelines_test.sh
-# fi
-
-# if [[ "${AWSLC_FUZZ}" == "1" ]]; then
-#   echo "Testing building fuzz tests."
-#   run_build -DFUZZ=1
-# fi
+if [[ "${AWSLC_FUZZ}" == "1" ]]; then
+  echo "Testing building fuzz tests."
+  run_build -DFUZZ=1
+fi
