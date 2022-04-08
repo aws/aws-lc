189a5b45bfb079d0ae6b27dd33a0ceb37bd26f77
Andrew Hopkins <andhop@amazon.com>
2022-04-07 17:31:22 -0700

    Fix indentation in exten c block

--- crypto/mem_test.cc
+++ crypto/mem_test.cc
@@ -12,29 +12,29 @@ int get_count = 0;
 int realloc_count = 0;
 
 extern "C" {
-__attribute__((visibility("default"))) void *OPENSSL_memory_alloc(size_t size);
-__attribute__((visibility("default"))) void OPENSSL_memory_free(void *ptr);
-__attribute__((visibility("default"))) size_t OPENSSL_memory_get_size(void *ptr);
-__attribute__((visibility("default"))) void *OPENSSL_memory_realloc(void *ptr, size_t size);
+  __attribute__((visibility("default"))) void *OPENSSL_memory_alloc(size_t size);
+  __attribute__((visibility("default"))) void OPENSSL_memory_free(void *ptr);
+  __attribute__((visibility("default"))) size_t OPENSSL_memory_get_size(void *ptr);
+  __attribute__((visibility("default"))) void *OPENSSL_memory_realloc(void *ptr, size_t size);
 
-void *OPENSSL_memory_alloc(size_t size) {
-  alloc_count++;
-  return test_pointer;
-}
+  void *OPENSSL_memory_alloc(size_t size) {
+    alloc_count++;
+    return test_pointer;
+  }
 
-void OPENSSL_memory_free(void *ptr) {
-  free_count++;
-}
+  void OPENSSL_memory_free(void *ptr) {
+    free_count++;
+  }
 
-size_t OPENSSL_memory_get_size(void *ptr) {
-  get_count++;
-  return test_size;
-}
+  size_t OPENSSL_memory_get_size(void *ptr) {
+    get_count++;
+    return test_size;
+  }
 
-void *OPENSSL_memory_realloc(void *ptr, size_t size) {
-  realloc_count++;
-  return test_pointer;
-}
+  void *OPENSSL_memory_realloc(void *ptr, size_t size) {
+    realloc_count++;
+    return test_pointer;
+  }
 }
 TEST(MemTest, BasicOverrides) {
   // Check setup conditions
