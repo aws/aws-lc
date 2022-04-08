eaee7a5a2e1d50145332e538ed8bfcfc6660fa94
Andrew Hopkins <andhop@amazon.com>
2022-04-07 21:02:19 -0700

    Fix FIPS power on test memory call issue and memory leak

--- crypto/mem_test.cc
+++ crypto/mem_test.cc
@@ -5,10 +5,9 @@
 #include <openssl/mem.h>
 
 int alloc_count = 0;
-void *test_pointer = NULL;
 size_t test_size = 0;
 int free_count = 0;
-int get_count = 0;
+int size_count = 0;
 int realloc_count = 0;
 
 extern "C" {
@@ -19,60 +18,78 @@ extern "C" {
 
   void *OPENSSL_memory_alloc(size_t size) {
     alloc_count++;
-    return test_pointer;
+    test_size = size;
+    return malloc(size);
   }
 
   void OPENSSL_memory_free(void *ptr) {
     free_count++;
+    free(ptr);
   }
 
   size_t OPENSSL_memory_get_size(void *ptr) {
-    get_count++;
+    size_count++;
     return test_size;
   }
 
   void *OPENSSL_memory_realloc(void *ptr, size_t size) {
     realloc_count++;
-    return test_pointer;
+    return realloc(ptr, size);
   }
 }
 TEST(MemTest, BasicOverrides) {
+  // The FIPS build which runs the power on self tests can call a lot of functions
+  // before this test. Therefore all of the expected counts are relative to the
+  // starting point
+  int starting_alloc = alloc_count;
+  int starting_free = free_count;
+  int starting_realloc = realloc_count;
+
+  void *malloc_ptr = nullptr;
   // Check setup conditions
-  ASSERT_EQ(alloc_count, 0);
-  ASSERT_EQ(free_count, 0);
-  ASSERT_EQ(realloc_count, 0);
-  ASSERT_EQ(get_count, 0);
-  ASSERT_EQ(test_pointer, nullptr);
+  ASSERT_EQ(starting_alloc, alloc_count);
+  ASSERT_EQ(starting_free, free_count);
+  ASSERT_EQ(starting_realloc, realloc_count);
+  ASSERT_EQ(0, size_count);
+  ASSERT_EQ(malloc_ptr, nullptr);
 
   // Verify malloc calls OPENSSL_memory_alloc and doesn't do anything else
   test_size = 10;
-  ASSERT_EQ(OPENSSL_malloc(test_size), nullptr);
-  ASSERT_EQ(alloc_count, 1);
+  malloc_ptr = OPENSSL_malloc(test_size);
+  ASSERT_NE(malloc_ptr, nullptr);
+  ASSERT_EQ(starting_alloc + 1, alloc_count);
+  ASSERT_EQ(starting_free, free_count);
+  ASSERT_EQ(starting_realloc, realloc_count);
+  ASSERT_EQ(0, size_count);
 
   // Calling realloc with null actually calls OPENSSL_malloc which calls
   // OPENSSL_memory_alloc
-  test_size = 20;
-  ASSERT_EQ(OPENSSL_realloc(test_pointer, test_size), nullptr);
-  ASSERT_EQ(alloc_count, 2);
-  ASSERT_EQ(free_count, 0);
-  ASSERT_EQ(realloc_count, 0);
-  ASSERT_EQ(get_count, 0);
-  ASSERT_EQ(test_pointer, nullptr);
+  void *null_realloc_ptr = nullptr;
+  test_size = 200;
+  void *realloc_ptr_1 = OPENSSL_realloc(null_realloc_ptr, test_size);
+  ASSERT_NE(realloc_ptr_1, nullptr);
+  ASSERT_EQ(starting_alloc + 2, alloc_count);
+  ASSERT_EQ(starting_free, free_count);
+  ASSERT_EQ(starting_realloc, realloc_count);
+  ASSERT_EQ(0, size_count);
+  ASSERT_EQ(nullptr, null_realloc_ptr);
 
   // Check realloc with a not null pointer calls OPENSSL_memory_realloc
-  test_pointer = malloc(10);
-  ASSERT_NE(test_pointer, nullptr);
-  ASSERT_EQ(OPENSSL_realloc(test_pointer, 20), test_pointer);
-  ASSERT_EQ(alloc_count, 2);
-  ASSERT_EQ(free_count, 0);
-  ASSERT_EQ(realloc_count, 1);
-  ASSERT_EQ(get_count, 0);
+  ASSERT_NE(realloc_ptr_1, nullptr);
+  test_size = 300;
+  void *realloc_ptr_2 = OPENSSL_realloc(malloc_ptr, test_size);
+  ASSERT_NE(realloc_ptr_2, realloc_ptr_1);
+  ASSERT_NE(realloc_ptr_2, malloc_ptr);
+  ASSERT_EQ(starting_alloc + 2, alloc_count);
+  ASSERT_EQ(starting_free, free_count);
+  ASSERT_EQ(starting_realloc + 1, realloc_count);
+  ASSERT_EQ(0, size_count);
 
   // Check OPENSSL_memory_free
-  OPENSSL_free(test_pointer);
-  ASSERT_NE(test_pointer, nullptr);
-  ASSERT_EQ(alloc_count, 2);
-  ASSERT_EQ(free_count, 1);
-  ASSERT_EQ(realloc_count, 1);
-  ASSERT_EQ(get_count, 0);
+  OPENSSL_free(realloc_ptr_1);
+  ASSERT_EQ(starting_alloc + 2, alloc_count);
+  ASSERT_EQ(starting_free + 1, free_count);
+  ASSERT_EQ(starting_realloc + 1, realloc_count);
+  ASSERT_EQ(0, size_count);
+  OPENSSL_free(realloc_ptr_2);
 }
