7a539d85ca6381d8b6b915dd41d1b20008afce5e
sachiang <sachiang@amazon.com>
2022-04-22 21:21:36 +0000

    add ci support for gcc4.1.3

---
+++ tests/ci/docker_images/linux-x86/build_legacy_images.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# `ubuntu-7.10-root.tar.xz` was taken from an unofficial 
+# repo: https://github.com/iComputer7/ancient-ubuntu-docker.
+# The file is predownloaded and checked in along with our CI 
+# docker images to prevent us from not being able to download
+# the file if the repo is deleted.
+
+# The wget version that comes with this docker image is too old to
+# download the cmake version from source, so we predownload the 
+# dependencies outside of the image and pull it in the container.
+wget -O ubuntu-7.10_gcc-4.1x/cmake-2.8.12.tar.gz https://cmake.org/files/v2.8/cmake-2.8.12.tar.gz
+docker build -t ubuntu-7.10:gcc-4.1x ubuntu-7.10_gcc-4.1x
---
+++ tests/ci/run_legacy_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -exo pipefail
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+source tests/ci/common_posix_setup.sh
+
+# Go is disabled because go tries to fetch from the server when building,
+# but the certificates on the ancient ubuntu are expired and the ancient 
+# tools are insufficient to update the certificates.
+# The build flags used are the same as our ancient internal build.
+echo "Testing gcc 4.1.2 support."
+run_build  -DDISABLE_GO=ON -DBUILD_TESTING="OFF" -DBUILD_LIBSSL="OFF"  \
+           -DCMAKE_BUILD_TYPE="Debug" \
+           "-DMY_ASSEMBLER_IS_TOO_OLD_FOR_AVX=ON -std=gnu99 -fgnu89-inline"
