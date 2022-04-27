df9f6646cfd38fedd8e3abe20b8a6db642df788f
sachiang <sachiang@amazon.com>
2022-04-27 01:46:18 +0000

    detail why gcc4.1.3

--- tests/ci/docker_images/linux-x86/build_legacy_images.sh
+++ tests/ci/docker_images/linux-x86/build_legacy_images.sh
@@ -2,22 +2,20 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# `ubuntu-7.10-root.tar.xz` was taken from an unofficial 
-# repo: https://github.com/iComputer7/ancient-ubuntu-docker.
-# The file is predownloaded and checked in along with our CI 
-# docker images to prevent us from not being able to download
-# the file if the repo is deleted.
 if [ -n "$1" ]; then
-  docker_folder="$1"
+  docker_name="$1"
 else
-  docker_folder='ubuntu-7.10_gcc-4.1x'
+  docker_name='ubuntu-7.10_gcc-4.1x'
 fi
 
+# `ubuntu-7.10-gutsy_gcc-4.1x` was rebuilt from an iso image
+# from: https://old-releases.ubuntu.com/releases/gutsy/
+# The docker tar file is checked into our team account.
 # Must be logged into team account to access docker tar file.
-mkdir -p ${docker_folder}/dependencies
-cd ${docker_folder}/dependencies
-aws s3 cp s3://ubuntu-7.10-gutsy-gcc-4.1x/ubuntu-7.10-gutsy_gcc-4.1x.tar ubuntu-7.10-gutsy_gcc-4.1x.tar
-docker load --input ubuntu-7.10-gutsy_gcc-4.1x.tar
+mkdir -p ${docker_name}/dependencies
+cd ${docker_name}/dependencies
+aws s3 cp s3://ubuntu-7.10-gutsy-gcc-4.1x/ubuntu-7.10-gutsy_gcc-4.1x.tar ${docker_name}.tar
+docker load --input ${docker_name}.tar
 # The wget version that comes with this docker image is too old to
 # download the cmake version from source, so we predownload the 
 # dependencies outside of the image and pull it in the container.
