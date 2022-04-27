56d55b469e36a62208db2ddad45abd502669a04d
sachiang <sachiang@amazon.com>
2022-04-27 20:59:21 +0000

    let cryptofuzz build

--- tests/ci/docker_images/linux-aarch/build_images.sh
+++ tests/ci/docker_images/linux-aarch/build_images.sh
@@ -6,7 +6,7 @@
 TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
 curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest
 
-# docker build -t amazonlinux-2-aarch:base amazonlinux-2_base
+docker build -t amazonlinux-2-aarch:base amazonlinux-2_base
 # docker build -t amazonlinux-2-aarch:gcc-7x amazonlinux-2_gcc-7x
 # docker build -t amazonlinux-2-aarch:clang-7x amazonlinux-2_clang-7x
 # docker build -t ubuntu-20.04-aarch:base ubuntu-20.04_base
@@ -16,7 +16,7 @@ curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ra
 # docker build -t ubuntu-20.04-aarch:clang-7x ubuntu-20.04_clang-7x
 # docker build -t ubuntu-20.04-aarch:clang-8x ubuntu-20.04_clang-8x
 # docker build -t ubuntu-20.04-aarch:clang-9x ubuntu-20.04_clang-9x
-# docker build -t ubuntu-20.04-aarch:clang-10x ubuntu-20.04_clang-10x
+docker build -t ubuntu-20.04-aarch:clang-10x ubuntu-20.04_clang-10x
 # docker build -t ubuntu-20.04-aarch:clang-7x-bm-framework ubuntu-20.04_clang-7x-bm-framework
 # This passes in the Dockerfile in the folder but uses the parent directory for the context so it has access to cryptofuzz_data.zip
 docker build -t ubuntu-20.04-aarch:cryptofuzz -f ubuntu-20.04_cryptofuzz/Dockerfile ../
