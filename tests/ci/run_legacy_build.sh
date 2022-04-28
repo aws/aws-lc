4e7fed16e6065a81d3aceb22b2652019acf66467
sachiang <sachiang@amazon.com>
2022-04-28 00:33:26 +0000

    lighter docker image and syntax improvements

--- tests/ci/run_legacy_build.sh
+++ tests/ci/run_legacy_build.sh
@@ -5,7 +5,6 @@ set -exo pipefail
 
 source tests/ci/common_posix_setup.sh
 
-
 # gcc 4.1.3 is used in our CI because ubuntu 7.10's `gcc4.1.3` and
 # `GNU assembler 2.18` resembles rhel5's binutils the most. SSE4
 # assembly instructions are not supported in ubuntu 7.04, while rhel5
@@ -16,4 +15,4 @@ echo "Testing gcc 4.1.2 / 4.1.3 support."
 # but the certificates on the ancient ubuntu are expired and the ancient 
 # tools are insufficient to update the certificates.
 # The build flags used are the same as our ancient internal build.
-run_build  -DBUILD_TESTING="OFF" -DBUILD_LIBSSL="OFF" -DCMAKE_BUILD_TYPE="Debug" "-DMY_ASSEMBLER_IS_TOO_OLD_FOR_AVX=ON -std=gnu99 -fgnu89-inline"
+run_build  -DBUILD_TESTING=OFF -DBUILD_LIBSSL=OFF -DCMAKE_BUILD_TYPE=Debug -DMY_ASSEMBLER_IS_TOO_OLD_FOR_AVX=ON -std=gnu99 -fgnu89-inline
