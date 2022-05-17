b00de136213eaee30ff8e8884b1afa8c41520b1a
Bryce Shang <shanggu@amazon.com>
2022-05-17 14:31:28 -0700

    Change the order of 'TLS-v1.2'.

--- util/fipstools/acvp/acvptool/subprocess/subprocess.go
+++ util/fipstools/acvp/acvptool/subprocess/subprocess.go
@@ -96,11 +96,11 @@ func NewWithIO(cmd *exec.Cmd, in io.WriteCloser, out io.ReadCloser) *Subprocess 
 		"ctrDRBG":           &drbg{"ctrDRBG", map[string]bool{"AES-128": true, "AES-192": true, "AES-256": true}},
 		"hmacDRBG":          &drbg{"hmacDRBG", map[string]bool{"SHA-1": true, "SHA2-224": true, "SHA2-256": true, "SHA2-384": true, "SHA2-512": true}},
 		"KDF":               &kdfPrimitive{},
-		"TLS-v1.2":          &tlsKDF{"TLS-v1.2"},
 		"KAS-KDF":           &hkdf{},
 		"CMAC-AES":          &keyedMACPrimitive{"CMAC-AES"},
 		"RSA":               &rsa{},
 		"kdf-components":    &tlsKDF{"kdf-components"},
+		"TLS-v1.2":          &tlsKDF{"TLS-v1.2"},
 		"KAS-ECC-SSC":       &kas{},
 		"KAS-FFC-SSC":       &kasDH{},
 	}
