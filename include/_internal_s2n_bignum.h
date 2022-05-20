cb2b1a1ee138ce9f63fb406a55d3ba8ffe1f2625
Justin Smith <justsmth@amazon.com>
2022-05-20 12:23:37 -0400

    Cleanup and indentation
    s2n-bignum original commit: https://github.com/awslabs/s2n-bignum/commit/f96fbcdc5a2ac4a4215bb08b0eeadd7bdbf87819

--- include/_internal_s2n_bignum.h
+++ include/_internal_s2n_bignum.h
@@ -1,18 +1,18 @@
 
 #ifdef __APPLE__
-#define S2N_BN_SYM_VISIBILITY_DIRECTIVE(name) .globl _##name
-#ifdef S2N_BN_HIDE_SYMBOLS
-#define S2N_BN_SYM_PRIVACY_DIRECTIVE(name) .private_extern _##name
+#   define S2N_BN_SYM_VISIBILITY_DIRECTIVE(name) .globl _##name
+#   ifdef S2N_BN_HIDE_SYMBOLS
+#      define S2N_BN_SYM_PRIVACY_DIRECTIVE(name) .private_extern _##name
+#   else
+#      define S2N_BN_SYM_PRIVACY_DIRECTIVE(name)  /* NO-OP: S2N_BN_SYM_PRIVACY_DIRECTIVE */
+#   endif
+#   define S2N_BN_SYMBOL(name) _##name
 #else
-#define S2N_BN_SYM_PRIVACY_DIRECTIVE(name)  /* NO-OP: S2N_BN_SYM_PRIVACY_DIRECTIVE */
-#endif
-#define S2N_BN_SYMBOL(name) _##name
-#else
-#define S2N_BN_SYM_VISIBILITY_DIRECTIVE(name) .globl name
-#ifdef S2N_BN_HIDE_SYMBOLS
-#define S2N_BN_SYM_PRIVACY_DIRECTIVE(name) .hidden name
-#else
-#define S2N_BN_SYM_PRIVACY_DIRECTIVE(name)  /* NO-OP: S2N_BN_SYM_PRIVACY_DIRECTIVE */
-#endif
-#define S2N_BN_SYMBOL(name) name
+#   define S2N_BN_SYM_VISIBILITY_DIRECTIVE(name) .globl name
+#   ifdef S2N_BN_HIDE_SYMBOLS
+#      define S2N_BN_SYM_PRIVACY_DIRECTIVE(name) .hidden name
+#   else
+#      define S2N_BN_SYM_PRIVACY_DIRECTIVE(name)  /* NO-OP: S2N_BN_SYM_PRIVACY_DIRECTIVE */
+#   endif
+#   define S2N_BN_SYMBOL(name) name
 #endif
