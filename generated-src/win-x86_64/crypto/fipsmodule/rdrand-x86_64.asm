1a0dd402064d1c5897522dddb14ff42bb78acabd
Anastasova <milaaws@f84d89923f12.ant.amazon.com>
2022-06-09 19:11:56 -0700

    Update build files in generated-src

---
+++ generated-src/win-x86_64/crypto/fipsmodule/rdrand-x86_64.asm
@@ -0,0 +1,69 @@
+; This file is generated from a similarly-named Perl script in the BoringSSL
+; source tree. Do not edit by hand.
+
+default	rel
+%define XMMWORD
+%define YMMWORD
+%define ZMMWORD
+
+%ifdef BORINGSSL_PREFIX
+%include "boringssl_prefix_symbols_nasm.inc"
+%endif
+section	.text code align=64
+
+
+
+
+
+global	CRYPTO_rdrand
+
+ALIGN	16
+CRYPTO_rdrand:
+
+	xor	rax,rax
+DB	73,15,199,240
+	test	r8,r8
+	jz	NEAR $L$err
+	cmp	r8,-1
+	je	NEAR $L$err
+
+	adc	rax,rax
+	mov	QWORD[rcx],r8
+	DB	0F3h,0C3h		;repret
+$L$err:
+	xor	rax,rax
+	DB	0F3h,0C3h		;repret
+
+
+
+
+
+
+
+global	CRYPTO_rdrand_multiple8_buf
+
+ALIGN	16
+CRYPTO_rdrand_multiple8_buf:
+
+	test	rdx,rdx
+	jz	NEAR $L$out
+	mov	r8,8
+$L$loop:
+DB	73,15,199,241
+	jnc	NEAR $L$err_multiple
+	test	r9,r9
+	jz	NEAR $L$err_multiple
+	cmp	r9,-1
+	je	NEAR $L$err_multiple
+	mov	QWORD[rcx],r9
+	add	rcx,r8
+	sub	rdx,r8
+	jnz	NEAR $L$loop
+$L$out:
+	mov	rax,1
+	DB	0F3h,0C3h		;repret
+$L$err_multiple:
+	xor	rax,rax
+	DB	0F3h,0C3h		;repret
+
+
