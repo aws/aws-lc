030fb99f7ac957bab33921259582eb34ab8a005d
David Benjamin <davidben@google.com>
2022-06-22 13:59:05 -0400

    Remove unnecessary parens on return.
    This is a mechanical change generated from the following command:
    
    find crypto/{asn1,pem,x509,x509v3} -name '*.c' -o -name '*.h' | xargs sed -i -e 's/return (\([^;()]*\));/return \1;/'
    
    Change-Id: I957295af96c4aa08d6006e27093fd3a07fb6fe75
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/53089
    Reviewed-by: Bob Beck <bbe@google.com>
    Commit-Queue: Bob Beck <bbe@google.com>
    (cherry picked from commit 56eeb201aeafc2354d6f172c1695fffca4a3dd04)

--- crypto/asn1/a_strex.c
+++ crypto/asn1/a_strex.c
@@ -435,7 +435,7 @@ int ASN1_STRING_print(BIO *bp, const ASN1_STRING *v) {
   const char *p;
 
   if (v == NULL) {
-    return (0);
+    return 0;
   }
   n = 0;
   p = (const char *)v->data;
@@ -448,17 +448,17 @@ int ASN1_STRING_print(BIO *bp, const ASN1_STRING *v) {
     n++;
     if (n >= 80) {
       if (BIO_write(bp, buf, n) <= 0) {
-        return (0);
+        return 0;
       }
       n = 0;
     }
   }
   if (n > 0) {
     if (BIO_write(bp, buf, n) <= 0) {
-      return (0);
+      return 0;
     }
   }
-  return (1);
+  return 1;
 }
 
 int ASN1_TIME_print(BIO *bp, const ASN1_TIME *tm) {
@@ -469,7 +469,7 @@ int ASN1_TIME_print(BIO *bp, const ASN1_TIME *tm) {
     return ASN1_GENERALIZEDTIME_print(bp, tm);
   }
   BIO_write(bp, "Bad time value", 14);
-  return (0);
+  return 0;
 }
 
 static const char *const mon[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
@@ -522,13 +522,13 @@ int ASN1_GENERALIZEDTIME_print(BIO *bp, const ASN1_GENERALIZEDTIME *tm) {
 
   if (BIO_printf(bp, "%s %2d %02d:%02d:%02d%.*s %d%s", mon[M - 1], d, h, m, s,
                  f_len, f, y, (gmt) ? " GMT" : "") <= 0) {
-    return (0);
+    return 0;
   } else {
-    return (1);
+    return 1;
   }
 err:
   BIO_write(bp, "Bad time value", 14);
-  return (0);
+  return 0;
 }
 
 // consume_two_digits is a helper function for ASN1_UTCTIME_print. If |*v|,
