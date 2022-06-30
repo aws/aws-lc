cc890a7260c861b658b3af08a3b54bdddfdafcbf
Dominic Dams <domdams@amazon.com>
2022-06-30 22:07:54 +0000

    Fixed different typo

--- tests/ci/run_benchmark_build_tests.sh
+++ tests/ci/run_benchmark_build_tests.sh
@@ -23,7 +23,7 @@ rm -rf "../openssl-1.1"
 
 function build_openssl_3_0 {
 echo "building OpenSSL 3.0"
-git clone --branch "${openssl_3.0_branch}" "${openssl_url}" "../openssl-3.0"
+git clone --branch "${openssl_3_0_branch}" "${openssl_url}" "../openssl-3.0"
 pushd "../openssl-3.0"
 mkdir -p "${install_dir}/openssl-3.0"
 ./Configure --prefix="${install_dir}/openssl-3.0" --openssldir="${install_dir}/openssl-3.0"
