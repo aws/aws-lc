f997843cc212081e9267701f0ff57d4cd7aa1a7d
Dominic Dams <domdams@amazon.com>
2022-06-30 20:33:52 +0000

    Added OpenSSL 3.0 test

--- tests/ci/run_benchmark_build_tests.sh
+++ tests/ci/run_benchmark_build_tests.sh
@@ -7,6 +7,7 @@ source tests/ci/common_posix_setup.sh
 install_dir="$(pwd)/../librypto_install_dir"
 openssl_url='https://github.com/openssl/openssl.git'
 openssl_1_1_branch='OpenSSL_1_1_1-stable'
+openssl_3_0_branch='openssl-3.0'
 
 function build_openssl_1_1 {
 echo "building OpenSSL 1.1"
@@ -20,6 +21,19 @@ popd
 rm -rf "../openssl-1.1"
 }
 
+function build_openssl_3_0 {
+echo "building OpenSSL 3.0"
+git clone --branch "${openssl_3.0_branch}" "${openssl_url}" "../openssl-3.0"
+pushd "../openssl-3.0"
+mkdir -p "${install_dir}/openssl-3.0"
+./Configure --prefix="${install_dir}/openssl-3.0" --openssldir="${install_dir}/openssl-3.0"
+make
+make install
+popd
+rm -rf "../openssl-1.1"
+}
+
+# We build each tool individually so we can have more insight into what is failing
 
 mkdir -p "${install_dir}"
 echo "Testing awslc_bm"
@@ -32,6 +46,10 @@ echo "Testing ossl_bm with OpenSSL 1.1"
 run_build -DOPENSSL_INSTALL_DIR="${install_dir}/openssl-1.1"
 "${BUILD_ROOT}/tool/ossl_bm"
 
+build_openssl_3.0
+echo "Testing ossl_bm with OpenSSL 3.0"
+run_build -DOPENSSL_INSTALL_DIR="${install_dir}/openssl-3.0"
+"${BUILD_ROOT}/tool/ossl3_bm"
 
 # echo "Testing ossl_bm with OpenSSL 1.1"
 # run_build -DOPENSSL_INSTALL_DIR="${install_dir}"
