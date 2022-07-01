ec856ac244f91c6a9673f54d427f1a00db037f6b
Justin Smith <justsmth@amazon.com>
2022-07-01 12:22:42 -0400

    Add sanity tests

--- bindings/rust/aws-lc-sys-template/src/lib.rs
+++ bindings/rust/aws-lc-sys-template/src/lib.rs
@@ -1,5 +1,24 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-pub mod bindings;
+mod bindings;
+pub use bindings::*;
 
+#[allow(non_snake_case)]
+pub fn ERR_GET_LIB(packed_error: u32) -> i32 {
+    unsafe { ERR_GET_LIB_RUST(packed_error) }
+}
+
+#[allow(non_snake_case)]
+pub fn ERR_GET_REASON(packed_error: u32) -> i32 {
+    unsafe { ERR_GET_REASON_RUST(packed_error) }
+}
+
+#[allow(non_snake_case)]
+pub fn ERR_GET_FUNC(packed_error: u32) -> i32 {
+    unsafe { ERR_GET_FUNC_RUST(packed_error) }
+}
+
+pub fn init() {
+    unsafe { CRYPTO_library_init() }
+}
---
+++ bindings/rust/aws-lc-sys-template/tests/sanity-tests.rs
@@ -0,0 +1,33 @@
+use std::mem;
+use std::mem::MaybeUninit;
+use Vec;
+use aws_lc_sys;
+
+use sha1::{Sha1, Digest};
+
+fn sha1_tester(input: &[u8]) -> [u8; 20] {
+    let mut hash = mem::MaybeUninit::<[u8; 20]>::uninit();
+
+    unsafe {
+        aws_lc_sys::SHA1(input.as_ptr(), input.len(), hash.as_mut_ptr().cast());
+        hash.assume_init()
+    }
+}
+
+fn compare(result: &[u8], expected_result: &[u8]) {
+    //println!("Comparing: {:?} to {:?}", result, expected_result);
+    assert_eq!(Vec::from(result), Vec::from(expected_result));
+}
+
+#[test]
+fn sha1() {
+    let input1 = b"hello";
+    let result1 = sha1_tester(input1);
+    let expected_result1 = Sha1::digest(input1);
+    compare(&result1, &expected_result1);
+}
+
+#[test]
+fn initialize() {
+    aws_lc_sys::init();
+}
