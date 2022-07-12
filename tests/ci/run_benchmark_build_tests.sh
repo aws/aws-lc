65e427e090e9509e16cad2b9f76e8c362433a384
Dominic Dams <domdams@amazon.com>
2022-07-12 19:52:37 +0000

    Addressing comments on PR

--- tests/ci/run_benchmark_build_tests.sh
+++ tests/ci/run_benchmark_build_tests.sh
@@ -10,27 +10,27 @@ openssl_1_1_branch='OpenSSL_1_1_1-stable'
 openssl_3_0_branch='openssl-3.0'
 
 function build_openssl_1_1 {
-echo "building OpenSSL 1.1"
-git clone --branch "${openssl_1_1_branch}" "${openssl_url}" "../openssl-1.1"
-pushd "../openssl-1.1"
-mkdir -p "${install_dir}/openssl-1.1"
-./config --prefix="${install_dir}/openssl-1.1" --openssldir="${install_dir}/openssl-1.1"
-make
-make install
-popd
-rm -rf "../openssl-1.1"
+    echo "building OpenSSL 1.1"
+    git clone --branch "${openssl_1_1_branch}" "${openssl_url}" "../openssl-1.1"
+    pushd "../openssl-1.1"
+    mkdir -p "${install_dir}/openssl-1.1"
+    ./config --prefix="${install_dir}/openssl-1.1" --openssldir="${install_dir}/openssl-1.1"
+    make "-j${NUM_CPU_THREADS}"
+    make install
+    popd
+    rm -rf "../openssl-1.1"
 }
 
 function build_openssl_3_0 {
-echo "building OpenSSL 3.0"
-git clone --branch "${openssl_3_0_branch}" "${openssl_url}" "../openssl-3.0"
-pushd "../openssl-3.0"
-mkdir -p "${install_dir}/openssl-3.0"
-./Configure --prefix="${install_dir}/openssl-3.0" --openssldir="${install_dir}/openssl-3.0"
-make
-make install
-popd
-rm -rf "../openssl-1.1"
+    echo "building OpenSSL 3.0"
+    git clone --branch "${openssl_3_0_branch}" "${openssl_url}" "../openssl-3.0"
+    pushd "../openssl-3.0"
+    mkdir -p "${install_dir}/openssl-3.0"
+    ./Configure --prefix="${install_dir}/openssl-3.0" --openssldir="${install_dir}/openssl-3.0"
+    make "-j${NUM_CPU_THREADS}"
+    make install
+    popd
+    rm -rf "../openssl-1.1"
 }
 
 # We build each tool individually so we can have more insight into what is failing
@@ -50,6 +50,3 @@ build_openssl_3_0
 echo "Testing ossl_bm with OpenSSL 3.0"
 run_build -DOPENSSL3_INSTALL_DIR="${install_dir}/openssl-3.0"
 "${BUILD_ROOT}/tool/ossl3_bm"
-
-# echo "Testing ossl_bm with OpenSSL 1.1"
-# run_build -DOPENSSL_INSTALL_DIR="${install_dir}"
