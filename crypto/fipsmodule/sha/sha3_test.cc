1d6d178bd1223b55759a745b9088af8351ab7b0f
Anastasova <milaaws@f84d89923f12.ant.amazon.com>
2022-07-14 11:17:16 -0700

    change copyright comment to //

--- crypto/fipsmodule/sha/sha3_test.cc
+++ crypto/fipsmodule/sha/sha3_test.cc
@@ -1,6 +1,5 @@
-/* Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0 
- */
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 
 
 #include <openssl/evp.h>
 #include <openssl/sha.h>
