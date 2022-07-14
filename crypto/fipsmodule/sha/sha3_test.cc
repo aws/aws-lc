fc3fb5455bb62b1e5f1abfbef05a4da6e1a5c0f4
Anastasova <milaaws@f84d89923f12.ant.amazon.com>
2022-07-14 08:30:36 -0700

    new line end of comment

--- crypto/fipsmodule/sha/sha3_test.cc
+++ crypto/fipsmodule/sha/sha3_test.cc
@@ -1,5 +1,6 @@
 /* Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0 */
+ * SPDX-License-Identifier: Apache-2.0 
+ */
 
 #include <openssl/evp.h>
 #include <openssl/sha.h>
