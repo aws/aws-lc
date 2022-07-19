c4bd5580e5c8e127e6141453dc1c1443ca1f1cb0
Dominic Dams <domdams@amazon.com>
2022-07-19 01:37:17 +0000

    Updated CI

--- tests/ci/run_benchmark_build_tests.sh
+++ tests/ci/run_benchmark_build_tests.sh
@@ -7,7 +7,20 @@ source tests/ci/common_posix_setup.sh
 install_dir="$(pwd)/../librypto_install_dir"
 openssl_url='https://github.com/openssl/openssl.git'
 openssl_1_1_branch='OpenSSL_1_1_1-stable'
+openssl_1_0_branch='OpenSSL_1_0_2-stable'
 openssl_3_0_branch='openssl-3.0'
+
+function build_openssl_1_0 {
+    echo "building OpenSSL 1.0"
+    git clone --branch "${openssl_1_0_branch}" "${openssl_url}" "../openssl-1.1"
+    pushd "../openssl-1.0"
+    mkdir -p "${install_dir}/openssl-1.0"
+    ./config --prefix="${install_dir}/openssl-1.0" --openssldir="${install_dir}/openssl-1.0"
+    make "-j${NUM_CPU_THREADS}"
+    make install
+    popd
+    rm -rf "../openssl-1.0"
+}
 
 function build_openssl_1_1 {
     echo "building OpenSSL 1.1"
@@ -34,19 +47,23 @@ function build_openssl_3_0 {
 }
 
 # We build each tool individually so we can have more insight into what is failing
-
 mkdir -p "${install_dir}"
 echo "Testing awslc_bm"
 mkdir "${install_dir}/aws_lc"
 run_build -DAWSLC_INSTALL_DIR="${install_dir}/aws_lc"
 "${BUILD_ROOT}/tool/awslc_bm"
 
+build_openssl_1_0
+echo "Testing ossl_bm with OpenSSL 1.0"
+run_build -DOPENSSL_1_0_INSTALL_DIR="${install_dir}/openssl-1.0"
+"${BUILD_ROOT}/tool/ossl_1_0_bm"
+
 build_openssl_1_1
 echo "Testing ossl_bm with OpenSSL 1.1"
-run_build -DOPENSSL_INSTALL_DIR="${install_dir}/openssl-1.1"
-"${BUILD_ROOT}/tool/ossl_bm"
+run_build -DOPENSSL_1_1_INSTALL_DIR="${install_dir}/openssl-1.1"
+"${BUILD_ROOT}/tool/ossl_1_1_bm"
 
 build_openssl_3_0
 echo "Testing ossl_bm with OpenSSL 3.0"
-run_build -DOPENSSL3_INSTALL_DIR="${install_dir}/openssl-3.0"
-"${BUILD_ROOT}/tool/ossl3_bm"
+run_build -DOPENSSL_3_0_INSTALL_DIR="${install_dir}/openssl-3.0"
+"${BUILD_ROOT}/tool/ossl_3_0_bm"
