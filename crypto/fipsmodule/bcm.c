8b06ac629ec7e0b68978532010a083ee0ef94649
manastasova <44320407+manastasova@users.noreply.github.com>
2022-07-25 11:10:25 -0700

    Import SHA3 reference implementation from OpenSSL && add EVP structs/functs with TestVector and speed bm (#515)

--- crypto/fipsmodule/bcm.c
+++ crypto/fipsmodule/bcm.c
@@ -126,9 +126,11 @@
 #include "self_check/fips.c"
 #include "self_check/self_check.c"
 #include "service_indicator/service_indicator.c"
+#include "sha/keccak1600.c"
 #include "sha/sha1-altivec.c"
 #include "sha/sha1.c"
 #include "sha/sha256.c"
+#include "sha/sha3.c"
 #include "sha/sha512.c"
 #include "tls/kdf.c"
 
--- crypto/fipsmodule/digest/digests.c
+++ crypto/fipsmodule/digest/digests.c
@@ -64,9 +64,9 @@
 #include <openssl/nid.h>
 #include <openssl/sha.h>
 
-#include "internal.h"
-#include "../delocate.h"
 #include "../../internal.h"
+#include "../sha/internal.h"
+#include "internal.h"
 
 #if defined(NDEBUG)
 #define CHECK(x) (void) (x)
@@ -266,6 +266,29 @@ DEFINE_METHOD_FUNCTION(EVP_MD, EVP_sha512_256) {
   out->ctx_size = sizeof(SHA512_CTX);
 }
 
+
+static void sha3_256_init(EVP_MD_CTX *ctx) {
+  CHECK(SHA3_Init(ctx->md_data, SHA3_PAD_CHAR, SHA3_256_DIGEST_BITLENGTH));
+}
+
+static void sha3_256_update(EVP_MD_CTX *ctx, const void *data, size_t count) {
+  CHECK(SHA3_Update(ctx->md_data, data, count));
+}
+
+static void sha3_256_final(EVP_MD_CTX *ctx, uint8_t *md) {
+  CHECK(SHA3_Final(md, ctx->md_data));
+}
+
+DEFINE_METHOD_FUNCTION(EVP_MD, EVP_sha3_256) {
+  out->type = NID_sha3_256;
+  out->md_size = SHA3_256_DIGEST_LENGTH;
+  out->flags = 0;
+  out->init = sha3_256_init;
+  out->update = sha3_256_update;
+  out->final = sha3_256_final;
+  out->block_size = SHA3_BLOCKSIZE(SHA3_256_DIGEST_BITLENGTH);
+  out->ctx_size = sizeof(KECCAK1600_CTX);
+}
 
 typedef struct {
   MD5_CTX md5;
