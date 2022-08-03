c5728a1a7e0dc9902bebd3cd7b3b39a10638d8a5
samuel40791765 <sachiang@amazon.com>
2022-08-03 17:49:14 +0000

    use OPENSSL_IS_AWSLC definition

--- tool/speed.cc
+++ tool/speed.cc
@@ -673,7 +673,7 @@ static bool SpeedHashChunk(const EVP_MD *md, std::string name,
 static bool SpeedHash(const EVP_MD *md, const std::string &name,
                       const std::string &selected) {
   // This SHA3 API is AWS-LC specific.
-#if defined(AWSLC_BENCHMARK)
+#if defined(OPENSSL_IS_AWSLC)
   if (name.find("SHA3") != std::string::npos) {
     EVP_MD_unstable_sha3_enable(true);
   }
@@ -690,7 +690,7 @@ static bool SpeedHash(const EVP_MD *md, const std::string &name,
   }
 
   // This SHA3 API is AWS-LC specific.
-#if defined(AWSLC_BENCHMARK)
+#if defined(OPENSSL_IS_AWSLC)
   EVP_MD_unstable_sha3_enable(false);
 #endif
   return true;
