ed363deff79d7655cc6c678ad021f32f88802b8e
Chris Herborth <herborth@amazon.com>
2022-09-02 11:02:51 -0400

    Review comments from Nevine

--- crypto/fipsmodule/hkdf/hkdf.c
+++ crypto/fipsmodule/hkdf/hkdf.c
@@ -23,6 +23,10 @@
 #include "../../internal.h"
 #include "../service_indicator/internal.h"
 
+// TODO(CryptoAlg-1281): We need to get our FIPS testing partner's opinion on
+// which API level(s) we need to check at. HKDF_extract() originally had checks
+// similar to HKDF_expand(), but we were unsure whether it was required at that
+// level.
 
 int HKDF(uint8_t *out_key, size_t out_len, const EVP_MD *digest,
          const uint8_t *secret, size_t secret_len, const uint8_t *salt,
@@ -30,14 +34,24 @@ int HKDF(uint8_t *out_key, size_t out_len, const EVP_MD *digest,
   // https://tools.ietf.org/html/rfc5869#section-2
   uint8_t prk[EVP_MAX_MD_SIZE];
   size_t prk_len;
+  int ret = 1;
+
+  // We have to avoid the underlying HKDF services updating the indicator
+  // state, so we lock the state here.
+  FIPS_service_indicator_lock_state();
 
   if (!HKDF_extract(prk, &prk_len, digest, secret, secret_len, salt,
                     salt_len) ||
       !HKDF_expand(out_key, out_len, digest, prk, prk_len, info, info_len)) {
-    return 0;
+    ret = 0;
+    goto out;
   }
 
-  return 1;
+out:
+  FIPS_service_indicator_unlock_state();
+  HKDF_verify_service_indicator(digest);
+
+  return ret;
 }
 
 int HKDF_extract(uint8_t *out_key, size_t *out_len, const EVP_MD *digest,
@@ -45,26 +59,16 @@ int HKDF_extract(uint8_t *out_key, size_t *out_len, const EVP_MD *digest,
                  size_t salt_len) {
   // https://tools.ietf.org/html/rfc5869#section-2.2
 
-  // We have to avoid the underlying HMAC services updating the indicator
-  // state, so we lock the state here.
-  FIPS_service_indicator_lock_state();
-
   // If salt is not given, HashLength zeros are used. However, HMAC does that
   // internally already so we can ignore it.
-  int ret = 0;
   unsigned len;
   if (HMAC(digest, salt, salt_len, secret, secret_len, out_key, &len) == NULL) {
     OPENSSL_PUT_ERROR(HKDF, ERR_R_HMAC_LIB);
-    goto out;
+    return 0;
   }
   *out_len = len;
   assert(*out_len == EVP_MD_size(digest));
-  ret = 1;
-
-out:
-  FIPS_service_indicator_unlock_state();
-  HKDF_verify_service_indicator(digest);
-  return ret;
+  return 1;
 }
 
 int HKDF_expand(uint8_t *out_key, size_t out_len, const EVP_MD *digest,
