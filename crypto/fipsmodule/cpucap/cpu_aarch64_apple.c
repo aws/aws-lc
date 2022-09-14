a505cb5492911821eff5c8b01d3d170e84e371cc
Chris Herborth <herborth@amazon.com>
2022-09-14 09:12:43 -0400

    Put cpu_aarch64_apple.c back the way it was

--- crypto/fipsmodule/cpucap/cpu_aarch64_apple.c
+++ crypto/fipsmodule/cpucap/cpu_aarch64_apple.c
@@ -17,11 +17,8 @@
 #if defined(OPENSSL_AARCH64) && defined(OPENSSL_APPLE) && \
     !defined(OPENSSL_STATIC_ARMCAP)
 
-#if !defined(_DARWIN_C_SOURCE)
-#define _DARWIN_C_SOURCE 1  // Needed for u_int and friends in sys/types.h
-#endif
-#include <sys/types.h>
 #include <sys/sysctl.h>
+#include <sys/types.h>
 
 #include <openssl/arm_arch.h>
 
