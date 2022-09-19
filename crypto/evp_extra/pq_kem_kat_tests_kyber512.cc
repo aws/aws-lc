724c96f482a1fd6d3f81fa02db55efa260c4df29
dkostic <25055813+dkostic@users.noreply.github.com>
2022-09-19 09:49:27 -0700

    Modify the implementation of KEMs and Kyber (#605)
    - Removed the OpenSSL 3.0-like structures and APIs from the implementation of PQ KEMs.
    - Added two new methods in EVP_PKEY, encapsulate and decapsulate, to be used for KEMs.
    - Renamed some macro and variable names to be consistent with NIST's notation.

--- crypto/evp_extra/pq_kem_kat_tests_kyber512.cc
+++ crypto/evp_extra/pq_kem_kat_tests_kyber512.cc
@@ -13,10 +13,10 @@ static void RunTest(FileTest *t)
   std::string count;
   std::vector<uint8_t> seed, pk, sk, ct, ss;
 
-  size_t shared_secret_len = KYBER512_KEM_SHARED_SECRET_BYTES;
-  size_t ciphertext_len = KYBER512_KEM_CIPHERTEXT_BYTES;
-  uint8_t shared_secret[KYBER512_KEM_SHARED_SECRET_BYTES];
-  uint8_t ciphertext[KYBER512_KEM_CIPHERTEXT_BYTES];
+  size_t shared_secret_len = KYBER512_SHARED_SECRET_BYTES;
+  size_t ciphertext_len = KYBER512_CIPHERTEXT_BYTES;
+  uint8_t shared_secret[KYBER512_SHARED_SECRET_BYTES];
+  uint8_t ciphertext[KYBER512_CIPHERTEXT_BYTES];
 
   ASSERT_TRUE(t->GetAttribute(&count, "count"));
   ASSERT_TRUE(t->GetBytes(&seed, "seed"));
@@ -40,19 +40,15 @@ static void RunTest(FileTest *t)
   EXPECT_EQ(Bytes(pk),
             Bytes(kyber512Key->pub, KYBER512_PUBLIC_KEY_BYTES));
   EXPECT_EQ(Bytes(sk),
-            Bytes(kyber512Key->priv, KYBER512_PRIVATE_KEY_BYTES));
+            Bytes(kyber512Key->priv, KYBER512_SECRET_KEY_BYTES));
 
-  ASSERT_TRUE(EVP_PKEY_encapsulate_init(kyber_pkey_ctx, NULL));
   ASSERT_TRUE(EVP_PKEY_encapsulate(kyber_pkey_ctx, ciphertext, &ciphertext_len, shared_secret, &shared_secret_len));
   EXPECT_EQ(Bytes(ct),
-            Bytes(ciphertext, KYBER512_KEM_CIPHERTEXT_BYTES));
-  //Specifically free the KEM because decapsulate_init on the reused ctx will
-  //allocate a new one, orphaning this one.
-  OPENSSL_free(kyber_pkey_ctx->op.encap.kem);
-  ASSERT_TRUE(EVP_PKEY_decapsulate_init(kyber_pkey_ctx, NULL));
+            Bytes(ciphertext, KYBER512_CIPHERTEXT_BYTES));
+
   ASSERT_TRUE(EVP_PKEY_decapsulate(kyber_pkey_ctx, shared_secret, &shared_secret_len, ciphertext, ciphertext_len));
   EXPECT_EQ(Bytes(ss),
-            Bytes(shared_secret, KYBER512_KEM_SHARED_SECRET_BYTES));
+            Bytes(shared_secret, KYBER512_SHARED_SECRET_BYTES));
 
   EVP_PKEY_CTX_free(kyber_pkey_ctx);
 }
--- crypto/fipsmodule/evp/kem.c
+++ crypto/fipsmodule/evp/kem.c
@@ -2,78 +2,28 @@
 #include <openssl/evp.h>
 #include <openssl/mem.h>
 
-#define SUCCESS (1)
-#define FAILURE (0)
-
-static int kem_fetch(unsigned int algorithm_type, EVP_KEM *kem) {
-  int ret = FAILURE;
-  switch (algorithm_type) {
-    case NID_KYBER512:
-      *kem = EVP_KEM_kyber512;
-      ret = SUCCESS;
-      break;
-    default:
-      ret = FAILURE;
-  }
-  return ret;
-}
-
-int EVP_PKEY_encapsulate_init(EVP_PKEY_CTX *ctx, const OSSL_PARAM params[]) {
-  EVP_KEM *kem = OPENSSL_malloc(sizeof(EVP_KEM));
-  if (kem == NULL) {
-    OPENSSL_PUT_ERROR(EVP, ERR_R_MALLOC_FAILURE);
-    return FAILURE;
-  }
-
-  if (ctx != NULL &&
-      ctx->pkey != NULL &&
-      kem_fetch(ctx->pkey->type, kem) &&
-      kem->encapsulate_init(ctx, ctx->pkey, params)) {
-    ctx->op.encap.kem = kem;
-    return SUCCESS;
-  } else {
-    OPENSSL_free(kem);
-    return FAILURE;
-  }
-}
-
 int EVP_PKEY_encapsulate(EVP_PKEY_CTX *ctx,
-                         unsigned char *out, size_t *out_len,
-                         unsigned char *secret, size_t *secret_len) {
-  if (ctx != NULL &&
-      ctx->op.encap.kem != NULL &&
-      ctx->op.encap.kem->encapsulate != NULL) {
-    return ctx->op.encap.kem->encapsulate(ctx, out, out_len, secret, secret_len);
-  }
-  return FAILURE;
-}
-
-int EVP_PKEY_decapsulate_init(EVP_PKEY_CTX *ctx, const OSSL_PARAM params[]) {
-  EVP_KEM *kem = OPENSSL_malloc(sizeof(EVP_KEM));
-  if (kem == NULL) {
-    OPENSSL_PUT_ERROR(EVP, ERR_R_MALLOC_FAILURE);
-    return FAILURE;
+                         uint8_t *ciphertext, size_t *ciphertext_len,
+                         uint8_t *shared_secret, size_t *shared_secret_len) {
+  if (ctx == NULL ||
+      ctx->pmeth == NULL ||
+      ctx->pmeth->encapsulate == NULL) {
+      return 0;
   }
 
-  if (ctx != NULL &&
-      ctx->pkey != NULL &&
-      kem_fetch(ctx->pkey->type, kem) &&
-      kem->decapsulate_init(ctx, ctx->pkey, params)) {
-    ctx->op.encap.kem = kem;
-    return SUCCESS;
-  } else {
-    OPENSSL_free(kem);
-    return FAILURE;
-  }
+  return ctx->pmeth->encapsulate(ctx, ciphertext, ciphertext_len,
+                                 shared_secret, shared_secret_len);
 }
 
 int EVP_PKEY_decapsulate(EVP_PKEY_CTX *ctx,
-                         unsigned char *secret, size_t *secret_len,
-                         const unsigned char *in, size_t in_len) {
-  if (ctx != NULL &&
-      ctx->op.encap.kem != NULL &&
-      ctx->op.encap.kem->decapsulate != NULL) {
-    return ctx->op.encap.kem->decapsulate(ctx, secret, secret_len, in, in_len);
+                         uint8_t *shared_secret, size_t *shared_secret_len,
+                         uint8_t *ciphertext, size_t ciphertext_len) {
+  if (ctx == NULL ||
+      ctx->pmeth == NULL ||
+      ctx->pmeth->decapsulate == NULL) {
+      return 0;
   }
-  return FAILURE;
+
+  return ctx->pmeth->decapsulate(ctx, shared_secret, shared_secret_len,
+                                 ciphertext, ciphertext_len);
 }
