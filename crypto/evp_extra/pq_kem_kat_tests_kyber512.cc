42fd4fcd0de002037316c56418e300277d7ae65b
dkostic <25055813+dkostic@users.noreply.github.com>
2022-09-30 09:48:56 -0700

    Add missing license

--- crypto/evp_extra/pq_kem_kat_tests_kyber512.cc
+++ crypto/evp_extra/pq_kem_kat_tests_kyber512.cc
@@ -1,3 +1,10 @@
+/*
+------------------------------------------------------------------------------------
+ Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ SPDX-License-Identifier: Apache-2.0
+------------------------------------------------------------------------------------
+*/
+
 #include <gtest/gtest.h>
 #include <openssl/evp.h>
 #include <vector>
--- crypto/kyber/kem_kyber512.c
+++ crypto/kyber/kem_kyber512.c
@@ -1,3 +1,10 @@
+/*
+------------------------------------------------------------------------------------
+ Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ SPDX-License-Identifier: Apache-2.0
+------------------------------------------------------------------------------------
+*/
+
 #include "../evp_extra/internal.h"
 #include "../fipsmodule/evp/internal.h"
 #include "kem_kyber.h"
