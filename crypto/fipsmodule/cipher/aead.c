874cea8ba548d76538635a46f8156a5d5133d4c1
Chris Herborth <chrish@pobox.com>
2022-10-19 08:36:16 -0400

    Deterministic IV for VPC (#649)
    * Add a deterministic IV generator for AES-GCM

--- crypto/fipsmodule/cipher/aead.c
+++ crypto/fipsmodule/cipher/aead.c
@@ -285,3 +285,20 @@ int EVP_AEAD_CTX_tag_len(const EVP_AEAD_CTX *ctx, size_t *out_tag_len,
   *out_tag_len = extra_in_len + ctx->tag_len;
   return 1;
 }
+
+// EVP_AEAD_iv_from_ipv4_nanosecs computes a deterministic IV compliant with
+// NIST SP 800-38D, built from an IPv4 address and the number of nanoseconds
+// since boot, writing it to |out_iv|. It returns one on success or zero for
+// error.
+int EVP_AEAD_get_iv_from_ipv4_nanosecs(const uint32_t ipv4_address,
+    const uint64_t nanosecs, uint8_t out_iv[FIPS_AES_GCM_NONCE_LENGTH]) {
+  if (out_iv == NULL) {
+    return 0;
+  }
+
+  OPENSSL_memcpy(out_iv, &ipv4_address, sizeof(ipv4_address));
+  OPENSSL_memcpy(out_iv + sizeof(ipv4_address), &nanosecs,
+                 sizeof(nanosecs));
+
+  return 1;
+}
