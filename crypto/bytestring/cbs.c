740dd4cca85708a26002a6c8c566cd7529175dce
David Benjamin <davidben@google.com>
2022-10-24 15:44:37 -0400

    More -Wshorten-64-to-32 fixes.
    I had a rewrite of the decrepit ciphers (CAST and Blowfish) to use
    CRYPTO_{load,store}_u32_be and drop the old macros, but this is probably
    not worth the effort to review. Instead, just fix the type in the macro.
    
    Bug: 516
    Change-Id: I1cdecc16f6108a6235f90cf9c2198bc797c6716e
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/54985
    Reviewed-by: Bob Beck <bbe@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    (cherry picked from commit 7ac94aa279bac74c3e486fce091e1f7a38577e68)

--- crypto/bytestring/cbs.c
+++ crypto/bytestring/cbs.c
@@ -141,7 +141,7 @@ int CBS_get_u24(CBS *cbs, uint32_t *out) {
   if (!cbs_get_u(cbs, &v, 3)) {
     return 0;
   }
-  *out = v;
+  *out = (uint32_t)v;
   return 1;
 }
 
@@ -150,7 +150,7 @@ int CBS_get_u32(CBS *cbs, uint32_t *out) {
   if (!cbs_get_u(cbs, &v, 4)) {
     return 0;
   }
-  *out = v;
+  *out = (uint32_t)v;
   return 1;
 }
 
