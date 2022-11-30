daff023c7be5a0fd869d0c4b675d28b2e8c09b6b
sachiang <sachiang@amazon.com>
2022-11-29 16:19:54 -0800

    warn when prebuilt bindings aren't available

--- bindings/rust/aws-lc-sys-template/src/lib.rs
+++ bindings/rust/aws-lc-sys-template/src/lib.rs
@@ -1,23 +1,57 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 
+// Warn to use feature native_bindings if building on a platform where prebuilt-bindings
+// aren't available
+#[cfg(all(
+    not(feature = "native_bindings"),
+    not(any(
+        all(target_os = "linux", target_arch = "x86"),
+        all(target_os = "linux", target_arch = "x86_64"),
+        all(target_os = "linux", target_arch = "aarch64"),
+        all(target_os = "macos", target_arch = "x86_64")
+    ))
+))]
+compile_error!("Prebuilt-bindings aren't available. Turn on feature native_bindings to build.");
+
+#[cfg(all(feature = "native_bindings", feature = "generate"))]
+compile_error!(
+    "Generate is only for internal usage. Only turn on feature native_bindings to build."
+);
+
 macro_rules! use_bindings {
     ($bindings:ident) => {
         mod $bindings;
         pub use $bindings::*;
-    }
+    };
 }
 
-#[cfg(all(not(feature = "native_bindings"), target_os = "linux", target_arch = "x86"))]
+#[cfg(all(
+    not(feature = "native_bindings"),
+    target_os = "linux",
+    target_arch = "x86"
+))]
 use_bindings!(linux_x86_bindings);
 
-#[cfg(all(not(feature = "native_bindings"), target_os = "linux", target_arch = "x86_64"))]
+#[cfg(all(
+    not(feature = "native_bindings"),
+    target_os = "linux",
+    target_arch = "x86_64"
+))]
 use_bindings!(linux_x86_64_bindings);
 
-#[cfg(all(not(feature = "native_bindings"), target_os = "linux", target_arch = "aarch64"))]
+#[cfg(all(
+    not(feature = "native_bindings"),
+    target_os = "linux",
+    target_arch = "aarch64"
+))]
 use_bindings!(linux_aarch64_bindings);
 
-#[cfg(all(not(feature = "native_bindings"), target_os = "macos", target_arch = "x86_64"))]
+#[cfg(all(
+    not(feature = "native_bindings"),
+    target_os = "macos",
+    target_arch = "x86_64"
+))]
 use_bindings!(macos_x86_64_bindings);
 
 #[cfg(feature = "native_bindings")]
