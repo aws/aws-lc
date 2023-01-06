6b0a6f3cd89129c8e8d15c1bbe8e206a917b4bcf
Andrew Hopkins <andhop@amazon.com>
2023-01-05 17:10:40 -0800

    Add support to delocate.go to parse output from GCC 11. Add CI configuration to build and run AWS-LC tests on Amazon Linux 2022.

--- util/fipstools/delocate/delocate.go
+++ util/fipstools/delocate/delocate.go
@@ -1136,7 +1136,7 @@ func classifyInstruction(instr string, args []*node32) instructionType {
 			return instrCompare
 		}
 
-	case "sarxq", "shlxq", "shrxq":
+	case "sarxq", "shlxq", "shrxq", "pinsrq":
 		if len(args) == 3 {
 			return instrThreeArg
 		}
@@ -1402,7 +1402,7 @@ Args:
 
 				classification := classifyInstruction(instructionName, argNodes)
 				if classification != instrThreeArg && classification != instrCompare && i != 0 {
-					return nil, errors.New("GOT access must be source operand")
+					return nil, fmt.Errorf("GOT access must be source operand, %w", classification)
 				}
 
 				// Reduce the instruction to movq symbol@GOTPCREL, targetReg.
