df47d26b0bec4e584ad0135f2595c4f549b24bf9
Andrew Hopkins <andhop@amazon.com>
2023-01-09 16:56:00 -0800

    Add ARM AL2022 CI (#746)

--- tests/ci/docker_images/linux-aarch/build_images.sh
+++ tests/ci/docker_images/linux-aarch/build_images.sh
@@ -9,6 +9,9 @@ curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ra
 docker build -t amazonlinux-2-aarch:base amazonlinux-2_base
 docker build -t amazonlinux-2-aarch:gcc-7x amazonlinux-2_gcc-7x
 docker build -t amazonlinux-2-aarch:clang-7x amazonlinux-2_clang-7x
+docker build -t amazonlinux-2022-aarch:base amazonlinux-2022_base
+docker build -t amazonlinux-2022-aarch:gcc-11x amazonlinux-2022_gcc-11x
+docker build -t amazonlinux-2022-aarch:clang-14x amazonlinux-2022_clang-14x
 docker build -t ubuntu-20.04-aarch:base ubuntu-20.04_base
 docker build -t ubuntu-20.04-aarch:gcc-7x ubuntu-20.04_gcc-7x
 docker build -t ubuntu-20.04-aarch:gcc-8x ubuntu-20.04_gcc-8x
