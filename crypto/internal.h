5b0a47d43b4b9a54121030220382e3d9b860a570
Bob Beck <bbe@google.com>
2023-02-07 16:11:58 -0700

    Add OPENSSL_asprintf and friends for asprintf(3) functionality.
    This includes an internal version which allows a flag to specify
    the use of system malloc, or OPENSSL_malloc - this in turn allows
    us to use this function in the ERR family of functions and allow
    for ERR to not call OPENSSL_malloc with a circular dependency.
    
    Bug: 564
    
    Change-Id: Ifd02d062fda9695cddbb0dbef2e1c1db0802a486
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/57005
    Auto-Submit: Bob Beck <bbe@google.com>
    Reviewed-by: David Benjamin <davidben@google.com>
    Commit-Queue: Bob Beck <bbe@google.com>
    (cherry picked from commit 350f8547cf2101669684ebdb99b49b11fff5e217)

--- crypto/internal.h
+++ crypto/internal.h
@@ -1028,6 +1028,13 @@ extern uint8_t BORINGSSL_function_hit[8];
 // This function is defined in |bcm.c|, see the comment therein for explanation.
 void dummy_func_for_constructor(void);
 #endif
+// OPENSSL_vasprintf_internal is just like |vasprintf(3)|. if |system_malloc| is
+// 0, memory will be allocated with |OPENSSL_malloc| and must be freed with
+// |OPENSSL_free|. Otherwise the system |malloc| function is used and the memory
+// must be freed with the system |free| function.
+OPENSSL_EXPORT int OPENSSL_vasprintf_internal(char **str, const char *format,
+                                              va_list args, int system_malloc)
+    OPENSSL_PRINTF_FORMAT_FUNC(2, 0);
 
 #if defined(__cplusplus)
 }  // extern C
