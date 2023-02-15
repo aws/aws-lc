22e07e44bdd1bef20dfc6a6dc34429459e8bcbcc
Andrew Hopkins <andhop@amazon.com>
2023-02-15 15:36:18 -0800

    Add bash script to install common depednecies, update version of Go everywhere to 1.20.1

--- tests/ci/docker_images/linux-x86/build_images.sh
+++ tests/ci/docker_images/linux-x86/build_images.sh
@@ -10,10 +10,11 @@
 TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
 curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest
 
-docker build -t ubuntu-18.04:base ubuntu-18.04_base
+# Every "base" image needs to build with this directory as the context so it has access to the install common dependencies
+docker build -t ubuntu-18.04:base -f ubuntu-18.04_base/Dockerfile .
 docker build -t ubuntu-18.04:gcc-7x ubuntu-18.04_gcc-7x
 docker build -t ubuntu-18.04:clang-6x ubuntu-18.04_clang-6x
-docker build -t ubuntu-20.04:base ubuntu-20.04_base
+docker build -t ubuntu-20.04:base -f ubuntu-20.04_base/Dockerfile .
 docker build -t ubuntu-20.04:gcc-7x ubuntu-20.04_gcc-7x
 docker build -t ubuntu-20.04:gcc-8x ubuntu-20.04_gcc-8x
 docker build -t ubuntu-20.04:clang-7x ubuntu-20.04_clang-7x
@@ -24,20 +25,20 @@ docker build -t ubuntu-20.04:android ubuntu-20.04_android
 docker build -t ubuntu-20.04:clang-7x-bm-framework ubuntu-20.04_clang-7x-bm-framework
 # This passes in the Dockerfile in the folder but uses the parent directory for the context so it has access to cryptofuzz_data.zip
 docker build -t ubuntu-20.04:cryptofuzz -f ubuntu-20.04_cryptofuzz/Dockerfile ../
-docker build -t ubuntu-22.04:base ubuntu-22.04_base
+docker build -t ubuntu-22.04:base -f ubuntu-22.04_base/Dockerfile .
 docker build -t ubuntu-22.04:gcc-11x ubuntu-22.04_gcc-11x
 docker build -t ubuntu-22.04:gcc-12x ubuntu-22.04_gcc-12x
-docker build -t amazonlinux-2:base amazonlinux-2_base
+docker build -t amazonlinux-2:base -f amazonlinux-2_base/Dockerfile .
 docker build -t amazonlinux-2:gcc-7x amazonlinux-2_gcc-7x
 docker build -t amazonlinux-2:gcc-7x-intel-sde amazonlinux-2_gcc-7x-intel-sde
 docker build -t amazonlinux-2:clang-7x amazonlinux-2_clang-7x
-docker build -t amazonlinux-2022:base amazonlinux-2022_base
+docker build -t amazonlinux-2022:base -f amazonlinux-2022_base/Dockerfile .
 docker build -t amazonlinux-2022:gcc-11x amazonlinux-2022_gcc-11x
 docker build -t amazonlinux-2022:clang-14x amazonlinux-2022_clang-14x
 docker build -t amazonlinux-2022:clang-14x-sanitizer amazonlinux-2022_clang-14x_sanitizer
-docker build -t ubuntu-16.04:gcc-5x ubuntu-16.04_gcc-5x
-docker build -t centos-7:gcc-4x centos-7_gcc-4x
-docker build -t fedora-31:clang-9x fedora-31_clang-9x
+docker build -t ubuntu-16.04:gcc-5x -f ubuntu-16.04_gcc-5x/Dockerfile .
+docker build -t centos-7:gcc-4x -f centos-7_gcc-4x/Dockerfile .
+docker build -t fedora-31:clang-9x -f fedora-31_clang-9x/Dockerfile .
 
 ###########################################################
 # Build images defined in aws-lc-verification GitHub repo #
---
+++ tests/ci/docker_images/linux-x86/install_common_x86_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -exo pipefail
+
+# Install golang
+mkdir $GOROOT
+GO_VERSION=1.20.1
+GO_ARCHIVE="go${GO_VERSION}.linux-amd64.tar.gz"
+wget "https://dl.google.com/go/${GO_ARCHIVE}"
+tar -xvf $GO_ARCHIVE
+mv go/* "$GOROOT"
+rm $GO_ARCHIVE
+
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://proxy.golang.org,direct
+go env -w GOFLAGS=-buildvcs=false
