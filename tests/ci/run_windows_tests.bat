f9048e35c4afa0ef6c2452bebff7f9de5b8e1c06
William Bo Yang <coolbillyang@gmail.com>
2023-02-27 18:13:17 -0600

    Fix RelWithDebInfo with FIPS build on Windows (#819) (#836)
    * add flags to generate .pdb files when building on windows
    
    * move logic under RelWithDebInfo
    
    * add relwithdebinfo to windows ci test script
    
    * add PDB files to install config

--- tests/ci/run_windows_tests.bat
+++ tests/ci/run_windows_tests.bat
@@ -21,10 +21,12 @@ call :build_and_test Release "-DOPENSSL_NO_ASM=1" || goto error
 set PATH=%BUILD_DIR%;%BUILD_DIR%\crypto;%BUILD_DIR%\ssl;%PATH%
 call :build_and_test Release "-DBUILD_SHARED_LIBS=1" || goto error
 call :build_and_test Release "-DBUILD_SHARED_LIBS=1 -DFIPS=1" || goto error
+@rem For FIPS on Windows we also have a RelWithDebInfo build to generate debug symbols.
+call :build_and_test RelWithDebInfo "-DBUILD_SHARED_LIBS=1 -DFIPS=1" || goto error
 
 goto :EOF
 
-@rem %1 is the build type Release/Debug
+@rem %1 is the build type (e.g. Release/Debug)
 @rem %2 is the additional full CMake args
 :build_and_test
 @echo on
