98685343abbb5c21b50dd5bfc0f269e08e288e0b
billbo-yang <coolbillyang@gmail.com>
2023-03-08 14:33:29 -0800

    convert memset to OPENSSL_memset for consistency

--- util/fipstools/test_fips.c
+++ util/fipstools/test_fips.c
@@ -80,7 +80,7 @@ int main(int argc, char **argv) {
   uint8_t output[256];
 
   /* AES-CBC Encryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   if (AES_set_encrypt_key(kAESKey, 8 * sizeof(kAESKey), &aes_key) != 0) {
     printf("AES_set_encrypt_key failed\n");
     goto err;
@@ -94,7 +94,7 @@ int main(int argc, char **argv) {
   hexdump(output, sizeof(kPlaintext));
 
   /* AES-CBC Decryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   if (AES_set_decrypt_key(kAESKey, 8 * sizeof(kAESKey), &aes_key) != 0) {
     printf("AES decrypt failed\n");
     goto err;
@@ -108,7 +108,7 @@ int main(int argc, char **argv) {
 
   OPENSSL_cleanse(&aes_key, sizeof(aes_key));
 
- /* AES-GCM */
+  /* AES-GCM */
   size_t out_len;
   uint8_t nonce[EVP_AEAD_MAX_NONCE_LENGTH];
   OPENSSL_memset(nonce, 0, sizeof(nonce));
@@ -120,7 +120,7 @@ int main(int argc, char **argv) {
   }
 
   /* AES-GCM Encryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   if (AES_set_encrypt_key(kAESKey, 8 * sizeof(kAESKey), &aes_key) != 0) {
     printf("AES_set_encrypt_key failed\n");
     goto err;
@@ -137,7 +137,7 @@ int main(int argc, char **argv) {
   hexdump(output, out_len);
 
   /* AES-GCM Decryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   printf("About to AES-GCM open ");
   hexdump(output, out_len);
   if (!EVP_AEAD_CTX_open(&aead_ctx, output, &out_len, sizeof(output), nonce,
@@ -161,7 +161,7 @@ int main(int argc, char **argv) {
   }
 
   /* AES-CCM Encryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   printf("About to AES-CCM seal ");
   hexdump(kPlaintext, sizeof(kPlaintext));
   if (!EVP_AEAD_CTX_seal(&aead_ctx, output, &out_len, sizeof(output), nonce,
@@ -175,7 +175,7 @@ int main(int argc, char **argv) {
   hexdump(output, out_len);
 
   /* AES-CCM Decryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   if (AES_set_decrypt_key(kAESKey, 8 * sizeof(kAESKey), &aes_key) != 0) {
     printf("AES decrypt failed\n");
     goto err;
@@ -197,7 +197,7 @@ int main(int argc, char **argv) {
 
   /* AES-ECB */
   /* AES-ECB Encryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   if (AES_set_encrypt_key(kAESKey, 8 * sizeof(kAESKey), &aes_key) != 0)
   {
     fprintf(stderr, "AES_set_encrypt_key failed.\n");
@@ -213,7 +213,7 @@ int main(int argc, char **argv) {
   hexdump(output, out_len);
 
   /* AES-ECB Decryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   if (AES_set_decrypt_key(kAESKey, 8 * sizeof(kAESKey), &aes_key) != 0) {
     printf("AES decrypt failed\n");
     goto err;
@@ -234,7 +234,7 @@ int main(int argc, char **argv) {
 
   /* AES-CTR */
   /* AES-CTR Encryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   if (AES_set_encrypt_key(kAESKey, 8 * sizeof(kAESKey), &aes_key) != 0)
   {
     fprintf(stderr, "AES_set_encrypt_key failed.\n");
@@ -248,7 +248,7 @@ int main(int argc, char **argv) {
 
 
   /* AES-CTR Decryption */
-  memset(aes_iv, 0, sizeof(aes_iv));
+  OPENSSL_memset(aes_iv, 0, sizeof(aes_iv));
   printf("About to AES-CTR Decrypt ");
   hexdump(output, out_len);
   AES_ctr128_encrypt(output, output, out_len, &aes_key, aes_iv, ecount_buf, &num);
