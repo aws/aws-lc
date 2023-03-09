fd1a18ca6e5a6d6c72c0de40d7139b6b7fb5c055
dkostic <25055813+dkostic@users.noreply.github.com>
2023-03-09 06:52:30 -0800

    KEM: fix key retrieval when public/secret key is missing (#855)
    Fixes key retrieval when public/secret key is missing. Specifically,
    when `EVP_PKEY_get_raw_private_key` is called on a key that has
    only the public key an error will be returned. The same when calling
    `EVP_PKEY_get_raw_public_key` with a key that has only the
    private key.
    
    Got rid of the `has_secret_key field` in the `KEM_KEY` structure
    because it is not necessary.
    
    Made KEM `encapsulate` function a bit stricter with the function
    arguments. Now, the arguments `ciphertext` and `shared_secret`
    have to be either both NULL or both non-NULL. When both NULL
    it's assumed the caller is performing a parameter length check.
    When both non-NULL, the actual encapsulation is done. Otherwise,
    the function fails.
    
    ---------
    
    Co-authored-by: dkostic <dkostic@amazon.com>

--- include/openssl/evp.h
+++ include/openssl/evp.h
@@ -710,12 +710,18 @@ OPENSSL_EXPORT int EVP_PKEY_keygen(EVP_PKEY_CTX *ctx, EVP_PKEY **out_pkey);
 //   3. writes the length of |ciphertext| and |shared_secret| to
 //      |ciphertext_len| and |shared_secret_len|.
 //
-// If the given |ciphertext| is NULL it is assumed that the caller is doing
-// a size check: the function will write the size of the ciphertext and the
-// shared secret in |ciphertext_len| and |shared_secret_len| and return 1.
-// If |ciphertext| is non-NULL it is assumed that the caller is performing
-// the actual operation, so it is checked if the lengths of the output buffers,
-// |ciphertext_len| and |shared_secret_len|, are large enough for the KEM.
+// The function requires that output buffers, |ciphertext| and |shared_secret|,
+// be either both NULL or both non-NULL. Otherwise, a failure is returned.
+//
+// If both |ciphertext| and |shared_secret| are NULL it is assumed that
+// the caller is doing a size check: the function will write the size of
+// the ciphertext and the shared secret in |ciphertext_len| and
+// |shared_secret_len| and return successfully.
+//
+// If both |ciphertext| and |shared_secret| are not NULL it is assumed that
+// the caller is performing the actual operation. The function will check
+// additionally if the lengths of the output buffers, |ciphertext_len| and
+// |shared_secret_len|, are large enough for the KEM.
 //
 // NOTE: no allocation is done in the function, the caller is expected to
 // provide large enough |ciphertext| and |shared_secret| buffers.
@@ -735,10 +741,11 @@ OPENSSL_EXPORT int EVP_PKEY_encapsulate(EVP_PKEY_CTX *ctx          /* IN  */,
 //
 // If the given |shared_secret| is NULL it is assumed that the caller is doing
 // a size check: the function will write the size of the shared secret in
-// |shared_secret_len| and return 1.
+// |shared_secret_len| and return successfully.
+//
 // If |shared_secret| is non-NULL it is assumed that the caller is performing
-// the actual operation, so it is checked if the length of the output buffer,
-// |shared_secret_len|, is large enough for the KEM.
+// the actual operation. The functions will check additionally if the length of
+// the output buffer |shared_secret_len| is large enough for the KEM.
 //
 // NOTE: no allocation is done in the function, the caller is expected to
 // provide large enough |shared_secret| buffer.
