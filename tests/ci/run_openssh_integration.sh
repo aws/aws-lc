a078b914db8e5c4539cfefcacd4099789a6f51b6
Justin Smith <justsmth@amazon.com>
2023-04-11 10:17:02 -0400

    CI script for openssh integration

---
+++ tests/ci/run_openssh_integration.sh
@@ -0,0 +1,86 @@
+#!/bin/bash -exu
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR ISC
+
+source tests/ci/common_posix_setup.sh
+
+# Set up environment.
+
+# ROOT
+#  |
+#  - AWS_LC_DIR
+#    |
+#    - aws-lc
+#  |
+#  - SCRATCH_FOLDER
+#    |
+#    - openssh-portable
+#    - AWS_LC_BUILD_FOLDER
+#    - AWS_LC_INSTALL_FOLDER
+#    - OPENSSH_INSTALL_FOLDER
+
+# Assumes script is executed from the root of aws-lc directory
+AWS_LC_DIR=$(pwd)
+pushd ..
+ROOT=$(pwd)
+popd
+
+SCRATCH_FOLDER="${ROOT}/SCRATCH_AWSLC_OPENSSH_INTERN_TEST"
+AWS_LC_BUILD_FOLDER="${SCRATCH_FOLDER}/aws-lc-build"
+AWS_LC_INSTALL_FOLDER="${SCRATCH_FOLDER}/aws-lc-install"
+OPENSSH_REPO_FOLDER="${SCRATCH_FOLDER}/openssh-portable"
+OPENSSH_INSTALL_FOLDER="${SCRATCH_FOLDER}/openssh-install"
+
+NINJA_COMMAND=ninja
+if ! ${NINJA_COMMAND} --version; then
+  NINJA_COMMAND=ninja-build
+fi
+
+# Make script execution idempotent.
+rm -rf "${SCRATCH_FOLDER}"
+mkdir -p "${SCRATCH_FOLDER}"
+pushd "${SCRATCH_FOLDER}"
+
+# Test helper functions.
+
+function aws_lc_build() {
+	${CMAKE_COMMAND} "${AWS_LC_DIR}" -GNinja "-B${AWS_LC_BUILD_FOLDER}" "-DCMAKE_INSTALL_PREFIX=${AWS_LC_INSTALL_FOLDER}" "$@"
+	${NINJA_COMMAND} -C "${AWS_LC_BUILD_FOLDER}" install
+	ls -R "${AWS_LC_INSTALL_FOLDER}"
+	rm -rf "${AWS_LC_BUILD_FOLDER:?}"/*
+}
+
+function openssh_build() {
+  pushd "${OPENSSH_REPO_FOLDER}"
+  autoreconf
+  export LD_LIBRARY_PATH
+  LD_LIBRARY_PATH=$(readlink -f "${AWS_LC_INSTALL_FOLDER}"/lib*)
+  export CFLAGS="-DAWS_LC_INTERNAL_IGNORE_BN_SET_FLAGS=1 -DHAVE_RSA_METH_FREE=1 -DHAVE_RSA_METH_DUP=1 -DHAVE_RSA_METH_SET1_NAME=1 -DHAVE_RSA_METH_SET_PRIV_ENC=1 -DHAVE_RSA_METH_SET_PRIV_DEC=1"
+	./configure --with-ssl-dir="${AWS_LC_INSTALL_FOLDER}" --prefix="${OPENSSH_INSTALL_FOLDER}" --disable-pkcs11
+	make install
+	ls -R "${OPENSSH_INSTALL_FOLDER}"
+	popd
+}
+
+function openssh_run_tests() {
+  pushd "${OPENSSH_REPO_FOLDER}"
+	export TEST_SSH_UNSAFE_PERMISSIONS=1
+	export SKIP_LTESTS="agent multiplex agent-restrict"
+	make tests
+	popd
+}
+
+mkdir -p "${AWS_LC_BUILD_FOLDER}" "${AWS_LC_INSTALL_FOLDER}" "${OPENSSH_INSTALL_FOLDER}"
+
+# Get latest OpenSSH version.
+git clone https://github.com/openssh/openssh-portable.git
+ls
+
+# Buld AWS-LC as a shared library
+aws_lc_build -DBUILD_SHARED_LIBS=1
+
+# Build openssh and run tests.
+openssh_build
+openssh_run_tests
+
+popd
