9af76570ec30b92f9632a7baf928ac0d49ef62b4
Dengke Tang <815825145@qq.com>
2023-04-17 21:00:37 -0700

    Make the AES-GCM avx512 Perl script compatible with old Perl (#960)
    (cherry picked from commit 44a1c471faf65cbfd57e62b8a5357ca6330c7c56)

--- crypto/fipsmodule/modes/asm/aesni-gcm-avx512.pl
+++ crypto/fipsmodule/modes/asm/aesni-gcm-avx512.pl
@@ -4722,8 +4722,8 @@ ___
 sub evex_byte1 {
   my ($mm, $src1, $dst) = @_;
   # set default to zero
-  $src1 //= 0;
-  $dst //= 0;
+  $src1 = 0 if (!defined($src1));
+  $dst = 0 if (!defined($dst));
 
   my $byte = 0xf0 | $mm;
 
