673574f9e731e803d54b853b80155e75542e2308
Justin Smith <justsmth@amazon.com>
2023-05-15 09:47:55 -0400

    Only build acvp/modulewrapper with BUILD_TESTING

--- tests/ci/run_fips_tests.sh
+++ tests/ci/run_fips_tests.sh
@@ -11,6 +11,8 @@ fips_build_and_test -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=1
 if [[ ("$(uname -s)" == 'Linux'*) && (("$(uname -p)" == 'x86_64'*) || ("$(uname -p)" == 'aarch64'*)) ]]; then
   echo "Testing AWS-LC static library in FIPS Release mode."
   fips_build_and_test -DCMAKE_BUILD_TYPE=Release
+
+  run_build -DFIPS=1 -DBUILD_LIBSSL=OFF -DBUILD_TESTING=OFF
 fi
 
 # The AL2 version of Clang does not have all of the required artifacts for address sanitizer, see P45594051
