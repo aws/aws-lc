f59ceb901fa53a7bfa7bb89fa471f4edd951a7a4
Andrew Hopkins <andhop@amazon.com>
2023-05-15 11:40:20 -0700

    Run FIPS break tests as a part of the CI (#1005)
    * Update FIPS break test values to align with self check executable
    
    * Always print CI information for debuging and run the break-kat test script for FIPS
    
    ---------
    
    Co-authored-by: Torben Hansen <50673096+torben-hansen@users.noreply.github.com>

--- tests/ci/common_posix_setup.sh
+++ tests/ci/common_posix_setup.sh
@@ -91,7 +91,6 @@ function generate_symbols_file {
   # read_symbols.go currently only support static libraries
   if [ ! -f  "$BUILD_ROOT"/crypto/libcrypto.a ]; then
     echo "Static library not found: ${BUILD_ROOT}/crypto/libcrypto.a"
-    print_system_and_dependency_information
     exit 1
   fi
 
@@ -208,26 +207,24 @@ function print_executable_information {
   fi
 }
 
-function print_system_and_dependency_information {
-  print_executable_information "cmake" "--version" "CMake version"
-  print_executable_information "cmake3" "--version" "CMake version (cmake3 executable)"
-  print_executable_information "go" "version" "Go version"
-  print_executable_information "perl" "--version" "Perl version"
-  # Ninja executable names are not uniform over operating systems
-  print_executable_information "ninja-build" "--version" "Ninja version (ninja-build executable)"
-  print_executable_information "ninja" "--version" "Ninja version (ninja executable)"
-  print_executable_information "gcc" "--version" "gcc version"
-  print_executable_information "g++" "--version" "g++ version"
-  print_executable_information "clang" "--version" "clang version"
-  print_executable_information "clang++" "--version" "clang++ version"
-  print_executable_information "cc" "--version" "cc version"
-  print_executable_information "c++" "--version" "c++ version"
-  print_executable_information "make" "--version" "Make version"
-  print_executable_information "rustup" "show" "Rust toolchain"
-  echo ""
-  echo "Operating system information:"
-  uname -a
-  echo ""
-  echo "Environment variables"
-  env
-}
+print_executable_information "cmake" "--version" "CMake version"
+print_executable_information "cmake3" "--version" "CMake version (cmake3 executable)"
+print_executable_information "go" "version" "Go version"
+print_executable_information "perl" "--version" "Perl version"
+# Ninja executable names are not uniform over operating systems
+print_executable_information "ninja-build" "--version" "Ninja version (ninja-build executable)"
+print_executable_information "ninja" "--version" "Ninja version (ninja executable)"
+print_executable_information "gcc" "--version" "gcc version"
+print_executable_information "g++" "--version" "g++ version"
+print_executable_information "clang" "--version" "clang version"
+print_executable_information "clang++" "--version" "clang++ version"
+print_executable_information "cc" "--version" "cc version"
+print_executable_information "c++" "--version" "c++ version"
+print_executable_information "make" "--version" "Make version"
+print_executable_information "rustup" "show" "Rust toolchain"
+echo ""
+echo "Operating system information:"
+uname -a
+echo ""
+echo "Environment variables:"
+env
