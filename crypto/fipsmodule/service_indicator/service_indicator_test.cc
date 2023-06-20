80c394324382c78d2cbd783babaad1f612c1642d
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2023-06-20 12:23:48 -0400

    Bump AWSLC_VERSION_NUMBER_STRING to 1.11.0 (#1056)

--- crypto/fipsmodule/service_indicator/service_indicator_test.cc
+++ crypto/fipsmodule/service_indicator/service_indicator_test.cc
@@ -4036,7 +4036,7 @@ TEST(ServiceIndicatorTest, DRBG) {
 // Since this is running in FIPS mode it should end in FIPS
 // Update this when the AWS-LC version number is modified
 TEST(ServiceIndicatorTest, AWSLCVersionString) {
-  ASSERT_STREQ(awslc_version_string(), "AWS-LC FIPS 1.10.0");
+  ASSERT_STREQ(awslc_version_string(), "AWS-LC FIPS 1.11.0");
 }
 
 #else
@@ -4079,6 +4079,6 @@ TEST(ServiceIndicatorTest, BasicTest) {
 // Since this is not running in FIPS mode it shouldn't end in FIPS
 // Update this when the AWS-LC version number is modified
 TEST(ServiceIndicatorTest, AWSLCVersionString) {
-  ASSERT_STREQ(awslc_version_string(), "AWS-LC 1.10.0");
+  ASSERT_STREQ(awslc_version_string(), "AWS-LC 1.11.0");
 }
 #endif // AWSLC_FIPS
