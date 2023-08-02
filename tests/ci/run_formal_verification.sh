b7b00b817a1654b8cfd95346d4d676cce1c8d0f2
samuel40791765 <sachiang@amazon.com>
2023-08-02 00:06:30 +0000

    fix formal verification CI

--- tests/ci/run_formal_verification.sh
+++ tests/ci/run_formal_verification.sh
@@ -7,8 +7,13 @@ cd ../
 ROOT=$(pwd)
 
 rm -rf aws-lc-verification-build
-git clone --recurse-submodules https://github.com/awslabs/aws-lc-verification.git aws-lc-verification-build
+git clone https://github.com/awslabs/aws-lc-verification.git aws-lc-verification-build
+
 cd aws-lc-verification-build
+# We avoid pulling the saw-script submodule since the repo will take forever to clone.
+git submodule update --init
+pushd Coq/fiat-crypto; git submodule update --init --recursive; popd
+
 # aws-lc-verification has aws-lc as one submodule under 'src' dir.
 # Below is to copy code of **target** aws-lc to 'src' dir.
 rm -rf ./src/* && cp -r "${ROOT}/${AWS_LC_DIR}/"* ./src
