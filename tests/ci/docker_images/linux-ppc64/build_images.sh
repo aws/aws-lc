44899554328bb5b8ccbefef0a25bf9b890c4f172
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2023-08-25 12:28:17 -0400

    ppcd64 use unstable not sid; comment out cpu-specific

--- tests/ci/docker_images/linux-ppc64/build_images.sh
+++ tests/ci/docker_images/linux-ppc64/build_images.sh
@@ -23,5 +23,5 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker context use default
 docker buildx build -t debian-ppc64:base "${SCRIPT_DIR}"/debian-base --load
 docker buildx build -t debian-ppc64:build-tools "${SCRIPT_DIR}"/debian-build-tools --load
-docker buildx build -t debian-ppc64:e6500 "${SCRIPT_DIR}"/debian-e6500 --load
-docker buildx build -t debian-ppc64:POWER8 "${SCRIPT_DIR}"/debian-POWER8 --load
+#docker buildx build -t debian-ppc64:e6500 "${SCRIPT_DIR}"/debian-e6500 --load
+#docker buildx build -t debian-ppc64:POWER8 "${SCRIPT_DIR}"/debian-POWER8 --load
