f7146a2b922b2bfe97cc32e6510234f8c3800763
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2023-09-25 17:34:52 -0400

    Include qemu-user with x-tools

--- tests/docker_images/linux-ppc64/ubuntu-x-tools/ppc64.cmake
+++ tests/docker_images/linux-ppc64/ubuntu-x-tools/ppc64.cmake
@@ -3,9 +3,9 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ppc64)
 
 # Specify the cross-compiler
-set(CMAKE_C_COMPILER /powerpc64-linux-gnu/bin/powerpc64-unknown-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER /powerpc64-linux-gnu/bin/powerpc64-unknown-linux-gnu-g++)
+set(CMAKE_C_COMPILER /powerpc64-unknown-linux-gnu/bin/powerpc64-unknown-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /powerpc64-unknown-linux-gnu/bin/powerpc64-unknown-linux-gnu-g++)
 
 # Specify the sysroot for the target system
-set(CMAKE_SYSROOT /powerpc64-linux-gnu/powerpc64-unknown-linux-gnu/sysroot)
+set(CMAKE_SYSROOT /powerpc64-unknown-linux-gnu/powerpc64-unknown-linux-gnu/sysroot/)
 set(CMAKE_GENERATOR Ninja)
