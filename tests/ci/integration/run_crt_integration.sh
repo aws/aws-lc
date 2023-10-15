3e7c79bfee120fc736f447706fda792a120e366a
Andrew Hopkins <andhop@amazon.com>
2023-10-15 15:32:21 -0700

    PR feedback: use env to find bash, change docker image to amazonlinux 2023 to get a more modern CMake.

--- tests/ci/integration/run_crt_integration.sh
+++ tests/ci/integration/run_crt_integration.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -exu
+#!/usr/bin/env bash
+set -exu
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0 OR ISC
 
 source tests/ci/common_posix_setup.sh
 
 # Assumes script is executed from the root of aws-lc directory
-SCRATCH_FOLDER=${SYS_ROOT}/"SCRATCH_AWSLC_CRT_TEST"
+SCRATCH_FOLDER=${SYS_ROOT}/SCRATCH_AWSLC_CRT_TEST
 
 # Make script execution idempotent.
 mkdir -p ${SCRATCH_FOLDER}
@@ -21,6 +22,6 @@ cp -r ${SRC_ROOT}/* crt/aws-lc/
 
 # Don't pre-build AWS-LC, the CRT will build all of it's dependencies how it wants them built
 mkdir build && cd build
-cmake -GNinja ../
+${CMAKE_COMMAND} -GNinja ../
 ninja
 ninja test
