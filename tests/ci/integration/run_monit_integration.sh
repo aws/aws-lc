6e89eeb322bf733704141ad0ccae20e23cdf7163
samuel40791765 <sachiang@amazon.com>
2023-11-06 23:32:51 +0000

    turn off building tests to save time

--- tests/ci/integration/run_monit_integration.sh
+++ tests/ci/integration/run_monit_integration.sh
@@ -45,7 +45,7 @@ git clone https://bitbucket.org/tildeslash/monit.git ${MONIT_SRC_FOLDER} --depth
 mkdir -p ${AWS_LC_BUILD_FOLDER} ${AWS_LC_INSTALL_FOLDER} ${MONIT_BUILD_FOLDER}
 ls
 
-aws_lc_build ${SRC_ROOT} ${AWS_LC_BUILD_FOLDER} ${AWS_LC_INSTALL_FOLDER}
+aws_lc_build ${SRC_ROOT} ${AWS_LC_BUILD_FOLDER} ${AWS_LC_INSTALL_FOLDER} -DBUILD_TESTING=OFF
 
 # Build monit from source.
 pushd ${MONIT_SRC_FOLDER}
