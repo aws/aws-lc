91a2d81b6bb4f21fbb8f809d9613ccbba597d751
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2023-11-16 18:17:26 -0500

    Improve Codecov reporting

--- util/codecov-ci.sh
+++ util/codecov-ci.sh
@@ -7,6 +7,7 @@
 set -xe
 
 SRC=$(pwd)
+SRC=$(readlink -f "$SRC")
 
 # Sanity check
 DIRNAME=$(basename -- "${SRC}")
@@ -18,25 +19,48 @@ fi
 BUILD="$1"
 if [ -n "$BUILD" ]; then
   if [ ! -e "$BUILD" ]; then
-    mkdir -p "$BUILD"
+    mkdir -p "$BUILD/default"
+    mkdir -p "$BUILD/no_asm"
+    mkdir -p "$BUILD/html"
   fi
   BUILD=$(readlink -f "$BUILD")
+  BUILD_DEFAULT=$(readlink -f "$BUILD/default")
+  BUILD_NO_ASM=$(readlink -f "$BUILD/no_asm")
+  BUILD_HTML=$(readlink -f "$BUILD/html")
 else
   echo "Must specify a build directory."
   exit 1
 fi
 
-cmake -DGCOV=1 -DDISABLE_PERL=1 -DBUILD_TESTING=1 -DBUILD_LIBSSL=1 -DCMAKE_BUILD_TYPE=Debug -S "${SRC}" -B "${BUILD}"
-cmake --build "${BUILD}" --target all_tests
-cmake --build "${BUILD}" --target run_tests
+# Default x86-64 build/test
+cmake -DGCOV=1 -DDISABLE_PERL=1 -DBUILD_TESTING=1 -DBUILD_LIBSSL=1 -DCMAKE_BUILD_TYPE=Debug -S "${SRC}" -B "${BUILD_DEFAULT}"
+cmake --build "${BUILD_DEFAULT}" --target all_tests
+cmake --build "${BUILD_DEFAULT}" --target run_tests
 
-#TODO: Use callgrind
-#mkdir "$BUILD/callgrind/"
-#go run "$SRC/util/all_tests.go" -build-dir "$BUILD" -callgrind -num-workers 16
+# No Assembly x86-64 build/test
+cmake -DGCOV=1 -DOPENSSL_NO_ASM=1 -DDISABLE_PERL=1 -DBUILD_TESTING=1 -DBUILD_LIBSSL=1 -DCMAKE_BUILD_TYPE=Debug -S "${SRC}" -B "${BUILD_NO_ASM}"
+cmake --build "${BUILD_NO_ASM}" --target all_tests
+cmake --build "${BUILD_NO_ASM}" --target run_tests
 
-shopt -s globstar
+LCOV_EXCLUDE_PARAMS=()
+LCOV_EXCLUDE_PARAMS+=(--exclude '*/third_party/*')
+LCOV_EXCLUDE_PARAMS+=(--exclude '*/tool/*')
+LCOV_EXCLUDE_PARAMS+=(--exclude '*_test.*')
+LCOV_EXCLUDE_PARAMS+=(--exclude '*/test_*')
+LCOV_EXCLUDE_PARAMS+=(--exclude '*_test_*')
+LCOV_EXCLUDE_PARAMS+=(--exclude '*/gtest_*')
+LCOV_EXCLUDE_PARAMS+=(--exclude '*/wycheproof_')
 
-pushd "${BUILD}"
-gcov --source-prefix "${SRC}" **/*.gcda
-lcov --capture --exclude "/Applications/*" --exclude "/usr/*" --exclude "/lib/*" --directory . --output-file coverage.info
-popd
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  LCOV_EXCLUDE_PARAMS+=(--exclude '/Applications/*')
+  lcov --capture "${LCOV_EXCLUDE_PARAMS[@]}" --ignore-errors inconsistent,inconsistent,gcov,gcov --directory "${BUILD_DEFAULT}" --output-file "${BUILD}/coverage-default.info"
+  lcov --capture "${LCOV_EXCLUDE_PARAMS[@]}" --ignore-errors inconsistent,inconsistent,gcov,gcov --directory "${BUILD_NO_ASM}" --output-file "${BUILD}/coverage-no-asm.info"
+
+  #genhtml --ignore-errors inconsistent,inconsistent,unmapped --output-directory "${BUILD_HTML}" "${BUILD}"/coverage-*.info
+  #open "${BUILD_HTML}"/index.html
+else
+  LCOV_EXCLUDE_PARAMS+=(--exclude '/usr/*')
+  LCOV_EXCLUDE_PARAMS+=(--exclude '/lib/*')
+  lcov --capture "${LCOV_EXCLUDE_PARAMS[@]}" --directory "${BUILD_DEFAULT}" --output-file "${BUILD}/coverage-default.info"
+  lcov --capture "${LCOV_EXCLUDE_PARAMS[@]}" --directory "${BUILD_NO_ASM}" --output-file "${BUILD}/coverage-no-asm.info"
+fi
