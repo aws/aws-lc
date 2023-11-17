0f658de3a6157be6844aeaab4f892ec19f55f81e
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2023-11-17 13:31:23 -0500

    Refactor; Improve data accuracy

--- util/codecov-ci.sh
+++ util/codecov-ci.sh
@@ -18,11 +18,9 @@ fi
 
 BUILD="$1"
 if [ -n "$BUILD" ]; then
-  if [ ! -e "$BUILD" ]; then
-    mkdir -p "$BUILD/default"
-    mkdir -p "$BUILD/no_asm"
-    mkdir -p "$BUILD/html"
-  fi
+  mkdir -p "$BUILD/default"
+  mkdir -p "$BUILD/no_asm"
+  mkdir -p "$BUILD/html"
   BUILD=$(readlink -f "$BUILD")
   BUILD_DEFAULT=$(readlink -f "$BUILD/default")
   BUILD_NO_ASM=$(readlink -f "$BUILD/no_asm")
@@ -32,37 +30,43 @@ else
   exit 1
 fi
 
+LCOV_PARAMS=()
+LCOV_PARAMS+=(--exclude '*/third_party/*')
+LCOV_PARAMS+=(--exclude '*/tool/*')
+LCOV_PARAMS+=(--exclude '*_test.*')
+LCOV_PARAMS+=(--exclude '*/test_*')
+LCOV_PARAMS+=(--exclude '*_test_*')
+LCOV_PARAMS+=(--exclude '*/gtest_*')
+LCOV_PARAMS+=(--exclude '*/wycheproof_*')
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  LCOV_PARAMS+=(--exclude '/Applications/*')
+  LCOV_IGNORE_ERRORS="inconsistent,inconsistent,gcov,gcov"
+  GENHTML_IGNORE_ERRORS="inconsistent,unmapped"
+else
+  LCOV_PARAMS+=(--exclude '/usr/*')
+  LCOV_PARAMS+=(--exclude '/lib/*')
+  LCOV_IGNORE_ERRORS="negative,mismatch,unused"
+  GENHTML_IGNORE_ERRORS="inconsistent,unmapped"
+fi
+LCOV_PARAMS+=(--ignore-errors ${LCOV_IGNORE_ERRORS})
+
 # Default x86-64 build/test
 cmake -DGCOV=1 -DDISABLE_PERL=1 -DBUILD_TESTING=1 -DBUILD_LIBSSL=1 -DCMAKE_BUILD_TYPE=Debug -S "${SRC}" -B "${BUILD_DEFAULT}"
 cmake --build "${BUILD_DEFAULT}" --target all_tests
+lcov --capture "${LCOV_PARAMS[@]}" --initial --directory "${BUILD_DEFAULT}" --output-file "${BUILD}/initial-default.info"
 cmake --build "${BUILD_DEFAULT}" --target run_tests
 
 # No Assembly x86-64 build/test
 cmake -DGCOV=1 -DOPENSSL_NO_ASM=1 -DDISABLE_PERL=1 -DBUILD_TESTING=1 -DBUILD_LIBSSL=1 -DCMAKE_BUILD_TYPE=Debug -S "${SRC}" -B "${BUILD_NO_ASM}"
 cmake --build "${BUILD_NO_ASM}" --target all_tests
+lcov --capture "${LCOV_PARAMS[@]}" --initial --directory "${BUILD_NO_ASM}" --output-file "${BUILD}/initial-no-asm.info"
 cmake --build "${BUILD_NO_ASM}" --target run_tests
 
-LCOV_EXCLUDE_PARAMS=()
-LCOV_EXCLUDE_PARAMS+=(--exclude '*/third_party/*')
-LCOV_EXCLUDE_PARAMS+=(--exclude '*/tool/*')
-LCOV_EXCLUDE_PARAMS+=(--exclude '*_test.*')
-LCOV_EXCLUDE_PARAMS+=(--exclude '*/test_*')
-LCOV_EXCLUDE_PARAMS+=(--exclude '*_test_*')
-LCOV_EXCLUDE_PARAMS+=(--exclude '*/gtest_*')
-LCOV_EXCLUDE_PARAMS+=(--exclude '*/wycheproof_')
+lcov --capture "${LCOV_PARAMS[@]}" --directory "${BUILD_DEFAULT}" --output-file "${BUILD}/test-default.info"
+lcov "${LCOV_PARAMS[@]}" --add-tracefile "${BUILD}/initial-default.info" --add-tracefile "${BUILD}/test-default.info" --output-file "${BUILD}/coverage-default.info"
+lcov --capture "${LCOV_PARAMS[@]}" --directory "${BUILD_NO_ASM}" --output-file "${BUILD}/test-no-asm.info"
+lcov "${LCOV_PARAMS[@]}" --add-tracefile "${BUILD}/initial-no-asm.info" --add-tracefile "${BUILD}/test-no-asm.info" --output-file "${BUILD}/coverage-no-asm.info"
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  LCOV_EXCLUDE_PARAMS+=(--exclude '/Applications/*')
-  lcov --capture "${LCOV_EXCLUDE_PARAMS[@]}" --ignore-errors inconsistent,inconsistent,gcov,gcov --directory "${BUILD_DEFAULT}" --output-file "${BUILD}/coverage-default.info"
-  lcov --capture "${LCOV_EXCLUDE_PARAMS[@]}" --ignore-errors inconsistent,inconsistent,gcov,gcov --directory "${BUILD_NO_ASM}" --output-file "${BUILD}/coverage-no-asm.info"
+#genhtml --ignore-errors ${GENHTML_IGNORE_ERRORS} --output-directory "${BUILD_HTML}" "${BUILD}"/coverage-*.info
+#open "${BUILD_HTML}"/index.html
 
-  #genhtml --ignore-errors inconsistent,inconsistent,unmapped --output-directory "${BUILD_HTML}" "${BUILD}"/coverage-*.info
-  #open "${BUILD_HTML}"/index.html
-else
-  LCOV_EXCLUDE_PARAMS+=(--exclude '/usr/*')
-  LCOV_EXCLUDE_PARAMS+=(--exclude '/lib/*')
-  lcov --capture "${LCOV_EXCLUDE_PARAMS[@]}" --ignore-errors negative,mismatch,unused --directory "${BUILD_DEFAULT}" --output-file "${BUILD}/coverage-default.info"
-  lcov --capture "${LCOV_EXCLUDE_PARAMS[@]}" --ignore-errors negative,mismatch,unused --directory "${BUILD_NO_ASM}" --output-file "${BUILD}/coverage-no-asm.info"
-
-  #genhtml --output-directory "${BUILD_HTML}" "${BUILD}"/coverage-*.info
-fi
