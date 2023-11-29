bd6d86b2baab98bae56052a5df84ec84042dbb73
Andrew Hopkins <andhop@amazon.com>
2023-11-29 14:20:07 -0800

    PR feedback: ensure grpc is linking with AWS-LC, fix variable quoting, and update all submodules

--- tests/ci/integration/run_grpc_integration.sh
+++ tests/ci/integration/run_grpc_integration.sh
@@ -16,7 +16,7 @@ source tests/ci/common_posix_setup.sh
 #    - GRPC_BUILD_FOLDER
 
 # Assumes script is executed from the root of aws-lc directory
-SCRATCH_FOLDER=${SYS_ROOT}/"GRPC_BUILD_ROOT"
+SCRATCH_FOLDER="${SYS_ROOT}/GRPC_BUILD_ROOT"
 GRPC_SRC_FOLDER="${SCRATCH_FOLDER}/grpc"
 GRPC_BUILD_FOLDER="${SCRATCH_FOLDER}/grpc/cmake/build"
 AWS_LC_BUILD_FOLDER="${SCRATCH_FOLDER}/aws-lc-build"
@@ -29,14 +29,18 @@ mkdir -p ${AWS_LC_BUILD_FOLDER} ${AWS_LC_INSTALL_FOLDER}
 
 git clone --depth 1 https://github.com/grpc/grpc.git ${GRPC_SRC_FOLDER}
 cd ${GRPC_SRC_FOLDER}
-git submodule update --init
+git submodule update --recursive --init
 
 aws_lc_build ${SRC_ROOT} ${AWS_LC_BUILD_FOLDER} ${AWS_LC_INSTALL_FOLDER} -DBUILD_SHARED_LIBS=1
 
-# gRPC has a ton of tests focus on just the tests relevant to AWS-LC --regex '.*(tls|ssl|cert).*'
-# https://github.com/grpc/grpc/tree/master/tools/run_tests#overview
+# gRPC has a ton of tests that take over an hour to run, instead focus on just the tests relevant to AWS-LC
+# --regex '.*(tls|ssl|cert).*' https://github.com/grpc/grpc/tree/master/tools/run_tests#overview
+#
 # Why the spaces in the cmake parameters? It's due to a bug in argparse not being able to decide what to do with:
-# -foo '-bar'
-# Is -bar is a value for the -foo argument or the name of a new argument bar? https://bugs.python.org/issue9334
+# `-foo '-bar'`. Is '-bar' a value for the -foo argument or the name of a new argument bar?
+# https://bugs.python.org/issue9334
 python3 tools/run_tests/run_tests.py -l c++ --regex '.*(tls|ssl|cert).*' --cmake_configure_extra_args \
   " -DgRPC_SSL_PROVIDER=package" " -DOPENSSL_ROOT_DIR=${AWS_LC_INSTALL_FOLDER}"
+
+ldd "${GRPC_SRC_FOLDER}/cmake/build/libgrpc.so" | grep "${AWS_LC_INSTALL_FOLDER}/lib/libcrypto.so" || exit 1
+ldd "${GRPC_SRC_FOLDER}/cmake/build/libgrpc.so" | grep "${AWS_LC_INSTALL_FOLDER}/lib/libssl.so" || exit 1
