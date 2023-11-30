c705ffe396e5475604131bb1a47bf48ebe9a8712
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2023-11-30 13:55:28 -0500

    Simplify

--- tests/ci/run_cross_ppc64_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0 OR ISC
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-SCRIPT_DIR="$(readlink -f "${SCRIPT_DIR}")"
-
-"${SCRIPT_DIR}/run_cross_tests.sh" ppc64 powerpc64-unknown-linux-gnu "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_BUILD_TYPE=Release -DFIPS=1 -DBUILD_SHARED_LIBS=1"
