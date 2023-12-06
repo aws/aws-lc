1aeb719d7528424cbd0aa7693f2490a1498fa3ed
Yan Peng <112029182+pennyannn@users.noreply.github.com>
2023-12-06 15:25:00 -0800

    Dockerfile and structure change for SAW proofs on X86_64 (#1345)
    This commit https://github.com/awslabs/aws-lc-verification/commit/a7affccf48eae6457094de4e56f6162208566e8e in AWS-LC-verification changes the Dockerfile name for SAW proofs on X86_64 and changes where the shell scripts are for running the proofs.

--- tests/ci/run_formal_verification.sh
+++ tests/ci/run_formal_verification.sh
@@ -10,14 +10,12 @@ rm -rf aws-lc-verification-build
 git clone https://github.com/awslabs/aws-lc-verification.git aws-lc-verification-build
 
 cd aws-lc-verification-build
-# We avoid pulling the saw-script submodule since the repo will take forever to clone.
 git submodule update --init
-pushd Coq/fiat-crypto; git submodule update --init --recursive; popd
 
 # aws-lc-verification has aws-lc as one submodule under 'src' dir.
 # Below is to copy code of **target** aws-lc to 'src' dir.
 rm -rf ./src/* && cp -r "${ROOT}/${AWS_LC_DIR}/"* ./src
 # execute the entry to saw scripts.
-./SAW/scripts/docker_entrypoint.sh
+./SAW/scripts/x86_64/docker_entrypoint.sh
 cd ..
 rm -rf aws-lc-verification-build
