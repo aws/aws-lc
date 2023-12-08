462d1ce1c5bcf9b6f40813ab46b449bde0dc1bf9
Yan Peng <yppe@amazon.com>
2023-12-08 01:43:01 +0000

    Refactor to reduce duplicated code

---
+++ tests/ci/run_formal_verification.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR ISC
+
+ENTRYDIR=$1
+AWS_LC_DIR=${PWD##*/}
+cd ../
+ROOT=$(pwd)
+
+rm -rf aws-lc-verification-build
+git clone https://github.com/awslabs/aws-lc-verification.git aws-lc-verification-build
+
+cd aws-lc-verification-build
+git submodule update --init
+
+# aws-lc-verification has aws-lc as one submodule under 'src' dir.
+# Below is to copy code of **target** aws-lc to 'src' dir.
+rm -rf ./src/* && cp -r "${ROOT}/${AWS_LC_DIR}/"* ./src
+# execute the entry to saw scripts.
+./$ENTRYDIR/docker_entrypoint.sh
+cd ..
+rm -rf aws-lc-verification-build
