e8d7ca8be56c8972a09aaed990920b2a2b71202b
Yan Peng <yppe@amazon.com>
2023-12-08 23:45:30 +0000

    Change file naming from nsym to nsym-aarch64 to reflect its target architecture

--- tests/ci/docker_images/linux-x86/push_images.sh
+++ tests/ci/docker_images/linux-x86/push_images.sh
@@ -12,7 +12,7 @@ fi
 
 echo "Uploading docker images to ${ECS_REPO}."
 
-$(aws ecr get-login --no-include-email)
+$(aws ecr get-login --no-include-email --region us-west-2)
 
 # Tag images with date to help find old images, CodeBuild uses the latest tag and gets updated automatically
 tag_and_push_img 'ubuntu-16.04:gcc-5x' "${ECS_REPO}:ubuntu-16.04_gcc-5x"
@@ -31,7 +31,7 @@ tag_and_push_img 'ubuntu-20.04:gcc-8x' "${ECS_REPO}:ubuntu-20.04_gcc-8x"
 tag_and_push_img 'ubuntu-22.04:clang-14x-sde' "${ECS_REPO}:ubuntu-22.04_clang-14x-sde"
 tag_and_push_img 'ubuntu-22.04:gcc-11x' "${ECS_REPO}:ubuntu-22.04_gcc-11x"
 tag_and_push_img 'ubuntu-22.04:gcc-12x' "${ECS_REPO}:ubuntu-22.04_gcc-12x"
-tag_and_push_img 'ubuntu-22.04:clang-14x_formal-verification-nsym' "${ECS_REPO}:ubuntu-22.04_clang-14x_formal-verification-nsym"
+tag_and_push_img 'ubuntu-22.04:clang-14x_formal-verification-nsym-aarch64' "${ECS_REPO}:ubuntu-22.04_clang-14x_formal-verification-nsym-aarch64"
 tag_and_push_img 'centos-7:gcc-4x' "${ECS_REPO}:centos-7_gcc-4x"
 tag_and_push_img 'centos-8:gcc-8x' "${ECS_REPO}:centos-8_gcc-8x"
 tag_and_push_img 'amazonlinux-2:gcc-7x' "${ECS_REPO}:amazonlinux-2_gcc-7x"
