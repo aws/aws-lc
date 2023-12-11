f9f742895d01150e1384d3a4521ccb9b920a7fb3
Yan Peng <112029182+pennyannn@users.noreply.github.com>
2023-12-11 13:14:31 -0800

    Enable Arm SHA384 proofs in AWS-LC CI (#1348)
    This commit enables NSym and SAW proofs for SHA384 on Arm machines (Graviton2 and Graviton3). Specifically,
    
    1. Remove legacy selectchecks for SHA2 and AES-GCM
    2. Rename previous run called `ubuntu2004_clang10x_formal_verification_quickcheck` to `ubuntu2004_clang10x_formal_verification_saw_x86_64` and a bunch of other renaming
    3. Built and uploaded two new docker images: `ubuntu-22.04_clang-14x_formal-verification-nsym-aarch64` and `ubuntu-20.04_clang-10x_formal-verification-saw-aarch64`
    4. Update relative files to add two new builds: `ubuntu2004_clang10x_formal_verification_saw_aarch64` and `ubuntu2204_clang14x_formal_verification_nsym_aarch64` in linux-x86 runs

--- tests/ci/docker_images/linux-x86/push_images.sh
+++ tests/ci/docker_images/linux-x86/push_images.sh
@@ -12,7 +12,7 @@ fi
 
 echo "Uploading docker images to ${ECS_REPO}."
 
-$(aws ecr get-login --no-include-email)
+$(aws ecr get-login --no-include-email --region us-west-2)
 
 # Tag images with date to help find old images, CodeBuild uses the latest tag and gets updated automatically
 tag_and_push_img 'ubuntu-16.04:gcc-5x' "${ECS_REPO}:ubuntu-16.04_gcc-5x"
@@ -24,12 +24,14 @@ tag_and_push_img 'ubuntu-20.04:clang-9x' "${ECS_REPO}:ubuntu-20.04_clang-9x"
 tag_and_push_img 'ubuntu-20.04:clang-10x' "${ECS_REPO}:ubuntu-20.04_clang-10x"
 tag_and_push_img 'ubuntu-20.04:android' "${ECS_REPO}:ubuntu-20.04_android"
 tag_and_push_img 'ubuntu-20.04:clang-7x-bm-framework' "${ECS_REPO}:ubuntu-20.04_clang-7x-bm-framework"
-tag_and_push_img 'ubuntu-20.04:clang-10x_formal-verification' "${ECS_REPO}:ubuntu-20.04_clang-10x_formal-verification"
+tag_and_push_img 'ubuntu-20.04:clang-10x_formal-verification-saw-x86_64' "${ECS_REPO}:ubuntu-20.04_clang-10x_formal-verification-saw-x86_64"
+tag_and_push_img 'ubuntu-20.04:clang-10x_formal-verification-saw-aarch64' "${ECS_REPO}:ubuntu-20.04_clang-10x_formal-verification-saw-aarch64"
 tag_and_push_img 'ubuntu-20.04:gcc-7x' "${ECS_REPO}:ubuntu-20.04_gcc-7x"
 tag_and_push_img 'ubuntu-20.04:gcc-8x' "${ECS_REPO}:ubuntu-20.04_gcc-8x"
 tag_and_push_img 'ubuntu-22.04:clang-14x-sde' "${ECS_REPO}:ubuntu-22.04_clang-14x-sde"
 tag_and_push_img 'ubuntu-22.04:gcc-11x' "${ECS_REPO}:ubuntu-22.04_gcc-11x"
 tag_and_push_img 'ubuntu-22.04:gcc-12x' "${ECS_REPO}:ubuntu-22.04_gcc-12x"
+tag_and_push_img 'ubuntu-22.04:clang-14x_formal-verification-nsym-aarch64' "${ECS_REPO}:ubuntu-22.04_clang-14x_formal-verification-nsym-aarch64"
 tag_and_push_img 'centos-7:gcc-4x' "${ECS_REPO}:centos-7_gcc-4x"
 tag_and_push_img 'centos-8:gcc-8x' "${ECS_REPO}:centos-8_gcc-8x"
 tag_and_push_img 'amazonlinux-2:gcc-7x' "${ECS_REPO}:amazonlinux-2_gcc-7x"
