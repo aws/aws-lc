d1138df663a9c43b08affa0ff239bed32086e735
Gerardo Ravago 🇵🇭 <gcr@amazon.com>
2024-01-05 15:25:13 -0500

    Add field-based accessors to SHA_CTX for compatibility
    Upstream OpenSSL defines the h_n values as individual fields rather than
    an array which BoringSSL introduced.

--- include/openssl/sha.h
+++ include/openssl/sha.h
@@ -97,7 +97,23 @@ OPENSSL_EXPORT void SHA1_Transform(SHA_CTX *sha,
                                    const uint8_t block[SHA_CBLOCK]);
 
 struct sha_state_st {
+#if defined(__cplusplus) || defined(OPENSSL_WINDOWS)
   uint32_t h[5];
+#else
+  // wpa_supplicant accesses |h0|..|h4| so we must support those names for
+  // compatibility with it until it can be updated. Anonymous unions are only
+  // standard in C11, so disable this workaround in C++.
+  union {
+    uint32_t h[5];
+    struct {
+      uint32_t h0;
+      uint32_t h1;
+      uint32_t h2;
+      uint32_t h3;
+      uint32_t h4;
+    };
+  };
+#endif
   uint32_t Nl, Nh;
   uint8_t data[SHA_CBLOCK];
   unsigned num;
