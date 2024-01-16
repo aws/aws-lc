dc1cc972c713d135a95bcbc5b4be8eb0f02d8e85
David Benjamin <davidben@google.com>
2024-01-15 19:28:39 -0500

    Clear some more false positives from constant-time validation
    Mostly bits of DSA and RSA keygen, flagged when we make the PRNG output
    secret by default. There's still a ton of RSA to resolve, mostly because
    our constant-time bignum strategy does not interact well with valgrind
    when handling RSA's secret-value / public-bit-length situation. Also
    RSA's ASN.1 serialization is unavoidably leaky.
    
    Bug: 676
    Change-Id: I08d273959065c4db6fd44180a6ac56a82f862fe8
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/65447
    Auto-Submit: David Benjamin <davidben@google.com>
    Reviewed-by: Bob Beck <bbe@google.com>
    Commit-Queue: David Benjamin <davidben@google.com>
    (cherry picked from commit fce5cf02378a839174935b83b58f54aba6c2bb3e)

--- crypto/dsa/dsa.c
+++ crypto/dsa/dsa.c
@@ -304,6 +304,8 @@ int dsa_internal_paramgen(DSA *dsa, size_t bits, const EVP_MD *evpmd,
         if (!RAND_bytes(seed, qsize)) {
           goto err;
         }
+        // DSA parameters are public.
+        CONSTTIME_DECLASSIFY(seed, qsize);
       } else {
         // If we come back through, use random seed next time.
         seed_in = NULL;
@@ -544,6 +546,9 @@ int DSA_generate_key(DSA *dsa) {
     goto err;
   }
 
+  // The public key is computed from the private key, but is public.
+  bn_declassify(pub_key);
+
   dsa->priv_key = priv_key;
   dsa->pub_key = pub_key;
   ok = 1;
@@ -679,6 +684,10 @@ redo:
       !mod_mul_consttime(s, s, kinv, dsa->method_mont_q, ctx)) {
     goto err;
   }
+
+  // The signature is computed from the private key, but is public.
+  bn_declassify(r);
+  bn_declassify(s);
 
   // Redo if r or s is zero as required by FIPS 186-3: this is
   // very unlikely.
@@ -931,15 +940,19 @@ static int dsa_sign_setup(const DSA *dsa, BN_CTX *ctx, BIGNUM **out_kinv,
                               ctx) ||
       // Compute r = (g^k mod p) mod q
       !BN_mod_exp_mont_consttime(r, dsa->g, &k, dsa->p, ctx,
-                                 dsa->method_mont_p) ||
-      // Note |BN_mod| below is not constant-time and may leak information about
-      // |r|. |dsa->p| may be significantly larger than |dsa->q|, so this is not
-      // easily performed in constant-time with Montgomery reduction.
-      //
-      // However, |r| at this point is g^k (mod p). It is almost the value of
-      // |r| revealed in the signature anyway (g^k (mod p) (mod q)), going from
-      // it to |k| would require computing a discrete log.
-      !BN_mod(r, r, dsa->q, ctx) ||
+                                 dsa->method_mont_p)) {
+    OPENSSL_PUT_ERROR(DSA, ERR_R_BN_LIB);
+    goto err;
+  }
+  // Note |BN_mod| below is not constant-time and may leak information about
+  // |r|. |dsa->p| may be significantly larger than |dsa->q|, so this is not
+  // easily performed in constant-time with Montgomery reduction.
+  //
+  // However, |r| at this point is g^k (mod p). It is almost the value of |r|
+  // revealed in the signature anyway (g^k (mod p) (mod q)), going from it to
+  // |k| would require computing a discrete log.
+  bn_declassify(r);
+  if (!BN_mod(r, r, dsa->q, ctx) ||
       // Compute part of 's = inv(k) (m + xr) mod q' using Fermat's Little
       // Theorem.
       !bn_mod_inverse_prime(kinv, &k, dsa->q, ctx, dsa->method_mont_q)) {
