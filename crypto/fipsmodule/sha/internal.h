441380edb628902417fe32878973f2aa0792e028
David Benjamin <davidben@google.com>
2024-01-27 18:03:01 -0500

    Move capability checks in sha1-586.pl to C
    sha256-586.pl and sha512-586.pl have their own unique challenges, so
    I'll do them separately.
    
    Bug: 673
    Change-Id: Ic9be0454fddf75e7f49bcccd8a86a4ff8862ff67
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/65872
    Commit-Queue: David Benjamin <davidben@google.com>
    Reviewed-by: Bob Beck <bbe@google.com>
    (cherry picked from commit 11688849bf1c9d4d0d1ad8aa7cd860275284317b)

--- crypto/fipsmodule/sha/internal.h
+++ crypto/fipsmodule/sha/internal.h
@@ -85,6 +85,7 @@ struct keccak_st {
   uint8_t pad;                                     // padding character
   uint8_t padded;                                  // denotes if padding has been performed
 };
+
 // Define SHA{n}[_{variant}]_ASM if sha{n}_block_data_order[_{variant}] is
 // defined in assembly.
 
@@ -92,18 +93,6 @@ struct keccak_st {
 #define SHA1_ALTIVEC
 
 void sha1_block_data_order(uint32_t *state, const uint8_t *data,
-                             size_t num_blocks);
-
-#elif !defined(OPENSSL_NO_ASM) && defined(OPENSSL_X86)
-#define SHA1_ASM
-#define SHA256_ASM
-#define SHA512_ASM
-
-void sha1_block_data_order(uint32_t state[5], const uint8_t *data,
-                           size_t num_blocks);
-void sha256_block_data_order(uint32_t state[8], const uint8_t *data,
-                             size_t num_blocks);
-void sha512_block_data_order(uint64_t state[8], const uint8_t *data,
                              size_t num_blocks);
 
 #elif !defined(OPENSSL_NO_ASM) && defined(OPENSSL_ARM)
@@ -155,6 +144,41 @@ OPENSSL_INLINE int sha256_hw_capable(void) {
 OPENSSL_INLINE int sha512_hw_capable(void) {
   return CRYPTO_is_ARMv8_SHA512_capable();
 }
+
+#elif !defined(OPENSSL_NO_ASM) && defined(OPENSSL_X86)
+
+#define SHA1_ASM_NOHW
+
+#define SHA1_ASM_SSSE3
+OPENSSL_INLINE int sha1_ssse3_capable(void) {
+  // TODO(davidben): Do we need to check the FXSR bit? The Intel manual does not
+  // say to.
+  return CRYPTO_is_SSSE3_capable() && CRYPTO_is_FXSR_capable();
+}
+void sha1_block_data_order_ssse3(uint32_t state[5], const uint8_t *data,
+                                 size_t num);
+
+#define SHA1_ASM_AVX
+OPENSSL_INLINE int sha1_avx_capable(void) {
+  // Pre-Zen AMD CPUs had slow SHLD/SHRD; Zen added the SHA extension; see the
+  // discussion in sha1-586.pl.
+  //
+  // TODO(davidben): Should we enable SHAEXT on 32-bit x86?
+  // TODO(davidben): Do we need to check the FXSR bit? The Intel manual does not
+  // say to.
+  return CRYPTO_is_AVX_capable() && CRYPTO_is_intel_cpu() &&
+         CRYPTO_is_FXSR_capable();
+}
+void sha1_block_data_order_avx(uint32_t state[5], const uint8_t *data,
+                               size_t num);
+
+// TODO(crbug.com/boringssl/673): Move the remaining CPU dispatch to C.
+#define SHA256_ASM
+#define SHA512_ASM
+void sha256_block_data_order(uint32_t state[8], const uint8_t *data,
+                             size_t num_blocks);
+void sha512_block_data_order(uint64_t state[8], const uint8_t *data,
+                             size_t num_blocks);
 
 #elif !defined(OPENSSL_NO_ASM) && defined(OPENSSL_X86_64)
 
