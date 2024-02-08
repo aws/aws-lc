cd24cc4d072c72ff127ee7be9c55666c7358c045
Bob Beck <bbe@google.com>
2024-02-08 15:50:19 -0700

    Expose OPENSSL_timegm in posix_time.h
    Ok so this one is purely for keeping this the same between forks.
    Boring today doesn't need this (although some of our outside
    uses might be able to use it). Effectively, this function
    is the same as converting to a posix time, followed by a safe
    check to see if you can put the result in a time_t.
    
    posix_time.h is about to get added as public in LibreSSL, and
    while not strictly necessary there because they could inline
    what it does, Libre is finding that may be needed in rpki-client,
    ocsp-client, as well as libtls (including for libtls portable).
    
      In the interests of keeping the same API in the same file, Libre
    would like this to be here so it's just consistent from both places.
    
    Change-Id: I2f234005e83cdea5e61fa41fbf03ef61516c45f8
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/66127
    Commit-Queue: David Benjamin <davidben@google.com>
    Reviewed-by: David Benjamin <davidben@google.com>
    (cherry picked from commit 5dd15f3a3b26995aeced71da4b404438df2a6363)

--- crypto/asn1/internal.h
+++ crypto/asn1/internal.h
@@ -63,6 +63,7 @@
 
 #include <openssl/asn1.h>
 #include <openssl/asn1t.h>
+#include <openssl/posix_time.h>
 
 #include "../internal.h"
 
@@ -73,26 +74,10 @@ extern "C" {
 
 // Wrapper functions for time functions.
 
-// OPENSSL_posix_to_tm converts a int64_t POSIX time value in |time| which must
-// be in the range of year 0000 to 9999 to a broken out time value in |tm|. It
-// returns one on success and zero on error.
-OPENSSL_EXPORT int OPENSSL_posix_to_tm(int64_t time, struct tm *out_tm);
-
-// OPENSSL_tm_to_posix converts a time value between the years 0 and 9999 in
-// |tm| to a POSIX time value in |out|. One is returned on success, zero is
-// returned on failure. It is a failure if the tm contains out of range values.
-OPENSSL_EXPORT int OPENSSL_tm_to_posix(const struct tm *tm, int64_t *out);
-
 // OPENSSL_gmtime converts a time_t value in |time| which must be in the range
 // of year 0000 to 9999 to a broken out time value in |tm|. On success |tm| is
 // returned. On failure NULL is returned.
 OPENSSL_EXPORT struct tm *OPENSSL_gmtime(const time_t *time, struct tm *result);
-
-// OPENSSL_timegm converts a time value between the years 0 and 9999 in |tm| to
-// a time_t value in |out|. One is returned on success, zero is returned on
-// failure. It is a failure if the converted time can not be represented in a
-// time_t, or if the tm contains out of range values.
-OPENSSL_EXPORT int OPENSSL_timegm(const struct tm *tm, time_t *out);
 
 // OPENSSL_gmtime_adj returns one on success, and updates |tm| by adding
 // |offset_day| days and |offset_sec| seconds. It returns zero on failure. |tm|
