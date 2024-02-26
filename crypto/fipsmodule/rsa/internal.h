0a470e450c6f5492478319abefc6daa0e9999954
dkostic <25055813+dkostic@users.noreply.github.com>
2024-02-26 08:13:11 -0800

    RSA key check consolidation part 1a (#1349)
    This is the first PR in a series of PRs that will attempt to
    consolidate the RSA key checking in AWS-LC. The high level
    plan is to do the following:
     1. Implement `RSA_check_key|` that performs checks equivalent to
        those in OpenSSL 1.x versions and 3.x non-FIPS versions of
        `RSA_check_key` with the exception of the primality tests, while
        retaining the ability to process public keys.
     2. Implement `RSA_check_fips` that performs SP 800-56b checks
        as OpenSSL 3.x `RSA_check_key` does when built in FIPS mode.
     3. When ready, modify `RSA_check_key` function to call
        `RSA_check_fips` when AWS-LC is built in FIPS mode,
        and deprecate `RSA_check_fips`.
    
    Two temporary functions will be introduced and used for development,
    until the whole solution is done:
     - `wip_do_not_use_rsa_check_key`,
     - `wip_do_not_use_rsa_check_key_fips`.
    
    After completing step 1 from above, `wip_do_not_use_rsa_check_key`
    should become `RSA_check_key` function.
    After completing step 2 from above, `wip_do_not_use_rsa_check_key_fips`
    should become `RSA_check_fips` function.
    After completing step 3 from above, `RSA_check_key` function should
    perform the `RSA_check_fips` checks when built in FIPS mode, and
    `RSA_check_fips` could be deprecated.
    
    This change introduces the two temporary functions, implements a few
    checks and adds tests alongside the existing tests for `RSA_check_key`.

--- crypto/fipsmodule/rsa/internal.h
+++ crypto/fipsmodule/rsa/internal.h
@@ -231,6 +231,10 @@ int rsa_digestverify_no_self_test(const EVP_MD *md, const uint8_t *input,
                                   size_t in_len, const uint8_t *sig,
                                   size_t sig_len, RSA *rsa);
 
+// ------ DO NOT USE! -------
+// These functions are work-in-progress to consolidate the RSA key checking.
+OPENSSL_EXPORT int wip_do_not_use_rsa_check_key(const RSA *key);
+OPENSSL_EXPORT int wip_do_not_use_rsa_check_key_fips(const RSA *key);
 
 #if defined(__cplusplus)
 }  // extern C
