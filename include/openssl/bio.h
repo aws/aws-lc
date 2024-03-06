8d8214ff30edba4fb0dc35c3b70ef29b298789bb
Shubham Mittal <smittals@amazon.com>
2024-03-05 18:06:43 -0800

    Moving BIO_s_secmem to seperate commit

--- include/openssl/bio.h
+++ include/openssl/bio.h
@@ -447,10 +447,6 @@ OPENSSL_EXPORT int BIO_set_mem_buf(BIO *bio, BUF_MEM *b, int take_ownership);
 // default is -1 so that additional data can be written once exhausted.
 OPENSSL_EXPORT int BIO_set_mem_eof_return(BIO *bio, int eof_value);
 
-// Returns the normal BIO_METHOD |BIO_s_mem|. Deprecated since AWS-LC does not
-// support secure heaps.
-OPENSSL_EXPORT OPENSSL_DEPRECATED const BIO_METHOD *BIO_s_secmem(void);
-
 // BIO close flags.
 //
 // These can be used as symbolic arguments when a "close flag" is passed to a
