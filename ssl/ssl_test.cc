9cedd02f6530fc70333d6d4b41fb0183a22851c8
Justin Smith <justsmth@amazon.com>
2024-03-14 10:29:28 -0400

    Max length for tmpnam_s is L_tmpnam_s

--- ssl/ssl_test.cc
+++ ssl/ssl_test.cc
@@ -12112,10 +12112,10 @@ TEST(SSLTest, SSLFileTests) {
   using ScopedFILE = std::unique_ptr<FILE, FileCloser>;
 
 #if defined(OPENSSL_WINDOWS)
-  char rsa_pem_filename[L_tmpnam];
-  char ecdsa_pem_filename[L_tmpnam];
-  ASSERT_EQ(tmpnam_s(rsa_pem_filename, L_tmpnam), 0);
-  ASSERT_EQ(tmpnam_s(ecdsa_pem_filename, L_tmpnam), 0);
+  char rsa_pem_filename[L_tmpnam_s];
+  char ecdsa_pem_filename[L_tmpnam_s];
+  ASSERT_EQ(tmpnam_s(rsa_pem_filename, L_tmpnam_s), 0);
+  ASSERT_EQ(tmpnam_s(ecdsa_pem_filename, L_tmpnam_s), 0);
 #else
   char rsa_pem_filename[] = "/tmp/fileXXXXXX";
   char ecdsa_pem_filename[] = "/tmp/fileXXXXXX";
