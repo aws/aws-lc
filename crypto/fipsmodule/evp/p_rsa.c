iuhjui-sugar/boringssl

Move RSA (en|de)cryption out of the FIPS module.
Change-Id: I330ac0fa7f0b2c9984d12da831d8f34019ea2c49

crypto/evp/p_rsa.c
@@ -67,7 +67,7 @@
 #include <openssl/rsa.h>
 
 #include "../internal.h"
-#include "../fipsmodule/rsa/internal.h"
+#include "../rsa_extra/internal.h"
 #include "internal.h"
 
 

aws/aws-lc

Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/58526
Reviewed-by: David Benjamin <davidben@google.com>
Commit-Queue: Adam Langley <agl@google.com>
(cherry picked from commit 89de6e1afe89670f022739cb720e4db0fb0a129e)

crypto/fipsmodule/evp/p_rsa.c
@@ -65,9 +65,8 @@
 #include <openssl/nid.h>
 #include <openssl/rsa.h>
 
-#include "../../internal.h"
-#include "../rsa/internal.h"
 #include "internal.h"
+#include "../rsa/internal.h"
 #include "../../rsa_extra/internal.h"
 
 #define NO_PSS_SALT_LEN_RESTRICTION -1