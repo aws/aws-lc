56f356950bc46375fbf5b8c7fd9a57834695cbfc
dkostic <25055813+dkostic@users.noreply.github.com>
2024-04-22 14:25:26 -0700

    [ML-KEM] Add experimental support for ML-KEM-512-IPD (#1516)
    Add support and testing for ML-KEM-512-IPD,
    as specified in FIPS 203 Initial Public Draft.
    This is an intermediate step to support the final
    standardized ML-KEM once FIPS 203 is finalized.
    
    We do not plan to support the IPD version long-term,
    it will be surpassed by the final FIPS 203 (ML-KEM) definition.

--- crypto/ml_kem/ml_kem_ipd_ref_common/params.h
+++ crypto/ml_kem/ml_kem_ipd_ref_common/params.h
@@ -1,6 +1,8 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
+#include <openssl/base.h>
+
 #ifndef KYBER_K
 #define KYBER_K 3	/* Change this for different security strengths */
 #endif
@@ -8,11 +10,11 @@
 
 /* Don't change parameters below this line */
 #if   (KYBER_K == 2)
-#define KYBER_NAMESPACE(s) pqcrystals_kyber512_ref_##s
+#define KYBER_NAMESPACE(s) ml_kem_512_ref_##s
 #elif (KYBER_K == 3)
-#define KYBER_NAMESPACE(s) pqcrystals_kyber768_ref_##s
+#define KYBER_NAMESPACE(s) ml_kem_768_ref_##s
 #elif (KYBER_K == 4)
-#define KYBER_NAMESPACE(s) pqcrystals_kyber1024_ref_##s
+#define KYBER_NAMESPACE(s) ml_kem_1024_ref_##s
 #else
 #error "KYBER_K must be in {2,3,4}"
 #endif
