9ecb994a82d803b15fe3bd21e4faeb676ae73f8b
Bill Yang <wbyang@amazon.com>
2024-04-30 16:11:42 -0700

    attempt to add bad hash library and test

--- util/fipstools/inject_hash/tests/inject_hash_tests.cc
+++ util/fipstools/inject_hash/tests/inject_hash_tests.cc
@@ -7,8 +7,8 @@
 #include "inject_hash_tests.h"
 
 constexpr char const *InjectHashTestFixture::good_lib_filename;
+constexpr char const *InjectHashTestFixture::bad_hash_lib_filename;
 
-// TODO: write tests here
 TEST_F(InjectHashTestFixture, TestGoodLib) {
     int argc = 6;
     char prog[] = "./inject_hash";
@@ -17,5 +17,20 @@ TEST_F(InjectHashTestFixture, TestGoodLib) {
     char flag3[] = "-f";
     char *argv[] = {prog, flag1, const_cast<char*>(good_lib_filename), flag2, const_cast<char*>(good_lib_filename), flag3};
 
+    // TODO: make sure that when we succeed, we don't actually write the correct hash to the file so repeat tests will pass
     ASSERT_EQ(1, inject_hash(argc, argv));
-};
+}
+
+TEST_F(InjectHashTestFixture, TestBadHashLib) {
+    int argc = 6;
+    char prog[] = "./inject_hash";
+    char flag1[] = "-p";
+    char flag2[] = "-o";
+    char flag3[] = "-f";
+    char *argv[] = {prog, flag1, const_cast<char*>(bad_hash_lib_filename), flag2, const_cast<char*>(bad_hash_lib_filename), flag3};
+
+    printf("%s\n", bad_hash_lib_filename);
+
+    // TODO: make sure it fails for the right reason somehow
+    ASSERT_EQ(0, inject_hash(argc, argv));
+}
--- util/fipstools/inject_hash/tests/inject_hash_tests.h
+++ util/fipstools/inject_hash/tests/inject_hash_tests.h
@@ -6,9 +6,11 @@
 #include "../common.h"
 
 // TODO: change this based on platform, we only care about Apple for now
-#define GOODLIB_NAME "test_libs/libgood_lib.dylib"
+#define GOOD_LIB_NAME "test_libs/libgood_lib.dylib"
+#define BAD_HASH_LIB_NAME "test_libs/libbad_hash_lib.dylib"
 
 class InjectHashTestFixture : public ::testing::Test {
 protected:
-    static constexpr char const *good_lib_filename = GOODLIB_NAME;
+    static constexpr char const *good_lib_filename = GOOD_LIB_NAME;
+    static constexpr char const *bad_hash_lib_filename = BAD_HASH_LIB_NAME;
 };
