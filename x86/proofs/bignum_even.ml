4d49cc4a4730dd23bd557f70e9d91254a32f34c6
John Harrison <jargh@amazon.com>
2024-04-30 12:55:36 -0700

    Avoid conditional move from memory in even/odd tests
    Our current understanding is that x86 implementations of CMOV will
    *always* do the memory load regardless of the condition, so it's not
    appropriate to use CMOV to restrict loads based on input sizes. The
    new code just uses trivial control flow as in the ARM version.

--- x86/proofs/bignum_even.ml
+++ x86/proofs/bignum_even.ml
@@ -12,11 +12,12 @@
 
 let bignum_even_mc = define_assert_from_elf "bignum_even_mc" "x86/generic/bignum_even.o"
 [
-  0x48; 0x31; 0xc0;        (* XOR (% rax) (% rax) *)
+  0xb8; 0x01; 0x00; 0x00; 0x00;
+                           (* MOV (% eax) (Imm32 (word 1)) *)
   0x48; 0x85; 0xff;        (* TEST (% rdi) (% rdi) *)
-  0x48; 0x0f; 0x45; 0x06;  (* CMOVNE (% rax) (Memop Quadword (%% (rsi,0))) *)
+  0x74; 0x07;              (* JE (Imm8 (word 7)) *)
+  0x48; 0x33; 0x06;        (* XOR (% rax) (Memop Quadword (%% (rsi,0))) *)
   0x48; 0x83; 0xe0; 0x01;  (* AND (% rax) (Imm8 (word 1)) *)
-  0x48; 0x83; 0xf0; 0x01;  (* XOR (% rax) (Imm8 (word 1)) *)
   0xc3                     (* RET *)
 ];;
 
@@ -33,7 +34,7 @@ let BIGNUM_EVEN_CORRECT = prove
                read RIP s = word pc /\
                C_ARGUMENTS [k;a] s /\
                bignum_from_memory(a,val k) s = x)
-          (\s. read RIP s = word(pc + 0x12) /\
+          (\s. read RIP s = word(pc + 0x11) /\
                C_RETURN s = if EVEN x then word 1 else word 0)
           (MAYCHANGE [RIP; RAX] ,, MAYCHANGE SOME_FLAGS)`,
   W64_GEN_TAC `k:num` THEN
@@ -42,14 +43,14 @@ let BIGNUM_EVEN_CORRECT = prove
   ASM_CASES_TAC `k = 0` THENL
    [ASM_REWRITE_TAC[BIGNUM_FROM_MEMORY_TRIVIAL] THEN
     ASM_CASES_TAC `x = 0` THEN ASM_REWRITE_TAC[ENSURES_TRIVIAL; EVEN] THEN
-    X86_SIM_TAC BIGNUM_EVEN_EXEC (1--5);
+    X86_SIM_TAC BIGNUM_EVEN_EXEC (1--3);
     ENSURES_INIT_TAC "s0" THEN EXPAND_TAC "x" THEN
     ONCE_REWRITE_TAC[BIGNUM_FROM_MEMORY_EXPAND] THEN
     ASM_REWRITE_TAC[EVEN_ADD; EVEN_MULT; EVEN_EXP; ARITH_EVEN; ARITH_EQ] THEN
     ABBREV_TAC `d = read (memory :> bytes64 a1) s0` THEN
     X86_STEPS_TAC BIGNUM_EVEN_EXEC (1--5) THEN ENSURES_FINAL_STATE_TAC THEN
-    ASM_REWRITE_TAC[WORD_AND_1; BIT_LSB; GSYM NOT_ODD; COND_SWAP] THEN
-    COND_CASES_TAC THEN ASM_REWRITE_TAC[] THEN CONV_TAC WORD_REDUCE_CONV]);;
+    ASM_REWRITE_TAC[GSYM WORD_BITVAL; EVEN_MOD; VAL_MOD_2; BITVAL_EQ_0] THEN
+    CONV_TAC WORD_BLAST]);;
 
 let BIGNUM_EVEN_SUBROUTINE_CORRECT = prove
  (`!k a x pc stackpointer returnaddress.
@@ -76,7 +77,7 @@ let windows_bignum_even_mc = define_from_elf
 let WINDOWS_BIGNUM_EVEN_SUBROUTINE_CORRECT = prove
  (`!k a x pc stackpointer returnaddress.
         ALL (nonoverlapping (word_sub stackpointer (word 16),16))
-            [(word pc,0x1d); (a,8 * val k)]
+            [(word pc,0x1c); (a,8 * val k)]
         ==> ensures x86
               (\s. bytes_loaded s (word pc) windows_bignum_even_mc /\
                    read RIP s = word pc /\
