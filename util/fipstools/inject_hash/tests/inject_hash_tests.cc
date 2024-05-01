584cf67d45a313630e240d843f9fe7bd5fb36101
Bill Yang <wbyang@amazon.com>
2024-05-01 11:21:37 -0700

    fix broken tests

--- util/fipstools/inject_hash/tests/inject_hash_tests.cc
+++ util/fipstools/inject_hash/tests/inject_hash_tests.cc
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 
 #include <assert.h>
+#include <iostream>
 #include <string>
 
 #include "inject_hash_tests.h"
@@ -10,27 +11,22 @@ constexpr char const *InjectHashTestFixture::good_lib_filename;
 constexpr char const *InjectHashTestFixture::bad_hash_lib_filename;
 
 TEST_F(InjectHashTestFixture, TestGoodLib) {
-    int argc = 6;
-    char prog[] = "./inject_hash";
-    char flag1[] = "-p";
-    char flag2[] = "-o";
-    char flag3[] = "-f";
-    char *argv[] = {prog, flag1, const_cast<char*>(good_lib_filename), flag2, const_cast<char*>(good_lib_filename), flag3};
+    uint8_t *object_bytes = nullptr;
+    size_t object_bytes_size;
 
-    // TODO: make sure that when we succeed, we don't actually write the correct hash to the file so repeat tests will pass
-    ASSERT_EQ(1, inject_hash(argc, argv));
+    ASSERT_EQ(1, inject_hash_no_write(NULL, const_cast<char*>(good_lib_filename), const_cast<char*>(good_lib_filename), 1, &object_bytes, &object_bytes_size));
 }
 
 TEST_F(InjectHashTestFixture, TestBadHashLib) {
-    int argc = 6;
-    char prog[] = "./inject_hash";
-    char flag1[] = "-p";
-    char flag2[] = "-o";
-    char flag3[] = "-f";
-    char *argv[] = {prog, flag1, const_cast<char*>(bad_hash_lib_filename), flag2, const_cast<char*>(bad_hash_lib_filename), flag3};
+    uint8_t *object_bytes = nullptr;
+    size_t object_bytes_size;
 
-    printf("%s\n", bad_hash_lib_filename);
+    int inject_hash_ret;
+    testing::internal::CaptureStderr();
 
-    // TODO: make sure it fails for the right reason somehow
-    ASSERT_EQ(0, inject_hash(argc, argv));
+    inject_hash_ret = inject_hash_no_write(NULL, const_cast<char*>(bad_hash_lib_filename), const_cast<char*>(bad_hash_lib_filename), 1, &object_bytes, &object_bytes_size);
+    std::string captured_stderr = testing::internal::GetCapturedStderr();
+
+    ASSERT_EQ(0, inject_hash_ret);
+    EXPECT_TRUE(captured_stderr.find("Error finding hash") != std::string::npos);
 }
