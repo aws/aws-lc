570d20089c4e8f6f9e1123044927aa6c787d0261
Bill Yang <wbyang@amazon.com>
2024-05-28 11:20:27 -0700

    add missing copyright

--- crypto/fips_hashing/fips_hashing.c
+++ crypto/fips_hashing/fips_hashing.c
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
 #include "../fipsmodule/delocate.h"
 
 #include "../fipsmodule/evp/p_hmac.c"
