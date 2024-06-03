c66c036b36b1ec25ab9f159cb23557b18a6004b5
Shubham Mittal <smittals@amazon.com>
2024-06-02 19:31:41 -0700

    changed comment for RSA function code

--- include/openssl/rsa.h
+++ include/openssl/rsa.h
@@ -935,7 +935,8 @@ BSSL_NAMESPACE_END
 #define RSA_R_BLOCK_TYPE_IS_NOT_02 148
 #define RSA_R_MISMATCHED_SIGNATURE 248
 
-// function code for OpenVPN compatibility
+// RSA_F_RSA_OSSL_PRIVATE_ENCRYPT is a function code defined
+// for OpenVPN compatibility, AWS-LC does not support them
 #define RSA_F_RSA_OSSL_PRIVATE_ENCRYPT 0
 
 #endif  // OPENSSL_HEADER_RSA_H
