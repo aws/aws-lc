ef7e6ee96e036d242a9f8523d818aebc4f3981fc
Will Childs-Klein <childw@amazon.com>
2024-06-19 18:59:47 +0000

    Update SSL_set_ciphersuites per PR feedback

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -1721,12 +1721,12 @@ OPENSSL_EXPORT int SSL_set_strict_cipher_list(SSL *ssl, const char *str);
 // zero on failure.
 OPENSSL_EXPORT int SSL_CTX_set_ciphersuites(SSL_CTX *ctx, const char *str);
 
-// SSL_set_ciphersuites simply wraps |SSL_CTX_set_ciphersuites|. In OpenSSL,
-// the only difference between |SSL_CTX_set_ciphersuites| and
-// |SSL_set_ciphersuites| is that the latter duplicates the |SSL| struct's
-// |cipher_list| onto the associated |SSL_CONNECTION| struct. In AWS-LC, we
-// track |tls13_cipher_list| and |cipher_list| on |SSL_CTX| so that behavior is
-// not applicable to this |SSL_set_ciphersuites|.
+// SSL_set_ciphersuites sets the available TLSv1.3 ciphersuites on an |ssl|,
+// returning one on success and zero on failure. In OpenSSL, the only
+// difference between |SSL_CTX_set_ciphersuites| and |SSL_set_ciphersuites| is
+// that the latter copies the |SSL|'s |cipher_list| to its associated
+// |SSL_CONNECTION|. In AWS-LC, we track everything on the |ssl|'s |config| so
+// duplication is not necessary.
 OPENSSL_EXPORT int SSL_set_ciphersuites(SSL *ssl, const char *str);
 
 // SSL_set_cipher_list configures the cipher list for |ssl|, evaluating |str| as
