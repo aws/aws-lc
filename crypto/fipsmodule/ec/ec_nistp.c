240ad032ff791b7ff3fc5646765f9711de52704b
dkostic <25055813+dkostic@users.noreply.github.com>
2024-07-08 10:53:58 -0700

    ec_nistp table generation for scalar multiplication (#1669)
    Generating the precomputed table for scalar multiplication for curves
    P-384 and P-521 was implemented for each curve separately.
    This commit refactors the code such that a single implementation
    can be used with both curves. It is a step towards moving the whole
    scalar multiplication code to ec_nistp.c.

--- crypto/fipsmodule/ec/ec_nistp.c
+++ crypto/fipsmodule/ec/ec_nistp.c
@@ -303,3 +303,43 @@ void scalar_rwnaf(int16_t *out, size_t window_size,
   }
   out[num_windows - 1] = window;
 }
+
+// Generate table of multiples of the input point P = (x_in, y_in, z_in):
+//  table <-- [2i + 1]P for i in [0, SCALAR_MUL_TABLE_NUM_POINTS - 1].
+void generate_table(const ec_nistp_meth *ctx,
+                    ec_nistp_felem_limb *table,
+                    ec_nistp_felem_limb *x_in,
+                    ec_nistp_felem_limb *y_in,
+                    ec_nistp_felem_limb *z_in)
+{
+  const size_t felem_num_limbs = ctx->felem_num_limbs;
+  const size_t felem_num_bytes = felem_num_limbs * sizeof(ec_nistp_felem_limb);
+
+  // Helper variables to access individual coordinates of a point.
+  const size_t x_idx = 0;
+  const size_t y_idx = felem_num_limbs;
+  const size_t z_idx = felem_num_limbs * 2;
+
+  // table[0] <-- P.
+  OPENSSL_memcpy(&table[x_idx], x_in, felem_num_bytes);
+  OPENSSL_memcpy(&table[y_idx], y_in, felem_num_bytes);
+  OPENSSL_memcpy(&table[z_idx], z_in, felem_num_bytes);
+
+  // Compute 2P.
+  ec_nistp_felem x_in_dbl, y_in_dbl, z_in_dbl;
+  ctx->point_dbl(x_in_dbl, y_in_dbl, z_in_dbl,
+                 &table[x_idx], &table[y_idx], &table[z_idx]);
+
+  // Compute the rest of the table.
+  for (size_t i = 1; i < SCALAR_MUL_TABLE_NUM_POINTS; i++) {
+    // Just getting pointers to i-th and (i-1)-th point in the table.
+    ec_nistp_felem_limb *point_i = &table[i * 3 * felem_num_limbs];
+    ec_nistp_felem_limb *point_im1 = &table[(i - 1) * 3 * felem_num_limbs];
+
+    // table[i] <-- table[i - 1] + 2P
+    ctx->point_add(&point_i[x_idx], &point_i[y_idx], &point_i[z_idx],
+                   &point_im1[x_idx], &point_im1[y_idx], &point_im1[z_idx],
+                   0, x_in_dbl, y_in_dbl, z_in_dbl);
+  }
+}
+
--- crypto/fipsmodule/ec/ec_nistp.h
+++ crypto/fipsmodule/ec/ec_nistp.h
@@ -96,7 +96,20 @@ void ec_nistp_point_add(const ec_nistp_meth *ctx,
                         const ec_nistp_felem_limb *y2,
                         const ec_nistp_felem_limb *z2);
 
+// These two functions and two macros are temporarily defined here.
+// They will be moved to ec_nistp.c as static function
+// once all the scalar multiplications are implemented.
 void scalar_rwnaf(int16_t *out, size_t window_size,
                   const EC_SCALAR *scalar, size_t scalar_bit_size);
+void generate_table(const ec_nistp_meth *ctx,
+                    ec_nistp_felem_limb *table,
+                    ec_nistp_felem_limb *x_in,
+                    ec_nistp_felem_limb *y_in,
+                    ec_nistp_felem_limb *z_in);
+
+// The window size for scalar multiplication is hard coded for now.
+#define SCALAR_MUL_WINDOW_SIZE (5)
+#define SCALAR_MUL_TABLE_NUM_POINTS (1 << (SCALAR_MUL_WINDOW_SIZE - 1))
+
 #endif // EC_NISTP_H
 
