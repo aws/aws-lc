5f7759ffac542cac27d09dc7c9606fb9c62dfb2d
samuel40791765 <sachiang@amazon.com>
2024-07-17 00:51:54 +0000

    use GUARD_PTR for null checks

--- crypto/ocsp/ocsp_server.c
+++ crypto/ocsp/ocsp_server.c
@@ -10,13 +10,17 @@
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 // Modifications Copyright Amazon.com, Inc. or its affiliates.
 
+#include "../internal.h"
 #include "internal.h"
 
 int OCSP_request_onereq_count(OCSP_REQUEST *req) {
+  GUARD_PTR(req);
+  GUARD_PTR(req->tbsRequest);
   return (int)sk_OCSP_ONEREQ_num(req->tbsRequest->requestList);
 }
 
 OCSP_ONEREQ *OCSP_request_onereq_get0(OCSP_REQUEST *req, int i) {
+  GUARD_PTR(req);
   return sk_OCSP_ONEREQ_value(req->tbsRequest->requestList, i);
 }
 
@@ -42,6 +46,7 @@ int OCSP_id_get0_info(ASN1_OCTET_STRING **nameHash, ASN1_OBJECT **algor,
 }
 
 int OCSP_request_is_signed(OCSP_REQUEST *req) {
+  GUARD_PTR(req);
   if (req->optionalSignature != NULL) {
     return 1;
   }
@@ -49,9 +54,7 @@ int OCSP_request_is_signed(OCSP_REQUEST *req) {
 }
 
 OCSP_CERTID *OCSP_onereq_get0_id(OCSP_ONEREQ *one) {
-  if(one == NULL) {
-    return NULL;
-  }
+  GUARD_PTR(one);
   return one->reqCert;
 }
 
