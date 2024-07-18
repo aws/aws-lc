3def331baea260362ea297b47f90c9518bf2d467
Deye <erindeye@amazon.com>
2024-07-18 15:25:23 -0700

    updated script

--- tests/ci/run_openssl_comparison_tests.sh
+++ tests/ci/run_openssl_comparison_tests.sh
@@ -64,7 +64,7 @@ openssl_branches=($openssl_1_0_2_branch $openssl_1_1_1_branch $openssl_3_1_branc
 
 # Run X509 Comparison Tests against all OpenSSL branches
 for branch in "${openssl_branches[@]}"; do
-  export OPENSSL_TOOL_PATH="${install_dir}/openssl-${branch}/apps/openssl"
+  export OPENSSL_TOOL_PATH="${install_dir}/openssl-${branch}/bin/openssl"
   echo "Running X509ComparisonTests against OpenSSL ${branch}"
   "${BUILD_ROOT}/tool-openssl/tool_openssl_test" --gtest_filter=X509ComparisonTest.*
 done
