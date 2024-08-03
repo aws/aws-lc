7b7b1e0fcb72e7c0bda7c15b5f931a021914541a
Keanan Gabertan <kexgaber@amazon.com>
2024-08-03 00:44:17 +0000

    add clang

--- .github/docker_images/alpine_linux/entry.sh
+++ .github/docker_images/alpine_linux/entry.sh
@@ -6,12 +6,13 @@
 set -ex -o pipefail
 
 CHECK_FIPS="$1"
+
 if [[ "${CHECK_FIPS}" == "0" ]]; then
     echo "Testing AWS-LC Non-FIPS"
-    /tests/ci/run_posix_tests.sh
+    tests/ci/run_posix_tests.sh
 elif [[ "${CHECK_FIPS}" == "1" ]]; then
     echo "Testing AWS-LC FIPS"
-    /tests/ci/run_fips_tests.sh
+    tests/ci/run_fips_tests.sh
 else
     exit 1
 fi
