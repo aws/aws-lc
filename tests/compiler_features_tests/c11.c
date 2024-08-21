92654dbdf3cc01312e72c3c5402f6528551a42e8
Andrew Hopkins <andhop@amazon.com>
2024-08-21 10:43:14 -0700

    Enable C11 automatically if the compiler supports it (#1729)
    ### Issues:
    Resolves https://github.com/aws/aws-lc/issues/1723
    
    ### Description of changes: 
    https://github.com/aws/aws-lc/issues/1723 pointed out the default AWS-LC
    behavior can be very slow because we enable C99 by default. The user has
    to opt into the better atomics by specifying -DCMAKE_C_STANDARD=11 and
    they probably won't find that option. This change turns C11 on by
    default if the compiler supports it.
    
    Add a new benchmark to test our ref count performance.
    
    ### Call-outs:
    If users do not change their CMake options those with modern compilers
    will get C11 atomics automatically, legacy customers will continue with
    the pthread implementation. Both legacy and modern users can continue to
    manually control this by setting the CMAKE_C_STANDARD option. This
    option is only available to CMake >= 3.1 customers.
    
    ### Testing:
    Tried different options and verified the build.ninja included the
    expected `-std=gnu99` or `-std=gnu11`. Performance numbers on an
    Graviton 4 R8g show a 1.6 times increase for single threaded uncontested
    locking performance, and **33.0 times** increase in heavily contested
    locking performance. depending on the use case real world applications
    will see less of an increase. Each thread attempts to increment the same
    `CRYPTO_refcount_t` 1,000 times:
    C99 (previous performance):
    ```
    cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_STANDARD=99 ../
    ./tool/bssl speed -filter CRYPTO_refcount_inc -timeout 10
    Did 225000 CRYPTO_refcount_inc 1000 iterations with 1 threads operations in 10043601us (22402.3 ops/sec)
    Did 78861 CRYPTO_refcount_inc 1000 iterations with 2 threads operations in 10008608us (7879.3 ops/sec)
    Did 26448 CRYPTO_refcount_inc 1000 iterations with 4 threads operations in 10032376us (2636.3 ops/sec)
    Did 8262 CRYPTO_refcount_inc 1000 iterations with 8 threads operations in 10035344us (823.3 ops/sec)
    Did 2520 CRYPTO_refcount_inc 1000 iterations with 16 threads operations in 10048296us (250.8 ops/sec)
    Did 684 CRYPTO_refcount_inc 1000 iterations with 32 threads operations in 10079403us (67.9 ops/sec)
    Did 218 CRYPTO_refcount_inc 1000 iterations with 64 threads operations in 10015418us (21.8 ops/sec)
    ```
    
    C11 (current performance):
    ```
    cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../ 
    ./tool/bssl speed -filter CRYPTO_refcount_inc -timeout 10
    Did 372000 CRYPTO_refcount_inc 1000 iterations with 1 threads operations in 10002129us (37192.1 ops/sec)
    Did 268000 CRYPTO_refcount_inc 1000 iterations with 2 threads operations in 10022289us (26740.4 ops/sec)
    Did 168000 CRYPTO_refcount_inc 1000 iterations with 4 threads operations in 10022843us (16761.7 ops/sec)
    Did 74943 CRYPTO_refcount_inc 1000 iterations with 8 threads operations in 10100478us (7419.7 ops/sec)
    Did 32550 CRYPTO_refcount_inc 1000 iterations with 16 threads operations in 10037521us (3242.8 ops/sec)
    Did 15246 CRYPTO_refcount_inc 1000 iterations with 32 threads operations in 10065910us (1514.6 ops/sec)
    Did 7208 CRYPTO_refcount_inc 1000 iterations with 64 threads operations in 10042097us (717.8 ops/sec)
    ```
    
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license and the ISC license.

---
+++ tests/compiler_features_tests/c11.c
@@ -0,0 +1,12 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
+// Simple program that should also be able to compiler, udeful to test different
+// compiler flags
+
+#include <stdlib.h>
+#include <stdatomic.h>
+
+int main(int argc, char **argv) {
+    return EXIT_SUCCESS;
+}
