62f96ec071c3406aacef9ebac25656d30e77f0bd
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2024-08-28 14:46:31 -0700

    PR feedback

--- crypto/ube/ube.c
+++ crypto/ube/ube.c
@@ -6,11 +6,11 @@
 #include "internal.h"
 #include "../internal.h"
 
-// What is an "UBE"?
+// What is a uniqueness-breaking event (UBE)?
 // AWS-LC manages state that must be unique on each usage. For example, the
 // CTR-DRBG state must be unique on each usage, otherwise the generation
-// function will produce duplicated values. We name such state "volatile state"
-// or "volatile memory" if referring directly to memory. Before using a volatile
+// function will produce duplicated values. We name such state "unique state"
+// or "unique memory" if referring directly to memory. Before using a unique
 // state, we must make sure to randomize it to preserve its uniqueness.
 //
 // When/how to randomize state depends on the context. For example, the CTR-DRBG
@@ -19,16 +19,16 @@
 // call such events "uniqueness breaking events" (UBE). Forking an address space
 // is an example of an UBE.
 //
-// By detecting UBE's we can ensure that volatile state is properly randomized.
+// By detecting UBE's we can ensure that unique state is properly randomized.
 // AWS-LC is currently able to detect two different type of UBE's that would
-// violate usage requirements of a volatile state. The two UBE types are:
+// violate usage requirements of a unique state. The two UBE types are:
 // Forking and VM resuming. Note, however, that detection methods rely on
 // technology that is unique to a platform. Hence, support for UBE detection
 // also depends on the platform AWS-LC is executed on.
 //
 // This file implements and manages the general machinery to detect an UBE. This
 // should be used by the rest of the code-base to implement proper randomization
-// of volatile states before usage.
+// of unique states before usage.
 
 static CRYPTO_once_t ube_state_initialize_once = CRYPTO_ONCE_INIT;
 static CRYPTO_once_t ube_detection_unavailable_once = CRYPTO_ONCE_INIT;
@@ -42,8 +42,8 @@ static uint64_t testing_fork_generation_number = 0;
 void set_fork_generation_number_FOR_TESTING(uint64_t fork_gn) {
   testing_fork_generation_number = fork_gn;
 }
-static uint64_t testing_snapsafe_generation_number = 0;
-void set_snapsafe_generation_number_FOR_TESTING(uint64_t snapsafe_gn) {
+static uint32_t testing_snapsafe_generation_number = 0;
+void set_snapsafe_generation_number_FOR_TESTING(uint32_t snapsafe_gn) {
   testing_snapsafe_generation_number = snapsafe_gn;
 }
 
@@ -51,14 +51,14 @@ void set_snapsafe_generation_number_FOR_TESTING(uint64_t snapsafe_gn) {
 // These two functions currently mocks the backend implementations of the fork
 // detection method and snapsafe detection method. They will be reimplemented in
 // a future change.
-static int get_snapsafe_generation_number_mocked(uint64_t *gn) {
+static int CRYPTO_get_snapsafe_generation_number_mocked(uint32_t *gn) {
   *gn = 1;
   if (testing_snapsafe_generation_number != 0) {
     *gn = testing_snapsafe_generation_number;
   }
   return 1;
 }
-static uint64_t get_fork_generation_number_mocked(void) {
+static uint64_t CRYPTO_get_fork_generation_number_mocked(void) {
   if (testing_fork_generation_number != 0) {
     return testing_fork_generation_number;
   }
@@ -68,23 +68,23 @@ static uint64_t get_fork_generation_number_mocked(void) {
 
 // The UBE generation number is shared for the entire address space. One could
 // implement a per-thread UBE generation number. However, this could be
-// inefficient for global volatile states if care is not taken. Because every
+// inefficient for global unique states if care is not taken. Because every
 // thread only have visibility on their own per-thread generation number, we
-// could trigger a new randomization of the volatile state per-thread. While a
-// single randomization of the global volatile state should be sufficient.
-struct ube_global_state {
+// could trigger a new randomization of the unique state per-thread. While a
+// single randomization of the global unique state should be sufficient.
+struct ube_state {
   uint64_t generation_number;
   uint64_t cached_fork_gn;
-  uint64_t cached_snapsafe_gn;
+  uint32_t cached_snapsafe_gn;
 };
-static struct ube_global_state ube_state = { 0 };
+static struct ube_state ube_global_state = { 0 };
 
 // Convenience object that makes it easier to extend the number of detection
 // methods without having to modify function signatures.
 struct detection_gn {
 #define NUMBER_OF_DETECTION_GENERATION_NUMBERS 2
   uint64_t current_fork_gn;
-  uint64_t current_snapsafe_gn;
+  uint32_t current_snapsafe_gn;
 };
 
 // set_ube_detection_unavailable_once is the single mutation point of
@@ -101,32 +101,32 @@ static void ube_failed(void) {
   CRYPTO_once(&ube_detection_unavailable_once, set_ube_detection_unavailable_once);
 }
 
-// ube_state_initialize initalizes the global state |ube_state|.
+// ube_state_initialize initializes the global state |ube_global_state|.
 static void ube_state_initialize(void) {
 
-  ube_state.generation_number = 0;
-  ube_state.cached_fork_gn = get_fork_generation_number_mocked();
+  ube_global_state.generation_number = 0;
+  ube_global_state.cached_fork_gn = CRYPTO_get_fork_generation_number_mocked();
   int ret_snapsafe_gn =
-    get_snapsafe_generation_number_mocked(&(ube_state.cached_snapsafe_gn));
+    CRYPTO_get_snapsafe_generation_number_mocked(&(ube_global_state.cached_snapsafe_gn));
 
-  if (ube_state.cached_fork_gn == 0 ||
+  if (ube_global_state.cached_fork_gn == 0 ||
       ret_snapsafe_gn == 0) {
     ube_failed();
   }
 }
 
-// ube_update_state updates the global state |ube_state| with the generation
-// numbers loaded into |current_detection_gn| and increments the UBE generation
-// number.
+// ube_update_state updates the global state |ube_global_state| with the
+// generation numbers loaded into |current_detection_gn| and increments the UBE
+// generation number.
 static void ube_update_state(struct detection_gn *current_detection_gn) {
 
-  ube_state.generation_number += 1;
+  ube_global_state.generation_number += 1;
 
   // Make sure we cache all new generation numbers. Otherwise, we might detect
   // a fork UBE but, in fact, both a fork and snapsafe UBE occurred. Then next
   // time we enter, a redundant reseed will be emitted.
-  ube_state.cached_fork_gn = current_detection_gn->current_fork_gn;
-  ube_state.cached_snapsafe_gn = current_detection_gn->current_snapsafe_gn;
+  ube_global_state.cached_fork_gn = current_detection_gn->current_fork_gn;
+  ube_global_state.cached_snapsafe_gn = current_detection_gn->current_snapsafe_gn;
 }
 
 // ube_get_detection_generation_numbers loads the current detection generation
@@ -142,8 +142,8 @@ static int ube_get_detection_generation_numbers(
   OPENSSL_STATIC_ASSERT(NUMBER_OF_DETECTION_GENERATION_NUMBERS == 2,
     not_handling_the_exact_number_of_detection_generation_numbers);
 
-  current_detection_gn->current_fork_gn = get_fork_generation_number_mocked();
-  int ret_snapsafe_gn = get_snapsafe_generation_number_mocked(
+  current_detection_gn->current_fork_gn = CRYPTO_get_fork_generation_number_mocked();
+  int ret_snapsafe_gn = CRYPTO_get_snapsafe_generation_number_mocked(
                           &(current_detection_gn->current_snapsafe_gn));
 
   if (current_detection_gn->current_fork_gn == 0 ||
@@ -162,15 +162,14 @@ static int ube_get_detection_generation_numbers(
 // Returns 1 if UBE has been detected and 0 if no UBE has been detected.
 static int ube_is_detected(struct detection_gn *current_detection_gn) {
 
-  if (ube_state.cached_fork_gn != current_detection_gn->current_fork_gn ||
-      ube_state.cached_snapsafe_gn != current_detection_gn->current_snapsafe_gn) {
+  if (ube_global_state.cached_fork_gn != current_detection_gn->current_fork_gn ||
+      ube_global_state.cached_snapsafe_gn != current_detection_gn->current_snapsafe_gn) {
     return 1;
   }
   return 0;
 }
 
-// stays invariant after a failure state has been reached.
-int get_ube_generation_number(uint64_t *current_generation_number) {
+int CRYPTO_get_ube_generation_number(uint64_t *current_generation_number) {
 
   GUARD_PTR(current_generation_number);
   *current_generation_number = 0;
@@ -188,19 +187,18 @@ int get_ube_generation_number(uint64_t *current_generation_number) {
   struct detection_gn current_detection_gn = { 0 };
 
   // First read generation numbers for each supported detection method. We do
-  // not mutate |ube_state|. So, a read lock is sufficient at this point. Each
-  // individual detection method will have their own concurrency controls if
-  // needed.
+  // not mutate |ube_global_state|. So, a read lock is sufficient at this point.
+  // Each individual detection method will have their own concurrency controls
+  // if needed.
 
-  CRYPTO_STATIC_MUTEX_lock_read(&ube_lock);
   if (ube_get_detection_generation_numbers(&current_detection_gn) != 1) {
     ube_failed();
-    CRYPTO_STATIC_MUTEX_unlock_read(&ube_lock);
     return 0;
   }
+  CRYPTO_STATIC_MUTEX_lock_read(&ube_lock);
   if (ube_is_detected(&current_detection_gn) == 0) {
     // No UBE detected, so just grab UBE generation number from the state.
-    *current_generation_number = ube_state.generation_number;
+    *current_generation_number = ube_global_state.generation_number;
     CRYPTO_STATIC_MUTEX_unlock_read(&ube_lock);
     return 1;
   }
@@ -224,14 +222,14 @@ int get_ube_generation_number(uint64_t *current_generation_number) {
   if (ube_is_detected(&current_detection_gn) == 0) {
     // Another thread already updated the global state. Just load the UBE
     // generation number instead.
-    *current_generation_number = ube_state.generation_number;
+    *current_generation_number = ube_global_state.generation_number;
     CRYPTO_STATIC_MUTEX_unlock_write(&ube_lock);
     return 1;
   }
 
   // Okay, we are really the first to update the state after detecting an UBE.
   ube_update_state(&current_detection_gn);
-  *current_generation_number = ube_state.generation_number;
+  *current_generation_number = ube_global_state.generation_number;
   CRYPTO_STATIC_MUTEX_unlock_write(&ube_lock);
 
   return 1;
