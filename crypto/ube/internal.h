c46e2d2425c0fd5918adfde34a881657f911cab3
torben-hansen <50673096+torben-hansen@users.noreply.github.com>
2024-08-29 17:03:10 -0700

    UBE backend (#1801)
    Implements the uniqueness-breaking event detection backend with the new
    function `get_ube_generation_number()` being the entry point. This is
    basically an abstraction on top of the existing detection methods AWS-LC
    currently support. The implementation in this PR includes the UBE logic
    and excludes:
    - Using the new function `get_ube_generation_number()` in the new
    randomness generation.
    - Using the fork/snapsafe event detection methods
    `CRYPTO_get_fork_generation` and `CRYPTO_get_snapsafe_generation`.
    
    These fill follow later.
    
    The UBE subsystem is stateful maintaining the global state `ube_state`
    which contain the global ube generation number. This value can be used
    to protect volatile state.

---
+++ crypto/ube/internal.h
@@ -0,0 +1,44 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
+#ifndef OPENSSL_HEADER_CRYPTO_UBE_INTERNAL_H
+#define OPENSSL_HEADER_CRYPTO_UBE_INTERNAL_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include <openssl/base.h>
+
+// get_ube_generation_number returns the uniqueness-breaking event (UBE)
+// generation number for the address space in |current_generation_number|. The
+// UBE generation number is a non-zero, strictly-monotonic counter with the
+// following property: if queried in an address space and then subsequently
+// queried, after an UBE occurred, a greater value will be observed.
+//
+// This function should be used to protect volatile memory. First cache a UBE
+// generation number associating it to the volatile memory at
+// creation/initialization time. Before using the volatile memory check whether
+// the generation number has changed. Note, however, that detection methods rely
+// on technology that is unique to a platform. Hence, support for UBE detection
+// also depends on the platform AWS-LC is executed on.
+//
+// The parameter |current_generation_number| must be synchronized by the caller.
+//
+// Returns 1 on success and 0 if not supported. 0 means that UBE detection is
+// not supported and any volatile state must randomize before usage.
+// In case of an error or if UBE detection is unavailable, all subsequent
+// entries will immediately return.
+OPENSSL_EXPORT int CRYPTO_get_ube_generation_number(uint64_t *current_generation_number);
+
+// TODO
+// Temporary overrides. Replace with something better. Used atm to test
+// implementation during development.
+OPENSSL_EXPORT void set_fork_generation_number_FOR_TESTING(uint64_t fork_gn);
+OPENSSL_EXPORT void set_snapsafe_generation_number_FOR_TESTING(uint32_t snapsafe_gn);
+
+#if defined(__cplusplus)
+}  // extern C
+#endif
+
+#endif  // OPENSSL_HEADER_CRYPTO_UBE_INTERNAL_H
