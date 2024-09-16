9d21f38b8ea04a70a753626708e3de21d8d0b733
Jake Massimo <jakemas@amazon.com>
2024-09-16 07:49:54 -0700

    Migrated ML-KEM SHA3/SHAKE usage to fipsmodule (#1851)
    Following the inclusion of SHAKE as an extensible-output-function (XOF) in #1839, we are now able to fully support ML-KEM with SHA3/SHAKE usage within crypto/fipsmodule. As such, all references to the internal implementation of SHA3 (within crypto/kyber/pqcrystals_kyber_ref_common/fips202.{h|c}) have been removed.

--- crypto/fipsmodule/ml_kem/ml_kem_ref/symmetric.h
+++ crypto/fipsmodule/ml_kem/ml_kem_ref/symmetric.h
@@ -5,12 +5,10 @@
 #include <stdint.h>
 #include "params.h"
 
-#include "../../../kyber/pqcrystals_kyber_ref_common/fips202.h"
-
-typedef keccak_state xof_state;
+#include "../../sha/internal.h"
 
 #define kyber_shake128_absorb KYBER_NAMESPACE(kyber_shake128_absorb)
-void kyber_shake128_absorb(keccak_state *s,
+void kyber_shake128_absorb(KECCAK1600_CTX *ctx,
                            const uint8_t seed[KYBER_SYMBYTES],
                            uint8_t x,
                            uint8_t y);
@@ -21,12 +19,13 @@ void kyber_shake256_prf(uint8_t *out, size_t outlen, const uint8_t key[KYBER_SYM
 #define kyber_shake256_rkprf KYBER_NAMESPACE(kyber_shake256_rkprf)
 void kyber_shake256_rkprf(ml_kem_params *params, uint8_t out[KYBER_SSBYTES], const uint8_t key[KYBER_SYMBYTES], const uint8_t *input);
 
-#define XOF_BLOCKBYTES SHAKE128_RATE
+#define kyber_shake128_squeeze KYBER_NAMESPACE(kyber_shake128_squeeze)
+void kyber_shake128_squeeze(KECCAK1600_CTX *ctx, uint8_t *out, int nblocks);
 
-#define hash_h(OUT, IN, INBYTES) sha3_256(OUT, IN, INBYTES)
-#define hash_g(OUT, IN, INBYTES) sha3_512(OUT, IN, INBYTES)
+#define hash_h(OUT, IN, INBYTES) SHA3_256(IN, INBYTES, OUT)
+#define hash_g(OUT, IN, INBYTES) SHA3_512(IN, INBYTES, OUT)
 #define xof_absorb(STATE, SEED, X, Y) kyber_shake128_absorb(STATE, SEED, X, Y)
-#define xof_squeezeblocks(OUT, OUTBLOCKS, STATE) shake128_squeezeblocks(OUT, OUTBLOCKS, STATE)
+#define xof_squeezeblocks(OUT, OUTBLOCKS, STATE) kyber_shake128_squeeze(STATE, OUT, OUTBLOCKS)
 #define prf(OUT, OUTBYTES, KEY, NONCE) kyber_shake256_prf(OUT, OUTBYTES, KEY, NONCE)
 #define rkprf(PARAMS, OUT, KEY, INPUT) kyber_shake256_rkprf(PARAMS, OUT, KEY, INPUT)
 
