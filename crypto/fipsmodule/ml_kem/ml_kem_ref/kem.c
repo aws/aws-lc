2835116571e8607861ba8dd0be2676f2c5cfaaab
dkostic <25055813+dkostic@users.noreply.github.com>
2024-09-23 14:39:25 -0700

    ML-KEM encapsulation key modulus check (#1868)
    ML-KEM encapsulation key modulus check as specified in Section 7.2 
    of FIPS 203: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.203.pdf

--- crypto/fipsmodule/ml_kem/ml_kem_ref/kem.c
+++ crypto/fipsmodule/ml_kem/ml_kem_ref/kem.c
@@ -5,6 +5,7 @@
 #include "kem.h"
 #include "indcpa.h"
 #include "verify.h"
+#include "reduce.h"
 #include "symmetric.h"
 #include "openssl/rand.h"
 
@@ -57,6 +58,102 @@ int crypto_kem_keypair(ml_kem_params *params,
   RAND_bytes(coins, 2*KYBER_SYMBYTES);
   crypto_kem_keypair_derand(params, pk, sk, coins);
   return 0;
+}
+
+// FIPS 203. Algorithm 3 BitsToBytes
+// Converts a bit array (of a length that is a multiple of eight)
+// into an array of bytes.
+static void bits_to_bytes(uint8_t *bytes, size_t num_bytes,
+                          const uint8_t *bits, size_t num_bits) {
+  assert(num_bits == num_bytes * 8);
+
+  for (size_t i = 0; i < num_bytes; i++) {
+    uint8_t byte = 0;
+    for (size_t j = 0; j < 8; j++) {
+      byte |= (bits[i * 8 + j] << j);
+    }
+    bytes[i] = byte;
+  }
+}
+
+// FIPS 203. Algorithm 4 BytesToBits
+// Performs the inverse of BitsToBytes, converting a byte array into a bit array.
+static void bytes_to_bits(uint8_t *bits, size_t num_bits,
+                          const uint8_t *bytes, size_t num_bytes) {
+  assert(num_bits == num_bytes * 8);
+
+  for (size_t i = 0; i < num_bytes; i++) {
+    uint8_t byte = bytes[i];
+    for (size_t j = 0; j < 8; j++) {
+      bits[i * 8 + j] = (byte >> j) & 1;
+    }
+  }
+}
+
+#define BYTE_ENCODE_12_IN_SIZE  (256)
+#define BYTE_ENCODE_12_OUT_SIZE (32 * 12)
+#define BYTE_ENCODE_12_NUM_BITS (256 * 12)
+
+// FIPS 203. Algorithm 5 ByteEncode_12
+// Encodes an array of 256 12-bit integers into a byte array.
+static void byte_encode_12(uint8_t out[BYTE_ENCODE_12_OUT_SIZE],
+                           const int16_t in[BYTE_ENCODE_12_IN_SIZE]) {
+  uint8_t bits[BYTE_ENCODE_12_NUM_BITS] = {0};
+  for (size_t i = 0; i < BYTE_ENCODE_12_IN_SIZE; i++) {
+    int16_t a = in[i];
+    for (size_t j = 0; j < 12; j++) {
+      bits[i * 12 + j] = a & 1;
+      a = a >> 1;
+    }
+  }
+  bits_to_bytes(out, BYTE_ENCODE_12_OUT_SIZE, bits, BYTE_ENCODE_12_NUM_BITS);
+}
+
+// Converts a centered representative |in| which is an integer in
+// {-(q-1)/2, ..., (q-1)/2}, to a positive representative in {0, ..., q-1}.
+// It implements in constant-time the following operation:
+//   return (in < 0) ? in + KYBER_Q : in;
+static int16_t centered_to_positive_representative(int16_t in) {
+  // mask = (in < 0) ? b11..11 : b00..00;
+  crypto_word_t mask = constant_time_is_zero_w(in >> 15);
+  int16_t in_fixed = in + KYBER_Q;
+  return constant_time_select_int(mask, in, in_fixed);
+}
+
+#define BYTE_DECODE_12_OUT_SIZE (256)
+#define BYTE_DECODE_12_IN_SIZE  (32 * 12)
+#define BYTE_DECODE_12_NUM_BITS (256 * 12)
+
+// FIPS 203. Algorithm 5 ByteDecode_12
+// Decodes a byte array into an array of 256 12-bit integers.
+static void byte_decode_12(int16_t out[BYTE_DECODE_12_OUT_SIZE],
+                           const uint8_t in[BYTE_DECODE_12_IN_SIZE]) {
+  uint8_t bits[BYTE_DECODE_12_NUM_BITS] = {0};
+  bytes_to_bits(bits, BYTE_DECODE_12_NUM_BITS, in, BYTE_DECODE_12_IN_SIZE);
+  for (size_t i = 0; i < BYTE_DECODE_12_OUT_SIZE; i++) {
+    int16_t val = 0;
+    for (size_t j = 0; j < 12; j++) {
+      val |= bits[i * 12 + j] << j;
+    }
+    out[i] = centered_to_positive_representative(barrett_reduce(val));
+  }
+}
+
+#define ENCAPS_KEY_ENCODED_MAX_SIZE (BYTE_ENCODE_12_OUT_SIZE * KYBER_K_MAX)
+#define ENCAPS_KEY_DECODED_MAX_SIZE (BYTE_DECODE_12_OUT_SIZE * KYBER_K_MAX)
+
+// FIPS 203. Section 7.2 Encapsulation key check.
+static int encapsulation_key_modulus_check(ml_kem_params *params, const uint8_t *ek) {
+
+  int16_t ek_decoded[ENCAPS_KEY_DECODED_MAX_SIZE];
+  uint8_t ek_recoded[ENCAPS_KEY_ENCODED_MAX_SIZE];
+
+  for (size_t i = 0; i < params->k; i++) {
+    byte_decode_12(&ek_decoded[i * BYTE_DECODE_12_OUT_SIZE], &ek[i * BYTE_DECODE_12_IN_SIZE]);
+    byte_encode_12(&ek_recoded[i * BYTE_ENCODE_12_OUT_SIZE], &ek_decoded[i * BYTE_ENCODE_12_IN_SIZE]);
+  }
+
+  return verify(ek_recoded, ek, params->k * BYTE_ENCODE_12_OUT_SIZE);
 }
 
 /*************************************************
@@ -112,13 +209,17 @@ int crypto_kem_enc_derand(ml_kem_params *params,
 *              - const uint8_t *pk: pointer to input public key
 *                (an already allocated array of KYBER_PUBLICKEYBYTES bytes)
 *
-* Returns 0 (success)
+* Returns 0 (success), or 1 when the encapsulation key check fails.
 **************************************************/
 int crypto_kem_enc(ml_kem_params *params,
                    uint8_t *ct,
                    uint8_t *ss,
                    const uint8_t *pk)
 {
+  if (encapsulation_key_modulus_check(params, pk) != 0) {
+    return 1;
+  }
+
   uint8_t coins[KYBER_SYMBYTES];
   RAND_bytes(coins, KYBER_SYMBYTES);
   crypto_kem_enc_derand(params, ct, ss, pk, coins);
