0fdf13d3964173c3c34b3a85cfc86fcc72c341b8
samuel40791765 <sachiang@amazon.com>
2024-09-27 22:42:57 +0000

    align bignum_to_string bit limitation with OpenSSL

--- crypto/x509/v3_utl.c
+++ crypto/x509/v3_utl.c
@@ -172,7 +172,7 @@ static char *bignum_to_string(const BIGNUM *bn) {
   // Display large numbers in hex and small numbers in decimal. Converting to
   // decimal takes quadratic time and is no more useful than hex for large
   // numbers.
-  if (BN_num_bits(bn) < 32) {
+  if (BN_num_bits(bn) < 128) {
     return BN_bn2dec(bn);
   }
 
