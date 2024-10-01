cd2cf5b6f463ef2750814328e92442ee31180a58
Andrew Hopkins <andhop@amazon.com>
2024-10-01 16:34:47 -0700

    Use older commit before P-384 changes

--- tests/ci/run_formal_verification.sh
+++ tests/ci/run_formal_verification.sh
@@ -10,7 +10,7 @@ rm -rf aws-lc-verification-build
 git clone https://github.com/awslabs/aws-lc-verification.git aws-lc-verification-build
 cd aws-lc-verification-build
 # Checkout a version of the formal verification repo that works with this version of AWS-LC
-git checkout 076973cc1ae011beab8df6af4f02464158374b24
+git checkout 7d0a46ac0841d1d9f1c078153d38409af056c482
 git submodule update --init
 # aws-lc-verification has aws-lc as one submodule under 'src' dir.
 # Below is to copy code of **target** aws-lc to 'src' dir.
