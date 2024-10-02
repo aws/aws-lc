88526d99b801346557933b82e5136790a601ec29
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2024-10-02 16:32:35 -0700

    Try a less intrusive method

--- third_party/s2n-bignum/include/_internal_s2n_bignum.h
+++ third_party/s2n-bignum/include/_internal_s2n_bignum.h
@@ -15,7 +15,3 @@
 #else
 #   define S2N_BN_SYM_PRIVACY_DIRECTIVE(name)  /* NO-OP: S2N_BN_SYM_PRIVACY_DIRECTIVE */
 #endif
-
-#ifdef AWSLC_PRIVATE_BUILD
-    #include <openssl/boringssl_prefix_symbols.h>
-#endif
