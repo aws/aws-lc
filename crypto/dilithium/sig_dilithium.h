bfffd27baff55ce003bb017a8e95b2eb079e8216
Jake Massimo <jakemas@amazon.com>
2024-10-03 10:26:25 -0700

    added documentation on ctx field

--- crypto/dilithium/sig_dilithium.h
+++ crypto/dilithium/sig_dilithium.h
@@ -9,8 +9,6 @@
 #include <openssl/base.h>
 #include <openssl/evp.h>
 
-// The values below are taken from the |api.h| file in the
-// |crypto/dilithium/pqcrystals_dilithium_ref_common| directory.
 #define DILITHIUM3_PUBLIC_KEY_BYTES 1952
 #define DILITHIUM3_PRIVATE_KEY_BYTES 4032
 #define DILITHIUM3_SIGNATURE_BYTES 3309
@@ -24,8 +22,9 @@ int DILITHIUM3_keypair(uint8_t *public_key,
 // DILITHIUM3_sign generates a Dilithium3 signature. Where |sig| is a pointer to
 // output signature, |sig_len| is a pointer to output length of signature,
 // |message| is a pointer to message to be signed, |message_len| is the length
-// of the message and |secret_key| is a pointer to bit-packed secret key.
-// It returns 0 upon success.
+// of the message, |ctx| is a pointer to the context string, |ctx_len| is the
+// length of the context string (max length 255 bytes), and |secret_key| is a
+// pointer to bit-packed secret key. It returns 0 upon success.
 int DILITHIUM3_sign(uint8_t *sig, size_t *sig_len,
                     const uint8_t *message,
                     size_t message_len,
@@ -35,9 +34,10 @@ int DILITHIUM3_sign(uint8_t *sig, size_t *sig_len,
 
 // DILITHIUM3_verify generates a Dilithium3 signature. Where |sig| is a pointer
 // to input signature, |sig_len| is the length of the signature, |message| is
-// a pointer to message, |message_len| is the length of the message and
-// |public_key| is a pointer to bit-packed public key. Returns 0 if signature
-// could be verified successfully and -1 otherwise.
+// a pointer to message, |message_len| is the length of the message, |ctx| is a
+// pointer to the context string, |ctx_len| is the length of the context string
+// (max length 255 bytes) and |public_key| is a pointer to bit-packed public key.
+// Returns 0 if signature could be verified successfully and -1 otherwise.
 int DILITHIUM3_verify(const uint8_t *message,
                       size_t message_len,
                       const uint8_t *sig,
