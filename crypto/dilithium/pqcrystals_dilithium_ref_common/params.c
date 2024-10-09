c37f75baaf20d6724cb877b2360c7a5e104c8bb9
Jake Massimo <jakemas@amazon.com>
2024-10-09 10:55:42 -0700

    refact params.c and add gen-src

--- crypto/dilithium/pqcrystals_dilithium_ref_common/params.c
+++ crypto/dilithium/pqcrystals_dilithium_ref_common/params.c
@@ -5,65 +5,70 @@
 
 static void ml_dsa_params_init(ml_dsa_params *params, size_t k) {
   assert((k == 2) || (k == 3) || (k == 5));
-  size_t K;
-  size_t L;
-  size_t tau;
-  size_t beta;
-  size_t omega;
-  size_t c_tilde_bytes;
 
   if (k == 2) {
-    K = 4;
-    L = 4;
-    tau = 39;
-    beta = 78;
-    omega = 80;
-    c_tilde_bytes = 32;
+    // Parameters for ML-DSA-44 from Table 1. FIPS-204: ML-DSA Parameter Sets.
+    params->k = 4;
+    params->l = 4;
+    params->tau = 39;
+    params->beta = 78;
+    params->omega = 80;
+    params->c_tilde_bytes = 32;
+    params->gamma1 = (1 << 17);
+    params->gamma2 = (Q-1)/88;
+    params->eta = 2;
+    params->poly_z_packed_bytes = 576;
+    params->poly_w1_packed_bytes = 192;
+    params->poly_eta_packed_bytes = 96;
+    params->poly_vech_packed_bytes = (params->omega + params->k);
+
+    // Sizes for ML-DSA-44 keys and signatures from Table 2. FIPS-204.
+    params->public_key_bytes = (SEEDBYTES + params->k * POLYT1_PACKEDBYTES);
+    params->secret_key_bytes = (2 * SEEDBYTES + TRBYTES + params->l * params->poly_eta_packed_bytes + params->k * params->poly_eta_packed_bytes + params->k * POLYT0_PACKEDBYTES);
+    params->bytes = (params->c_tilde_bytes + params->l *  params->poly_z_packed_bytes  + params->poly_vech_packed_bytes);
   }
   else if (k == 3) {
-    K = 6;
-    L = 5;
-    tau = 49;
-    beta = 196;
-    omega = 55;
-    c_tilde_bytes = 48;
-  }
-  else { //k == 5
-    K = 8;
-    L = 7;
-    tau = 60;
-    beta = 120;
-    omega = 75;
-    c_tilde_bytes = 64;
-  }
+    // Parameters for ML-DSA-65 from Table 1. FIPS-204: ML-DSA Parameter Sets.
+    params->k = 6;
+    params->l = 5;
+    params->tau = 49;
+    params->beta = 196;
+    params->omega = 55;
+    params->c_tilde_bytes = 48;
+    params->gamma1 = (1 << 19);
+    params->gamma2 = (Q-1)/32;
+    params->eta = 4;
+    params->poly_z_packed_bytes = 640;
+    params->poly_w1_packed_bytes = 128;
+    params->poly_eta_packed_bytes = 128;
+    params->poly_vech_packed_bytes = (params->omega + params->k);
 
-  size_t eta = (k == 3) ? 4 : 2;
-  size_t gamma1 = (k == 2) ? (1 << 17) : (1 << 19);
-  int32_t gamma2 = (k == 2) ? ((Q-1)/88) : ((Q-1)/32);
-  size_t poly_vech_packed_bytes = (omega + K);
-  size_t poly_z_packed_bytes = (k == 2) ? 576 : 640;
-  size_t poly_w1_packed_bytes = (k == 2) ? 192 : 128;
-  size_t poly_eta_packed_bytes = (k == 3) ? 128 : 96;
-  size_t public_key_bytes = (SEEDBYTES + K * POLYT1_PACKEDBYTES);
-  size_t secret_key_bytes = (2 * SEEDBYTES + TRBYTES + L * poly_eta_packed_bytes + K * poly_eta_packed_bytes + K * POLYT0_PACKEDBYTES);
-  size_t bytes = (c_tilde_bytes + L * poly_z_packed_bytes + poly_vech_packed_bytes);
+    // Sizes for ML-DSA-65 keys and signatures from Table 2. FIPS-204.
+    params->public_key_bytes = (SEEDBYTES + params->k * POLYT1_PACKEDBYTES);
+    params->secret_key_bytes = (2 * SEEDBYTES + TRBYTES + params->l * params->poly_eta_packed_bytes + params->k * params->poly_eta_packed_bytes + params->k * POLYT0_PACKEDBYTES);
+    params->bytes = (params->c_tilde_bytes + params->l *  params->poly_z_packed_bytes  + params->poly_vech_packed_bytes);
+  }
+  else {
+    // Parameters for ML-DSA-87 from Table 1. FIPS-204: ML-DSA Parameter Sets.
+    params->k = 8;
+    params->l = 47;
+    params->tau = 60;
+    params->beta = 120;
+    params->omega = 75;
+    params->c_tilde_bytes = 64;
+    params->gamma1 = (1 << 19);
+    params->gamma2 = (Q-1)/32;
+    params->eta = 2;
+    params->poly_z_packed_bytes = 640;
+    params->poly_w1_packed_bytes = 128;
+    params->poly_eta_packed_bytes = 96;
+    params->poly_vech_packed_bytes = (params->omega + params->k);
 
-  params->k = K;
-  params->l = L;
-  params->eta = eta;
-  params->tau = tau;
-  params->beta = beta;
-  params->gamma1 = gamma1;
-  params->gamma2 = gamma2;
-  params->omega = omega;
-  params->c_tilde_bytes = c_tilde_bytes;
-  params->poly_vech_packed_bytes = poly_vech_packed_bytes;
-  params->poly_z_packed_bytes = poly_z_packed_bytes;
-  params->poly_w1_packed_bytes = poly_w1_packed_bytes;
-  params->poly_eta_packed_bytes = poly_eta_packed_bytes;
-  params->public_key_bytes = public_key_bytes;
-  params->secret_key_bytes = secret_key_bytes;
-  params->bytes = bytes;
+    // Sizes for ML-DSA-87 keys and signatures from Table 2. FIPS-204.
+    params->public_key_bytes = (SEEDBYTES + params->k * POLYT1_PACKEDBYTES);
+    params->secret_key_bytes = (2 * SEEDBYTES + TRBYTES + params->l * params->poly_eta_packed_bytes + params->k * params->poly_eta_packed_bytes + params->k * POLYT0_PACKEDBYTES);
+    params->bytes = (params->c_tilde_bytes + params->l *  params->poly_z_packed_bytes  + params->poly_vech_packed_bytes);
+  }
 }
 
 void ml_dsa_44_params_init(ml_dsa_params *params) {
