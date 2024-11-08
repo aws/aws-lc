fa1f03627a941a83e9592c806e3ec7a4846e1a28
Jake Massimo <jakemas@amazon.com>
2024-11-08 10:51:25 -0800

    added internal functions for ml-dsa

--- crypto/dilithium/pqcrystals_dilithium_ref_common/sign.h
+++ crypto/dilithium/pqcrystals_dilithium_ref_common/sign.h
@@ -9,11 +9,26 @@
 
 int crypto_sign_keypair(ml_dsa_params *params, uint8_t *pk, uint8_t *sk);
 
+int crypto_sign_keypair_internal(ml_dsa_params *params,
+                                 uint8_t *pk,
+                                 uint8_t *sk,
+                                 uint8_t *seedbuf);
+
 int crypto_sign_signature(ml_dsa_params *params,
                           uint8_t *sig, size_t *siglen,
                           const uint8_t *m, size_t mlen,
                           const uint8_t *ctx, size_t ctxlen,
                           const uint8_t *sk);
+
+int crypto_sign_signature_internal(ml_dsa_params *params,
+                                   uint8_t *sig,
+                                   size_t *siglen,
+                                   const uint8_t *m,
+                                   size_t mlen,
+                                   const uint8_t *pre,
+                                   size_t prelen,
+                                   const uint8_t rnd[RNDBYTES],
+                                   const uint8_t *sk);
 
 int crypto_sign(ml_dsa_params *params,
                 uint8_t *sm, size_t *smlen,
@@ -26,6 +41,15 @@ int crypto_sign_verify(ml_dsa_params *params,
                        const uint8_t *m, size_t mlen,
                        const uint8_t *ctx, size_t ctxlen,
                        const uint8_t *pk);
+
+int crypto_sign_verify_internal(ml_dsa_params *params,
+                                const uint8_t *sig,
+                                size_t siglen,
+                                const uint8_t *m,
+                                size_t mlen,
+                                const uint8_t *pre,
+                                size_t prelen,
+                                const uint8_t *pk);
 
 int crypto_sign_open(ml_dsa_params *params,
                      uint8_t *m, size_t *mlen,
