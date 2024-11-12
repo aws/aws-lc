8f2d5bad80feed81963c3688a8f388371d6375c7
Juneyoung Lee <lebjuney@amazon.com>
2024-11-12 03:17:25 +0000

    Use native OCaml compiler to build & run proofs
    This patch compiles proofs using native OCaml compiler, and updates
    `make proofs` to run the newly created `*.native` executable files.
    
    The new `tools/build-proof.sh` script builds a specific `.ml` proof file.
    It inlines all loads/needs in the `.ml` file using HOL Light's
    `inline_load.ml`.
    Then, it collects all specifications in the original `.ml` file using already existing
    `tools/collect-specs.sh`, and adds a print statement at the end of the
    file, to make its standard output pass the proof checker script.
    It also adds timer and prints the running time of the proof.
    Then it is compiled with `ocamlopt.byte`.
    
    Building all proofs can be done with `make build_proofs`. This uses a
    lot of memory, so compared to the previous version `-j` number given to
    `make` must be reduced.
    
    This also makes the prerequisite lists tidier using the '*.native'
    items. Each '.native' file is considered as depending on the '.o' file.

--- tools/run-proof.sh
+++ tools/run-proof.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
-if [ "$#" -ne 4 ]; then
-  echo "../tools/run-proof.sh <dir (arm/x86)>"
-  echo "      <asm filename without .S (e.g., bignum_copy)>"
-  echo "      <HOL Light command> <output log path>"
-  echo "This script runs HOL Light proof at '<dir>/proofs/<asm filename>.ml', and".
-  echo "inspects the log file to check whether all proofs are done successfully."
+if [ "$#" -ne 2 ]; then
+  echo "../tools/run-proof.sh <.native file to run> <output log path>"
+  echo "This script runs precompiled HOL Light proof '<.native file>', prints the output"
+  echo "at <output log path>, and inspects the log file to check whether all proofs "
+  echo "are done successfully."
   exit 1
 fi
 
 # Return the exit code if any statement fails
 set -e
 
+s2n_bignum_arch=$(basename "$(pwd)")
+
 cd ..
 
-s2n_bignum_arch=$1
-asm_filename=$2
-hol_light_cmd=$3
-output_path=${s2n_bignum_arch}/$4
+native_path=${s2n_bignum_arch}/$1
+output_path=${s2n_bignum_arch}/$2
 
-(echo 'Topdirs.dir_directory "+unix";;'; \
- echo 'Topdirs.dir_load Format.std_formatter "unix.cma";;'; \
- echo 'let start_time = Unix.time();;'; \
- echo "loadt \"${s2n_bignum_arch}/proofs/${asm_filename}.ml\";;"; \
- echo "check_axioms ();;"; \
- echo 'let end_time = Unix.time();;'; \
- echo 'Printf.printf "Running time: %f sec, Start unixtime: %f, End unixtime: %f\n" (end_time -. start_time) start_time end_time;;') | eval "$hol_light_cmd" 2>&1 > "$output_path"
+"$native_path" 2>&1 > "$output_path"
 
 # Revert the exit code option since 'grep' may return non-zero.
 set +e
 
 grep -r -i "error\|exception" --include "$output_path"
 if [ $? -eq 0 ]; then
-  echo "${s2n_bignum_arch}/proofs/${asm_filename}.ml had error(s)"
+  echo "${s2n_bignum_arch}/${native_path} had error(s)"
   exit 1
 fi
