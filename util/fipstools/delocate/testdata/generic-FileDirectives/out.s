86bee875c50d184fa4764b1b27a5834424ebe489
dkostic <dkostic@amazon.com>
2024-12-09 15:27:30 -0800

    move text_hash in delocate.go

--- util/fipstools/delocate/testdata/generic-FileDirectives/out.s
+++ util/fipstools/delocate/testdata/generic-FileDirectives/out.s
@@ -1,22 +1,6 @@
 .text
 .file 1002 "inserted_by_delocate.c" md5 0x00000000000000000000000000000000
 .loc 1002 1 0
-BORINGSSL_bcm_text_start:
-.file 10 "some/path/file.c" "file.c"
-.file 1000 "some/path/file2.c" "file2.c"
-.file 1001 "some/path/file_with_md5.c" "other_name.c" md5 0x5eba7844df6449a7f2fff1556fe7ba8d239f8e2f
-
-# An instruction is needed to satisfy the architecture auto-detection.
-        movq %rax, %rbx
-.text
-.loc 1002 2 0
-BORINGSSL_bcm_text_end:
-.type OPENSSL_ia32cap_get, @function
-.globl OPENSSL_ia32cap_get
-.LOPENSSL_ia32cap_get_local_target:
-OPENSSL_ia32cap_get:
-	leaq OPENSSL_ia32cap_P(%rip), %rax
-	ret
 .type BORINGSSL_bcm_text_hash, @object
 .size BORINGSSL_bcm_text_hash, 32
 BORINGSSL_bcm_text_hash:
@@ -52,3 +36,19 @@ BORINGSSL_bcm_text_hash:
 .byte 0xff
 .byte 0x31
 .byte 0x80
+BORINGSSL_bcm_text_start:
+.file 10 "some/path/file.c" "file.c"
+.file 1000 "some/path/file2.c" "file2.c"
+.file 1001 "some/path/file_with_md5.c" "other_name.c" md5 0x5eba7844df6449a7f2fff1556fe7ba8d239f8e2f
+
+# An instruction is needed to satisfy the architecture auto-detection.
+        movq %rax, %rbx
+.text
+.loc 1002 2 0
+BORINGSSL_bcm_text_end:
+.type OPENSSL_ia32cap_get, @function
+.globl OPENSSL_ia32cap_get
+.LOPENSSL_ia32cap_get_local_target:
+OPENSSL_ia32cap_get:
+	leaq OPENSSL_ia32cap_P(%rip), %rax
+	ret
--- util/fipstools/delocate/testdata/ppc64le-LoadToR0/out.s
+++ util/fipstools/delocate/testdata/ppc64le-LoadToR0/out.s
@@ -1,40 +1,6 @@
 .text
 .file 1 "inserted_by_delocate.c"
 .loc 1 1 0
-BORINGSSL_bcm_text_start:
-	.text
-.Lfoo_local_target:
-foo:
-# WAS addis 22,2,bar@toc@ha
-# WAS ld 0,bar@toc@l(22)
-	addi 1, 1, -288
-	mflr 0
-	std 0, -8(1)
-	std 3, -16(1)
-	bl .Lbcm_loadtoc_bar
-	std 3, -24(1)
-	ld 3, -8(1)
-	mtlr 3
-	ld 0, -24(1)
-	ld 3, -16(1)
-	addi 1, 1, 288
-	addi 1, 1, -288
-	std 3, -8(1)
-	mr 3, 0
-	ld 0, 0(3)
-	ld 3, -8(1)
-	addi 1, 1, 288
-.text
-.loc 1 2 0
-BORINGSSL_bcm_text_end:
-.type bcm_loadtoc_bar, @function
-bcm_loadtoc_bar:
-.Lbcm_loadtoc_bar:
-	addis 3, 2, bar@toc@ha
-	addi 3, 3, bar@toc@l
-	blr
-.LBORINGSSL_external_toc:
-.quad .TOC.-.LBORINGSSL_external_toc
 .type BORINGSSL_bcm_text_hash, @object
 .size BORINGSSL_bcm_text_hash, 32
 BORINGSSL_bcm_text_hash:
@@ -70,3 +36,37 @@ BORINGSSL_bcm_text_hash:
 .byte 0xff
 .byte 0x31
 .byte 0x80
+BORINGSSL_bcm_text_start:
+	.text
+.Lfoo_local_target:
+foo:
+# WAS addis 22,2,bar@toc@ha
+# WAS ld 0,bar@toc@l(22)
+	addi 1, 1, -288
+	mflr 0
+	std 0, -8(1)
+	std 3, -16(1)
+	bl .Lbcm_loadtoc_bar
+	std 3, -24(1)
+	ld 3, -8(1)
+	mtlr 3
+	ld 0, -24(1)
+	ld 3, -16(1)
+	addi 1, 1, 288
+	addi 1, 1, -288
+	std 3, -8(1)
+	mr 3, 0
+	ld 0, 0(3)
+	ld 3, -8(1)
+	addi 1, 1, 288
+.text
+.loc 1 2 0
+BORINGSSL_bcm_text_end:
+.type bcm_loadtoc_bar, @function
+bcm_loadtoc_bar:
+.Lbcm_loadtoc_bar:
+	addis 3, 2, bar@toc@ha
+	addi 3, 3, bar@toc@l
+	blr
+.LBORINGSSL_external_toc:
+.quad .TOC.-.LBORINGSSL_external_toc
