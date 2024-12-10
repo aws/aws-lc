fd3b1d3d4c7fb034e37cc679df7dea9c377e33e0
Justin Smith <justsmth@amazon.com>
2024-12-10 17:27:39 -0500

    Prepare release 1.41.1

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -122,7 +122,7 @@ extern "C" {
 // ServiceIndicatorTest.AWSLCVersionString
 // Note: there are two versions of this test. Only one test is compiled
 // depending on FIPS mode.
-#define AWSLC_VERSION_NUMBER_STRING "1.40.0"
+#define AWSLC_VERSION_NUMBER_STRING "1.41.1"
 
 #if defined(BORINGSSL_SHARED_LIBRARY)
 
