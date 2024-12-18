b15fa33e8af89c75ef5dfc02069fe588423f3071
Jake Massimo <jakemas@amazon.com>
2024-12-18 11:35:10 -0800

    missing comma

--- util/fipstools/acvp/modulewrapper/modulewrapper.cc
+++ util/fipstools/acvp/modulewrapper/modulewrapper.cc
@@ -1367,7 +1367,7 @@ static bool GetConfig(const Span<const uint8_t> args[],
         "parameterSets": ["ML-DSA-44", "ML-DSA-65", "ML-DSA-87"],
         "deterministic": [false],
         "messageLength": [{"min": 8, "max": 65536", "increment": 8}]
-      }
+      },
       {
         "algorithm": "ML-DSA",
         "mode": "sigVer",
