ce5cd9bb8c60f022dd15390989b5d79e3bca3d1b
Jake Massimo <jakemas@amazon.com>
2025-02-24 10:25:04 -0800

    CR fix

--- crypto/evp_extra/p_pqdsa_asn1.c
+++ crypto/evp_extra/p_pqdsa_asn1.c
@@ -142,7 +142,7 @@ static int pqdsa_priv_encode_seed(CBB *out, const EVP_PKEY *pkey) {
   // See https://datatracker.ietf.org/doc/draft-ietf-lamps-dilithium-certificates/ section 6.
   CBB pkcs8, algorithm, oid, seed;
   if (!CBB_add_asn1(out, &pkcs8, CBS_ASN1_SEQUENCE) ||
-      !CBB_add_asn1_uint64(&pkcs8, 0 /* version */) ||
+      !CBB_add_asn1_uint64(&pkcs8, PKCS8_VERSION_ONE /* version */) ||
       !CBB_add_asn1(&pkcs8, &algorithm, CBS_ASN1_SEQUENCE) ||
       !CBB_add_asn1(&algorithm, &oid, CBS_ASN1_OBJECT) ||
       !CBB_add_bytes(&oid, pqdsa->oid, pqdsa->oid_len) ||
