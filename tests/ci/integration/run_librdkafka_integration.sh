70c60294ec162bccae83ddef67d8e922482d6d8c
Shubham Mittal <smittals@amazon.com>
2025-02-26 14:01:53 -0800

    kafka build script

---
+++ tests/ci/integration/run_librdkafka_integration.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR ISC
+
+set -ex
+
+source tests/ci/common_posix_setup.sh
+
+# Set up environment.
+
+# SYS_ROOT
+#  - SRC_ROOT(aws-lc)
+#    - SCRATCH_FOLDER
+#      - KAFKA_SRC_FOLDER
+#      - AWS_LC_BUILD_FOLDER
+#      - AWS_LC_INSTALL_FOLDER
+
+# Assumes script is executed from the root of aws-lc directory
+SCRATCH_FOLDER="${SRC_ROOT}/KAFKA_BUILD_ROOT"
+KAFKA_SRC_FOLDER="${SCRATCH_FOLDER}/librdkafka"
+KAFKA_BUILD_PREFIX="${KAFKA_SRC_FOLDER}/build/install"
+
+AWS_LC_BUILD_FOLDER="${SCRATCH_FOLDER}/aws-lc-build"
+AWS_LC_INSTALL_FOLDER="${SCRATCH_FOLDER}/aws-lc-install"
+
+#mkdir -p ${SCRATCH_FOLDER}
+rm -rf "${SCRATCH_FOLDER:?}"/*
+#rm -rf ${KAFKA_SRC_FOLDER}
+cd ${SCRATCH_FOLDER}
+
+function kafka_build() {
+  export CFLAGS="-I${AWS_LC_INSTALL_FOLDER}/include ${CFLAGS}"
+  export CXXFLAGS="-I${AWS_LC_INSTALL_FOLDER}/include ${CXXFLAGS}"
+  export LDFLAGS="-L${AWS_LC_INSTALL_FOLDER}/lib ${LDFLAGS}"
+
+  ./configure --prefix="$KAFKA_BUILD_PREFIX"
+  make -j install
+
+#  local kafka_executable="${KAFKA_SRC_FOLDER}/build/install/"
+#  ldd ${kafka_executable} \
+#    | grep "${AWS_LC_INSTALL_FOLDER}/lib/libcrypto.so" || exit 1
+}
+
+function kafka_run_tests() {
+  sudo make check
+}
+
+git clone https://github.com/confluentinc/librdkafka.git ${KAFKA_SRC_FOLDER}
+mkdir -p ${AWS_LC_BUILD_FOLDER} ${AWS_LC_INSTALL_FOLDER}
+ls
+
+aws_lc_build "$SRC_ROOT" "$AWS_LC_BUILD_FOLDER" "$AWS_LC_INSTALL_FOLDER" -DBUILD_TESTING=OFF -DBUILD_TOOL=OFF -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=1
+
+# Build openvpn from source.
+pushd ${KAFKA_SRC_FOLDER}
+kafka_build
+kafka_run_tests
