2ab6cdb7f2e244a56d67b9c9b8fbd36a780de212
Shubham Mittal <smittals@amazon.com>
2025-02-27 14:22:50 -0800

    test out kafka tests with cluster

--- tests/ci/integration/run_librdkafka_integration.sh
+++ tests/ci/integration/run_librdkafka_integration.sh
@@ -23,23 +23,35 @@ KAFKA_BUILD_PREFIX="${KAFKA_SRC_FOLDER}/build/install"
 AWS_LC_BUILD_FOLDER="${SCRATCH_FOLDER}/aws-lc-build"
 AWS_LC_INSTALL_FOLDER="${SCRATCH_FOLDER}/aws-lc-install"
 
-#mkdir -p ${SCRATCH_FOLDER}
+mkdir -p ${SCRATCH_FOLDER}
 rm -rf "${SCRATCH_FOLDER:?}"/*
-#rm -rf ${KAFKA_SRC_FOLDER}
 cd ${SCRATCH_FOLDER}
 
 function kafka_build() {
   export CFLAGS="-I${AWS_LC_INSTALL_FOLDER}/include ${CFLAGS}"
   export CXXFLAGS="-I${AWS_LC_INSTALL_FOLDER}/include ${CXXFLAGS}"
   export LDFLAGS="-L${AWS_LC_INSTALL_FOLDER}/lib ${LDFLAGS}"
+  export LD_LIBRARY_PATH="${AWS_LC_INSTALL_FOLDER}/lib"
 
   ./configure --prefix="$KAFKA_BUILD_PREFIX"
   make -j install
+  make check
+
+  local kafka_executable="${KAFKA_BUILD_PREFIX}/lib/librdkafka.so"
+  ldd ${kafka_executable} \
+    | grep "${AWS_LC_INSTALL_FOLDER}/lib/libcrypto.so" || exit 1
 }
 
 function kafka_run_tests() {
   export LD_LIBRARY_PATH="${AWS_LC_INSTALL_FOLDER}/lib"
-  TESTS_SKIP=0092 make tests
+  python3 -m venv venv --without-pip --system-site-packages
+  source venv/bin/activate
+
+  pushd ${KAFKA_SRC_FOLDER}/tests
+  python3 -m pip install -U -r requirements.txt
+  python3 -m trivup.clusters.KafkaCluster --version 2.8.0
+  sleep 30
+  TESTS_SKIP=0092 make quick
 }
 
 git clone https://github.com/confluentinc/librdkafka.git ${KAFKA_SRC_FOLDER}
