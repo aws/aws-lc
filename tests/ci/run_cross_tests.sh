5b16e57ad07d4f1eca79e4c204d9807eedcc1806
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2025-03-06 16:44:23 -0800

    Actually run on in all CI dimensions?

--- tests/ci/run_cross_tests.sh
+++ tests/ci/run_cross_tests.sh
@@ -65,6 +65,7 @@ for BO in "${BUILD_OPTIONS[@]}"; do
   shard_gtest ${BUILD_ROOT}/crypto/urandom_test
   shard_gtest ${BUILD_ROOT}/crypto/mem_test
   shard_gtest ${BUILD_ROOT}/crypto/mem_set_test
+  shard_gtest ${BUILD_ROOT}/crypto/rand_concurrency_test
   shard_gtest ${BUILD_ROOT}/crypto/rwlock_static_init
 
   shard_gtest ${BUILD_ROOT}/ssl/ssl_test
