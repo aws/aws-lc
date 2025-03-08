50b3d468396b20f5f2d5f6031cab763211f24100
Shubham Mittal <smittals@amazon.com>
2025-03-07 16:30:31 -0800

    complete hashtable functionality and processing

--- tool-openssl/rehash.cc
+++ tool-openssl/rehash.cc
@@ -12,142 +12,123 @@
 #include <openssl/pem.h>
 #include <openssl/x509.h>
 
+#include "../crypto/internal.h"
 #include "internal.h"
 
-#ifndef PATH_MAX
-#define PATH_MAX 4096
-#endif
-#ifndef NAME_MAX
-#define NAME_MAX 255
-#endif
 #define MAX_COLLISIONS 256
-
 #define NUM_ELEMENTS(x) (sizeof(x)/sizeof((x)[0]))
 
 static const std::vector<std::string> valid_extensions = {".pem", ".crt", ".cer", ".crl"};
 
-//typedef struct hentry_st {              // Represents a single certificate/CRL file
-//    struct hentry_st *next;             // Links to next entry in same bucket
-//    char *filename;                     // Actual filename
-//    uint16_t old_id;                    // Previous numerical suffix if any
-//    uint8_t need_symlink;               // Whether this entry needs a symbolic link
-//    uint8_t digest[EVP_MAX_MD_SIZE];    // File's cryptographic digest
-//} HENTRY;
-//
-//typedef struct bucket_st {      // Groups entries with same hash
-//    struct bucket_st *next;     // Links to next bucket in hash table slot
-//    HENTRY *first_entry;        // Start of entry list
-//    HENTRY *last_entry;         // End of entry list
-//    uint32_t hash;              // Hash value of the certificates/CRLs
-//    uint16_t type;              // CERT or CRL
-//    uint16_t num_needed;        // Count of entries needing symlinks
-//} BUCKET;
-//
-//enum Type {
-//    TYPE_CERT=0, TYPE_CRL=1
-//};
-//
-//static int evpmdsize;
-//static const EVP_MD *evpmd;
-//static int remove_links = 1;
-//static int verbose = 0;
+// status_flag tracks if any errors were encountered during processing of the
+// directory. Some errors are not fatal, therefore this flag tracks if we have
+// encountered any errors to return at the end of processing.
+// OpenSSL has slightly different behavior, they return the total # of errors
+// encountered. We do not need this functionality, however, it should be easy
+// to use this variable as a counter instead of a flag.
+static bool status_flag = true;
+
+typedef struct hash_entry_st {        // Represents a single certificate/CRL file
+  struct hash_entry_st *next;         // Links to next entry in same bucket
+  char *filename;                     // Actual filename
+  uint8_t digest[EVP_MAX_MD_SIZE];    // File's cryptographic digest
+} HASH_ENTRY;
+
+typedef struct bucket_st {    // Groups entries with same hash
+  struct bucket_st *next;     // Links to next bucket in hash table slot
+  HASH_ENTRY *first_entry;    // Start of entry list
+  HASH_ENTRY *last_entry;     // End of entry list
+  uint32_t hash;              // Hash value of the certificates/CRLs
+  uint16_t type;              // CERT or CRL Bucket
+  uint16_t num_entries;       // Count of entries
+} BUCKET;
+
+enum Type {
+    TYPE_CERT=0, TYPE_CRL=1
+};
+
+static size_t evpmdsize = EVP_MD_size(EVP_sha1());
 
 // Each index may point to a list of |BUCKET|'s. Each |BUCKET| may point
-// to a list of |HENTRY|'s.
-// A new |BUCKET| is made when a
-//static BUCKET *hash_table[257];
-//
-//static const char *suffixes[] = { "", "r" };
-//static const char *extensions[] = { "pem", "crt", "cer", "crl" };
-
-//// Utility functions for bit operations
-//static void bit_set(uint8_t *set, uint32_t bit) {
-//    set[bit >> 3] |= 1 << (bit & 0x7);
-//}
-//
-//static int bit_isset(const uint8_t *set, uint32_t bit) {
-//    return set[bit >> 3] & (1 << (bit & 0x7));
-//}
+// to a list of |HASH_ENTRY|'s.
+// A |BUCKET| list may be created at an index when a collision occurs for a
+// type + hash combo that does not already exist at that index. A |HASH_ENTRY|
+// list may be created when the bucket for a type + hash combo has one
+// or more |HASH_ENTRY|'s and the cert/crl doesn't already exist in the table.
+static BUCKET *hash_table[257];
 
 // Add an entry to the hash table
-//static int add_entry(enum Type type,        // CERT or CRL
-//                    uint32_t hash,          // Hash of the name
-//                    const char *filename,   // File to process
-//                    const uint8_t *digest,  // File's digest
-//                    int need_symlink,       // Whether symlink needed
-//                    uint16_t old_id) {      // Previous ID if any
-//
-//    static BUCKET nilbucket;
-//    static HENTRY nilhentry;
-//    BUCKET *bucket;
-//    HENTRY *entry, *found = NULL;
-//    uint32_t hash_idx = (type + hash) % NUM_ELEMENTS(hash_table);
-//
-//    // Find an existing bucket if any for this |type| + |hash| combination at |hash_idx|
-//    for (bucket = hash_table[hash_idx]; bucket; bucket = bucket->next) {
-//        if (bucket->type == type && bucket->hash == hash) {
-//            break;
-//        }
-//    }
-//
-//    // Create a new bucket at |hash_idx|
-//    if (bucket == NULL) {
-//        bucket = (BUCKET *)OPENSSL_malloc(sizeof(BUCKET));
-//        *bucket = nilbucket;
-//        // Store any previous bucket at the same index but with a different |type| + |hash| combination
-//        bucket->next = hash_table[hash_idx];
-//        bucket->type = type;
-//        bucket->hash = hash;
-//        hash_table[hash_idx] = bucket;
-//    }
-//
-//    // Check for duplicates via fingerprint
-//    for (entry = bucket->first_entry; entry; entry = entry->next) {
-//        if (digest && memcmp(digest, entry->digest, evpmdsize) == 0) {
-//            fprintf(stderr, "Warning: skipping duplicate %s in %s\n",
-//                      type == TYPE_CERT ? "certificate" : "CRL", filename);
-//            return 0;
-//        }
-//        if (strcmp(filename, entry->filename) == 0) {
-//            found = entry;
-//            if (digest == NULL) { break; }
-//        }
-//    }
-//
-//    // Create new entry if needed
-//    if (found == NULL) {
-//        if (bucket->num_needed >= MAX_COLLISIONS) {
-//            fprintf(stderr, "Error: hash table overflow for %s\n", filename);
-//            return 1;
-//        }
-//        entry = (HENTRY *)OPENSSL_malloc(sizeof(*entry));
-//        *entry = nilhentry;
-//        entry->old_id = ~0;
-//        entry->filename = OPENSSL_strdup(filename);
-//        if (bucket->last_entry)
-//            bucket->last_entry->next = entry;
-//        if (bucket->first_entry == NULL)
-//            bucket->first_entry = entry;
-//        bucket->last_entry = entry;
-//    } else {
-//        entry = found;
-//    }
-//
-//    // Update entry
-//    if (old_id < entry->old_id)
-//        entry->old_id = old_id;
-//    if (need_symlink && !entry->need_symlink) {
-//        entry->need_symlink = 1;
-//        bucket->num_needed++;
-//        if (digest)
-//            memcpy(entry->digest, digest, evpmdsize);
-//    }
-//    return 0;
-//}
+static void add_entry(enum Type type, uint32_t hash, const char *filename,
+                     const uint8_t *digest) {
+  BUCKET *bucket;
+  HASH_ENTRY *entry = NULL;
+  uint32_t hash_idx = (type + hash) % NUM_ELEMENTS(hash_table);
+
+  // Find an existing bucket if any for this |type| + |hash| combination at
+  // |hash_idx|
+  for (bucket = hash_table[hash_idx]; bucket; bucket = bucket->next) {
+    if (bucket->type == type && bucket->hash == hash) {
+      break;
+    }
+  }
+
+  // If not found, create a new bucket at |hash_idx|
+  if (bucket == NULL) {
+    bucket = (BUCKET *)OPENSSL_zalloc(sizeof(*bucket));
+    if (bucket == NULL) {
+      fprintf(stderr, "ERROR: Failed to allocate new bucket\n");
+      return;
+    }
+    // Store any previous bucket at the same index but with a
+    // different |type| + |hash| combination or NULL
+    bucket->next = hash_table[hash_idx];
+    bucket->type = type;
+    bucket->hash = hash;
+    hash_table[hash_idx] = bucket;
+  }
+
+  // Check for duplicates via fingerprint
+  for (entry = bucket->first_entry; entry; entry = entry->next) {
+    if (digest && OPENSSL_memcmp(digest, entry->digest, evpmdsize) == 0) {
+      fprintf(stderr, "Warning: skipping duplicate %s in %s\n",
+              type == TYPE_CERT ? "certificate" : "CRL", filename);
+      return;
+    }
+  }
+
+  // Create new entry
+  if (bucket->num_entries >= MAX_COLLISIONS) {
+    fprintf(stderr, "Error: hash table overflow for %s\n", filename);
+    status_flag = false;
+    return;
+  }
+
+  entry = (HASH_ENTRY *)OPENSSL_zalloc(sizeof(*entry));
+  if (entry == NULL) {
+    fprintf(stderr, "ERROR: Failed to allocate new entry\n");
+    return;
+  }
+
+  entry->filename = OPENSSL_strdup(filename);
+  if (entry->filename == NULL) {
+    fprintf(stderr, "ERROR: Failed to duplicate filename\n");
+    OPENSSL_free(entry);
+    return;
+  }
+
+  OPENSSL_memcpy(entry->digest, digest, evpmdsize);
+  if (bucket->last_entry)
+    bucket->last_entry->next = entry;
+  if (bucket->first_entry == NULL)
+    bucket->first_entry = entry;
+  bucket->last_entry = entry;
+  bucket->num_entries++;
+}
 
 
 // returns one for all reasons such as not having a valid extension, more than one cert/crl in a file, or not being able to add to hashtable, and zero for error.
-static int process_file(std::string filename, std::string fullpath) {
+static void process_file(const std::string &filename,
+                         const std::string &fullpath) {
   // Skip files with invalid extensions
   size_t dot_pos = filename.find_last_of('.');
   if (dot_pos == std::string::npos ||
@@ -155,23 +136,24 @@ static int process_file(std::string filename, std::string fullpath) {
                 [&filename, dot_pos](const std::string& ext) {
                     return strcasecmp(filename.c_str() + dot_pos, ext.c_str()) == 0;
                   })) {
-    // Skip files with invalid extensions
-    return 1;
+    return;
   }
 
   // Ensure file contains X.509 data
   BIO* bio = BIO_new_file(fullpath.c_str(), "r");
   if (!bio) {
     fprintf(stderr, "Error: Cannot open file %s\n", filename.c_str());
-    return 0;
+    status_flag = false;
+    return;
   }
 
-  bssl::UniquePtr<STACK_OF(X509_INFO)> x509_info_stack(PEM_X509_INFO_read_bio(bio, nullptr, nullptr, nullptr));
+  bssl::UniquePtr<STACK_OF(X509_INFO)> x509_info_stack(
+    PEM_X509_INFO_read_bio(bio, nullptr, nullptr, nullptr));
   BIO_free(bio);
 
-  // Ensure there is only one cert/CRL in the file
+  // Ensure there is only one cert/CRL in the file, this is not an error
   if (!x509_info_stack || sk_X509_INFO_num(x509_info_stack.get()) != 1) {
-    return 1;
+    return;
   }
 
   // Process single cert/CRL
@@ -179,60 +161,71 @@ static int process_file(std::string filename, std::string fullpath) {
   X509_NAME* x509_name;
   unsigned char digest[EVP_MAX_MD_SIZE];
   unsigned int digest_len;
+  Type type;
 
   if (x509_info->x509) {
     // Handle certificate
+    type = TYPE_CERT;
     if (!X509_digest(x509_info->x509, EVP_sha1(), digest, &digest_len)) {
-      return 0;
+      status_flag = false;
+      return;
     }
     x509_name = X509_get_subject_name(x509_info->x509);
 
   } else if (x509_info->crl) {
     // Handle CRL
+    type = TYPE_CRL;
     if (!X509_CRL_digest(x509_info->crl, EVP_sha1(), digest, &digest_len)) {
-      return 0;
+      status_flag = false;
+      return;
     }
     x509_name = X509_CRL_get_issuer(x509_info->crl);
+  } else {
+    status_flag = false;
+    return;
   }
 
   // Now do the add_entry call. Need to simplofy this struct and remove the need_symlink field etc.
   // Also need to add a type var above that we can then pass into add_entry.
+  // add_entry should update the errs var on its own.
+  add_entry(type, X509_NAME_hash(x509_name), filename.c_str(), digest);
 
-
-  return 1;
 }
 
-// symlink_check determines if |filename| is a symbolic link matching the regex [0-9a-f]{8}.([r])?[0-9]+.
+// symlink_check determines if |filename| is a symbolic link matching the regex
+// [0-9a-f]{8}.([r])?[0-9]+.
 // If so, it deletes the symbolic link and sets |is_symlink| to true.
-// It returns one on success, and zero on failure.
-static int symlink_check(std::string filename, std::string fullpath, const std::regex &pattern, bool &is_symlink) {
+static void symlink_check(const std::string &filename,
+                          const std::string &fullpath,
+                          const std::regex &pattern, bool &is_symlink) {
     struct stat path_stat;
 
     if (lstat(fullpath.c_str(), &path_stat) != 0) {
-      fprintf(stderr, "Error: Cannot stat '%s': %s\n", fullpath.c_str(), strerror(errno));
-      return 0;
+      fprintf(stderr, "Warning: Cannot stat '%s': %s\n", fullpath.c_str(), strerror(errno));
+      status_flag = false;
+      return;
     }
 
     // If it's a symlink and matches our pattern, remove it
     if (S_ISLNK(path_stat.st_mode) && std::regex_match(filename, pattern)) {
       if (unlink(fullpath.c_str()) != 0) {
-        fprintf(stderr, "Error: Failed to remove symlink '%s': %s\n", fullpath.c_str(), strerror(errno));
-        return 0;
+        fprintf(stderr, "Warning: Failed to remove symlink '%s': %s\n", fullpath.c_str(), strerror(errno));
+        status_flag = false;
+        return;
       }
       is_symlink = true;
     }
-
-    return 1;
 }
 
-static int process_directory(std::string directory_path) {
-  // MAYBE COUPLE THE FILE PROCESSING. SO in one pass we idenitfy and remove any symlinks, and otherwise
+static void process_directory(const std::string &directory_path) {
+  // MAYBE COUPLE THE FILE PROCESSING. SO in one pass we identify and remove any symlinks, and otherwise
   // we process an appropriate file and at least store the info in the hashtable. Then we can create them all at once.
 
   DIR* dir = opendir(directory_path.c_str());
   if (dir == nullptr) {
     fprintf(stderr, "Error opening directory '%s': %s\n", directory_path.c_str(), strerror(errno));
-    return 0;
+    status_flag = false;
+    return;
   }
 
   // Process every file. Remove any symlinks matching the regex.
@@ -248,48 +241,39 @@ static int process_directory(std::string directory_path) {
     }
     std::string full_path = directory_path + "/" + filename;
 
-    // Check if it's a symlink matching the regex
+    // Check if it's a symlink matching the regex, continue processing even with
+    // errors.
     bool is_symlink = false;
-    if (!symlink_check(filename.c_str(), full_path.c_str(), symlink_pattern, is_symlink)) {
-        return 0;
-    }
+    symlink_check(filename, full_path, symlink_pattern, is_symlink);
     if (is_symlink) {
       continue;
     }
 
-    // If its a valid file, add a symlink mapping to hashtable
-    if (!process_file(full_path.c_str(), filename.c_str())) {
-      return 0;
-    }
-
-
+    // If it's a valid file, add a mapping to hashtable. Continue
+    // processing even if we encounter errors.
+    process_file(filename, full_path);
   }
 
   // Pass 2: Process generated hash table to create new symlinks.
 
   closedir(dir);
-  return 1;
 }
 
 static const argument_t kArguments[] = {
-        { "-help", kBooleanArgument, "Usage: openssl rehash [cert-directory]\n" \
-                                     "This tool scans a directory and calculates a hash value of each \n" \
-                                     ".pem, .crt, .cer, or .crl file. It then creates a symbolic link for each file, \n"\
-                                     "where the name of the link is the hash value. The symlink follows the format \n" \
-                                     "HHHHHHHH.D, where each H is a hexadecimal character and D is a whole number. \n" \
-                                     "This tool also removes any existing symbolic links that match the regex \n" \
-                                     "[0-9a-f]{8}.([r])?[0-9]+ in that directory. \n" \
-                                     "If any directory is not named on the command line, then the SSL_CERT_DIR \n"
-                                     "environmental variable will be consulted. If that is not set, then the default \n"\
-                                     "directory will be used. \n" },
-        { "", kOptionalArgument, "Path to directory. If not provided, the default directory will be used." },
+        { "-help", kBooleanArgument, "Display option summary"},
+        { "", kOptionalArgument, "Path to directory. "\
+                    "then the SSL_CERT_DIR environmental variable will be \n" \
+                    "consulted. If that is not set, then the default \n" \
+                    "directory will be used. You must have write \n"\
+                    "access to the directory." },
         { "", kOptionalArgument, "" }
 };
 
 bool RehashTool(const args_list_t &args) {
   args_map_t parsed_args;
   args_list_t extra_args;
-  if (!ParseKeyValueArguments(parsed_args, extra_args, args, kArguments) || extra_args.size() > 1) {
+  if (!ParseKeyValueArguments(parsed_args, extra_args, args,
+    kArguments) || extra_args.size() > 1) {
     PrintUsage(kArguments);
     return false;
   }
@@ -301,20 +285,18 @@ bool RehashTool(const args_list_t &args) {
 
   if (help) {
     fprintf(stderr, "Usage: openssl rehash [cert-directory]\n" \
-                    "This tool scans a directory and calculates a hash value of each \n" \
-                    ".pem, .crt, .cer, or .crl file. It then creates a symbolic link for each file, \n"\
-                    "where the name of the link is the hash value. The symlink follows the format \n" \
-                    "HHHHHHHH.D, where each H is a hexadecimal character and D is a whole number. \n" \
-                    "This tool also removes any existing symbolic links that match the regex \n" \
-                    "[0-9a-f]{8}.([r])?[0-9]+ in that directory. \n" \
-                    "If any directory is not named on the command line, then the SSL_CERT_DIR \n"
-                    "environmental variable will be consulted. If that is not set, then the default \n"\
-                    "directory will be used. You must have write access to the directory. \n");
+      "This tool scans a directory and calculates a hash value of each \n" \
+      "pem, .crt, .cer, or .crl file. It then creates a symbolic link \n"\
+      "for each file, where the name of the link is the hash value. The \n" \
+      "symlink follows the format HHHHHHHH.D, where each H is a \n" \
+      "hexadecimal character and D is a whole number. This tool also \n" \
+      "removes any existing symbolic links that match the regex \n" \
+      "[0-9a-f]{8}.([r])?[0-9]+ in that directory. \n");
     PrintUsage(kArguments);
     return false;
   }
 
-  if (extra_args.size() == 0) { // No directory path provided on command line
+  if (extra_args.empty()) { // No directory path provided on command line
     const char* ssl_cert_dir = getenv("SSL_CERT_DIR");
     if (ssl_cert_dir != nullptr) {
       directory_path = ssl_cert_dir;
@@ -328,7 +310,8 @@ bool RehashTool(const args_list_t &args) {
   // Get absolute path
   char resolved_path[PATH_MAX];
   if (realpath(directory_path.c_str(), resolved_path) == nullptr) {
-    fprintf(stderr, "Error: Unable to resolve directory path: %s\n", strerror(errno));
+    fprintf(stderr, "Error: Unable to resolve directory path: %s\n",
+            strerror(errno));
     return false;
   }
   directory_path = resolved_path;
@@ -354,10 +337,7 @@ bool RehashTool(const args_list_t &args) {
   }
 
   // Process directory
-  if (!process_directory(directory_path)) {
-    fprintf(stderr, "Error: Unable while processing directory '%s'\n", directory_path.c_str());
-    return false;
-  }
+  process_directory(directory_path);
 
-  return true;
+  return status_flag;
 }
