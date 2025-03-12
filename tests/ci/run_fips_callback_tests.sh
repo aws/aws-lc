ef3db5d81cc4f2c76cde72209d139c6ca4d45ff9
Andrew Hopkins <andhop@amazon.com>
2025-03-12 12:42:46 -0700

    Make AWS_LC_fips_failure_callback optional in builds with AWSLC_FIPS_FAILURE_CALLBACK. Create a new test target that partially replicates our internal FIPS callback build

--- tests/ci/run_fips_callback_tests.sh
+++ tests/ci/run_fips_callback_tests.sh
@@ -4,26 +4,86 @@ set -ex
 # SPDX-License-Identifier: Apache-2.0 OR ISC
 source tests/ci/common_posix_setup.sh
 
-original_test="${BUILD_ROOT}/crypto/crypto_test"
-broken_test="${BUILD_ROOT}/crypto/crypto_test_broken"
+# This test file is designed to replicate the internal FIPS callback build defined in the build-glue package
 
-# By default the test should pass
-$original_test --gtest_filter=FIPSCallback.PowerOnSelfTests
-$original_test --gtest_filter=FIPSCallback.PWCT
+# This should follow fips_tests.sh maybe_run_fips_tests
+function maybe_run_fips_tests() {
+  expect_fips_mode=1
+  module_status=$("${BUILD_ROOT}/tool/bssl" isfips)
+  [[ "${expect_fips_mode}" == "${module_status}" ]] || {
+    echo >&2 "FIPS Mode validation failed."
+    exit 1
+  }
+  # Mainline AWS-LC does not have the CAVP tests anymore so only run the test_fips branch
+  "${BUILD_ROOT}/util/fipstools/test_fips"
+}
 
-# Break the tests
-KATS=$(go run "${SRC_ROOT}/util/fipstools/break-kat.go" --list-tests)
-for kat in $KATS; do
-  go run "${SRC_ROOT}/util/fipstools/break-kat.go" "$original_test" "$kat" > "$broken_test"
-  chmod +x "$broken_test"
-  export FIPS_CALLBACK_TEST_EXPECTED_FAILURE="$kat"
-  # When a callback is defined AWS-LC will not abort and the test should exit successfully
-  $broken_test --gtest_filter=FIPSCallback.PowerOnSelfTests
-  unset FIPS_CALLBACK_TEST_EXPECTED_FAILURE
-done
+# This should follow fips_tests.sh maybe_run_fips_break_tests
+function maybe_run_fips_break_tests() {
+  working_bssl="${BUILD_ROOT}/tool/bssl"
+  "$working_bssl" isfips
 
-for TEST in RSA_PWCT ECDSA_PWCT EDDSA_PWCT MLKEM_PWCT MLDSA_PWCT; do
-    export FIPS_CALLBACK_TEST_EXPECTED_FAILURE="${TEST}"
-    export BORINGSSL_FIPS_BREAK_TEST="${TEST}"
-    $original_test --gtest_filter=FIPSCallback.PWCT
-done
+  break_kat_executable="${BUILD_ROOT}/break-kat"
+  pushd "${SRC_ROOT}"
+  go build -o "$break_kat_executable" "./util/fipstools/break-kat.go"
+  "$break_kat_executable" -list-tests
+
+  # This breaks a local copy of bssl that will not be included in the build artifacts
+  broken_bssl="${BUILD_ROOT}/brokenbssl"
+  "$break_kat_executable" "$working_bssl" DRBG >"$broken_bssl"
+  chmod +x "$broken_bssl"
+  if ! ("$broken_bssl" 2>&1 >/dev/null || true) |
+    grep -q "DRBG"; then
+    echo "Broken bssl did not mention DRBG failure in startup"
+    exit 1
+  fi
+  popd
+}
+
+function run_all_break_tests() {
+  original_test="${BUILD_ROOT}/crypto/crypto_test"
+  broken_test="${BUILD_ROOT}/crypto/crypto_test_broken"
+
+  # By default the test should pass
+  $original_test --gtest_filter=FIPSCallback.PowerOnSelfTests
+  $original_test --gtest_filter=FIPSCallback.PWCT
+
+  # Break the tests
+  KATS=$(go run "${SRC_ROOT}/util/fipstools/break-kat.go" --list-tests)
+  for kat in $KATS; do
+    go run "${SRC_ROOT}/util/fipstools/break-kat.go" "$original_test" "$kat" > "$broken_test"
+    chmod +x "$broken_test"
+    export FIPS_CALLBACK_TEST_EXPECTED_FAILURE="$kat"
+    # When a callback is defined AWS-LC will not abort and the test should exit successfully
+    $broken_test --gtest_filter=FIPSCallback.PowerOnSelfTests
+    unset FIPS_CALLBACK_TEST_EXPECTED_FAILURE
+  done
+
+  for TEST in RSA_PWCT ECDSA_PWCT EDDSA_PWCT MLKEM_PWCT MLDSA_PWCT; do
+      export FIPS_CALLBACK_TEST_EXPECTED_FAILURE="${TEST}"
+      export BORINGSSL_FIPS_BREAK_TEST="${TEST}"
+      $original_test --gtest_filter=FIPSCallback.PWCT
+      unset FIPS_CALLBACK_TEST_EXPECTED_FAILURE
+      unset BORINGSSL_FIPS_BREAK_TEST
+  done
+
+}
+
+echo "Testing AWS-LC static breakable build with custom callback and Jitter enabled"
+build_and_test -DFIPS=1 \
+  -DCMAKE_C_FLAGS="-DBORINGSSL_FIPS_BREAK_TESTS -DAWSLC_FIPS_FAILURE_CALLBACK" \
+  -DCMAKE_CXX_FLAGS="-DAWSLC_FIPS_FAILURE_CALLBACK" \
+  -DENABLE_FIPS_ENTROPY_CPU_JITTER=1
+
+maybe_run_fips_tests
+maybe_run_fips_break_tests
+run_all_break_tests
+
+echo "Testing AWS-LC static build with custom callback and Jitter enabled"
+build_and_test -DFIPS=1 \
+  -DCMAKE_C_FLAGS="-DAWSLC_FIPS_FAILURE_CALLBACK" \
+  -DCMAKE_CXX_FLAGS="-DAWSLC_FIPS_FAILURE_CALLBACK" \
+  -DENABLE_FIPS_ENTROPY_CPU_JITTER=1
+
+maybe_run_fips_tests
+# Can't run maybe_run_fips_break_tests or run_all_break_tests since they require BORINGSSL_FIPS_BREAK_TESTS
