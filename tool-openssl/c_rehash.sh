2536d078b0b32204155854ed225efd4d6b5dbc36
Shubham Mittal <smittals@amazon.com>
2025-03-12 16:34:33 -0700

    generate c_rehash script and add a dummy test that should fail

---
+++ tool-openssl/c_rehash.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR ISC
+
+# Get absolute path to script directory
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+
+# Check for openssl binary in same directory
+if [ -f "${SCRIPT_DIR}/openssl" ]; then
+    "${SCRIPT_DIR}/openssl" rehash "$@"
+else
+    echo "Error: AWS-LC openssl binary not found at '${SCRIPT_DIR}/openssl'"
+    exit 1
+fi
