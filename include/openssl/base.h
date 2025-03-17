85e7cc5e18d63c57df3326d2679e1446c81edeaa
Justin Smith <justsmth@amazon.com>
2025-03-17 13:53:21 -0400

    Prepare AWS-LC v1.48.5

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -122,7 +122,7 @@ extern "C" {
 // ServiceIndicatorTest.AWSLCVersionString
 // Note: there are two versions of this test. Only one test is compiled
 // depending on FIPS mode.
-#define AWSLC_VERSION_NUMBER_STRING "1.48.4"
+#define AWSLC_VERSION_NUMBER_STRING "1.48.5"
 
 #if defined(BORINGSSL_SHARED_LIBRARY)
 
