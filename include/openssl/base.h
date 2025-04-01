b1420f27a7c95762cd11b249ece3d049f530d9e6
Shubham Mittal <107728331+smittals2@users.noreply.github.com>
2025-03-31 20:07:47 -0700

    Prepare for v1.49.1 (#2303)
    ## What's Changed
    * FIPS Integrity Hash Tooling by @skmcgrail in
    https://github.com/aws/aws-lc/pull/2296
    * Add more build options to match callback build by @andrewhop in
    https://github.com/aws/aws-lc/pull/2279
    * Add req to OpenSSL CLI tool by @smittals2 in
    https://github.com/aws/aws-lc/pull/2284
    * Turn on better logging for EC2 test framework by @andrewhop in
    https://github.com/aws/aws-lc/pull/2298
    
    
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license and the ISC license.

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -122,7 +122,7 @@ extern "C" {
 // ServiceIndicatorTest.AWSLCVersionString
 // Note: there are two versions of this test. Only one test is compiled
 // depending on FIPS mode.
-#define AWSLC_VERSION_NUMBER_STRING "1.49.0"
+#define AWSLC_VERSION_NUMBER_STRING "1.49.1"
 
 #if defined(BORINGSSL_SHARED_LIBRARY)
 
