9c379a3693409e86811d4f3b535d8011be1c03e4
torben-hansen <50673096+torben-hansen@users.noreply.github.com>
2025-05-28 11:45:30 -0700

    Remove FIPS_ENTROPY_SOURCE_JITTER_CPU macro completely (#2443)
    FIPS_ENTROPY_SOURCE_JITTER_CPU ain't necessary anymore. Axe it. In the speed tool, try to stay backwards compatible and buildable with OpenSSL and its derivatives.

--- crypto/fipsmodule/bcm.c
+++ crypto/fipsmodule/bcm.c
@@ -277,11 +277,9 @@ static void BORINGSSL_bcm_power_on_self_test(void) {
   OPENSSL_cpuid_setup();
 #endif
 
-#if defined(FIPS_ENTROPY_SOURCE_JITTER_CPU)
   if (jent_entropy_init()) {
     AWS_LC_FIPS_failure("CPU Jitter entropy RNG initialization failed");
   }
-#endif
 
 #if !defined(OPENSSL_ASAN)
   // Integrity tests cannot run under ASAN because it involves reading the full
