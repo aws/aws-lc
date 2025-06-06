6cf898b2744f3570e29daf6c95dbbc3bf0d04495
Nghi Ho <hhaileyh@amazon.com>
2025-06-06 12:57:38 -0700

    Update docker images

--- tests/ci/docker_images/windows/build_images.ps1
+++ tests/ci/docker_images/windows/build_images.ps1
@@ -1,6 +1,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0 OR ISC
 
-docker build -t aws-lc/windows_base:2019 .\windows_base
-docker build -t vs2015 .\vs2015
-docker build -t vs2017 .\vs2017
+. .\common.ps1
+
+Invoke-Command { docker build -t aws-lc/windows_base:2022 .\windows_base }
+Invoke-Command { docker build -t vs2015 .\vs2015 }
+Invoke-Command { docker build -t vs2017 .\vs2017 }
