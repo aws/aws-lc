d3c6d24fc603773255f48bb56bdeb654ccf9ec4d
Erin Chapman <13040499+m271828@users.noreply.github.com>
2025-06-10 00:24:43 -0700

    Revert "Add flag to exclude algorithms"
    This reverts commit fd352ecd2389c46b762afce8c99130136e44049c.

--- include/openssl/md5.h
+++ include/openssl/md5.h
@@ -57,8 +57,6 @@
 #ifndef OPENSSL_HEADER_MD5_H
 #define OPENSSL_HEADER_MD5_H
 
-#ifndef FIPS_ONLY_BUILD
-
 #include <openssl/base.h>
 
 #if defined(__cplusplus)
@@ -107,7 +105,5 @@ struct md5_state_st {
 #if defined(__cplusplus)
 }  // extern C
 #endif
-
-#endif // FIPS_ONLY_BUILD
 
 #endif  // OPENSSL_HEADER_MD5_H
