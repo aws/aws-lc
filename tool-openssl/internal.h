17530c17dac77d5e562b9516db56d31a344748f7
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2025-06-19 15:49:55 -0400

    Order tool output by options provided - x509 (#2454)
    ### Context
    * The OpenSSL CLI tool orders its output based on the order of the
    options passed to it.
    * Our tool was using a static order that was unaffected by the order of
    the options.
    
    ### Description of changes: 
    * This change adds an alternative suite of functions to parse options in
    a way that preserves their order.
    * The new argument parser was integrated into the "X509" subcommand
    logic for the tool.
    
    ### Call-outs:
    * The new parser still needs to be integrated with the other subcommands
    as well.
    
    ### Testing:
    Added new tests that vary the order of the parameters provided.
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license and the ISC license.

--- tool-openssl/internal.h
+++ tool-openssl/internal.h
@@ -1,13 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 
-#ifndef INTERNAL_H
-#define INTERNAL_H
+#ifndef TOOL_OPENSSL_INTERNAL_H
+#define TOOL_OPENSSL_INTERNAL_H
 
-#include "../tool/internal.h"
 #include <openssl/digest.h>
+#include <algorithm>
 #include <string>
+#include <utility>
 #include <vector>
+#include "../tool/internal.h"
 
 #if !defined(O_BINARY)
 #define O_BINARY 0
@@ -20,12 +22,13 @@ struct Tool {
   tool_func_t func;
 };
 
-bool IsNumeric(const std::string& str);
+bool IsNumeric(const std::string &str);
 
-X509* CreateAndSignX509Certificate();
-X509_CRL* createTestCRL();
+X509 *CreateAndSignX509Certificate();
+X509_CRL *createTestCRL();
 
-bool LoadPrivateKeyAndSignCertificate(X509 *x509, const std::string &signkey_path);
+bool LoadPrivateKeyAndSignCertificate(X509 *x509,
+                                      const std::string &signkey_path);
 
 tool_func_t FindTool(const std::string &name);
 tool_func_t FindTool(int argc, char **argv, int &starting_arg);
@@ -46,27 +49,76 @@ bssl::UniquePtr<X509_NAME> parse_subject_name(std::string &subject_string);
 
 
 // Rehash tool Utils
-typedef struct hash_entry_st {        // Represents a single certificate/CRL file
-  struct hash_entry_st *next;         // Links to next entry in same bucket
-  char *filename;                     // Actual filename
-  uint8_t digest[EVP_MAX_MD_SIZE];    // File's cryptographic digest
+typedef struct hash_entry_st {      // Represents a single certificate/CRL file
+  struct hash_entry_st *next;       // Links to next entry in same bucket
+  char *filename;                   // Actual filename
+  uint8_t digest[EVP_MAX_MD_SIZE];  // File's cryptographic digest
 } HASH_ENTRY;
 
-typedef struct bucket_st {    // Groups entries with same hash
-  struct bucket_st *next;     // Links to next bucket in hash table slot
-  HASH_ENTRY *first_entry;    // Start of entry list
-  HASH_ENTRY *last_entry;     // End of entry list
-  uint32_t hash;              // Hash value of the certificates/CRLs
-  uint16_t type;              // CERT or CRL Bucket
-  uint16_t num_entries;       // Count of entries
+typedef struct bucket_st {  // Groups entries with same hash
+  struct bucket_st *next;   // Links to next bucket in hash table slot
+  HASH_ENTRY *first_entry;  // Start of entry list
+  HASH_ENTRY *last_entry;   // End of entry list
+  uint32_t hash;            // Hash value of the certificates/CRLs
+  uint16_t type;            // CERT or CRL Bucket
+  uint16_t num_entries;     // Count of entries
 } BUCKET;
 
-enum Type {
-  TYPE_CERT=0, TYPE_CRL=1
-};
+enum Type { TYPE_CERT = 0, TYPE_CRL = 1 };
 void add_entry(enum Type type, uint32_t hash, const char *filename,
-                     const uint8_t *digest);
-BUCKET** get_table();
+               const uint8_t *digest);
+BUCKET **get_table();
 void cleanup_hash_table();
 
-#endif //INTERNAL_H
+// Ordered argument processing (specific to tool-openssl)
+namespace ordered_args {
+typedef std::vector<std::pair<std::string, std::string>> ordered_args_map_t;
+
+// Helper function to find an argument in the ordered args vector
+static inline bool HasArgument(const ordered_args_map_t &args,
+                               const std::string &arg_name) {
+  return std::find_if(
+             args.begin(), args.end(),
+             [&arg_name](const std::pair<std::string, std::string> &pair) {
+               return pair.first == arg_name;
+             }) != args.end();
+}
+
+// Helper function to count occurrences of an argument
+static inline size_t CountArgument(const ordered_args_map_t &args,
+                                   const std::string &arg_name) {
+  size_t count = 0;
+  for (const auto &pair : args) {
+    if (pair.first == arg_name) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// Helper function to find an argument in the ordered args vector
+static inline ordered_args_map_t::const_iterator FindArg(
+    const ordered_args_map_t &args, const std::string &arg_name) {
+  return std::find_if(
+      args.begin(), args.end(),
+      [&arg_name](const std::pair<std::string, std::string> &pair) {
+        return pair.first == arg_name;
+      });
+}
+
+// Parse arguments in order of appearance
+bool ParseOrderedKeyValueArguments(ordered_args_map_t &out_args,
+                                   args_list_t &extra_args,
+                                   const args_list_t &args,
+                                   const argument_t *templates);
+
+// Get helpers for ordered arguments
+bool GetUnsigned(unsigned *out, const std::string &arg_name,
+                 unsigned default_value, const ordered_args_map_t &args);
+bool GetString(std::string *out, const std::string &arg_name,
+               std::string default_value, const ordered_args_map_t &args);
+bool GetBoolArgument(bool *out, const std::string &arg_name,
+                     const ordered_args_map_t &args);
+}  // namespace ordered_args
+
+#endif  // TOOL_OPENSSL_INTERNAL_H
