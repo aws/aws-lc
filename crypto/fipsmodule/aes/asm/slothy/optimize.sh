860be6a2d28846390e69f40352b6b4ed9d7d1687
Yan Peng <yppe@amazon.com>
2025-06-30 16:34:52 +0000

    Slothy config changes with Hanno

--- crypto/fipsmodule/aes/asm/slothy/optimize.sh
+++ crypto/fipsmodule/aes/asm/slothy/optimize.sh
@@ -122,10 +122,10 @@ optimize_x5() {
     -c sw_pipelining.optimize_postamble=false        \
     -c inputs_are_outputs                              \
     -c sw_pipelining.minimize_overlapping=False        \
-    -c sw_pipelining.allow_post                        \
+    -c sw_pipelining.unknown_iteration_count=true    \
     -c variable_size                                   \
     -c constraints.stalls_first_attempt=64             \
-    -c reserved_regs="[x18,x23--x30,w19,x3,x21,sp]" \
+    -c reserved_regs="[x18,x23--x30,w19,x2,x3,x21,x0,x1,x4,x5,sp]" \
     -c selftest=false                                  \
     -c constraints.allow_reordering=true             \
     -c constraints.allow_renaming=true             \
