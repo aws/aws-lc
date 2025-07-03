bbc86eb4f40468b288feedbada43eb6e275ccbbc
Nghi Ho <hhaileyh@amazon.com>
2025-07-03 09:53:50 -0700

    Clean up

--- tests/ci/docker_images/windows/build_images.ps1
+++ tests/ci/docker_images/windows/build_images.ps1
@@ -3,10 +3,7 @@
 
 . .\common.ps1
 
-#Invoke-Command { docker build -t aws-lc/windows-2019:base .\windows-2019_base }
 Invoke-Command { docker build -t aws-lc/windows-2022:base .\windows-2022_base }
-#Invoke-Command { docker build -t windows-2019:vs2015 .\windows-2019_vs2015 }
-#Invoke-Command { docker build -t windows-2019:vs2017-sde .\windows-2019_vs2017-sde }
 Invoke-Command { docker build -t windows-2022:vs2022-sde .\windows-2022_vs2022-sde }
 Invoke-Command { docker build -t windows-2022:vs2017-sde .\windows-2022_vs2017-sde }
 Invoke-Command { docker build -t windows-2022:vs2015 .\windows-2022_vs2015 }
