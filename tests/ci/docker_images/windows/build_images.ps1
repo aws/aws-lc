1ee56f69dbb802b0c4402479e3d83547df65195b
Nghi Ho <hhaileyh@amazon.com>
2025-07-08 10:31:33 -0700

    nit

--- tests/ci/docker_images/windows/build_images.ps1
+++ tests/ci/docker_images/windows/build_images.ps1
@@ -4,6 +4,6 @@
 . .\common.ps1
 
 Invoke-Command { docker build -t aws-lc/windows-2022:base .\windows-2022_base }
-Invoke-Command { docker build -t windows-2022:vs2022-sde .\windows-2022_vs2022-sde }
-Invoke-Command { docker build -t windows-2022:vs2017-sde .\windows-2022_vs2017-sde }
 Invoke-Command { docker build -t windows-2022:vs2015 .\windows-2022_vs2015 }
+Invoke-Command { docker build -t windows-2022:vs2017-sde .\windows-2022_vs2017-sde }
+Invoke-Command { docker build -t windows-2022:vs2022-sde .\windows-2022_vs2022-sde }
