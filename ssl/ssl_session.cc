40750d71988416c34744873a1700227ebd4bb856
Hailey Ho <hhaileyh@amazon.com>
2025-07-22 21:40:43 +0000

    Revert "Implement SSL_set_verify_result (#2526)"
    This reverts commit 127633fc77e752b12e12d8e05304e52b3230b65f.

--- ssl/ssl_session.cc
+++ ssl/ssl_session.cc
@@ -800,10 +800,6 @@ void ssl_set_session(SSL *ssl, SSL_SESSION *session) {
   }
 
   ssl->session = UpRef(session);
-
-  if (ssl->session) {
-    ssl->verify_result = session->verify_result;
-  }
 }
 
 // locked by SSL_CTX in the calling function
--- ssl/ssl_x509.cc
+++ ssl/ssl_x509.cc
@@ -744,17 +744,13 @@ int SSL_CTX_load_verify_locations(SSL_CTX *ctx, const char *ca_file,
   return X509_STORE_load_locations(ctx->cert_store, ca_file, ca_dir);
 }
 
-void SSL_set_verify_result(SSL *ssl, long arg) {
-  check_ssl_x509_method(ssl);
-  ssl->verify_result = arg;
-}
-
 long SSL_get_verify_result(const SSL *ssl) {
   check_ssl_x509_method(ssl);
-  if (SSL_get_session(ssl) == nullptr) {
+  SSL_SESSION *session = SSL_get_session(ssl);
+  if (session == NULL) {
     return X509_V_ERR_INVALID_CALL;
   }
-  return ssl->verify_result;
+  return session->verify_result;
 }
 
 X509_STORE *SSL_CTX_get_cert_store(const SSL_CTX *ctx) {
