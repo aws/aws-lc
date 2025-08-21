5530e151b5dfb8d03e984a971e7ec3f432dff540
Nevine Ebeid <nebeid@amazon.com>
2025-08-21 15:08:53 -0400

    Add aes-xts AArch64 implementation that will eventually be imported from s2n-bignum.
    - The added code does not cause a performance degradation.
    - Experimenting with SLOTHY to optimize the code showed that there was no
     noticeable performance gain across AArch64 CPUs.
    - The new code is mostly clean, i.e., de-interleaved, which makes it more readable, maintainable and relatively easier to formally verify for correctness.
    - It is still 5x unrolled; this was the best unrolling factor based on experiments with other factors: 3x, 4x and 6x.

--- crypto/fipsmodule/aes/internal.h
+++ crypto/fipsmodule/aes/internal.h
@@ -161,16 +161,6 @@ OPENSSL_EXPORT int aes_hw_xts_cipher(const uint8_t *in, uint8_t *out, size_t len
                                       const AES_KEY *key1, const AES_KEY *key2,
                                       const uint8_t iv[16], int enc);
 
-#define USE_SLOTHY_XTS
-#if defined(USE_SLOTHY_XTS)
-// void aes_hw_slothy_xts_encrypt(const uint8_t *in, uint8_t *out, size_t length,
-//                         const AES_KEY *key1, const AES_KEY *key2,
-//                         const uint8_t iv[16]);
-void aes_hw_slothy_xts_decrypt(const uint8_t *in, uint8_t *out, size_t length,
-                        const AES_KEY *key1, const AES_KEY *key2,
-                        const uint8_t iv[16]);
-#endif
-
 #if defined(OPENSSL_X86_64) && !defined(MY_ASSEMBLER_IS_TOO_OLD_FOR_512AVX) && !defined(OPENSSL_WINDOWS)
 #define AES_XTS_X86_64_AVX512
 void aes_hw_xts_encrypt_avx512(const uint8_t *in, uint8_t *out, size_t length,
