aws/aws-lc

commit 10a0611d1f1fe5818c03b48d31f7730fa2a95653

simplify importer.sh
Signed-off-by: Dusan Kostic <dkostic@protonmail.com>

crypto/fipsmodule/ml_kem/importer.sh
@@ -88,7 +88,7 @@ cp $TMP/mlkem/src/native/aarch64/src/* $SRC/native/aarch64/src
 mkdir -p $SRC/native/x86_64/src
 # Backend API and specification assumed by mlkem-native frontend
 cp $TMP/mlkem/src/native/api.h $SRC/native
-# Copy AArch64 backend implementation
+# Copy x86_64 backend implementation
 cp $TMP/mlkem/src/native/x86_64/meta.h $SRC/native/x86_64
 cp $TMP/mlkem/src/native/x86_64/src/* $SRC/native/x86_64/src
 
@@ -105,26 +105,25 @@ cp $TMP/.clang-format $SRC
 # Moreover, exclude POLY_COMPRESS/DECOMPRESS functions from the x86 backend.
 unifdef -DMLK_CONFIG_FIPS202_CUSTOM_HEADER                             \
         -UMLK_CONFIG_USE_NATIVE_BACKEND_FIPS202                        \
-        -UMLK_SYS_X86_64                                               \
-        -UMLK_USE_NATIVE_POLY_COMPRESS_D4                              \
-        -UMLK_USE_NATIVE_POLY_COMPRESS_D5                              \
-        -UMLK_USE_NATIVE_POLY_COMPRESS_D10                             \
-        -UMLK_USE_NATIVE_POLY_COMPRESS_D11                             \
-        -UMLK_USE_NATIVE_POLY_DECOMPRESS_D4                            \
-        -UMLK_USE_NATIVE_POLY_DECOMPRESS_D5                            \
-        -UMLK_USE_NATIVE_POLY_DECOMPRESS_D10                           \
-        -UMLK_USE_NATIVE_POLY_DECOMPRESS_D11                           \
         $TMP/mlkem/mlkem_native.c                                      \
         > $SRC/mlkem_native_bcm.c
-
-# Exclude POLY_COMPRESS/DECOMPRESS functions from the x86 backend.
-sed ${SED_I[@]} '/compress_avx2.c/d' $SRC/mlkem_native_bcm.c
 
 if [[ "$(uname)" == "Darwin" ]]; then
   SED_I=(-i "")
 else
   SED_I=(-i)
 fi
+
+# Exclude POLY_COMPRESS/DECOMPRESS functions from the x86 backend for now.
+sed ${SED_I[@]} '/compress_avx2.c/d' $SRC/mlkem_native_bcm.c
+sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_COMPRESS_D4/d' $SRC/native/x86_64/meta.h
+sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_COMPRESS_D5/d' $SRC/native/x86_64/meta.h
+sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_COMPRESS_D10/d' $SRC/native/x86_64/meta.h
+sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_COMPRESS_D11/d' $SRC/native/x86_64/meta.h
+sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_DECOMPRESS_D4/d' $SRC/native/x86_64/meta.h
+sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_DECOMPRESS_D5/d' $SRC/native/x86_64/meta.h
+sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_DECOMPRESS_D10/d' $SRC/native/x86_64/meta.h
+sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_DECOMPRESS_D11/d' $SRC/native/x86_64/meta.h
 
 # Copy mlkem-native header
 # This is only needed for access to the various macros defining key sizes.
@@ -139,14 +138,16 @@ cp $TMP/mlkem/mlkem_native.h $SRC
 echo "Fixup include paths"
 sed "${SED_I[@]}" 's/#include "src\/\([^"]*\)"/#include "\1"/' $SRC/mlkem_native_bcm.c
 
-echo "Fixup AArch64 assembly backend to use s2n-bignum macros"
+echo "Fixup AArch64 and x86_64 assembly backend to use s2n-bignum macros"
 for file in $SRC/native/aarch64/src/*.S $SRC/native/x86_64/src/*.S; do
   echo "Processing $file"
   tmp_file=$(mktemp)
 
+  backend_define=$(if [[ "$file" == *"aarch64"* ]]; then echo "MLK_ARITH_BACKEND_AARCH64"; else echo "MLK_ARITH_BACKEND_X86_64_DEFAULT"; fi)
+
   # Flatten multiline preprocessor directives, then process with unifdef
   sed -e ':a' -e 'N' -e '$!ba' -e 's/\\\n/ /g' "$file" | \
-    unifdef -DMLK_ARITH_BACKEND_AARCH64 -DMLK_ARITH_BACKEND_X86_64 -UMLK_CONFIG_MULTILEVEL_NO_SHARED -DMLK_CONFIG_MULTILEVEL_WITH_SHARED > "$tmp_file"
+    unifdef -D$backend_define -UMLK_CONFIG_MULTILEVEL_NO_SHARED -DMLK_CONFIG_MULTILEVEL_WITH_SHARED > "$tmp_file"
   mv "$tmp_file" "$file"
 
   # Replace common.h include and assembly macros

commit a259728550f06c688eec3b3670d87577ba060e76

ML-KEM: Fix mlkem-native importer.sh
Commit 10a0611 broke the mlkem-native importer on MacOS
(both AArch64 and x86_64) because of a missing quotation
around `sed` invocations. This commit fixes this.

Found by mlkem-native CI.

Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

crypto/fipsmodule/ml_kem/importer.sh
@@ -115,15 +115,15 @@ else
 fi
 
 # Exclude POLY_COMPRESS/DECOMPRESS functions from the x86 backend for now.
-sed ${SED_I[@]} '/compress_avx2.c/d' $SRC/mlkem_native_bcm.c
-sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_COMPRESS_D4/d' $SRC/native/x86_64/meta.h
-sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_COMPRESS_D5/d' $SRC/native/x86_64/meta.h
-sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_COMPRESS_D10/d' $SRC/native/x86_64/meta.h
-sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_COMPRESS_D11/d' $SRC/native/x86_64/meta.h
-sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_DECOMPRESS_D4/d' $SRC/native/x86_64/meta.h
-sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_DECOMPRESS_D5/d' $SRC/native/x86_64/meta.h
-sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_DECOMPRESS_D10/d' $SRC/native/x86_64/meta.h
-sed ${SED_I[@]} '/MLK_USE_NATIVE_POLY_DECOMPRESS_D11/d' $SRC/native/x86_64/meta.h
+sed "${SED_I[@]}" '/compress_avx2.c/d' $SRC/mlkem_native_bcm.c
+sed "${SED_I[@]}" '/MLK_USE_NATIVE_POLY_COMPRESS_D4/d' $SRC/native/x86_64/meta.h
+sed "${SED_I[@]}" '/MLK_USE_NATIVE_POLY_COMPRESS_D5/d' $SRC/native/x86_64/meta.h
+sed "${SED_I[@]}" '/MLK_USE_NATIVE_POLY_COMPRESS_D10/d' $SRC/native/x86_64/meta.h
+sed "${SED_I[@]}" '/MLK_USE_NATIVE_POLY_COMPRESS_D11/d' $SRC/native/x86_64/meta.h
+sed "${SED_I[@]}" '/MLK_USE_NATIVE_POLY_DECOMPRESS_D4/d' $SRC/native/x86_64/meta.h
+sed "${SED_I[@]}" '/MLK_USE_NATIVE_POLY_DECOMPRESS_D5/d' $SRC/native/x86_64/meta.h
+sed "${SED_I[@]}" '/MLK_USE_NATIVE_POLY_DECOMPRESS_D10/d' $SRC/native/x86_64/meta.h
+sed "${SED_I[@]}" '/MLK_USE_NATIVE_POLY_DECOMPRESS_D11/d' $SRC/native/x86_64/meta.h
 
 # Copy mlkem-native header
 # This is only needed for access to the various macros defining key sizes.