6d337f97fea0fe3f1f0c2d69079ad8b04faea1fc
Shubham Mittal <smittals@amazon.com>
2025-09-15 00:15:13 -0700

    add new util, use appropriate test target for GO disabled build

--- tests/ci/run_legacy_avx_tests.sh
+++ tests/ci/run_legacy_avx_tests.sh
@@ -6,12 +6,9 @@ set -exo pipefail
 
 source tests/ci/common_posix_setup.sh
 
-echo "Testing AWS-LC in release mode."
-build_and_test -DCMAKE_BUILD_TYPE=Release
-
-# Lightly verify that uncommon build options does not break the build. Fist
+# Lightly verify that uncommon build options does not break the build. First
 # define a list of typical build options to verify the special build option with
-build_options_to_test=("" "-DBUILD_SHARED_LIBS=1" "-DCMAKE_BUILD_TYPE=Release" "-DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release" "-DDISABLE_PERL=ON -DDISABLE_GO=ON" "-DENABLE_PRE_SONAME_BUILD=0")
+build_options_to_test=("" "-DBUILD_SHARED_LIBS=1" "-DCMAKE_BUILD_TYPE=Release" "-DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release" "-DENABLE_PRE_SONAME_BUILD=0")
 
 ## Build option: MY_ASSEMBLER_IS_TOO_OLD_FOR_AVX
 for build_option in "${build_options_to_test[@]}"; do
@@ -22,3 +19,7 @@ done
 for build_option in "${build_options_to_test[@]}"; do
   build_and_test ${build_option} -DMY_ASSEMBLER_IS_TOO_OLD_FOR_512AVX=ON
 done
+
+# When Go is disabled, a different test target is produced
+build_and_test_target "run_minimal_tests" -DDISABLE_PERL=ON -DDISABLE_GO=ON -DMY_ASSEMBLER_IS_TOO_OLD_FOR_AVX=ON
+build_and_test_target "run_minimal_tests" -DDISABLE_PERL=ON -DDISABLE_GO=ON -DMY_ASSEMBLER_IS_TOO_OLD_FOR_512AVX=ON
