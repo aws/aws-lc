103a5238d0b73e19161dbddbb19096ceb351e4cb
Justin Smith <justsmth@amazon.com>
2025-10-10 10:55:04 -0400

    Accept a --run-target option

--- util/build_and_test.sh
+++ util/build_and_test.sh
@@ -8,16 +8,26 @@ AWS_LC_BUILD="${BASE_DIR}/build"
 
 DEFAULT_CMAKE_FLAGS=("-DCMAKE_BUILD_TYPE=Debug" "-GNinja")
 
+RUN_TARGET="run_tests"
 BUILD_ONLY=false
 CMAKE_ARGS=()
 
-for arg in "$@"; do
-  if [ "$arg" == "--build-only" ]; then
-    BUILD_ONLY=true
-  else
-    # Everything else gets passed to CMake
-    CMAKE_ARGS+=("$arg")
-  fi
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --build-only)
+      BUILD_ONLY=true
+      shift
+      ;;
+    --run-target)
+      RUN_TARGET="$2"
+      shift 2
+      ;;
+    *)
+      # Everything else gets passed to CMake
+      CMAKE_ARGS+=("$1")
+      shift
+      ;;
+  esac
 done
 
 mkdir -p "${AWS_LC_BUILD}"
@@ -26,7 +36,7 @@ cmake "${BASE_DIR}" -B "${AWS_LC_BUILD}" "${DEFAULT_CMAKE_FLAGS[@]}" "${CMAKE_AR
 
 cmake --build "${AWS_LC_BUILD}" --target all_tests -j
 
-# Only run tests if --no-tests was not specified
+# Only run tests if --build-only was not specified
 if [ "$BUILD_ONLY" = false ]; then
-  cmake --build "${AWS_LC_BUILD}" --target run_tests
+  cmake --build "${AWS_LC_BUILD}" --target ${RUN_TARGET}
 fi
