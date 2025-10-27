52e9615f8d883f244151474967e20e2982427ce4
Justin Smith <justsmth@amazon.com>
2025-10-27 14:49:30 -0400

    no-op impl for UI

--- include/openssl/ui.h
+++ include/openssl/ui.h
@@ -4,6 +4,28 @@
 #ifndef UI_H
 #define UI_H
 
+struct ui_st {
+  // EMPTY
+};
+
+struct ui_method_st {
+  // EMPTY
+};
+
+typedef struct ui_st UI;
+typedef struct ui_method_st UI_METHOD;
+
+UI *UI_new(void);
+void UI_free(UI *ui);
+
+int UI_add_input_string(UI *ui, const char *prompt, int flags,
+        char *result_buf, int minsize, int maxsize);
+int UI_add_verify_string(UI *ui, const char *prompt, int flags,
+        char *result_buf, int minsize, int maxsize, const char *test_buf);
+int UI_add_info_string(UI *ui, const char *text);
+
+int UI_process(UI *ui);
+
 #endif //UI_H
 
 // Intentionally empty.
