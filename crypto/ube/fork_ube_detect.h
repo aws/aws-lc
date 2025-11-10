ff55e99fbb45d5036794656194f6b39a884d6bc5
torben-hansen <50673096+torben-hansen@users.noreply.github.com>
2025-11-10 10:30:07 -0800

    Rename fork to fork UBE (#2803)
    Follow naming scheme from #2800

--- crypto/ube/fork_ube_detect.h
+++ crypto/ube/fork_ube_detect.h
@@ -12,8 +12,8 @@
  * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
 
-#ifndef OPENSSL_HEADER_CRYPTO_FORK_DETECT_H
-#define OPENSSL_HEADER_CRYPTO_FORK_DETECT_H
+#ifndef OPENSSL_HEADER_CRYPTO_FORK_UBE_DETECT_H
+#define OPENSSL_HEADER_CRYPTO_FORK_UBE_DETECT_H
 
 #include <openssl/base.h>
 
@@ -24,11 +24,11 @@ extern "C" {
 #endif
 
 
-// crypto_get_fork_generation returns the fork generation number for the current
-// process, or zero if not supported on the platform. The fork generation number
-// is a non-zero, strictly-monotonic counter with the property that, if queried
-// in an address space and then again in a subsequently forked copy, the forked
-// address space will observe a greater value.
+// CRYPTO_get_fork_ube_generation returns the fork generation number for the
+// current process, or zero if not supported on the platform. The fork
+// generation number is a non-zero, strictly-monotonic counter with the property
+// that, if queried in an address space and then again in a subsequently forked
+// copy, the forked address space will observe a greater value.
 //
 // This function may be used to clear cached values across a fork. When
 // initializing a cache, record the fork generation. Before using the cache,
@@ -38,7 +38,7 @@ extern "C" {
 //
 // This is not reliably supported on all platforms which implement |fork|, so it
 // should only be used as a hardening measure.
-OPENSSL_EXPORT uint64_t CRYPTO_get_fork_generation(void);
+OPENSSL_EXPORT uint64_t CRYPTO_get_fork_ube_generation(void);
 
 // CRYPTO_fork_detect_ignore_wipeonfork_FOR_TESTING is an internal detail
 // used for testing purposes.
@@ -53,4 +53,4 @@ OPENSSL_EXPORT void CRYPTO_fork_detect_ignore_inheritzero_FOR_TESTING(void);
 }  // extern C
 #endif
 
-#endif  // OPENSSL_HEADER_CRYPTO_FORK_DETECT_H
+#endif  // OPENSSL_HEADER_CRYPTO_FORK_UBE_DETECT_H
