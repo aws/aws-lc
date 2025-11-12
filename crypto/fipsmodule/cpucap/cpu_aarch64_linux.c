a80d2e5335377a2120108f1103f73a0f545c6f10
Nevine Ebeid <nebeid@amazon.com>
2025-11-12 14:50:17 -0500

    Fix typo in a comment.

--- crypto/fipsmodule/cpucap/cpu_aarch64_linux.c
+++ crypto/fipsmodule/cpucap/cpu_aarch64_linux.c
@@ -94,7 +94,7 @@ void OPENSSL_cpuid_setup(void) {
     }
     if (MIDR_IS_CPU_MODEL(OPENSSL_arm_midr, ARM_CPU_IMP_ARM, ARM_CPU_PART_V2)) {
       OPENSSL_armcap_P |= ARMV8_NEOVERSE_V2;
-      // CPU capabilities of N1 are a subset of CPU capabilities of V1
+      // CPU capabilities of N1 are a subset of CPU capabilities of V2
       OPENSSL_armcap_P |= ARMV8_NEOVERSE_N1;
     }
   }
