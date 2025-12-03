cd1d2512adcc4a1cf35ad1cecc8a42e248f1931d
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2025-12-03 11:04:06 -0800

    Prepare FIPS 3.1.0 release

--- crypto/fipsmodule/service_indicator/service_indicator_test.cc
+++ crypto/fipsmodule/service_indicator/service_indicator_test.cc
@@ -5254,7 +5254,7 @@ TEST(ServiceIndicatorTest, ED25519SigGenVerify) {
 // Since this is running in FIPS mode it should end in FIPS
 // Update this when the AWS-LC version number is modified
 TEST(ServiceIndicatorTest, AWSLCVersionString) {
-  ASSERT_STREQ(awslc_version_string(), "AWS-LC FIPS 3.0.0");
+  ASSERT_STREQ(awslc_version_string(), "AWS-LC FIPS 3.1.0");
 }
 
 #else
@@ -5297,6 +5297,6 @@ TEST(ServiceIndicatorTest, BasicTest) {
 // Since this is not running in FIPS mode it shouldn't end in FIPS
 // Update this when the AWS-LC version number is modified
 TEST(ServiceIndicatorTest, AWSLCVersionString) {
-  ASSERT_STREQ(awslc_version_string(), "AWS-LC 3.0.0");
+  ASSERT_STREQ(awslc_version_string(), "AWS-LC 3.1.0");
 }
 #endif // AWSLC_FIPS
