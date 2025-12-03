3aa7951df23785abc66915e9e4e92628b28fbf0d
Jake Massimo <jakemas@amazon.com>
2025-12-03 12:35:12 -0800

    fix modulewrapper rebase

--- util/fipstools/acvp/modulewrapper/modulewrapper.cc
+++ util/fipstools/acvp/modulewrapper/modulewrapper.cc
@@ -58,7 +58,6 @@
 #include "../../../../crypto/fipsmodule/rand/internal.h"
 #include "../../../../crypto/fipsmodule/curve25519/internal.h"
 #include "../../../../crypto/fipsmodule/ml_dsa/ml_dsa.h"
-#include "../../../../crypto/fipsmodule/ml_dsa/ml_dsa_ref/params.h"
 #include "../../../../crypto/fipsmodule/ml_kem/ml_kem.h"
 #include "modulewrapper.h"
 
