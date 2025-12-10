0b1daded1a18af61ad6bfac4e367828020122527
Jake Massimo <jakemas@amazon.com>
2025-12-09 16:28:16 -0800

    edit

--- crypto/fipsmodule/aes/mode_wrappers.c
+++ crypto/fipsmodule/aes/mode_wrappers.c
@@ -55,46 +55,9 @@
 #include "../cipher/internal.h"
 
 // The following wrappers ensure that the delocator can handle the
-// function pointer calculation in AES_ctr128_encrypt. Without it,
+// function pointer calculation. Without them,
 // on AArch64 there is risk of the calculations requiring a PC-relative
 // offset outside of the range (-1MB,1MB) addressable using `ADR`.
-static inline void aes_hw_encrypt_wrapper(const uint8_t *in, uint8_t *out,
-                                          const AES_KEY *key) {
-  aes_hw_encrypt(in, out, key);
-}
-
-static inline void aes_nohw_encrypt_wrapper(const uint8_t *in, uint8_t *out,
-                                            const AES_KEY *key) {
-  aes_nohw_encrypt(in, out, key);
-}
-
-static inline void aes_hw_ctr32_encrypt_blocks_wrapper(const uint8_t *in,
-                                                       uint8_t *out, size_t len,
-                                                       const AES_KEY *key,
-                                                       const uint8_t ivec[16]) {
-  aes_hw_ctr32_encrypt_blocks(in, out, len, key, ivec);
-}
-
-static inline void aes_nohw_ctr32_encrypt_blocks_wrapper(const uint8_t *in,
-                                                         uint8_t *out, size_t len,
-                                                         const AES_KEY *key,
-                                                         const uint8_t ivec[16]) {
-  aes_nohw_ctr32_encrypt_blocks(in, out, len, key, ivec);
-}
-
-static inline void vpaes_encrypt_wrapper(const uint8_t *in, uint8_t *out,
-                                         const AES_KEY *key) {
-  vpaes_encrypt(in, out, key);
-}
-
-#if defined(VPAES_CTR32)
-static inline void vpaes_ctr32_encrypt_blocks_wrapper(const uint8_t *in,
-                                                      uint8_t *out, size_t len,
-                                                      const AES_KEY *key,
-                                                      const uint8_t ivec[16]) {
-  vpaes_ctr32_encrypt_blocks(in, out, len, key, ivec);
-}
-#endif
 
 // GCM wrappers for AArch64 to avoid delocator issues
 #if defined(GHASH_ASM_ARM)
@@ -122,20 +85,20 @@ void AES_ctr128_encrypt(const uint8_t *in, uint8_t *out, size_t len,
                         uint8_t ecount_buf[AES_BLOCK_SIZE], unsigned int *num) {
   if (hwaes_capable()) {
     CRYPTO_ctr128_encrypt_ctr32(in, out, len, key, ivec, ecount_buf, num,
-                                aes_hw_ctr32_encrypt_blocks_wrapper);
+                                aes_hw_ctr32_encrypt_blocks);
   } else if (vpaes_capable()) {
 #if defined(VPAES_CTR32)
     // TODO(davidben): On ARM, where |BSAES| is additionally defined, this could
     // use |vpaes_ctr32_encrypt_blocks_with_bsaes|.
     CRYPTO_ctr128_encrypt_ctr32(in, out, len, key, ivec, ecount_buf, num,
-                                vpaes_ctr32_encrypt_blocks_wrapper);
+                                vpaes_ctr32_encrypt_blocks);
 #else
     CRYPTO_ctr128_encrypt(in, out, len, key, ivec, ecount_buf, num,
-                          vpaes_encrypt_wrapper);
+                          vpaes_encrypt);
 #endif
   } else {
     CRYPTO_ctr128_encrypt_ctr32(in, out, len, key, ivec, ecount_buf, num,
-                                aes_nohw_ctr32_encrypt_blocks_wrapper);
+                                aes_nohw_ctr32_encrypt_blocks);
   }
 
   FIPS_service_indicator_update_state();
