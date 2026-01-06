728811eecec794802c78105be6dbfe9d79870ac7
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2026-01-06 16:22:05 -0500

    Prepare v1.66.2 (#2930)
    ### Description of changes: 
    Prepare AWS-LC release v1.66.2
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license and the ISC license.

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -122,7 +122,7 @@ extern "C" {
 // ServiceIndicatorTest.AWSLCVersionString
 // Note: there are two versions of this test. Only one test is compiled
 // depending on FIPS mode.
-#define AWSLC_VERSION_NUMBER_STRING "1.66.1"
+#define AWSLC_VERSION_NUMBER_STRING "1.66.2"
 
 #if defined(BORINGSSL_SHARED_LIBRARY)
 
