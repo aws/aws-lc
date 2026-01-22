4b2fea4c35124e97f3485b16cf8ad181ee006569
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2026-01-21 20:15:51 -0500

    Fix CMake CI jobs (#2953)

--- .github/docker_images/cmake_build_versions/cmake_build.sh
+++ .github/docker_images/cmake_build_versions/cmake_build.sh
@@ -9,8 +9,21 @@ echo "Building CMake Version: ${CMAKE_VERSION:-unknown}"
 
 NUM_CPU_THREADS=$(grep -c ^processor /proc/cpuinfo)
 
-# At the moment this works fine for all versions, in the future build logic can be modified to
-# look at it ${CMAKE_VERSION}.
-./configure --prefix=/opt/cmake --system-curl --system-libarchive
+# CMake versions before 4.0 have compatibility issues with newer system curl libraries.
+# The curl headers define CURL_NETRC_* as long int values, but older CMake code expects
+# them to be CURL_NETRC_OPTION enum values, causing compilation errors.
+# For older versions, we use CMake's bundled curl instead of the system curl.
+CONFIGURE_OPTS="--prefix=/opt/cmake --system-libarchive"
+
+if [[ "${CMAKE_VERSION}" =~ ^[0-3]\. ]]; then
+    # CMake versions 3.x and earlier: use bundled curl to avoid compatibility issues
+    echo "Using bundled curl for CMake ${CMAKE_VERSION}"
+else
+    # CMake 4.0 and later: safe to use system curl
+    echo "Using system curl for CMake ${CMAKE_VERSION}"
+    CONFIGURE_OPTS="${CONFIGURE_OPTS} --system-curl"
+fi
+
+./configure ${CONFIGURE_OPTS}
 make -j"${NUM_CPU_THREADS}"
 make install
