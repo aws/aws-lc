b481197df8e7daa8fdc20c95d458dc8e7d6b83ab
Sean McGrail <549813+skmcgrail@users.noreply.github.com>
2026-01-29 16:24:24 -0800

    pkcs8: cap ciphertext length before allocating in pkcs8_pbe_decrypt
    pkcs8_pbe_decrypt() allocates OPENSSL_malloc(in_len) where in_len is derived
    from attacker-influenced ASN.1 OCTET STRING lengths. The existing INT_MAX
    check only guards EVP_DecryptUpdate’s int parameter and occurs after the
    allocation.
    
    We would like to thank Joshua Rogers (https://joshua.hu/) of AISLE Research Team (https://aisle.com/) for reporting this issue.
    
    (cherry picked from commit e17506cdbde19ce68e21681b2fb9581b1fe93037)

--- crypto/pkcs8/pkcs8.c
+++ crypto/pkcs8/pkcs8.c
@@ -389,13 +389,13 @@ int pkcs8_pbe_decrypt(uint8_t **out, size_t *out_len, CBS *algorithm,
     goto err;
   }
 
-  buf = OPENSSL_malloc(in_len);
-  if (buf == NULL) {
+  if (in_len > INT_MAX) {
+    OPENSSL_PUT_ERROR(PKCS8, ERR_R_OVERFLOW);
     goto err;
   }
 
-  if (in_len > INT_MAX) {
-    OPENSSL_PUT_ERROR(PKCS8, ERR_R_OVERFLOW);
+  buf = OPENSSL_malloc(in_len);
+  if (buf == NULL) {
     goto err;
   }
 
