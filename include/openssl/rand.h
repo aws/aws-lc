453905dec5064576f890beb14ea18bf409de67dc
Sean McGrail <549813+skmcgrail@users.noreply.github.com>
2026-03-06 19:29:34 +0000

    Remove dead declarations in public headers (#3053)
    # Description
    These symbols are declared in our public headers but are not actually
    defined anywhere in libcrypto or libssl.
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license and the ISC license.
    
    Co-authored-by: Justin W Smith <103147162+justsmth@users.noreply.github.com>

--- include/openssl/rand.h
+++ include/openssl/rand.h
@@ -57,16 +57,6 @@ OPENSSL_EXPORT int RAND_bytes_with_user_prediction_resistance(uint8_t *out,
 OPENSSL_EXPORT void RAND_reset_for_fuzzing(void);
 #endif
 
-// RAND_get_system_entropy_for_custom_prng writes |len| bytes of random data
-// from a system entropy source to |buf|. The maximum length of entropy which
-// may be requested is 256 bytes. If more than 256 bytes of data is requested,
-// or if sufficient random data can not be obtained, |abort| is called.
-// |RAND_bytes| should normally be used instead of this function. This function
-// should only be used for seed values or where |malloc| should not be called
-// from BoringSSL. This function is not FIPS compliant.
-OPENSSL_EXPORT void RAND_get_system_entropy_for_custom_prng(uint8_t *buf,
-                                                            size_t len);
-
 
 // Deprecated functions
 
