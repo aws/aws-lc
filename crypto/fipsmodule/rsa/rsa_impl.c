c8bb87cd0dd6089d7753fb2dcb470395be0ada5f
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2026-03-18 15:03:25 -0700

    Consistently set outlen to zero for all error paths

--- crypto/fipsmodule/rsa/rsa_impl.c
+++ crypto/fipsmodule/rsa/rsa_impl.c
@@ -359,10 +359,11 @@ static int mod_exp(BIGNUM *r0, const BIGNUM *I, RSA *rsa, BN_CTX *ctx);
 int rsa_verify_raw_no_self_test(RSA *rsa, size_t *out_len, uint8_t *out,
                                 size_t max_out, const uint8_t *in,
                                 size_t in_len, int padding) {
+  *out_len = 0;
+
   if(rsa->meth && rsa->meth->verify_raw) {
     if (max_out > INT_MAX) {
       OPENSSL_PUT_ERROR(RSA, ERR_R_OVERFLOW);
-      *out_len = 0;
       return 0;
     }
     // In OpenSSL, the RSA_METHOD |verify_raw| or |pub_dec| operation does
@@ -374,7 +375,6 @@ int rsa_verify_raw_no_self_test(RSA *rsa, size_t *out_len, uint8_t *out,
     // here.
     int ret = rsa->meth->verify_raw((int)max_out, in, out, rsa, padding);
     if(ret < 0) {
-      *out_len = 0;
       return 0;
     }
     *out_len = ret;
