a75e930cecced7221631220475f2589335d4d67f
Gerardo Ravago <gcr@amazon.com>
2026-03-30 13:51:11 -0400

    Add openssl version -a and -p flag support (#3092)
    ### Description of changes: 
    For compatibility, we should implement these CLI flags to prevent
    certain test scripts from erroring out.
    
    Add -a (all) and -p (platform) boolean flags to the version subcommand:
    - No flags: print version line only (preserves existing behavior)
    - -a: print version, built on, platform, compiler, OPENSSLDIR
    - -p: print platform line only
    
    ### Call-outs:
    This doesn't actually print anything useful, but it does call the
    OpenSSL APIs and prints the stub strings that we had in place. If I had
    to hazard a guess this was either an intentional attempt to avoid
    leaking implementation details or they were lazy (Occam's razor suggests
    the latter).
    
    ### Testing:
    How is this change tested (unit tests, fuzz tests, etc.)? Are there any
    testing steps to be verified by the reviewer?
    
    ```
    # Sample outputs
    ➜  aws-lc git:(version-a) ✗ ./build/tool-openssl/openssl version -p
    platform: n/a
    ➜  aws-lc git:(version-a) ✗ ./build/tool-openssl/openssl version -a
    OpenSSL 1.1.1 (compatible; AWS-LC 1.69.0)
    built on: n/a
    platform: n/a
    compiler: n/a
    OPENSSLDIR: n/a
    ```
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license and the ISC license.

--- tool-openssl/version.cc
+++ tool-openssl/version.cc
@@ -2,21 +2,51 @@
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 
 #include <openssl/base.h>
+#include <openssl/crypto.h>
 #include "internal.h"
 
 static const argument_t kArguments[] = {
-    { "", kOptionalArgument, "" }
+    {"-help", kBooleanArgument, "Display option summary"},
+    {"-a", kBooleanArgument, "Print all version information"},
+    {"-p", kBooleanArgument, "Print platform"},
+    {"", kOptionalArgument, ""}
 };
 
 bool VersionTool(const args_list_t &args) {
   using namespace ordered_args;
   ordered_args_map_t parsed_args;
   args_list_t extra_args;
-  if (!ParseOrderedKeyValueArguments(parsed_args, extra_args, args, kArguments) ||
+  if (!ParseOrderedKeyValueArguments(parsed_args, extra_args, args,
+                                     kArguments) ||
       extra_args.size() > 0) {
     PrintUsage(kArguments);
     return false;
   }
+
+  if (HasArgument(parsed_args, "-help")) {
+    PrintUsage(kArguments);
+    return true;
+  }
+
+  bool all = false;
+  bool platform = false;
+  GetBoolArgument(&all, "-a", parsed_args);
+  GetBoolArgument(&platform, "-p", parsed_args);
+
+  if (all) {
+    printf("%s\n", OPENSSL_VERSION_TEXT);
+    printf("%s\n", OpenSSL_version(OPENSSL_BUILT_ON));
+    printf("%s\n", OpenSSL_version(OPENSSL_PLATFORM));
+    printf("%s\n", OpenSSL_version(OPENSSL_CFLAGS));
+    printf("%s\n", OpenSSL_version(OPENSSL_DIR));
+    return true;
+  }
+
+  if (platform) {
+    printf("%s\n", OpenSSL_version(OPENSSL_PLATFORM));
+    return true;
+  }
+
   printf("%s\n", OPENSSL_VERSION_TEXT);
   return true;
 }
