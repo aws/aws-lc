f50b561ff06bcf624f86eb985d52cd86a99c43a0
Prasanth Dendukuri <prasden@amazon.com>
2026-09-04 14:33:29 -0700

    pr feedback

--- ssl/test/bssl_shim.cc
+++ ssl/test/bssl_shim.cc
@@ -1256,11 +1256,16 @@ static bool DoExchange(bssl::UniquePtr<SSL_SESSION> *out_session,
         int n = DoRead(ssl_uniqueptr, buf.get(), read_size);
         ssl = ssl_uniqueptr->get();
         int err = SSL_get_error(ssl, n);
+        bool is_unexpected_eof = false;
+        if (err == SSL_ERROR_SSL) {
+          uint32_t packed_error = ERR_peek_error();
+          is_unexpected_eof =
+              ERR_GET_LIB(packed_error) == ERR_LIB_SSL &&
+              ERR_GET_REASON(packed_error) == SSL_R_UNEXPECTED_EOF_WHILE_READING;
+        }
         if (err == SSL_ERROR_ZERO_RETURN ||
             (n == 0 && err == SSL_ERROR_SYSCALL) ||
-            (err == SSL_ERROR_SSL &&
-             ERR_GET_REASON(ERR_peek_error()) ==
-                 SSL_R_UNEXPECTED_EOF_WHILE_READING)) {
+            is_unexpected_eof) {
           if (n > 0) {
             fprintf(stderr, "Invalid SSL_get_error output\n");
             return false;
