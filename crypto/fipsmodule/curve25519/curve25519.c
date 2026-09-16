61e8c4d70d551b879772eca2ce9650e5a43ae12a
Doug Chapman <54039637+dougch@users.noreply.github.com>
2026-09-16 15:19:09 -0700

    Revert "fix: include service_indicator/internal.h in curve25519.c"
    This reverts commit 64a06fb9b410b3ce91b896264436af98a2ae7105.

--- crypto/fipsmodule/curve25519/curve25519.c
+++ crypto/fipsmodule/curve25519/curve25519.c
@@ -19,7 +19,6 @@
 
 #include "../../internal.h"
 #include "../cpucap/internal.h"
-#include "../service_indicator/internal.h"
 #include "internal.h"
 
 const uint8_t RFC8032_DOM2_PREFIX[DOM2_PREFIX_SIZE] = {
